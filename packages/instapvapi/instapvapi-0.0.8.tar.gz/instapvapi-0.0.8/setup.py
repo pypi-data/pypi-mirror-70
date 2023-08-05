1cb995f7ac85d0c0df0685646288c7a6b9c0a599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:08 +0000

    Add package instapvapi/0.0.8/instapvapi-0.0.8.tar.gz

---
+++ packages/instapvapi/instapvapi-0.0.8.tar.gz/instapvapi-0.0.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="instapvapi",
+    version="0.0.8",
+    author="UWSGI",
+    url="https://github.com/its0x4d/instagramapi-python",
+    author_email="mosydev2016@gmail.com",
+    description="An unofficial instagram private api.",
+    packages=setuptools.find_packages(include=['requests', 'colorama', 'packaging', 'redis']),
+    platforms=['any'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

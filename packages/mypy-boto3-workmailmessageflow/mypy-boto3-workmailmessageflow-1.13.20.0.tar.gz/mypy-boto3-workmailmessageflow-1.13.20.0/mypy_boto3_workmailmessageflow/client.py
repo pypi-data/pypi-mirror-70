1a30d38ef59202d7062bc095c6186acbc0b3877b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package mypy-boto3-workmailmessageflow/1.13.20.0/mypy-boto3-workmailmessageflow-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-workmailmessageflow/mypy-boto3-workmailmessageflow-1.13.20.0.tar.gz/mypy-boto3-workmailmessageflow-1.13.20.0/mypy_boto3_workmailmessageflow/client.py
@@ -0,0 +1,54 @@
+"""
+Main interface for workmailmessageflow service client
+
+Usage::
+
+    import boto3
+    from mypy_boto3.workmailmessageflow import WorkMailMessageFlowClient
+
+    session = boto3.Session()
+
+    client: WorkMailMessageFlowClient = boto3.client("workmailmessageflow")
+    session_client: WorkMailMessageFlowClient = session.client("workmailmessageflow")
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Any, Dict, TYPE_CHECKING, Type
+from botocore.exceptions import ClientError as Boto3ClientError
+from mypy_boto3_workmailmessageflow.type_defs import GetRawMessageContentResponseTypeDef
+
+
+__all__ = ("WorkMailMessageFlowClient",)
+
+
+class Exceptions:
+    ClientError: Type[Boto3ClientError]
+    ResourceNotFoundException: Type[Boto3ClientError]
+
+
+class WorkMailMessageFlowClient:
+    """
+    [WorkMailMessageFlow.Client documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/workmailmessageflow.html#WorkMailMessageFlow.Client)
+    """
+
+    exceptions: Exceptions
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Client.can_paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/workmailmessageflow.html#WorkMailMessageFlow.Client.can_paginate)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> str:
+        """
+        [Client.generate_presigned_url documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/workmailmessageflow.html#WorkMailMessageFlow.Client.generate_presigned_url)
+        """
+
+    def get_raw_message_content(self, messageId: str) -> GetRawMessageContentResponseTypeDef:
+        """
+        [Client.get_raw_message_content documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/workmailmessageflow.html#WorkMailMessageFlow.Client.get_raw_message_content)
+        """

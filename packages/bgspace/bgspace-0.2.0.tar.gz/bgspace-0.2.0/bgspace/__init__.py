ff133c5dbdb4dcea0c277a18e92d1ea3a2da6dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:00 +0000

    Add package bgspace/0.2.0/bgspace-0.2.0.tar.gz

---
+++ packages/bgspace/bgspace-0.2.0.tar.gz/bgspace-0.2.0/bgspace/__init__.py
@@ -0,0 +1,10 @@
+__author__ = """Luigi Petrucco @portugueslab"""
+__version__ = "0.2.0"
+
+from bgspace.core import SpaceConvention
+from bgspace.functions import (
+    map_to,
+    map_stack_to,
+    transformation_matrix_to,
+    transform_points_to,
+)

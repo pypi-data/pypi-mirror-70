2aa7d7ad529abb6c14bab38149afc24c1e7dda67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package screenshots/0.0.2.dev2/screenshots-0.0.2.dev2.tar.gz

---
+++ packages/screenshots/screenshots-0.0.2.dev2.tar.gz/screenshots-0.0.2.dev2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="screenshots",
+    version="0.0.2.dev2",
+    author="chincheta",
+    author_email="dev@chincheta.com",
+    description="Python library to make screenshots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chincheta/screenshots",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'selenium==3.141.0',
+        'Pillow==7.1.2',
+        'tweepy==3.8.0'
+    ],
+    python_requires='>=3.6',
+)

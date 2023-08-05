77dbabba57b2ba5553992db6bf8976e5bd89ee29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package distributions-test-quandt/0.1/distributions_test_quandt-0.1.tar.gz

---
+++ packages/distributions-test-quandt/distributions_test_quandt-0.1.tar.gz/distributions_test_quandt-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_test_quandt',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_test_quandt'],
+      zip_safe=False)

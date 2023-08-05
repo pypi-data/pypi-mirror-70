9b108bb8d944ee80ca9c1ea93adb079e07a2e03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package kaleidoscope/0.0.3/kaleidoscope-0.0.3.tar.gz

---
+++ packages/kaleidoscope/kaleidoscope-0.0.3.tar.gz/kaleidoscope-0.0.3/docs/_build/jupyter_execute/index.py
@@ -0,0 +1,12 @@
+import numpy as np
+from qiskit import *
+from qiskit.quantum_info import Statevector
+import plotly.graph_objects as go
+
+from kaleidoscope.interactive import bloch_disc
+qc = QuantumCircuit(1)
+qc.ry(np.pi*np.random.random(), 0)
+qc.rz(np.pi*np.random.random(), 0)
+
+state = Statevector.from_instruction(qc)
+bloch_disc(state, as_widget=True)

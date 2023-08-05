78a008b232bc710355d5c911e0c3a73394947878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package dgb-distributions/0.1/dgb_distributions-0.1.tar.gz

---
+++ packages/dgb-distributions/dgb_distributions-0.1.tar.gz/dgb_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dgb_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dgb_distributions'],
+      author='Alexander Bennett',
+      author_email = 'zanderwhiteleybennett@gmail.com',
+      zip_safe=False)

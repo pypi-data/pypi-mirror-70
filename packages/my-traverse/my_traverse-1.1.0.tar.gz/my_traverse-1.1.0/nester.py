dccd443df07704728c1990a40f014bd79cc386f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:06 +0000

    Add package my-traverse/1.1.0/my_traverse-1.1.0.tar.gz

---
+++ packages/my-traverse/my_traverse-1.1.0.tar.gz/my_traverse-1.1.0/nester.py
@@ -0,0 +1,26 @@
+# movies = ["The Holy Grail", "The Life of Brian", "The Meaning of Life"]
+# movies.insert(1,1975);
+# movies.insert(3, 1979);
+# movies.insert(5, 1983);
+# # print(movies);
+#
+# movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91, ['Graham Chapman', ['Michael Palin',
+# 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
+#
+# cute_mom = [
+#     "Mengfei Yang",
+#     30.75,
+#     True,
+#     ["Pink", "Coffee","Lovely"]
+# ]
+
+def traverse_list(list_par, level=-1):
+    if isinstance(list_par, list):
+        for list_item in list_par:
+            traverse_list(list_item, level+1)
+    else:
+        indent="";
+        for time in range(level):
+            # indent += "\t"
+            print(end="\t")
+        print(indent+str(list_par))

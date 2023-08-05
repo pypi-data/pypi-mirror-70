e38ad47cef85336979877bf82740d24a9b42504c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package scatterd/0.1.2/scatterd-0.1.2.tar.gz

---
+++ packages/scatterd/scatterd-0.1.2.tar.gz/scatterd-0.1.2/scatterd/__init__.py
@@ -0,0 +1,25 @@
+from scatterd.scatterd import scatterd
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.2'
+
+# module level doc-string
+__doc__ = """
+scatterd
+=====================================================================
+
+Description
+-----------
+Scatterd is an easy and fast way of creating scatter plots.
+
+Example
+-------
+>>> from scaterd import scatterd
+>>> scatterd(X,Y)
+
+References
+----------
+https://github.com/erdogant/scatterd
+
+"""

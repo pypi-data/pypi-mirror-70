16171f1c2ea337b9e7a9840a44e967d445d52b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package robotframework-wadlibrary/20.6.4/robotframework-wadlibrary-20.6.4.tar.gz

---
+++ packages/robotframework-wadlibrary/robotframework-wadlibrary-20.6.4.tar.gz/robotframework-wadlibrary-20.6.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robotframework-wadlibrary",
+    version="20.06.04",
+    author="Elias Hachichou, Adwisit",
+    author_email="elias.hachichou@adwisit.se",
+    description="WADLibrary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Adwisit/WADLibrary",
+    install_requires = ['robotframework', 'requests', 'psutil'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

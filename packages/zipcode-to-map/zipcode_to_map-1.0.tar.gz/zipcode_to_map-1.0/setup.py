d7ab6f9ddd56c273defcdc8a54fc5669a6d5f6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:41 +0000

    Add package zipcode-to-map/1.0/zipcode_to_map-1.0.tar.gz

---
+++ packages/zipcode-to-map/zipcode_to_map-1.0.tar.gz/zipcode_to_map-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name = 'zipcode_to_map',
+    packages = ['zipcode_to_map'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Provide a Country code and postal code.It will generate a map in a html file based on these details.Used pgeocode and folium packages.',
+    author = 'JINKA RANGA BHARATH',
+    author_email = 'bharathjinka09@gmail.com',
+    url = 'https://github.com/bharathjinka09/zipcode_to_map',
+    download_url = 'https://github.com/bharathjinka09/zipcode_to_map/archive/1.0.tar.gz',
+    keywords = ['zipcode', 'HTML map file'],
+    classifiers = [],
+    python_requires='>=3.7',
+    setup_requires=['wheel'],
+
+)

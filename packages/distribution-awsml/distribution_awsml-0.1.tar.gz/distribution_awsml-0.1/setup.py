25be06a98759ea3bcfe50525f55193a77b1ab928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:55 +0000

    Add package distribution-awsml/0.1/distribution_awsml-0.1.tar.gz

---
+++ packages/distribution-awsml/distribution_awsml-0.1.tar.gz/distribution_awsml-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distribution_awsml',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Bidhya Shrestha',
+      zip_safe=False)

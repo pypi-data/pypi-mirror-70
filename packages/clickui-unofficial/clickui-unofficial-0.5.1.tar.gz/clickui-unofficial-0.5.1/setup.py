cd002bad61dd2c4250970e2bec48003fd9427ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:32 +0000

    Add package clickui-unofficial/0.5.1/clickui-unofficial-0.5.1.tar.gz

---
+++ packages/clickui-unofficial/clickui-unofficial-0.5.1.tar.gz/clickui-unofficial-0.5.1/setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import find_packages, setup
+
+description: str = ''
+
+with open('README.MD', 'r') as r:
+    description = r.read()
+    del r
+
+setup(
+    name='clickui-unofficial',
+    author='Rakesh Bute',
+    description="Unofficial UI wrapper on click commands",
+    long_description_content_type='text/markdown',
+    long_description=description,
+    author_email=os.getenv('MY_EMAIL', ''),
+    version='0.5.1',
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    python_requires=">=3.*",
+    project_urls={
+        "Code": "https://github.com/rbute/clickui-unofficial",
+    },
+    install_requires=[
+        'click',
+        'tkcalendar',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ]
+)

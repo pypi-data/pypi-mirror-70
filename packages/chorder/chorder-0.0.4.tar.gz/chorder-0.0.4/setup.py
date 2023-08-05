702f50c87bcf90b6c9489219ef1619fe34bae3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:14 +0000

    Add package chorder/0.0.4/chorder-0.0.4.tar.gz

---
+++ packages/chorder/chorder-0.0.4.tar.gz/chorder-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='chorder',
+    version='0.0.4',
+    author='Joshua Chang',
+    author_email='chchang6@illinois.edu',
+    description='A chord identifier and harmonizer for MIDI files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://bit.ly/wallylay',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

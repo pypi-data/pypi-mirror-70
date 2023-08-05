919ae112f1488fdb596f49286abdeb5ed23f3efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package printListModuleNevil/1.0.0/printListModuleNevil-1.0.0.tar.gz

---
+++ packages/printListModuleNevil/printListModuleNevil-1.0.0.tar.gz/printListModuleNevil-1.0.0/printListModuleNevil.py
@@ -0,0 +1,8 @@
+
+#Recurive print list function
+def rec_print_list(lst):
+    for i in lst:
+        if (isinstance(i,list)):
+            rec_print_list(i)
+        else:
+            print(i)
---
+++ packages/printListModuleNevil/printListModuleNevil-1.0.0.tar.gz/printListModuleNevil-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name="printListModuleNevil",version="1.0.0",description=" on API to print a list recursively",\
+    author="Nevil",author_email="nevil.alex@ignitarium.com",py_modules=["printListModuleNevil"])

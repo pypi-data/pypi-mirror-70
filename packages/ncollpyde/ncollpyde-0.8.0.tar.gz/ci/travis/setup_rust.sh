b0ed25767641421df3a9491fa4069a493d0b9d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package ncollpyde/0.8.0/ncollpyde-0.8.0.tar.gz

---
+++ packages/ncollpyde/ncollpyde-0.8.0.tar.gz/ci/travis/setup_rust.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+
+### Setup Rust toolchain #######################################################
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
+export PATH=$PATH:$HOME/.cargo/bin
+
+set +ex

506ae85186f54caa13b006452d59bc830dc84a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:11 +0000

    Add package revscoring/2.8.2/revscoring-2.8.2-py2.py3-none-any.whl

---
+++ packages/revscoring/revscoring-2.8.2-py2.py3-none-any.whl/revscoring/utilities/__init__.py
@@ -0,0 +1,39 @@
+"""
+This module implements a set of utilities for extracting features and
+train/testing :class:`revscoring.Model` from the command-line.  When the
+revscoring python package is installed, a `revscoring` utility should be
+available from the commandline.  Run `revscoring -h` for more
+information:
+
+cv_train
+++++++++
+.. automodule:: revscoring.utilities.cv_train
+
+extract
++++++++
+.. automodule:: revscoring.utilities.extract
+
+fetch_text
+++++++++++
+.. automodule:: revscoring.utilities.fetch_text
+
+fit
++++
+.. automodule:: revscoring.utilities.fit
+
+model_info
+++++++++++
+.. automodule:: revscoring.utilities.model_info
+
+score
++++++
+.. automodule:: revscoring.utilities.score
+
+test_model
+++++++++++
+.. automodule:: revscoring.utilities.test_model
+
+tune
+++++
+.. automodule:: revscoring.utilities.tune
+"""

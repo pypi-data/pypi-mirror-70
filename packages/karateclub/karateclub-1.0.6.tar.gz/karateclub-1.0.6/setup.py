e3dd2b7138b42e9d6ac3c399501f2222c2de64bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:19 +0000

    Add package karateclub/1.0.6/karateclub-1.0.6.tar.gz

---
+++ packages/karateclub/karateclub-1.0.6.tar.gz/karateclub-1.0.6/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = "karateclub",
+  packages = find_packages(),
+  version = "1.0.6",
+  license = "MIT",
+  description = "A general purpose library for community detection, network embedding, and graph mining research.",
+  author = "Benedek Rozemberczki",
+  author_email = "benedek.rozemberczki@gmail.com",
+  url = "https://github.com/benedekrozemberczki/karateclub",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_10006.tar.gz",
+  keywords = ["community", "detection", "networkx", "graph", "clustering", "embedding","network","deepwalk","graph2vec","node2vec","deep","learning","louvain","machine-learning","deep-learning","deeplearning"],
+  install_requires=[
+          "numpy",
+          "networkx",
+          "tqdm",
+          "python-louvain",
+          "sklearn",
+          "scipy",
+          "pygsp",
+          "gensim",
+          "pandas",
+          "six",
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.5",
+  ],
+)

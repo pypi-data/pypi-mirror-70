eb28ae403385f9aa2b8a490398fa19fbe58ec9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:30 +0000

    Add package morphapi/0.0.8/morphapi-0.0.8.tar.gz

---
+++ packages/morphapi/morphapi-0.0.8.tar.gz/morphapi-0.0.8/examples/download/allen_morphology_api.py
@@ -0,0 +1,21 @@
+from vtkplotter import Plotter
+
+from morphapi.api.mouselight import MouseLightAPI
+
+
+# ---------------------------- Downloading neurons --------------------------- #
+mlapi = MouseLightAPI()
+
+# Fetch metadata for neurons with some in the secondary motor cortex
+neurons_metadata = mlapi.fetch_neurons_metadata(filterby='soma', filter_regions=['MOs'])
+
+neurons = mlapi.download_neurons(neurons_metadata[0]) # downloading only one neuron to speed things up
+
+# ------------------------------- Visualisation ------------------------------ #
+print('creating meshes')
+neurons = [neuron.create_mesh()[1] for neuron in neurons]
+
+print('visualizing')
+vp = Plotter(shape=(1, len(neurons)), axes=1)
+
+vp.show(neurons)

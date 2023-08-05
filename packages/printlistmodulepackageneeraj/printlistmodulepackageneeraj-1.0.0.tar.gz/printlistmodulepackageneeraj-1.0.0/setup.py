36198ef8fcf57ff5b5021645aa57fdd0af7fa660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package printlistmodulepackageneeraj/1.0.0/printlistmodulepackageneeraj-1.0.0.tar.gz

---
+++ packages/printlistmodulepackageneeraj/printlistmodulepackageneeraj-1.0.0.tar.gz/printlistmodulepackageneeraj-1.0.0/setup.py
@@ -0,0 +1,9 @@
+
+
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name = "printlistmodulepackageneeraj", version="1.0.0", description="an API to print \
+      a list recursively", author="Neeraj", author_email="neeraj.pm@ignitarium.com", \
+          py_modules=["printlistmodule"])

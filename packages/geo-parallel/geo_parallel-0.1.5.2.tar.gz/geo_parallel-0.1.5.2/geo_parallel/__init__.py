f72a9181a02addec7bbbb5a3f3e739ec63074322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:51 +0000

    Add package geo-parallel/0.1.5.2/geo_parallel-0.1.5.2.tar.gz

---
+++ packages/geo-parallel/geo_parallel-0.1.5.2.tar.gz/geo_parallel-0.1.5.2/geo_parallel/__init__.py
@@ -0,0 +1,18 @@
+import rasterio
+
+
+# Open raster file
+def open_raster(raster_file):
+    return rasterio.open(raster_file)
+
+
+# Save raster file
+def save_raster(data, raster_file, width, height, crs, transform):
+    raster = rasterio.open(raster_file, 'w', driver='Gtiff',
+                           width=width, height=height,
+                           count=1,
+                           crs=crs,
+                           transform=transform,
+                           dtype='float64')
+    raster.write(data, 1)
+    raster.close()

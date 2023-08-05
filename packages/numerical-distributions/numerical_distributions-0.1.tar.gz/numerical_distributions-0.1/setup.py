94dffc9aebeeae83ec0dca372463b133b0dbdfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:29 +0000

    Add package numerical-distributions/0.1/numerical_distributions-0.1.tar.gz

---
+++ packages/numerical-distributions/numerical_distributions-0.1.tar.gz/numerical_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='numerical_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['numerical_distributions'],
+      author='Palak Gupta',
+      author_email='palakgupta889@gmail.com',
+      zip_safe=False)

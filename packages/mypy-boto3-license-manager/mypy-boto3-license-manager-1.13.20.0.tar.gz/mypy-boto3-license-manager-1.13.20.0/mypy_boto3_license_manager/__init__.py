00dc6b957c7fb773518e407f003b77fd49db1b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package mypy-boto3-license-manager/1.13.20.0/mypy-boto3-license-manager-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-license-manager/mypy-boto3-license-manager-1.13.20.0.tar.gz/mypy-boto3-license-manager-1.13.20.0/mypy_boto3_license_manager/__init__.py
@@ -0,0 +1,46 @@
+"""
+Main interface for license-manager service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.license_manager import (
+        Client,
+        LicenseManagerClient,
+        ListAssociationsForLicenseConfigurationPaginator,
+        ListLicenseConfigurationsPaginator,
+        ListLicenseSpecificationsForResourcePaginator,
+        ListResourceInventoryPaginator,
+        ListUsageForLicenseConfigurationPaginator,
+        )
+
+    session = boto3.Session()
+
+    client: LicenseManagerClient = boto3.client("license-manager")
+    session_client: LicenseManagerClient = session.client("license-manager")
+
+    list_associations_for_license_configuration_paginator: ListAssociationsForLicenseConfigurationPaginator = client.get_paginator("list_associations_for_license_configuration")
+    list_license_configurations_paginator: ListLicenseConfigurationsPaginator = client.get_paginator("list_license_configurations")
+    list_license_specifications_for_resource_paginator: ListLicenseSpecificationsForResourcePaginator = client.get_paginator("list_license_specifications_for_resource")
+    list_resource_inventory_paginator: ListResourceInventoryPaginator = client.get_paginator("list_resource_inventory")
+    list_usage_for_license_configuration_paginator: ListUsageForLicenseConfigurationPaginator = client.get_paginator("list_usage_for_license_configuration")
+"""
+from mypy_boto3_license_manager.client import LicenseManagerClient as Client, LicenseManagerClient
+from mypy_boto3_license_manager.paginator import (
+    ListAssociationsForLicenseConfigurationPaginator,
+    ListLicenseConfigurationsPaginator,
+    ListLicenseSpecificationsForResourcePaginator,
+    ListResourceInventoryPaginator,
+    ListUsageForLicenseConfigurationPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "LicenseManagerClient",
+    "ListAssociationsForLicenseConfigurationPaginator",
+    "ListLicenseConfigurationsPaginator",
+    "ListLicenseSpecificationsForResourcePaginator",
+    "ListResourceInventoryPaginator",
+    "ListUsageForLicenseConfigurationPaginator",
+)

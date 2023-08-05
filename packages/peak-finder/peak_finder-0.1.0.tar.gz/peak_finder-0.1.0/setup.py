8124e3ee3217de827def8c0fbdfbdb26550634f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package peak-finder/0.1.0/peak_finder-0.1.0.tar.gz

---
+++ packages/peak-finder/peak_finder-0.1.0.tar.gz/peak_finder-0.1.0/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name = 'peak_finder',
+    version = '0.1.0',
+    author = 'Gabriel Perko-Engel',
+    author_email = 'gpe@berkeley.edu',
+    description = 'For finding and processing Lorentzian line shapes.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    license = 'MIT',
+    url = 'https://github.com/GabePoel/ML-Peak-Tracker',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    python_requires = '>=3/6',
+    install_requires = [
+        'numpy', 
+        'scipy',
+        'matplotlib', 
+        'nptdms', 
+        'tensorflow',
+        ],
+    package_data = {
+        # Include anything found in the "trained_models" subdirectory
+        'src/trained_models': [
+            '*/*',
+            '*/*/*'
+            # 'simple_class/*', 
+            # 'simple_class_backup/*', 
+            # 'simple_count/*', 
+            # 'simple_count_backup/*'
+            # 'tight_wiggle/*',
+            # 'tight_wiggle_backup/*',
+            # 'wide_wiggle/*',
+            # 'wide_wiggle_backup/*'
+            ]
+    }
+)

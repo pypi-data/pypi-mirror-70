f04bb4a302c057d0bde3937763cfdda8fcfdebfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package pixl/0.1.8.7/pixl-0.1.8.7.tar.gz

---
+++ packages/pixl/pixl-0.1.8.7.tar.gz/pixl-0.1.8.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='pixl',
+      version='0.1.8.7',
+      description='A simple package that takes in an image and returns a vector.',
+      url='https://github.com/justinhtn/pixl',
+      author='Justin Houghton',
+      author_email='justin.houghton@gmail.com',
+      license='MIT',
+      packages=find_packages(),     
+      include_package_data=True,
+      zip_safe=False,
+      python_requires='>=3.6'
+      )

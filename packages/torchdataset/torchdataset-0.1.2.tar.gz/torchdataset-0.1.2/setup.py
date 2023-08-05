06f11b8ddd543cc4397d8bb518ef79149bc87ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:08 +0000

    Add package torchdataset/0.1.2/torchdataset-0.1.2.tar.gz

---
+++ packages/torchdataset/torchdataset-0.1.2.tar.gz/torchdataset-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['torchdataset']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=7.1.2,<8.0.0', 'sox>=1.3.7,<2.0.0', 'torch>=1.5.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'torchdataset',
+    'version': '0.1.2',
+    'description': 'This is a package to handle various kinds of data in a unified way with Pytorch.',
+    'long_description': None,
+    'author': 'popura',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

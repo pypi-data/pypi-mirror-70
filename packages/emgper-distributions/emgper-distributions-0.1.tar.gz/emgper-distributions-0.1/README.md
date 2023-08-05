7d5f99e0b1b08c029c8d0bb2d2aa429c982a91e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:44 +0000

    Add package emgper-distributions/0.1/emgper-distributions-0.1.tar.gz

---
+++ packages/emgper-distributions/emgper-distributions-0.1.tar.gz/emgper-distributions-0.1/README.md
@@ -0,0 +1,3 @@
+# Upload to PyPi exercise
+
+- This is the part of the lesson where we practice uploading a package to PyPi.
---
+++ packages/emgper-distributions/emgper-distributions-0.1.tar.gz/emgper-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='emgper-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)

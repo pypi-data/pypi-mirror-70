f3e1026bd0dd5da6b77fbae54d03f335267731b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:53 +0000

    Add package POSM/0.1.2/POSM-0.1.2.tar.gz

---
+++ packages/POSM/POSM-0.1.2.tar.gz/POSM-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(name="POSM",
+                 author="Philipp Rigoll",
+                 version="0.1.2",
+                 author_email="philipp@rigoll.de",
+                 url="https://github.com/PhilippRigoll/POSM",
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 install_requires=[
+                     "numpy",
+                     "Pillow",
+                     "PyYAML",
+                     "PySide2",
+                     "easydict",
+                     "lxml",
+                     "requests",
+                     "gpxpy"
+                 ],
+                 python_requires=">=3.6",
+                 entry_points={
+                     "console_scripts": ["POSM=POSM.main:main"]
+                 })

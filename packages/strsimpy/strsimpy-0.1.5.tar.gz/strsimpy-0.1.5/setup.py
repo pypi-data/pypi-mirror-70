d9e074f5638fa026a402f2761ee7f50fcc25ac85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package strsimpy/0.1.5/strsimpy-0.1.5.tar.gz

---
+++ packages/strsimpy/strsimpy-0.1.5.tar.gz/strsimpy-0.1.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="strsimpy",
+    version="0.1.5",
+    description="A library implementing different string similarity and distance measures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luozhouyang/python-string-similarity",
+    author="ZhouYang Luo",
+    author_email="stupidme.me.lzy@gmail.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    license="MIT License",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)

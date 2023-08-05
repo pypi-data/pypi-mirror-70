469a07d6c8e8a0603c43a4bdddce32ba63f77e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:08 +0000

    Add package dnsd-dist/0.1/dnsd_dist-0.1.tar.gz

---
+++ packages/dnsd-dist/dnsd_dist-0.1.tar.gz/dnsd_dist-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dnsd_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dnsd_dist'],
+      license='MIT',    
+      author = 'Bello Musa',                 
+      author_email = 'musbell007@gmail.com', 
+      zip_safe=False)

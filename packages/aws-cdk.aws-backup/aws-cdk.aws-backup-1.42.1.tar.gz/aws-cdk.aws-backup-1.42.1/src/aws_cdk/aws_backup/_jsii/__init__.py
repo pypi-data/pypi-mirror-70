0d5254b3fde66efb8a3dd44b40a35a11035ed122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package aws-cdk.aws-backup/1.42.1/aws-cdk.aws-backup-1.42.1.tar.gz

---
+++ packages/aws-cdk.aws-backup/aws-cdk.aws-backup-1.42.1.tar.gz/aws-cdk.aws-backup-1.42.1/src/aws_cdk/aws_backup/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_ec2
+import aws_cdk.aws_efs
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_rds
+import aws_cdk.aws_sns
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-backup", "1.42.1", __name__[0:-6], "aws-backup@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

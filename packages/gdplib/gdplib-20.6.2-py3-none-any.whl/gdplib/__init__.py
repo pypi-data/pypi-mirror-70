9c509a8ec406f0421d677fc8a1b4ece72c122bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package gdplib/20.6.2/gdplib-20.6.2-py3-none-any.whl

---
+++ packages/gdplib/gdplib-20.6.2-py3-none-any.whl/gdplib/__init__.py
@@ -0,0 +1,7 @@
+import gdplib.mod_hens
+import gdplib.modprodnet
+import gdplib.biofuel
+import gdplib.logical  # Requires logical expression system
+import gdplib.pyomo_examples
+import gdplib.stranded_gas  # Requires logical expression system
+import gdplib.gdp_col

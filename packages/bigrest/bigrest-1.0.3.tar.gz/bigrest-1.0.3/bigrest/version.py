e4d1abeeba21ca7f4b8c698c7f8e1f8cbea3118f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:46 +0000

    Add package bigrest/1.0.3/bigrest-1.0.3.tar.gz

---
+++ packages/bigrest/bigrest-1.0.3.tar.gz/bigrest-1.0.3/bigrest/version.py
@@ -0,0 +1,15 @@
+"""File with version and history information."""
+
+__version__ = '1.0.3'
+
+"""
+History Information:
+1.0.0
+    First Version
+1.0.1
+    Removed tests folder from pip
+1.0.2
+    Fixed documentation
+1.0.3
+    Fixed documentation
+"""

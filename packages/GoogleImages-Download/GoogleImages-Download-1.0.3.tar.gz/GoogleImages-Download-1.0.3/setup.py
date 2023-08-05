721e39f539cca40a30af65fdaeb64e84a8a57b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:24 +0000

    Add package GoogleImages-Download/1.0.3/GoogleImages-Download-1.0.3.tar.gz

---
+++ packages/GoogleImages-Download/GoogleImages-Download-1.0.3.tar.gz/GoogleImages-Download-1.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="GoogleImages-Download",
+    version="1.0.3",
+    description="A Python Script for downloading bulk of images from google.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/khannoaman/GoogleImages-Download",
+    author="MOHAMMAD NOAMAN KHAN",
+    author_email="khan.noamaan@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["GoogleImages_Download"],
+    include_package_data=True,
+    install_requires=["requests","selenium","tqdm"],
+    entry_points={
+        "console_scripts": [
+            "GoogleImages-Download=GoogleImages_Download.GoogleImagesDownload:main",
+        ]
+    },
+)

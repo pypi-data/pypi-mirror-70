80e55d2ce6c97a62b2455053f659f67e7c427ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package ambit/0.3.0/ambit-0.3.0.tar.gz

---
+++ packages/ambit/ambit-0.3.0.tar.gz/ambit-0.3.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+setuptools.setup(
+    name='ambit',
+    version='0.3.0',
+    description='Take control of your Palette.',
+    long_description=(
+        '**ambit** is a Python library for interacting with PaletteGear '
+        'and MonogramCC devices, a graphical simulator for device-free '
+        'development, and an accompanying set of configurable end user '
+        'tools and demos.'
+    ),
+    long_description_content_type='text/markdown',
+    keywords=['monogramcc', 'palettegear', 'demoscene', 'pygame', 'linux', 'pyusb'],
+    url='https://github.com/khimaros/ambit',
+    author='khimaros',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    scripts=[
+        'bin/ambit',
+        'bin/ambit_gui',
+        'bin/ambit_simulator',
+        'bin/ambit_image_convert',
+        'bin/ambit_image_display',
+        'bin/ambit_push_assets',
+        'bin/ambit_map_midi',
+        'bin/ambit_map_hid',
+        'bin/ambit_demoscene',
+        'bin/ambit_demoscene_simulator',
+        'bin/ambit_lavalamp',
+        'bin/ambit_lavalamp_simulator',
+        'bin/ambit_lightshow',
+        'bin/ambit_lightshow_gui',
+        'bin/ambit_lightshow_simulator',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Topic :: System :: Hardware',
+    ],
+    python_requires='>=3.6',
+)

ea30c99ae36717a6f3533d838c193f7a7220f826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package distributions-tortoise/0.1/distributions_tortoise-0.1.tar.gz

---
+++ packages/distributions-tortoise/distributions_tortoise-0.1.tar.gz/distributions_tortoise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_tortoise',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tortoise'],
+      zip_safe=False)

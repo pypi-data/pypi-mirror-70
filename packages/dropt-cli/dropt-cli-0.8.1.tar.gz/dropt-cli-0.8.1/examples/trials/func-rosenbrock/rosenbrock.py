ff7b0ea5831f8b213dafa122924b193ed730ec7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:29 +0000

    Add package dropt-cli/0.8.1/dropt-cli-0.8.1.tar.gz

---
+++ packages/dropt-cli/dropt-cli-0.8.1.tar.gz/dropt-cli-0.8.1/examples/trials/func-rosenbrock/rosenbrock.py
@@ -0,0 +1,46 @@
+'''
+Reference
+---
+https://en.wikipedia.org/wiki/Test_functions_for_optimization
+'''
+
+
+import logging
+from dropt.util.log import UserLogger
+from argparse import ArgumentParser
+
+
+# logger
+logger = UserLogger('rosenbrock')
+logger.add_console_handler(logging.INFO)
+logger.add_file_handler(logging.INFO, filename='log/rosenbrock.log')
+
+
+def run(params):
+    '''Evaluate the test function'''
+    logger.info(f'parameters = {params}')
+    x = params['x']
+    y = params['y']
+    z = params['z']
+    value = 0
+    value = value + 100*(y-x**2)**2+(1-x)**2
+    value = value + 100*(z-y**2)**2+(1-y)**2
+    logger.info(f"value = {value}")
+    return value
+
+
+def param_loader():
+    '''Get parameters'''
+    parser = ArgumentParser(description='Rosenbrock function')
+    parser.add_argument('x', type=float)
+    parser.add_argument('y', type=float)
+    parser.add_argument('z', type=float)
+
+    args, _ = parser.parse_known_args()
+    return vars(args)
+
+
+if __name__ == '__main__':
+    logger.info('Evaluation of the eggholder function')
+    params = param_loader()
+    print(run(params))

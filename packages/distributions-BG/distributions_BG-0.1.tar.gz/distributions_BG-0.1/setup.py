6f4aa83f23e087965dbe5fa79b443c12d5de354a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package distributions-BG/0.1/distributions_BG-0.1.tar.gz

---
+++ packages/distributions-BG/distributions_BG-0.1.tar.gz/distributions_BG-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_BG',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_BG'],
+      author = 'Antonio Gonzalez',
+      zip_safe=False)

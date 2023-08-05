2e0dd72cd39b4a79413afd7a9a29d093478c0d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:56 +0000

    Add package lalgebra/0.0.1/lalgebra-0.0.1.tar.gz

---
+++ packages/lalgebra/lalgebra-0.0.1.tar.gz/lalgebra-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name = 'lalgebra',
+    packages = ['lalgebra'],
+    version = '0.0.1',
+    license='MIT',
+    description = 'Includes basic support for points, lines and vectors',
+    author = 'Jonathan Tan Jiayi',
+    author_email = 'jonathantanatlol@gmail.com',
+    url = 'https://github.com/peaceknight05/linear',
+    download_url = 'https://github.com/peaceknight05/linear/archive/v0.0.1-alpha.2.tar.gz',
+    keywords = ['mathematics', 'linear algebra', 'algebra', 'topography', 'geometry', 'vectors', 'lines', 'points'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)

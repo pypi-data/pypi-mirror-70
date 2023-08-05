a4e568259590921ee605ec50b0fa838323ef4e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:40 +0000

    Add package platform-agent/0.0.26/platform-agent-0.0.26.tar.gz

---
+++ packages/platform-agent/platform-agent-0.0.26.tar.gz/platform-agent-0.0.26/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="platform-agent",
+    version='0.0.26',
+    py_modules=['platform-agent'],
+    install_requires=[
+        'pyroute2==0.5.12',
+        'websocket-client==0.57.0',
+        'requests==2.22.0',
+        'PyNaCl==1.3.0',
+        'docker-py==1.10.6',
+        'icmplibv2==1.0.5'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'noia_agent = platform_agent.__main__:main'
+        ]
+    },
+)

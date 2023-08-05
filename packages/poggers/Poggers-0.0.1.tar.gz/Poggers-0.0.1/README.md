3935616ec38bb1b2bc587b3fdac7ed20c871e044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:56 +0000

    Add package poggers/0.0.1/Poggers-0.0.1.tar.gz

---
+++ packages/poggers/Poggers-0.0.1.tar.gz/Poggers-0.0.1/README.md
@@ -0,0 +1,23 @@
+A small package for converting text to ascii, or just basic maths.
+
+Usage:
+
+```
+import poggers  
+text = 'Hello world'  
+binary = poggers.Binary(text)  
+print(binary)
+```
+
+The following options include:
+
+```
+poggers.Add(x, y)
+poggers.Minus(x, y)
+poggers.Times(x, y)
+poggers.Divide(x, y)
+poggers.Binary(text)
+poggers.Asciify(text)
+poggers.Morse(text)
+```
+
---
+++ packages/poggers/Poggers-0.0.1.tar.gz/Poggers-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Poggers", # Replace with your own username
+    version="0.0.1",
+    author="Rika",
+    author_email="jayden05jaydenhigginsjayden05@hotmail.com",
+    description="A small package for converting text to ascii",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://google.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

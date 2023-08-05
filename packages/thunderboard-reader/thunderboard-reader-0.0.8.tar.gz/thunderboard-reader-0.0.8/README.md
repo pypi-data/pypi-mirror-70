094bdf8dc60e89692d4fdf1ef0f3cbbeacbad1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package thunderboard-reader/0.0.8/thunderboard-reader-0.0.8.tar.gz

---
+++ packages/thunderboard-reader/thunderboard-reader-0.0.8.tar.gz/thunderboard-reader-0.0.8/README.md
@@ -0,0 +1,2 @@
+# thunderboard-reader
+Read data over USB from the Silicon Labs Thunderboard Sense2 for plotting and saving to CSV
---
+++ packages/thunderboard-reader/thunderboard-reader-0.0.8.tar.gz/thunderboard-reader-0.0.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thunderboard-reader", # Replace with your own username
+    version="0.0.8",
+    author="Lucas J. Koerner",
+    author_email="koerner.lucas@stthomas.edu",
+    # entry_points={'console_scripts': [
+    #    CLI + ' = gdm.cli:main',
+    #    'git-' + PLUGIN + ' = gdm.plugin:main',
+    #]},
+    scripts = ['tboard_read/tboard_read.py'],
+    description="Reads data over the USB interface from the Silicon Labs Thunderboard Sense2",
+    url="https://github.com/lucask07/thunderboard-reader/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+    license = 'MIT Licence',
+    install_requires = ['pyserial', 'numpy', 'matplotlib',
+                'appnope', 'pandas']
+)

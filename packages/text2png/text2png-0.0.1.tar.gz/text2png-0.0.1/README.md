5c43b96ffd0c674f2ec0008f703b1d845a2f0755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:13 +0000

    Add package text2png/0.0.1/text2png-0.0.1.tar.gz

---
+++ packages/text2png/text2png-0.0.1.tar.gz/text2png-0.0.1/README.md
@@ -0,0 +1,24 @@
+# Text to Image (Text to PNG)
+
+<badges>[![version](https://img.shields.io/pypi/v/text2png.svg)](https://pypi.org/project/text2png/)
+[![license](https://img.shields.io/pypi/l/text2png.svg)](https://pypi.org/project/text2png/)
+[![pyversions](https://img.shields.io/pypi/pyversions/text2png.svg)](https://pypi.org/project/text2png/)  
+[![donate](https://img.shields.io/badge/Donate-Paypal-0070ba.svg)](https://paypal.me/foxe6)
+[![powered](https://img.shields.io/badge/Powered%20by-UTF8-red.svg)](https://paypal.me/foxe6)
+[![made](https://img.shields.io/badge/Made%20with-PyCharm-red.svg)](https://paypal.me/foxe6)
+</badges>
+
+<i>Easiest way to create text thumbnail.</i>
+
+# Hierarchy
+
+```
+text2png
+```
+
+# Example
+
+## python
+```python
+
+```
---
+++ packages/text2png/text2png-0.0.1.tar.gz/text2png-0.0.1/text2png/core.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw, ImageFont
+from filehandling import join_path
+from omnitools import color_value, text_with_padding
+import os
+
+
+__ALL__ = ["TextToPng"]
+
+
+class TextToPng(object):
+    def __init__(self, font_file: str, font_size: int,
+                 background_color: color_value = (0, 0, 0),
+                 text_color: color_value = (255, 255, 255),
+                 save_dir: str = os.environ["TEMP"]) -> None:
+        self.font_file = font_file
+        self.font_size = font_size
+        self.background_color = background_color
+        self.text_color = text_color
+        self.save_dir = save_dir
+
+    def create(self, text: str, padding: int = -1) -> str:
+        font = ImageFont.truetype(self.font_file, self.font_size)
+        shape, pos = text_with_padding(font, text, padding)
+        img = Image.new("RGB", shape, color=self.background_color)
+        brush = ImageDraw.Draw(img)
+        brush.text(pos, text, font=font, fill=self.text_color)
+        save_path = join_path(self.save_dir, "__tmp.png")
+        img.save(save_path)
+        return save_path
+
+

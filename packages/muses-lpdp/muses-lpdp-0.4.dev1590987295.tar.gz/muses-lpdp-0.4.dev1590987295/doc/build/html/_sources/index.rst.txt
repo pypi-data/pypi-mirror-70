69aa23a6bee9fa35f6323669f9ea355209df0866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:11 +0000

    Add package muses-lpdp/0.4.dev1590987295/muses-lpdp-0.4.dev1590987295.tar.gz

---
+++ packages/muses-lpdp/muses-lpdp-0.4.dev1590987295.tar.gz/muses-lpdp-0.4.dev1590987295/doc/build/html/_sources/index.rst.txt
@@ -0,0 +1,29 @@
+.. muses_doc documentation master file, created by
+sphinx-quickstart on Sun Jan  5 10:52:17 2020.
+You can adapt this file completely to your liking, but it should at least
+contain the root `toctree` directive.
+
+Bienvenue sur la documentation de Muses (LPDP)
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   usage/installation
+   usage/quickstart
+
+
+.. automodule:: core
+   :members:
+
+.. automodule:: db
+   :members:
+
+
+Index and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/muses-lpdp/muses-lpdp-0.4.dev1590987295.tar.gz/muses-lpdp-0.4.dev1590987295/setup.py
@@ -0,0 +1,65 @@
+from glob import glob
+import os
+from setuptools import setup
+from setuptools.command.install import install
+from os import getenv, getcwd
+
+__pkgname__ = "./"
+
+with open('requirements.txt', 'r') as f:
+    install_reqs = [
+        s for s in [
+            line.split('#', 1)[0].strip(' \t\n') for line in f
+        ] if s != ''
+    ]
+
+data_files = []
+
+start_point = os.path.join(__pkgname__, 'static')
+for root, dirs, files in os.walk(start_point):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+start_point = os.path.join(__pkgname__, 'media')
+for root, dirs, files in os.walk(start_point):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+start_point = os.path.join(__pkgname__, 'doc')
+for root, dirs, files in os.walk(start_point):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+start_point = os.path.join(__pkgname__, 'templates')
+for root, dirs, files in os.walk(start_point):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+start_point = os.path.join(__pkgname__, 'logos')
+for root, dirs, files in os.walk(start_point):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+
+class InstallWrapper(install):
+    media_path = "media"
+    static_path = "static"
+
+    def run(self):
+        super().run()
+        self.static_path = getenv("STATIC_PATH", f"{getcwd()}/{self.static_path}")
+
+
+setup(
+    packages=[
+        'api',
+        'core',
+        'db',
+        'editorjs',
+        'web'
+    ],
+    install_requires=install_reqs,
+    include_package_data=True,
+    setup_requires=['wheel'],
+    data_files=data_files
+)

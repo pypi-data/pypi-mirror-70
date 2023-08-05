5703b46561e13ff99174de7888980b2eb81abaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package aws-cdk.aws-mediastore/1.42.1/aws-cdk.aws-mediastore-1.42.1.tar.gz

---
+++ packages/aws-cdk.aws-mediastore/aws-cdk.aws-mediastore-1.42.1.tar.gz/aws-cdk.aws-mediastore-1.42.1/src/aws_cdk/aws_mediastore/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-mediastore", "1.42.1", __name__[0:-6], "aws-mediastore@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

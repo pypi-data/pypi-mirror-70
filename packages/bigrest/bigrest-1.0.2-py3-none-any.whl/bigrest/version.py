c6bda3cb18674e9935e0168ebe2baf626a327381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package bigrest/1.0.2/bigrest-1.0.2-py3-none-any.whl

---
+++ packages/bigrest/bigrest-1.0.2-py3-none-any.whl/bigrest/version.py
@@ -0,0 +1,13 @@
+"""File with version and history information."""
+
+__version__ = '1.0.2'
+
+"""
+History Information:
+1.0.0
+    First Version
+1.0.1
+    Removed tests folder from pip
+1.0.2
+    Fixed documentation
+"""

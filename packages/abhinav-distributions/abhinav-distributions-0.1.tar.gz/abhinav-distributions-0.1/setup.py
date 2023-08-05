9c8a2f43dda2192934facc0852d1335eefe1b76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:47 +0000

    Add package abhinav-distributions/0.1/abhinav-distributions-0.1.tar.gz

---
+++ packages/abhinav-distributions/abhinav-distributions-0.1.tar.gz/abhinav-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='abhinav-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['abhinav-distributions'],
+      zip_safe=False)

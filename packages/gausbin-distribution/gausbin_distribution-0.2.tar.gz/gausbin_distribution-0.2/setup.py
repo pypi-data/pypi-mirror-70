119026ba1f91453ea562a54d7a59227d9e2fdc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:35 +0000

    Add package gausbin-distribution/0.2/gausbin_distribution-0.2.tar.gz

---
+++ packages/gausbin-distribution/gausbin_distribution-0.2.tar.gz/gausbin_distribution-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="gausbin_distribution",
+      version="0.2",
+      description="Gaussian and Binomial Distribution",
+      packages=["gausbin_distributions"],
+      author="Victor Oguche",
+      author_email = "victoroguche@gmail.com",
+      zip_safe=False)

ae8bd2fc0383b1f1548c72e40f9bda443d9c47c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:36 +0000

    Add package envirocar-py/0.0.3/envirocar-py-0.0.3.tar.gz

---
+++ packages/envirocar-py/envirocar-py-0.0.3.tar.gz/envirocar-py-0.0.3/envirocar/client/api/track_api.py
@@ -0,0 +1,116 @@
+import json
+import pandas as pd
+import geopandas as gpd
+
+from ..request_param import RequestParam, BboxSelector, TimeSelector
+from ..download_client import DownloadClient
+
+class TrackAPI():
+    """Handles the API access to the enviroCar backend and returns queried results as dataframes"""
+    TRACKS_ENDPOINT = "tracks"
+    TRACK_ENDPOINT = "tracks/{}"
+    USERTRACKS_ENDPOINT = "users/{}/tracks"
+    
+    def __init__(self, api_client=None):
+        self.api_client = api_client or DownloadClient()
+
+    def get_tracks(self, username=None, bbox:BboxSelector=None, time_interval:TimeSelector=None, num_results=10, page_limit=100):
+        """Handles queries against the enviroCar api
+        
+        Keyword Arguments:
+            username {str} -- the username to request tracks for (default: {None})
+            bbox {BboxSelector} -- bbox query parameter (default: {None})
+            time_interval {TimeSelector} -- time interval query parameter (default: {None})
+            num_results {int} -- maximum number of tracks to request (default: {10})
+            page_limit {int} -- page limit (default: {100})
+        
+        Returns:
+            GeoDataFrame -- A GeoDataFrame containing the tracks matching the request parameters
+        """
+        path = self._get_path(username=username)
+
+        # creating download_requests
+        download_requests = []
+        current_results = 0
+        current_page = 1
+        while current_results < num_results:
+            request_params = {
+                'limit': page_limit,
+                'page': current_page
+            }
+            if bbox:
+                request_params.update(bbox.param)
+
+            request = RequestParam(path=path, params=request_params)
+            download_requests.append(request)
+            
+            current_results += page_limit
+            current_page += 1
+
+        # request for /tracks
+        tracks_meta_df = self.api_client.download(download_requests, decoder=_parse_tracks_list_df)
+        tracks_meta_df = tracks_meta_df[:num_results]
+
+        if not tracks_meta_df.empty: 
+            ids = tracks_meta_df['track.id'].values
+            tracks_df = self._get_tracks_by_ids(ids)
+            return tracks_df
+
+        return gpd.GeoDataFrame()
+
+    def get_track(self, track_id: str):
+        return self.api_client.download(
+            RequestParam(path=self._get_path(trackid=track_id)), 
+            decoder=_parse_track_df)
+
+    def _get_tracks_by_ids(self, ids: [str]):
+        download_requests = [RequestParam(path=self._get_path(trackid=id)) for id in ids]
+        return self.api_client.download(download_requests, decoder=_parse_track_df)
+
+    def _get_path(self, *, username=None, trackid=None):
+        if username is None and trackid is None:
+            return self.TRACKS_ENDPOINT
+        if username:
+            return self.USERTRACKS_ENDPOINT.format(username)
+        if trackid:
+            return self.TRACK_ENDPOINT.format(trackid)
+
+def _parse_tracks_list_df(tracks_jsons):
+    if not isinstance(tracks_jsons, list):
+        tracks_jsons = [tracks_jsons]
+
+    tracks_meta_df = pd.DataFrame()
+    for tracks_json in tracks_jsons:
+        if tracks_json:
+            ec_data = json.loads(tracks_json)
+            df = pd.json_normalize(ec_data, 'tracks')
+            df.rename(columns=__rename_track_columns, inplace=True)
+            tracks_meta_df = tracks_meta_df.append(df)
+
+    return tracks_meta_df
+
+def _parse_track_df(track_jsons):
+    if not isinstance(track_jsons, list):
+        track_jsons = [track_jsons]
+    
+    tracks_df = gpd.GeoDataFrame()
+    for track_json in track_jsons:
+        # read properties
+        car_df = pd.json_normalize(json.loads(track_json)['properties'])
+        car_df.columns = car_df.columns.str.replace('sensor.properties.', 'sensor.')
+        car_df.rename(columns=__rename_track_columns, inplace=True)
+
+        # read geojson values
+        track_df = gpd.read_file(track_json)
+        track_df = track_df.join(pd.json_normalize(track_df['phenomenons'])).drop(['phenomenons'], axis=1)
+        
+        # combine dataframes
+        car_df = pd.concat([car_df]*len(track_df.index), ignore_index=True)
+        tracks_df = tracks_df.append(track_df.join(car_df))
+        
+    return tracks_df
+
+def __rename_track_columns(x):
+    if not x.startswith('sensor'):
+        return 'track.' + x
+    return x
---
+++ packages/envirocar-py/envirocar-py-0.0.3.tar.gz/envirocar-py-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import io
+
+from pip._internal.req import parse_requirements
+from setuptools import setup, find_packages
+
+# loading requirements
+requirements = list(parse_requirements('requirements.txt', session='hack'))
+requirements = [r.requirement for r in requirements]
+
+
+def parse_long_description():
+    return io.open('README.md', encoding="utf-8").read()
+
+setup(
+    name="envirocar-py",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={
+        "": ["*.txt"]
+    },
+    include_package_data=True,
+    version="0.0.3",
+    description="Python Utilities for enviroCar",
+    long_description=parse_long_description(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/enviroCar/envirocar-py",
+    keywords=["enviroCar", "trajectory", "xFCD"],
+    install_requires=requirements,
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)

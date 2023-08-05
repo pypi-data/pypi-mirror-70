5121629995add4d6fa0d0114a6025cc8f6ef4429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:09 +0000

    Add package auquan-toolbox/2.1.10/auquan_toolbox-2.1.10.tar.gz

---
+++ packages/auquan-toolbox/auquan_toolbox-2.1.10.tar.gz/auquan_toolbox-2.1.10/backtester/instruments/__init__.py
@@ -0,0 +1,11 @@
+from backtester.instruments.instrument import Instrument
+from backtester.instruments.future_instrument import FutureInstrument
+from backtester.instruments.option_instrument import OptionInstrument
+from backtester.instruments.stock_instrument import StockInstrument
+
+__all__ = [
+    'Instrument',
+    'FutureInstrument',
+    'OptionInstrument',
+    'StockInstrument',
+]

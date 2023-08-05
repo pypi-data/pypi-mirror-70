20117392cceeebc8ad5aac3516ab932baa09144f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:52 +0000

    Add package dg-predictor/0.1.2/dg-predictor-0.1.2.tar.gz

---
+++ packages/dg-predictor/dg-predictor-0.1.2.tar.gz/dg-predictor-0.1.2/src/dg_predictor/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=wrong-import-position
+"""Introduce"""
+
+from __future__ import absolute_import
+
+from .base_predictor import Engine, BasePredictor
+from .predictor import load_config, create_predictor
+
+__version__ = '0.1.2'

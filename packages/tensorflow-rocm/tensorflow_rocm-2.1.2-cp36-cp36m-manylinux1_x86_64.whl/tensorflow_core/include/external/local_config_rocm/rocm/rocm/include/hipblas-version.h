7ba83556bc47b800e7c9608c2cec6d09859bc26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:35 +0000

    Add package tensorflow-rocm/2.1.2/tensorflow_rocm-2.1.2-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.1.2-cp36-cp36m-manylinux1_x86_64.whl/tensorflow_core/include/external/local_config_rocm/rocm/rocm/include/hipblas-version.h
@@ -0,0 +1,17 @@
+/* ************************************************************************
+ * Copyright 2016-2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#ifndef HIPBLAS_VERSION_H_
+#define HIPBLAS_VERSION_H_
+
+/* the configured version and settings
+ */
+// clang-format off
+#define hipblasVersionMajor 0
+#define hipblaseVersionMinor 28
+#define hipblasVersionPatch 0
+#define hipblasVersionTweak 382-rocm-rel-3.5-30-7d2cb89
+// clang-format on
+
+#endif

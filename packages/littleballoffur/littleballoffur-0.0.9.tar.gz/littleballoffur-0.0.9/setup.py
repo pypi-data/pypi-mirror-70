bdfef826b19854923c7d5cf296ae9b20410f6353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package littleballoffur/0.0.9/littleballoffur-0.0.9.tar.gz

---
+++ packages/littleballoffur/littleballoffur-0.0.9.tar.gz/littleballoffur-0.0.9/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = "littleballoffur",
+  packages = find_packages(),
+  version = "0.0.9",
+  license = "MIT",
+  description = "A general purpose library for subsampling large graphs.",
+  author = "Benedek Rozemberczki",
+  author_email = "benedek.rozemberczki@gmail.com",
+  url = "https://github.com/benedekrozemberczki/littleballoffur",
+  download_url = "https://github.com/benedekrozemberczki/littleballoffur/archive/v_00009.tar.gz",
+  keywords = ["community", "detection", "networkx", "graph", "clustering", 
+              "graph-sampling", "sampling", "random-walk", "forest-fire",
+              "embedding", "network", "deepwalk", "graph2vec", "node2vec",
+              "deep", "learning", "louvain", "machine-learning", "deep-learning", "deeplearning"],
+  install_requires = [
+          "networkx",
+          "tqdm",
+          "python-louvain",
+          "pandas",
+          "numpy",
+          "six",
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.5",
+  ],
+)

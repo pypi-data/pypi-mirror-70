4f467c7ac2d28ccba7c48a0b568eda777bf25485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:36 +0000

    Add package mirs-distribution-package/1.0/mirs_distribution_package-1.0.tar.gz

---
+++ packages/mirs-distribution-package/mirs_distribution_package-1.0.tar.gz/mirs_distribution_package-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mirs_distribution_package',
+      version='1.0',
+      description='Gaussian mirs_distribution_package',
+      packages=['mirs_distribution_package'],
+      zip_safe=False)

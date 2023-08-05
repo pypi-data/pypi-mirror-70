375d33671f2fbaceb13bf5be650e0131fa3ae3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:17 +0000

    Add package gradle-bodyguard/0.0.3/gradle_bodyguard-0.0.3-py3-none-any.whl

---
+++ packages/gradle-bodyguard/gradle_bodyguard-0.0.3-py3-none-any.whl/gradle_bodyguard/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.0.3'
+
+from . import app
+
+
+def main(argv=None):
+    app.main(argv)
---
+++ packages/gradle-bodyguard/gradle_bodyguard-0.0.3-py3-none-any.whl/gradle_bodyguard/cli_parser.py
@@ -0,0 +1,53 @@
+# cli_parser.py
+
+import argparse
+import sys
+
+from .utils.cli_colorizer import blue
+
+
+def parse(args):
+    parser = argparse.ArgumentParser(
+        prog='Gradle Bodyguard',
+        description='Scans a Gradle project and warns about potential security issues'
+    )
+
+    parser.add_argument(
+        '-p',
+        '--project',
+        action='store',
+        required=True,
+        help='Path to the target Gradle project you want to scan'
+    )
+
+    parser.add_argument(
+        '-d',
+        '--destination',
+        action='store',
+        default='stdout',
+        help='Directory where you want the security report save'
+    )
+
+    parser.add_argument(
+        '-i',
+        '--ignore',
+        action='store',
+        default='',
+        help='CVEs to ignore (separed with commas)'
+    )
+
+    parser.add_argument(
+        '-f',
+        '--force_exit',
+        action='store',
+        default=False,
+        help='Force program to exit with failure when issues were found'
+    )
+
+    try:
+        parsed = parser.parse_args(args)
+        ignored = [] if parsed.ignore == '' else parsed.ignore.split(',')
+        return [parsed.project, parsed.destination, ignored, parsed.force_exit]
+    except:
+        print(f"\nLearn more about with {blue('gradle-bodyguard --help')}")
+        sys.exit(0)

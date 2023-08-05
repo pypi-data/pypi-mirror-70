056b2de28a0279d9778087f72dc544aa6a1e64c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:14 +0000

    Add package bybn-distributions/1.1/bybn-distributions-1.1.tar.gz

---
+++ packages/bybn-distributions/bybn-distributions-1.1.tar.gz/bybn-distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bybn-distributions',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['bybn-distributions'],
+      author = "bharat kumar" ,
+      author_email = "smartboypmj@gmail.com",
+    zip_safe=False)

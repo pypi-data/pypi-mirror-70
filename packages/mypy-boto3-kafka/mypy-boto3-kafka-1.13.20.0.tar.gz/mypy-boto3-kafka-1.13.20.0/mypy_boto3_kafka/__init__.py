eadabf4a43f5ef252d79c72e0a4db5de8615064b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package mypy-boto3-kafka/1.13.20.0/mypy-boto3-kafka-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-kafka/mypy-boto3-kafka-1.13.20.0.tar.gz/mypy-boto3-kafka-1.13.20.0/mypy_boto3_kafka/__init__.py
@@ -0,0 +1,50 @@
+"""
+Main interface for kafka service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.kafka import (
+        Client,
+        KafkaClient,
+        ListClusterOperationsPaginator,
+        ListClustersPaginator,
+        ListConfigurationRevisionsPaginator,
+        ListConfigurationsPaginator,
+        ListKafkaVersionsPaginator,
+        ListNodesPaginator,
+        )
+
+    session = boto3.Session()
+
+    client: KafkaClient = boto3.client("kafka")
+    session_client: KafkaClient = session.client("kafka")
+
+    list_cluster_operations_paginator: ListClusterOperationsPaginator = client.get_paginator("list_cluster_operations")
+    list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+    list_configuration_revisions_paginator: ListConfigurationRevisionsPaginator = client.get_paginator("list_configuration_revisions")
+    list_configurations_paginator: ListConfigurationsPaginator = client.get_paginator("list_configurations")
+    list_kafka_versions_paginator: ListKafkaVersionsPaginator = client.get_paginator("list_kafka_versions")
+    list_nodes_paginator: ListNodesPaginator = client.get_paginator("list_nodes")
+"""
+from mypy_boto3_kafka.client import KafkaClient as Client, KafkaClient
+from mypy_boto3_kafka.paginator import (
+    ListClusterOperationsPaginator,
+    ListClustersPaginator,
+    ListConfigurationRevisionsPaginator,
+    ListConfigurationsPaginator,
+    ListKafkaVersionsPaginator,
+    ListNodesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "KafkaClient",
+    "ListClusterOperationsPaginator",
+    "ListClustersPaginator",
+    "ListConfigurationRevisionsPaginator",
+    "ListConfigurationsPaginator",
+    "ListKafkaVersionsPaginator",
+    "ListNodesPaginator",
+)

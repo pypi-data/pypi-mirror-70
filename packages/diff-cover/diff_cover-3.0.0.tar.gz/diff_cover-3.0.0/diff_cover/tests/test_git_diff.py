b4618a0388adab0c4fe6ff3858fafb8ce37f2496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:30 +0000

    Add package diff-cover/3.0.0/diff_cover-3.0.0.tar.gz

---
+++ packages/diff-cover/diff_cover-3.0.0.tar.gz/diff_cover-3.0.0/diff_cover/tests/test_git_diff.py
@@ -0,0 +1,107 @@
+from __future__ import unicode_literals
+import mock
+
+from diff_cover.command_runner import CommandError
+from diff_cover.git_diff import GitDiffTool
+import unittest
+
+
+class TestGitDiffTool(unittest.TestCase):
+
+    def setUp(self):
+
+        # Create mock subprocess to simulate `git diff`
+        self.process = mock.Mock()
+        self.process.returncode = 0
+        self.subprocess = mock.patch('diff_cover.command_runner.subprocess').start()
+        self.subprocess.Popen.return_value = self.process
+        self.addCleanup(mock.patch.stopall)
+        # Create the git diff tool
+        self.tool = GitDiffTool('...')
+
+    def check_diff_committed(self, diff_range_notation):
+        self.tool = GitDiffTool(diff_range_notation)
+        self._set_git_diff_output('test output', '')
+        output = self.tool.diff_committed()
+
+        # Expect that we get the correct output
+        self.assertEqual(output, 'test output')
+
+        # Expect that the correct command was executed
+        expected = ['git', '-c', 'diff.mnemonicprefix=no', '-c',
+                    'diff.noprefix=no', 'diff', 'origin/master{}HEAD'.format(diff_range_notation),
+                    '--no-color', '--no-ext-diff', '-U0']
+        self.subprocess.Popen.assert_called_with(
+            expected, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE
+        )
+
+    def test_diff_commited(self):
+        self.check_diff_committed('...')
+        self.check_diff_committed('..')
+
+    def test_diff_unstaged(self):
+        self._set_git_diff_output('test output', '')
+        output = self.tool.diff_unstaged()
+
+        # Expect that we get the correct output
+        self.assertEqual(output, 'test output')
+
+        # Expect that the correct command was executed
+        expected = ['git', '-c', 'diff.mnemonicprefix=no', '-c',
+                    'diff.noprefix=no', 'diff', '--no-color', '--no-ext-diff',
+                    '-U0']
+        self.subprocess.Popen.assert_called_with(
+            expected, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE
+        )
+
+    def test_diff_staged(self):
+        self._set_git_diff_output('test output', '')
+        output = self.tool.diff_staged()
+
+        # Expect that we get the correct output
+        self.assertEqual(output, 'test output')
+
+        # Expect that the correct command was executed
+        expected = ['git', '-c', 'diff.mnemonicprefix=no', '-c',
+                    'diff.noprefix=no', 'diff', '--cached', '--no-color',
+                    '--no-ext-diff', '-U0']
+        self.subprocess.Popen.assert_called_with(
+            expected, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE
+        )
+
+    def test_diff_committed_compare_branch(self):
+
+        # Override the default compare branch
+        self._set_git_diff_output('test output', '')
+        output = self.tool.diff_committed(compare_branch='release')
+
+        # Expect that we get the correct output
+        self.assertEqual(output, 'test output')
+
+        # Expect that the correct command was executed
+        expected = ['git', '-c', 'diff.mnemonicprefix=no', '-c',
+                    'diff.noprefix=no', 'diff', 'release...HEAD', '--no-color',
+                    '--no-ext-diff', '-U0']
+        self.subprocess.Popen.assert_called_with(
+            expected, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE
+        )
+
+    def test_errors(self):
+        self._set_git_diff_output('test output', 'fatal error', 1)
+
+        with self.assertRaises(CommandError):
+            self.tool.diff_unstaged()
+
+        with self.assertRaises(CommandError):
+            self.tool.diff_staged()
+
+        with self.assertRaises(CommandError):
+            self.tool.diff_unstaged()
+
+    def _set_git_diff_output(self, stdout, stderr, returncode=0):
+        """
+        Configure the `git diff` mock to output `stdout`
+        and `stderr` to stdout and stderr, respectively.
+        """
+        self.process.communicate.return_value = (stdout, stderr)
+        self.process.returncode = returncode

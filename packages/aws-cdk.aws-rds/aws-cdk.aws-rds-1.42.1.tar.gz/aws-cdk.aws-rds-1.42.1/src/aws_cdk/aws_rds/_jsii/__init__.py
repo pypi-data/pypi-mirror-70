1efc5d03210aab3e0576d193ada8a6ce2e185056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:02 +0000

    Add package aws-cdk.aws-rds/1.42.1/aws-cdk.aws-rds-1.42.1.tar.gz

---
+++ packages/aws-cdk.aws-rds/aws-cdk.aws-rds-1.42.1.tar.gz/aws-cdk.aws-rds-1.42.1/src/aws_cdk/aws_rds/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_ec2
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_lambda
+import aws_cdk.aws_logs
+import aws_cdk.aws_s3
+import aws_cdk.aws_secretsmanager
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-rds", "1.42.1", __name__[0:-6], "aws-rds@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

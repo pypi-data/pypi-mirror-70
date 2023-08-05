f379ea0435e6a996fb52c330c80644d49201d41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package aws-cdk.aws-codepipeline-actions/1.42.1/aws_cdk.aws_codepipeline_actions-1.42.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-codepipeline-actions/aws_cdk.aws_codepipeline_actions-1.42.1-py3-none-any.whl/aws_cdk/aws_codepipeline_actions/_jsii/__init__.py
@@ -0,0 +1,35 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudformation
+import aws_cdk.aws_codebuild
+import aws_cdk.aws_codecommit
+import aws_cdk.aws_codedeploy
+import aws_cdk.aws_codepipeline
+import aws_cdk.aws_ec2
+import aws_cdk.aws_ecr
+import aws_cdk.aws_ecs
+import aws_cdk.aws_events
+import aws_cdk.aws_events_targets
+import aws_cdk.aws_iam
+import aws_cdk.aws_lambda
+import aws_cdk.aws_s3
+import aws_cdk.aws_sns
+import aws_cdk.aws_sns_subscriptions
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-codepipeline-actions", "1.42.1", __name__[0:-6], "aws-codepipeline-actions@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

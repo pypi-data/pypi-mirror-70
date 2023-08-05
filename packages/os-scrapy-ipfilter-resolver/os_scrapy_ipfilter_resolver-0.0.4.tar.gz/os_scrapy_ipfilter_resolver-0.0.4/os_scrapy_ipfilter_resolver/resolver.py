be09655a2c13d1302380d41c70a65e77f0247523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:00 +0000

    Add package os-scrapy-ipfilter-resolver/0.0.4/os_scrapy_ipfilter_resolver-0.0.4.tar.gz

---
+++ packages/os-scrapy-ipfilter-resolver/os_scrapy_ipfilter_resolver-0.0.4.tar.gz/os_scrapy_ipfilter_resolver-0.0.4/os_scrapy_ipfilter_resolver/resolver.py
@@ -0,0 +1,79 @@
+import logging
+
+from expiringdict import ExpiringDict
+from IPy import IP, IPSet
+from scrapy.resolver import dnscache
+from twisted.internet import defer
+from twisted.internet.base import ThreadedResolver
+from twisted.python import failure
+
+
+class IPFilteredException(Exception):
+    pass
+
+
+class Resolver(ThreadedResolver):
+    def __init__(
+        self, reactor, cache_size, cache_expire, timeout, disallowed=None, allowed=None
+    ):
+        super(Resolver, self).__init__(reactor)
+        self.disallowed = IPSet(
+            [IP(ip) for ip in (disallowed if disallowed is not None else [])]
+        )
+        self.allowed = IPSet(
+            [IP(ip) for ip in (allowed if allowed is not None else [])]
+        )
+
+        self.cache_allowed = self.cache_disallowed = None
+        if cache_size > 0:
+            self.cache_allowed = ExpiringDict(
+                max_len=cache_size, max_age_seconds=cache_expire
+            )
+            self.cache_disallowed = ExpiringDict(
+                max_len=cache_size, max_age_seconds=cache_expire
+            )
+
+        self.timeout = timeout if isinstance(timeout, tuple) else (timeout,)
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def getHostByName(self, name, timeout=()):
+        if self.cache_allowed is not None:
+            if name in self.cache_allowed:
+                return defer.succeed(self.cache_allowed[name])
+            elif name in self.cache_disallowed:
+                result = self.cache_disallowed[name]
+                return defer.fail(IPFilteredException(f"ip filtered {name} {result}"))
+        if not timeout:
+            timeout = self.timeout
+        d = super(Resolver, self).getHostByName(name, timeout)
+        d.addCallback(self.callback, name)
+        return d
+
+    def callback(self, result, name):
+        dnscache[name] = result
+        ip = IP(result)
+        if ip not in self.allowed and ip in self.disallowed:
+            if self.cache_disallowed is not None and name not in self.cache_disallowed:
+                self.logger.warning(f"cache disallowed {name} {result}")
+                self.cache_disallowed[name] = result
+            return failure.Failure(IPFilteredException(f"ip filtered {name} {result}"))
+        if self.cache_allowed is not None and name not in self.cache_allowed:
+            self.logger.debug(f"cache allowed {name} {result}")
+            self.cache_allowed[name] = result
+        return result
+
+    def install_on_reactor(self):
+        self.reactor.installResolver(self)
+
+    @classmethod
+    def from_crawler(cls, crawler, reactor):
+        settings = crawler.settings
+        cache_size = settings.getint("DNSCACHE_SIZE", 10000)
+        cache_expire = settings.getint("DNSCACHE_EXPIRE", 24 * 60 * 60)
+        timeout = settings.getfloat("DNS_TIMEOUT")
+        if not settings.getbool("DNSCACHE_ENABLED"):
+            cache_size = 0
+        disallowed = settings.getlist("IP_DISALLOWED", [])
+        allowed = settings.getlist("IP_ALLOWED", [])
+        dnscache.limit = cache_size
+        return cls(reactor, cache_size, cache_expire, timeout, disallowed, allowed)

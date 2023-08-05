8ac67045a3b5bbc9d55a24b4598b58739193db74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:05 +0000

    Add package jc-distributions/0.1/jc_distributions-0.1.tar.gz

---
+++ packages/jc-distributions/jc_distributions-0.1.tar.gz/jc_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jc_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jc_distributions'],
+      zip_safe=False)

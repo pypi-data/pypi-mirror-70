f71f54b313f688e4e2b6a8a36f57679e2e322952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package gor/0.1.3/gor-0.1.3.tar.gz

---
+++ packages/gor/gor-0.1.3.tar.gz/gor-0.1.3/gor/middleware.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+
+import sys
+import errno
+import logging
+
+from .base import Gor
+
+from tornado import gen, ioloop, queues
+
+
+import contextlib
+from tornado.stack_context import StackContext
+
+@contextlib.contextmanager
+def die_on_error():
+    try:
+        yield
+    except Exception:
+        logging.error("exception in asynchronous operation", exc_info=True)
+        sys.exit(1)
+
+
+class TornadoGor(Gor):
+
+    def __init__(self, *args, **kwargs):
+        super(TornadoGor, self).__init__(*args, **kwargs)
+        self.q = queues.Queue()
+        self.concurrency = kwargs.get('concurrency', 2)
+
+    @gen.coroutine
+    def _process(self):
+        line = yield self.q.get()
+        try:
+            msg = self.parse_message(line)
+            if msg:
+                self.emit(msg)
+        finally:
+            self.q.task_done()
+
+    @gen.coroutine
+    def _worker(self):
+        while True:
+            yield self._process()
+
+    @gen.coroutine
+    def _run(self):
+        for _ in range(self.concurrency):
+            self._worker()
+
+        while True:
+            try:
+                line = sys.stdin.readline()
+            except KeyboardInterrupt:
+                ioloop.IOLoop.instance().stop()
+                break
+            self.q.put(line)
+            yield
+
+    def run(self):
+        with StackContext(die_on_error):
+            self.io_loop = ioloop.IOLoop.current()
+            self.io_loop.run_sync(self._run)
+            sys.exit(errno.EINTR)

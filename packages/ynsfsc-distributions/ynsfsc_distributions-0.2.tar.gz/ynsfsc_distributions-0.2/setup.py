7214bc7d7e7131d918ac2eeee1f2484cd18ab36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package ynsfsc-distributions/0.2/ynsfsc_distributions-0.2.tar.gz

---
+++ packages/ynsfsc-distributions/ynsfsc_distributions-0.2.tar.gz/ynsfsc_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ynsfsc_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['ynsfsc_distributions'],
+      zip_safe=False)

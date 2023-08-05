06cfec65359a86e9303916fdc91717e37d929784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:06 +0000

    Add package distributions-test-iln/0.1/distributions_test_iln-0.1.tar.gz

---
+++ packages/distributions-test-iln/distributions_test_iln-0.1.tar.gz/distributions_test_iln-0.1/README.md
@@ -0,0 +1,9 @@
+# dsnd-distributions package
+
+Summary of package
+
+# Files
+
+Explanation of files in package
+
+# Installation
---
+++ packages/distributions-test-iln/distributions_test_iln-0.1.tar.gz/distributions_test_iln-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_test_iln',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Ali Rahman',
+      packages=['distributions'],
+      author_email='alirman02@gmail.com',
+      zip_safe=False)

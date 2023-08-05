d4496460738f82d7337172c4061fbdaec3b19fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package axial-attention/0.1.4/axial_attention-0.1.4.tar.gz

---
+++ packages/axial-attention/axial_attention-0.1.4.tar.gz/axial_attention-0.1.4/axial_attention/axial_attention.py
@@ -0,0 +1,148 @@
+import torch
+from torch import nn
+from operator import itemgetter
+from axial_attention.reversible import ReversibleSequence
+
+def map_el_ind(arr, ind):
+    return list(map(itemgetter(ind), arr))
+
+def sort_and_return_indices(arr):
+    indices = [ind for ind in range(len(arr))]
+    arr = zip(arr, indices)
+    arr = sorted(arr)
+    return map_el_ind(arr, 0), map_el_ind(arr, 1)
+
+# calculates the permutation to bring the input tensor to something attend-able
+# also calculates the inverse permutation to bring the tensor back to its original shape
+
+def calculate_permutations(num_dimensions, emb_dim):
+    total_dimensions = num_dimensions + 2
+    emb_dim = emb_dim if emb_dim > 0 else (emb_dim + total_dimensions)
+    axial_dims = [ind for ind in range(1, total_dimensions) if ind != emb_dim]
+
+    permutations = []
+
+    for axial_dim in axial_dims:
+        last_two_dims = [axial_dim, emb_dim]
+        dims_rest = set(range(0, total_dimensions)) - set(last_two_dims)
+        permutation = [*dims_rest, *last_two_dims]
+        permutations.append(permutation)
+      
+    return permutations
+
+# helper classes
+
+class Rezero(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+        self.g = nn.Parameter(torch.tensor(0.))
+    def forward(self, x):
+        return self.fn(x) * self.g
+
+class Sequential(nn.Module):
+    def __init__(self, blocks):
+        super().__init__()
+        self.blocks = blocks
+    def forward(self, x):
+        for f, g in self.blocks:
+            x = x + f(x) + g(x)
+        return x
+
+class PermuteToFrom(nn.Module):
+    def __init__(self, permutation, fn):
+        super().__init__()
+        self.fn = fn
+        _, inv_permutation = sort_and_return_indices(permutation)
+        self.permutation = permutation
+        self.inv_permutation = inv_permutation
+
+    def forward(self, x, **kwargs):
+        axial = x.permute(*self.permutation)
+
+        shape = axial.shape
+        *_, t, d = shape
+
+        # merge all but axial dimension
+        axial = axial.reshape(-1, t, d)
+
+        # attention
+        axial = self.fn(axial, **kwargs)
+
+        # restore to original shape and permutation
+        axial = axial.reshape(*shape)
+        axial = axial.permute(*self.inv_permutation)
+        return axial
+
+# classic multi-head attention
+
+class SelfAttention(nn.Module):
+    def __init__(self, dim, heads, dim_heads = None):
+        super().__init__()
+        self.dim_heads = (dim // heads) if dim_heads is None else dim_heads
+        dim_hidden = self.dim_heads * heads
+
+        self.heads = heads
+        self.to_qkv = nn.Linear(dim, 3 * dim_hidden, bias = False)
+        self.to_out = nn.Linear(dim_hidden, dim, bias = False)
+
+    def forward(self, x):
+        b, t, d, h = *x.shape, self.heads
+        q, k, v = self.to_qkv(x).chunk(3, dim=-1)
+        merge_heads = lambda x: x.reshape(b, t, h, -1).transpose(1, 2).reshape(b * h, t, -1)
+        q, k, v = map(merge_heads, (q, k, v))
+        dots = torch.einsum('bie,bje->bij', q, k) * (d ** -0.5)
+        dots = dots.softmax(dim=-1)
+        out = torch.einsum('bij,bje->bie', dots, v)
+        out = out.reshape(b, h, t, -1).transpose(1, 2).reshape(b, t, -1)
+        out = self.to_out(out)
+        return out
+
+# axial attention class
+
+class AxialAttention(nn.Module):
+    def __init__(self, dim, num_dimensions = 2, heads = 8, dim_heads = None, dim_index = -1):
+        assert (dim % heads) == 0, 'hidden dimension must be divisible by number of heads'
+        super().__init__()
+        self.dim = dim
+        self.total_dimensions = num_dimensions + 2
+        self.dim_index = dim_index if dim_index > 0 else (dim_index + self.total_dimensions)
+
+        attentions = []
+        for permutation in calculate_permutations(num_dimensions, dim_index):
+            attentions.append(PermuteToFrom(permutation, SelfAttention(dim, heads, dim_heads)))
+
+        self.axial_attentions = nn.ModuleList(attentions)
+
+    def forward(self, x):
+        assert len(x.shape) == self.total_dimensions, 'input tensor does not have the correct number of dimensions'
+        assert x.shape[self.dim_index] == self.dim, 'input tensor does not have the correct input dimension'
+
+        out = [axial_attn(x) for axial_attn in self.axial_attentions]
+        return sum(out)
+
+# image transformer - with reversibility
+
+class ImageTransformer(nn.Module):
+    def __init__(self, dim, depth, heads = 8, dim_heads = None, dim_index = 1, reversible = True):
+        super().__init__()
+        permutations = calculate_permutations(2, dim_index)
+
+        get_ff = lambda: nn.Sequential(
+            nn.Conv2d(dim, dim * 4, 3, padding=1),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(dim * 4, dim, 3, padding=1),
+        )
+
+        layers = nn.ModuleList([])
+        for _ in range(depth):
+            attn_functions = nn.ModuleList([PermuteToFrom(permutation, Rezero(SelfAttention(dim, heads, dim_heads))) for permutation in permutations])
+            conv_functions = nn.ModuleList([get_ff(), get_ff()])
+            layers.append(attn_functions)
+            layers.append(conv_functions)            
+
+        execute_type = ReversibleSequence if reversible else Sequential
+        self.layers = execute_type(layers)
+
+    def forward(self, x):
+        return self.layers(x)

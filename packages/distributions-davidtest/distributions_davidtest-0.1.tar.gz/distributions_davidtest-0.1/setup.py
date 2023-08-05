d36155431f9c1d47f2e8b78326a98c5d73b29064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package distributions-davidtest/0.1/distributions_davidtest-0.1.tar.gz

---
+++ packages/distributions-davidtest/distributions_davidtest-0.1.tar.gz/distributions_davidtest-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_davidtest',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_davidtest'],
+      zip_safe=False)

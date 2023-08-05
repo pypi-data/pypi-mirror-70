d41750614eb3bb91a55c7c569c1c0ff0278fceb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:54 +0000

    Add package pysyntect/0.2.0/pysyntect-0.2.0-cp36-none-win_amd64.whl

---
+++ packages/pysyntect/pysyntect-0.2.0-cp36-none-win_amd64.whl/syntect/__init__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# ----------------------------------------------------------------------------
+
+
+"""Python wrapper around syntect library."""
+
+# Standard library imports
+import os
+import os.path as osp
+
+# Local imports
+from .syntect import (highlight, load_syntax_folder, load_theme_folder,
+                      escape_to_console, escape_to_latex,
+                      Style, Color, FontStyleConst,
+                      Syntax, Theme, SyntaxSet, ThemeSet, __version__)
+
+HERE = osp.dirname(osp.abspath(__file__))
+GRAMMARS = osp.join(HERE, 'grammars')
+
+
+class FontStyle:
+    """Font style modifiers"""
+    BOLD = FontStyleConst.bold()
+    ITALIC = FontStyleConst.italic()
+    UNDERLINE = FontStyleConst.underline()
+
+
+def load_default_syntax():
+    """Load the default syntax definitions included with pysyntect."""
+    return load_syntax_folder(GRAMMARS)
+
+
+# Package functions
+highlight
+load_syntax_folder
+load_theme_folder
+escape_to_console
+escape_to_latex
+
+# Package classes
+Style
+Color
+FontStyle
+Syntax
+Theme
+SyntaxSet
+ThemeSet
+
+# Package version
+__version__

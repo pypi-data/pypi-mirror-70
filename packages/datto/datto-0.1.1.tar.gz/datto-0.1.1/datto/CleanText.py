085ec987407373696c995281aa44afadc310b2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package datto/0.1.1/datto-0.1.1.tar.gz

---
+++ packages/datto/datto-0.1.1.tar.gz/datto-0.1.1/datto/CleanText.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import re
+
+
+class CleanText:
+    def __init__(self):
+        self.all_names = pd.read_pickle("datto/data/all_names")
+
+    def remove_names(self, text):
+        cleaned_text = text
+        for i, row in self.all_names.iterrows():
+            cleaned_text = re.sub(
+                row["name"] + "[^\w\s]*[\s]+", "<NAME> ", cleaned_text
+            )
+        return cleaned_text
+
+    def remove_links(self, text):
+        cleaned_text = text
+        links_found = re.findall(
+            "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})",
+            cleaned_text,
+        )
+
+        for link in links_found:
+            cleaned_text = cleaned_text.replace(link, "<LINK>")
+
+        return cleaned_text

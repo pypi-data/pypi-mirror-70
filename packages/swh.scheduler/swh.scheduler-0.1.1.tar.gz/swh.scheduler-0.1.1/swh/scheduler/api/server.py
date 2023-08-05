4941ae1175b33687f62e891bce9b44e9beb46bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:25 +0000

    Add package swh.scheduler/0.1.1/swh.scheduler-0.1.1.tar.gz

---
+++ packages/swh.scheduler/swh.scheduler-0.1.1.tar.gz/swh.scheduler-0.1.1/swh/scheduler/api/server.py
@@ -0,0 +1,266 @@
+# Copyright (C) 2018-2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import os
+import logging
+
+from flask import request, Flask
+
+from swh.core import config
+from swh.core.api import (
+    decode_request,
+    error_handler,
+    encode_data_server as encode_data,
+)
+
+from swh.core.api import negotiate, JSONFormatter, MsgpackFormatter
+from swh.scheduler import get_scheduler as get_scheduler_from
+
+
+app = Flask(__name__)
+scheduler = None
+
+
+@app.errorhandler(Exception)
+def my_error_handler(exception):
+    return error_handler(exception, encode_data)
+
+
+def get_sched():
+    global scheduler
+    if not scheduler:
+        scheduler = get_scheduler_from(**app.config["scheduler"])
+    return scheduler
+
+
+def has_no_empty_params(rule):
+    return len(rule.defaults or ()) >= len(rule.arguments or ())
+
+
+@app.route("/")
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def index():
+    return "SWH Scheduler API server"
+
+
+@app.route("/close_connection", methods=["GET", "POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def close_connection():
+    return get_sched().close_connection()
+
+
+@app.route("/set_status_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def set_status_tasks():
+    return get_sched().set_status_tasks(**decode_request(request))
+
+
+@app.route("/create_task_type", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def create_task_type():
+    return get_sched().create_task_type(**decode_request(request))
+
+
+@app.route("/get_task_type", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def get_task_type():
+    return get_sched().get_task_type(**decode_request(request))
+
+
+@app.route("/get_task_types", methods=["GET", "POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def get_task_types():
+    return get_sched().get_task_types(**decode_request(request))
+
+
+@app.route("/create_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def create_tasks():
+    return get_sched().create_tasks(**decode_request(request))
+
+
+@app.route("/disable_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def disable_tasks():
+    return get_sched().disable_tasks(**decode_request(request))
+
+
+@app.route("/get_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def get_tasks():
+    return get_sched().get_tasks(**decode_request(request))
+
+
+@app.route("/get_task_runs", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def get_task_runs():
+    return get_sched().get_task_runs(**decode_request(request))
+
+
+@app.route("/search_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def search_tasks():
+    return get_sched().search_tasks(**decode_request(request))
+
+
+@app.route("/peek_ready_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def peek_ready_tasks():
+    return get_sched().peek_ready_tasks(**decode_request(request))
+
+
+@app.route("/grab_ready_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def grab_ready_tasks():
+    return get_sched().grab_ready_tasks(**decode_request(request))
+
+
+@app.route("/schedule_task_run", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def schedule_task_run():
+    return get_sched().schedule_task_run(**decode_request(request))
+
+
+@app.route("/mass_schedule_task_runs", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def mass_schedule_task_runs():
+    return get_sched().mass_schedule_task_runs(**decode_request(request))
+
+
+@app.route("/start_task_run", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def start_task_run():
+    return get_sched().start_task_run(**decode_request(request))
+
+
+@app.route("/end_task_run", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def end_task_run():
+    return get_sched().end_task_run(**decode_request(request))
+
+
+@app.route("/filter_task_to_archive", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def filter_task_to_archive():
+    return get_sched().filter_task_to_archive(**decode_request(request))
+
+
+@app.route("/delete_archived_tasks", methods=["POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def delete_archived_tasks():
+    return get_sched().delete_archived_tasks(**decode_request(request))
+
+
+@app.route("/get_priority_ratios", methods=["GET", "POST"])
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def get_priority_ratios():
+    return get_sched().get_priority_ratios(**decode_request(request))
+
+
+@app.route("/site-map")
+@negotiate(MsgpackFormatter)
+@negotiate(JSONFormatter)
+def site_map():
+    links = []
+    sched = get_sched()
+    for rule in app.url_map.iter_rules():
+        if has_no_empty_params(rule) and hasattr(sched, rule.endpoint):
+            links.append(
+                dict(rule=rule.rule, description=getattr(sched, rule.endpoint).__doc__)
+            )
+    # links is now a list of url, endpoint tuples
+    return links
+
+
+def load_and_check_config(config_file, type="local"):
+    """Check the minimal configuration is set to run the api or raise an
+       error explanation.
+
+    Args:
+        config_file (str): Path to the configuration file to load
+        type (str): configuration type. For 'local' type, more
+                    checks are done.
+
+    Raises:
+        Error if the setup is not as expected
+
+    Returns:
+        configuration as a dict
+
+    """
+    if not config_file:
+        raise EnvironmentError("Configuration file must be defined")
+
+    if not os.path.exists(config_file):
+        raise FileNotFoundError("Configuration file %s does not exist" % (config_file,))
+
+    cfg = config.read(config_file)
+
+    vcfg = cfg.get("scheduler")
+    if not vcfg:
+        raise KeyError("Missing '%scheduler' configuration")
+
+    if type == "local":
+        cls = vcfg.get("cls")
+        if cls != "local":
+            raise ValueError(
+                "The scheduler backend can only be started with a 'local' "
+                "configuration"
+            )
+
+        args = vcfg.get("args")
+        if not args:
+            raise KeyError("Invalid configuration; missing 'args' config entry")
+
+        db = args.get("db")
+        if not db:
+            raise KeyError("Invalid configuration; missing 'db' config entry")
+
+    return cfg
+
+
+api_cfg = None
+
+
+def make_app_from_configfile():
+    """Run the WSGI app from the webserver, loading the configuration from
+       a configuration file.
+
+       SWH_CONFIG_FILENAME environment variable defines the
+       configuration path to load.
+
+    """
+    global api_cfg
+    if not api_cfg:
+        config_file = os.environ.get("SWH_CONFIG_FILENAME")
+        api_cfg = load_and_check_config(config_file)
+        app.config.update(api_cfg)
+    handler = logging.StreamHandler()
+    app.logger.addHandler(handler)
+    return app
+
+
+if __name__ == "__main__":
+    print('Please use the "swh-scheduler api-server" command')
---
+++ packages/swh.scheduler/swh.scheduler-0.1.1.tar.gz/swh.scheduler-0.1.1/swh/scheduler/sql/30-swh-schema.sql
@@ -0,0 +1,112 @@
+create table dbversion
+(
+  version     int primary key,
+  release     timestamptz not null,
+  description text not null
+);
+
+comment on table dbversion is 'Schema update tracking';
+comment on column dbversion.version is 'SQL schema version';
+comment on column dbversion.release is 'Version deployment timestamp';
+comment on column dbversion.description is 'Version description';
+
+insert into dbversion (version, release, description)
+       values (15, now(), 'Work In Progress');
+
+create table task_type (
+  type text primary key,
+  description text not null,
+  backend_name text not null,
+  default_interval interval,
+  min_interval interval,
+  max_interval interval,
+  backoff_factor float,
+  max_queue_length bigint,
+  num_retries bigint,
+  retry_delay interval
+);
+
+comment on table task_type is 'Types of schedulable tasks';
+comment on column task_type.type is 'Short identifier for the task type';
+comment on column task_type.description is 'Human-readable task description';
+comment on column task_type.backend_name is 'Name of the task in the job-running backend';
+comment on column task_type.default_interval is 'Default interval for newly scheduled tasks';
+comment on column task_type.min_interval is 'Minimum interval between two runs of a task';
+comment on column task_type.max_interval is 'Maximum interval between two runs of a task';
+comment on column task_type.backoff_factor is 'Adjustment factor for the backoff between two task runs';
+comment on column task_type.max_queue_length is 'Maximum length of the queue for this type of tasks';
+comment on column task_type.num_retries is 'Default number of retries on transient failures';
+comment on column task_type.retry_delay is 'Retry delay for the task';
+
+create type task_status as enum ('next_run_not_scheduled', 'next_run_scheduled', 'completed', 'disabled');
+comment on type task_status is 'Status of a given task';
+
+create type task_policy as enum ('recurring', 'oneshot');
+comment on type task_policy is 'Recurrence policy of the given task';
+
+create type task_priority as enum('high', 'normal', 'low');
+comment on type task_priority is 'Priority of the given task';
+
+create table priority_ratio(
+  id task_priority primary key,
+  ratio float not null
+);
+
+comment on table priority_ratio is 'Oneshot task''s reading ratio per priority';
+comment on column priority_ratio.id is 'Task priority id';
+comment on column priority_ratio.ratio is 'Percentage of tasks to read per priority';
+
+insert into priority_ratio (id, ratio) values ('high', 0.5);
+insert into priority_ratio (id, ratio) values ('normal', 0.3);
+insert into priority_ratio (id, ratio) values ('low', 0.2);
+
+create table task (
+  id bigserial primary key,
+  type text not null references task_type(type),
+  arguments jsonb not null,
+  next_run timestamptz not null,
+  current_interval interval,
+  status task_status not null,
+  policy task_policy not null default 'recurring',
+  retries_left bigint not null default 0,
+  priority task_priority references priority_ratio(id),
+  check (policy <> 'recurring' or current_interval is not null)
+);
+
+comment on table task is 'Schedule of recurring tasks';
+comment on column task.arguments is 'Arguments passed to the underlying job scheduler. '
+                                    'Contains two keys, ''args'' (list) and ''kwargs'' (object).';
+comment on column task.next_run is 'The next run of this task should be run on or after that time';
+comment on column task.current_interval is 'The interval between two runs of this task, '
+                                           'taking into account the backoff factor';
+comment on column task.policy is 'Whether the task is one-shot or recurring';
+comment on column task.retries_left is 'The number of "short delay" retries of the task in case of '
+                                       'transient failure';
+comment on column task.priority is 'Policy of the given task';
+comment on column task.id is 'Task Identifier';
+comment on column task.type is 'References task_type table';
+comment on column task.status is 'Task status (''next_run_not_scheduled'', ''next_run_scheduled'', ''completed'', ''disabled'')';
+
+create type task_run_status as enum ('scheduled', 'started', 'eventful', 'uneventful', 'failed', 'permfailed', 'lost');
+comment on type task_run_status is 'Status of a given task run';
+
+create table task_run (
+  id bigserial primary key,
+  task bigint not null references task(id),
+  backend_id text,
+  scheduled timestamptz,
+  started timestamptz,
+  ended timestamptz,
+  metadata jsonb,
+  status task_run_status not null default 'scheduled'
+);
+comment on table task_run is 'History of task runs sent to the job-running backend';
+comment on column task_run.backend_id is 'id of the task run in the job-running backend';
+comment on column task_run.metadata is 'Useful metadata for the given task run. '
+                                       'For instance, the worker that took on the job, '
+                                       'or the logs for the run.';
+comment on column task_run.id is 'Task run identifier';
+comment on column task_run.task is 'References task table';
+comment on column task_run.scheduled is 'Scheduled run time for task';
+comment on column task_run.started is 'Task starting time';
+comment on column task_run.ended is 'Task ending time';

70e0eb17aed15506e3784d4c7494680070d7a482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package SK7-distributions/0.1/SK7-distributions-0.1.tar.gz

---
+++ packages/SK7-distributions/SK7-distributions-0.1.tar.gz/SK7-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='SK7-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['SK7-distributions'],
+      zip_safe=False)

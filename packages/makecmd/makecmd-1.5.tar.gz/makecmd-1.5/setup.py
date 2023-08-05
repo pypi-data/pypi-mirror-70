87df6d7709918ecea1fe3a7d8c369db824d52fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:19 +0000

    Add package makecmd/1.5/makecmd-1.5.tar.gz

---
+++ packages/makecmd/makecmd-1.5.tar.gz/makecmd-1.5/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="makecmd",
+    version=1.5,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['argparse'],
+    entry_points={'console_scripts': ['makecmd = makecmd.makecmd:main']}
+)

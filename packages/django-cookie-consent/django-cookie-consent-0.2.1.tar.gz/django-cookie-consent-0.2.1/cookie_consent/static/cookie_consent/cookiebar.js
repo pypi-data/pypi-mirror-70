335580064e0fdd291b44fc0388aa0b9353b9219d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package django-cookie-consent/0.2.1/django-cookie-consent-0.2.1.tar.gz

---
+++ packages/django-cookie-consent/django-cookie-consent-0.2.1.tar.gz/django-cookie-consent-0.2.1/cookie_consent/static/cookie_consent/cookiebar.js
@@ -0,0 +1,58 @@
+(function ($) {
+
+  $.fn.evalXCookieConsent = function() {
+    return this.each(function() {
+      var src = $(this).attr('src');
+      if (src) {
+        var script = document.createElement('script');
+        script.src = src;
+        document.getElementsByTagName("head")[0].appendChild(script);
+      } else {
+        $.globalEval(this.innerHTML);
+      }
+      $(this).remove();
+    });
+  };
+
+  $.showCookieBar = function (options) {
+    var defaults = {
+      content: '',
+      cookie_groups: [],
+
+      cookie_decline: null, // set cookie_consent decline on client immediately
+      beforeDeclined: null
+    };
+
+    var opts = $.extend(defaults, options);
+    var $content = $(opts.content);
+
+    $('body').append($content).addClass('with-cookie-bar');
+
+    $('.cc-cookie-accept', $content).click(function() {
+      $.post($(this).attr('href'));
+      // document.cookie = cookie_accept;
+      $content.hide();
+      $('body').removeClass('with-cookie-bar');
+      $("script[type='x/cookie_consent']").each(function() {
+        if (cookie_groups.indexOf($(this).attr('data-varname')) != -1) {
+          $(this).evalXCookieConsent();
+        }
+      });
+      return false;
+    });
+
+    $('.cc-cookie-decline', $content).click(function() {
+      if ($.isFunction(opts.declined)) {
+          opts.declined();
+        }
+      $.post($(this).attr('href'));
+      if (opts.cookie_decline) {
+        document.cookie = opts.cookie_decline;
+      }
+      $content.hide();
+      $('body').removeClass('with-cookie-bar');
+      return false;
+    });
+
+ };
+})(jQuery);

46ca4793080691cfcdd399d895034bc8afcef453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:29 +0000

    Add package hypothesis-auto-draw/0.1.4/hypothesis_auto_draw-0.1.4-py3-none-any.whl

---
+++ packages/hypothesis-auto-draw/hypothesis_auto_draw-0.1.4-py3-none-any.whl/hypothesis_auto_draw/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from hypothesis_auto_draw.hypothesis_auto_draw import auto_draw_flatmap
+from hypothesis_auto_draw.hypothesis_auto_draw import auto_draw_map
+
+
+__all__ = ["auto_draw_flatmap", "auto_draw_map"]
+__version__ = "0.1.4"

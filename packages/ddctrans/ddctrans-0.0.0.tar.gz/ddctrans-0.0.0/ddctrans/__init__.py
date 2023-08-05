842658fea29e1adbe724321b27bdd9f056e824b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package ddctrans/0.0.0/ddctrans-0.0.0.tar.gz

---
+++ packages/ddctrans/ddctrans-0.0.0.tar.gz/ddctrans-0.0.0/ddctrans/__init__.py
@@ -0,0 +1,5 @@
+NAME = "ddctrans"
+__version__ = "0.0.0"
+__metaclass__ = type
+
+
---
+++ packages/ddctrans/ddctrans-0.0.0.tar.gz/ddctrans-0.0.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import ddctrans
+with open("describe.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+NAME = ddctrans.NAME
+VERSION = ddctrans.__version__
+
+
+PACKAGES = setuptools.find_packages()
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    description='  ',
+    long_description=long_description,
+    author='fastbiubiu',
+    author_email='fastbiubiu@163.com',
+    url='https://github.com/NocoldBob/robot',
+    # package_dir=PACKAGE_DIR,
+    include_package_data=True,
+    install_requires=["bottle",  "flask"],
+    packages=PACKAGES,
+    platforms='Linux',
+    classifiers=[
+        "Topic :: System :: Operating System Kernels :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+
+    ],
+    python_requires='>=3',
+)

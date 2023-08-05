46ad1505f0ea7966669e9835dce98103e453c7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package aws-cdk.aws-docdb/1.42.1/aws-cdk.aws-docdb-1.42.1.tar.gz

---
+++ packages/aws-cdk.aws-docdb/aws-cdk.aws-docdb-1.42.1.tar.gz/aws-cdk.aws-docdb-1.42.1/src/aws_cdk/aws_docdb/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_ec2
+import aws_cdk.aws_efs
+import aws_cdk.aws_kms
+import aws_cdk.aws_secretsmanager
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-docdb", "1.42.1", __name__[0:-6], "aws-docdb@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

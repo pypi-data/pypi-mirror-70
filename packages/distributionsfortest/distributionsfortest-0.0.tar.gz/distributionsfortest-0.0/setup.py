ed0f7b7fdce975e420da32135f46aed1e670b513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package distributionsfortest/0.0/distributionsfortest-0.0.tar.gz

---
+++ packages/distributionsfortest/distributionsfortest-0.0.tar.gz/distributionsfortest-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsfortest',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      author = 'Kalky',
+      packages=['distributionsfortest'],
+      zip_safe=False)

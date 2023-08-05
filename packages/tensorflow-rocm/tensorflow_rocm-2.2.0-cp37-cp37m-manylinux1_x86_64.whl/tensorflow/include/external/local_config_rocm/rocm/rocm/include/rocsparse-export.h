8555bacb0583645ea47883a7d0763b212691683d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:58 +0000

    Add package tensorflow-rocm/2.2.0/tensorflow_rocm-2.2.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.2.0-cp37-cp37m-manylinux1_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/rocsparse-export.h
@@ -0,0 +1,41 @@
+
+#ifndef ROCSPARSE_EXPORT_H
+#define ROCSPARSE_EXPORT_H
+
+#ifdef ROCSPARSE_STATIC_DEFINE
+#  define ROCSPARSE_EXPORT
+#  define ROCSPARSE_NO_EXPORT
+#else
+#  ifndef ROCSPARSE_EXPORT
+#    ifdef rocsparse_EXPORTS
+        /* We are building this library */
+#      define ROCSPARSE_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define ROCSPARSE_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef ROCSPARSE_NO_EXPORT
+#    define ROCSPARSE_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef ROCSPARSE_DEPRECATED
+#  define ROCSPARSE_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef ROCSPARSE_DEPRECATED_EXPORT
+#  define ROCSPARSE_DEPRECATED_EXPORT ROCSPARSE_EXPORT ROCSPARSE_DEPRECATED
+#endif
+
+#ifndef ROCSPARSE_DEPRECATED_NO_EXPORT
+#  define ROCSPARSE_DEPRECATED_NO_EXPORT ROCSPARSE_NO_EXPORT ROCSPARSE_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define ROCSPARSE_NO_DEPRECATED
+#endif
+
+#endif

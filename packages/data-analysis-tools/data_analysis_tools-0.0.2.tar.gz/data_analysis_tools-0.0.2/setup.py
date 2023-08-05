cdb5647147e6bb70e22c2b2d34e53bfc1ef8b799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package data-analysis-tools/0.0.2/data_analysis_tools-0.0.2.tar.gz

---
+++ packages/data-analysis-tools/data_analysis_tools-0.0.2.tar.gz/data_analysis_tools-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data_analysis_tools",
+    packages=setuptools.find_packages(),
+    version="0.0.2",
+    license='GPLv3',
+    author="Alrik Durand",
+    author_email="alrik.durand@gmail.com",
+    description="Tools to read and analyse data files and Qudi data files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alrik-durand/data_analysis_tools",
+    python_requires='>=3.6',
+    install_requires=["numpy", "pandas", "matplotlib", "scipy", "lmfit"]
+)

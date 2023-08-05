e50780cd4ee04e1d5d9dc3ec7ca4526aaecf4e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:55 +0000

    Add package pangeamt-nlp/0.4.24/pangeamt-nlp-0.4.24.tar.gz

---
+++ packages/pangeamt-nlp/pangeamt-nlp-0.4.24.tar.gz/pangeamt-nlp-0.4.24/pangeamt_nlp/processor/normalizer/convert_empty_lines_postprocess.py
@@ -0,0 +1,29 @@
+from pangeamt_nlp.processor.base.normalizer_base import NormalizerBase
+from pangeamt_nlp.seg import Seg
+
+
+class ConvertEmptyLines(NormalizerBase):
+    NAME = "convert_empty_lines_postprocess"
+
+    DESCRIPTION_TRAINING = """"""
+
+    DESCRIPTION_DECODING = """
+        Convert an empty line from src to an empty line in decoding
+    """
+
+    def __init__(self, src_lang: str, tgt_lang: str) -> None:
+        super().__init__(src_lang, tgt_lang)
+
+    def process_train(self, seg: Seg) -> None:
+        pass
+
+    def process_src_decoding(self, seg: Seg) -> None:
+        pass
+
+    def process_tgt_decoding(self, seg: Seg) -> None:
+        if seg.src_raw == "\n" or seg.src_raw.isspace() or seg.src_raw == "":
+            seg.tgt = ""
+        elif (len(seg.src_raw) > 1):
+            if (seg.src_raw[:-1].isspace() and seg.src_raw[-1]=='\n') \
+                    or ((seg.src_raw[:-1].replace('\t','') == '' or seg.src_raw[:-1].replace('\t','').isspace()) and seg.src_raw[-1] == '\n'):
+                seg.tgt = ""

78daa5334f321d90bb8f0cf32fefb4df4eed26a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package deepsensemaking/0.1.2020.6.1.1/deepsensemaking-0.1.2020.6.1.1-py3-none-any.whl

---
+++ packages/deepsensemaking/deepsensemaking-0.1.2020.6.1.1-py3-none-any.whl/deepsensemaking/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+"""
+deepsensemaking (dsm)
+
+
+"""
+from deepsensemaking._version import __version__
+
+from deepsensemaking import info
+from deepsensemaking import base
+from deepsensemaking import bids
+from deepsensemaking import jiko
+from deepsensemaking import dicts
+from deepsensemaking import strings
+
+
+from deepsensemaking.base import set_cwd, inside_emacs
+
+
+if __name__ == "__main__":
+    pass

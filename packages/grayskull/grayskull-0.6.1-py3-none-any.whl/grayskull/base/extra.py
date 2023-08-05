1e614c7787124bc0275d69008cf606f326226509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:17 +0000

    Add package grayskull/0.6.1/grayskull-0.6.1-py3-none-any.whl

---
+++ packages/grayskull/grayskull-0.6.1-py3-none-any.whl/grayskull/base/extra.py
@@ -0,0 +1,36 @@
+import logging
+import subprocess
+
+import requests
+from colorama import Fore
+
+from grayskull.cli.stdout import print_msg
+
+log = logging.getLogger(__name__)
+
+
+def get_git_current_user_metadata() -> dict:
+    git_out = subprocess.check_output(["git", "config", "user.name"])
+    return requests.get(
+        url="https://api.github.com/search/users",
+        params={"q": git_out.strip()},
+        timeout=5,
+    ).json()
+
+
+def get_git_current_user() -> str:
+    try:
+        github_search = get_git_current_user_metadata()
+        if github_search["total_count"] == 1:
+            login_github = github_search["items"][0]["login"]
+            return login_github
+    except Exception as err:
+        log.debug(
+            f"Exception occurred when trying to recover user information from github."
+            f" Exception: {err}"
+        )
+    print_msg(
+        f"{Fore.LIGHTBLACK_EX}Using default recipe maintainer:"
+        f" {Fore.LIGHTMAGENTA_EX}AddYourGitHubIdHere"
+    )
+    return "AddYourGitHubIdHere"

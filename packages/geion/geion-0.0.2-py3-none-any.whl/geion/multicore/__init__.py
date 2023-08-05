11441f87070bf313b56ea2601456d480974731c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:24 +0000

    Add package geion/0.0.2/geion-0.0.2-py3-none-any.whl

---
+++ packages/geion/geion-0.0.2-py3-none-any.whl/geion/multicore/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from geion.multicore.backend import function_wrapper, wrap_population, run_population
+from geion.multicore.backend import kill_population_actors, unpin_objects, partitioned_run
+from geion.multicore.backend import RayManager, IndividualWrapper, silence_warnings

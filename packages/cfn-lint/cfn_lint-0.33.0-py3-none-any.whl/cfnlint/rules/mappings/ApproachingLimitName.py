7e2d6b83be7c6dede4eedd6949284e1ab8163360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:35 +0000

    Add package cfn-lint/0.33.0/cfn_lint-0.33.0-py3-none-any.whl

---
+++ packages/cfn-lint/cfn_lint-0.33.0-py3-none-any.whl/cfnlint/rules/mappings/ApproachingLimitName.py
@@ -0,0 +1,19 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule
+from cfnlint.rules.limits import approaching_name_limit
+
+
+class LimitName(CloudFormationLintRule):
+    """Check maximum Mapping name size limit"""
+    id = 'I7011'
+    shortdesc = 'Mapping name limit'
+    description = 'Check the size of Mapping names in the template is approaching the upper limit'
+    source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html'
+    tags = ['mappings', 'limits']
+
+    def match(self, cfn):
+        """Check CloudFormation Mappings"""
+        return approaching_name_limit(cfn, 'Mappings')

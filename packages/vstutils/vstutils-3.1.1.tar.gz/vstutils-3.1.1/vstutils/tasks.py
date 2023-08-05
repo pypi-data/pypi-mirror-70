22517edad9634fbeb86532971ec00283a1b8e0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package vstutils/3.1.1/vstutils-3.1.1.tar.gz

---
+++ packages/vstutils/vstutils-3.1.1.tar.gz/vstutils-3.1.1/vstutils/tasks.py
@@ -0,0 +1,17 @@
+from celery.app.task import BaseTask
+
+
+class TaskClass(BaseTask):
+    # pylint: disable=abstract-method
+
+    @property
+    def name(self):
+        return self.__name__
+
+    @property
+    def __name__(self):
+        return f'{self.__class__.__module__}.{self.__class__.__name__}'
+
+    @classmethod
+    def do(cls, *args, **kwargs):
+        return cls().delay(*args, **kwargs)

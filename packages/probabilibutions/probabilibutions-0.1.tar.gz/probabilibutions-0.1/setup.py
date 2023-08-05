5326046252e0b6993087a5eb19db50a0135d3e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:11 +0000

    Add package probabilibutions/0.1/probabilibutions-0.1.tar.gz

---
+++ packages/probabilibutions/probabilibutions-0.1.tar.gz/probabilibutions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'probabilibutions',
+      version = '0.1',
+      description = 'Gaussian and binomial distributions',
+      packages = ['probabilibutions'],
+      zip_safe = False)

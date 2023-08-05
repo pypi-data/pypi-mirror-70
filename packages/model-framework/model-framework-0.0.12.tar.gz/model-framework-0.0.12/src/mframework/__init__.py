a3f6ce1ce516f25031f7c4dcdbea114cde7a6779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:29 +0000

    Add package model-framework/0.0.12/model-framework-0.0.12.tar.gz

---
+++ packages/model-framework/model-framework-0.0.12.tar.gz/model-framework-0.0.12/src/mframework/__init__.py
@@ -0,0 +1,5 @@
+MODULE_NAME = 'mframework'
+
+from ._mframework import FeatureSpec, ModelSpec, SchemadictValidators, ItemDict
+from ._docgen import doc2rst
+from ._log import disable_logger, enable_logger

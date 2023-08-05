943270a302e2985ad3e9dec121e52fbb24b7601d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package developmental-index/2.0/developmental_index-2.0.tar.gz

---
+++ packages/developmental-index/developmental_index-2.0.tar.gz/developmental_index-2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'developmental_index',
+    version = '2.0',
+    author = 'Ben Devlin',
+    author_email = 'bad36@duke.edu',
+    url = 'https://github.com/bendevlin18/microglia-seq',
+    packages = ['developmental_index'],
+    license = 'MIT'
+
+)

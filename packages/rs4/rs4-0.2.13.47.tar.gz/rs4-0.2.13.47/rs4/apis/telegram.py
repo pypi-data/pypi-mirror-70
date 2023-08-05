53e0fd6aff6fb1694dc5823693a4928d3bff2594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package rs4/0.2.13.47/rs4-0.2.13.47.tar.gz

---
+++ packages/rs4/rs4-0.2.13.47.tar.gz/rs4-0.2.13.47/rs4/apis/telegram.py
@@ -0,0 +1,51 @@
+# pip3 install python-telegram-bot
+# BotFather /setprivacy disable
+# add to group
+
+import telegram
+
+class Telegram:
+    def __init__ (self, token):
+        self.token = token
+        self.bot = telegram.Bot(token = token)
+        self.chat_id = None
+
+    def show_messages (self):
+        updates = self.bot.getUpdates()
+        for u in updates:
+            print(u.message)
+
+    def updates (self):
+        return self.bot.getUpdates()
+
+    def send (self, msg, chat_id = None):
+        msgs = self.updates ()
+        if msgs:
+            self.chat_id = msgs [-1].message.chat.id
+
+        self.bot.sendMessage (
+            chat_id or self.chat_id,
+            msg
+        )
+
+
+if __name__ == "__main__":
+    import sys, os
+    import time
+    from telegram.error import TimedOut
+
+    token = os.environ.get ('TELEGRAM_TOKEN')
+    if not token:
+        raise SystemExit ('telegram token reqired')
+    commit_title = os.environ.get ("CI_COMMIT_TITLE", "")
+    if commit_title.find ('--tg-silent') != -1:
+        sys.exit ()
+
+    bot = Telegram (token)
+    for i in range (7):
+        try:
+            bot.send (' '.join (sys.argv [1:]))
+        except TimedOut:
+            time.sleep (2)
+            continue
+        break

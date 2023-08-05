30997c111053b1219cc15506ebe7e9c75d205865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:18 +0000

    Add package py-pde/0.8.0/py-pde-0.8.0.tar.gz

---
+++ packages/py-pde/py-pde-0.8.0.tar.gz/py-pde-0.8.0/pde/visualization/__init__.py
@@ -0,0 +1,16 @@
+'''
+Functions and classes for visualizing simulations.
+
+
+.. autosummary::
+   :nosignatures:
+
+   contexts
+   movies
+   plotting
+   
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
+'''
+
+from .plotting import plot_magnitudes, plot_kymograph
+from .movies import movie_scalar, movie_multiple

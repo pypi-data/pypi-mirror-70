55b44854cc1f0a9ac97a032eb669908303c9e3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:05 +0000

    Add package curve/0.0.6/curve-0.0.6.tar.gz

---
+++ packages/curve/curve-0.0.6.tar.gz/curve-0.0.6/setup.py
@@ -0,0 +1,26 @@
+# History
+# 0.0.3: Exposed EyeDiagram
+# 0.0.4: Added Curve.filter()
+# 0.0.6: Pickle support, equality operand.
+
+import setuptools
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="curve",
+    version="0.0.6",
+    description="A waveform manipulation and analysis library for Python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Juan Pablo Caram",
+    author_email="jpcaram@gmail.com",
+    url="https://bitbucket.org/jpcgt/curve",
+    requires=['matplotlib', 'numpy', 'scipy'],
+    install_requires=['matplotlib', 'numpy', 'scipy'],
+    packages=['curve'],
+    python_requires='>=3.6',
+    package_dir={'curve': 'curve'},
+)

9d60a20a226797816e6a41b5f1a3933fb158d51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package tribanco-utils-bi/1.0.2/tribanco-utils-bi-1.0.2.tar.gz

---
+++ packages/tribanco-utils-bi/tribanco-utils-bi-1.0.2.tar.gz/tribanco-utils-bi-1.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='tribanco-utils-bi',
+    version='1.0.2',
+    packages=setuptools.find_packages(),
+    url='',
+    license='MIT',
+    author='Herculano Cunha',
+    author_email='herculanocm@outlook.com',
+    description='Utilitario para rotinas BI Tribanco',
+    keywords='bi dw sqlserver tools utils',
+    install_requires=['pandas','pymssql'],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

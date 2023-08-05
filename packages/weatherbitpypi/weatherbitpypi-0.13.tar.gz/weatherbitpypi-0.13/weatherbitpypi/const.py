cd23a1f8adaf2cd119cba7d280641ec608182983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:14 +0000

    Add package weatherbitpypi/0.13/weatherbitpypi-0.13.tar.gz

---
+++ packages/weatherbitpypi/weatherbitpypi-0.13.tar.gz/weatherbitpypi-0.13/weatherbitpypi/const.py
@@ -0,0 +1,11 @@
+"""Constant definitions for Weatherbit."""
+
+BASE_URL = "https://api.weatherbit.io/v2.0"
+
+DATA_TYPES = [
+    "current",
+    "forecast",
+    "alert",
+]
+
+DEFAULT_TIMEOUT = 10

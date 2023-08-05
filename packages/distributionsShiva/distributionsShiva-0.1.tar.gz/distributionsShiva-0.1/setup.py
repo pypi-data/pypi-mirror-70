407478bc7e7e071f4cc3bcb1922d85389f301205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:12 +0000

    Add package distributionsShiva/0.1/distributionsShiva-0.1.tar.gz

---
+++ packages/distributionsShiva/distributionsShiva-0.1.tar.gz/distributionsShiva-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsShiva',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Samarth Srivastava',
+      packages=['distributions'],
+      zip_safe=False)

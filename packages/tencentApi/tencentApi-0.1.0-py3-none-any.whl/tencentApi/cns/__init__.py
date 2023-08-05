df18135dc6c7be24001c929f67bd155ddd62934c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:38 +0000

    Add package tencentApi/0.1.0/tencentApi-0.1.0-py3-none-any.whl

---
+++ packages/tencentApi/tencentApi-0.1.0-py3-none-any.whl/tencentApi/cns/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+# File Name: __init__.py
+# Author: Shechucheng
+# Created Time: 2020-05-21 17:51:45
+
+
+from .cns import CnsApi
+from .ddns import get_host_ip, DDns, ip_check, get_addr_ip
+

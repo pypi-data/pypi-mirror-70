987aa2ec954bf203e2f774e073025459dbdee124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:02 +0000

    Add package aws-cdk.aws-route53-patterns/1.42.1/aws-cdk.aws-route53-patterns-1.42.1.tar.gz

---
+++ packages/aws-cdk.aws-route53-patterns/aws-cdk.aws-route53-patterns-1.42.1.tar.gz/aws-cdk.aws-route53-patterns-1.42.1/src/aws_cdk/aws_route53_patterns/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_certificatemanager
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_iam
+import aws_cdk.aws_route53
+import aws_cdk.aws_route53_targets
+import aws_cdk.aws_s3
+import aws_cdk.core
+import aws_cdk.region_info
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-route53-patterns", "1.42.1", __name__[0:-6], "aws-route53-patterns@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

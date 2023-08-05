3f048d4367dadcbae5b815c6b7fad80fa16a09b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:41 +0000

    Add package printListModule/1.0.0/printListModule-1.0.0.tar.gz

---
+++ packages/printListModule/printListModule-1.0.0.tar.gz/printListModule-1.0.0/printListModule.py
@@ -0,0 +1,7 @@
+#Recursive list printing
+def rec_print_list(lst):
+    for i in lst:
+        if isinstance(i, list):
+            rec_print_list(i)
+        else:
+            print(i)
---
+++ packages/printListModule/printListModule-1.0.0.tar.gz/printListModule-1.0.0/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name = 'printListModule', version = '1.0.0',
+      description= 'An API to print list recursively', author = 'Maulik',
+      author_email='maulik.jodhani@ignitarium.com', py_modules=['printListModule'])

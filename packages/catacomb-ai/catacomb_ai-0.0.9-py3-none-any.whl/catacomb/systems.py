69073e9aba164ae099da735c6175cd85f9c238a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:47 +0000

    Add package catacomb-ai/0.0.9/catacomb_ai-0.0.9-py3-none-any.whl

---
+++ packages/catacomb-ai/catacomb_ai-0.0.9-py3-none-any.whl/catacomb/systems.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Types(Enum):
+    TEXT = 1
+    NUMBER = 2
+
+
+class System():
+    @staticmethod
+    def input_type(self, type):
+        pass
+
+    @staticmethod
+    def output_type(self, type):
+        pass

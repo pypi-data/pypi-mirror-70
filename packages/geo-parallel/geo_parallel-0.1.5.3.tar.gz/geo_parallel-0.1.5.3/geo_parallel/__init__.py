c93fb1b4da714a0fb4e88e16f0c0e6f7b925bd77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:26 +0000

    Add package geo-parallel/0.1.5.3/geo_parallel-0.1.5.3.tar.gz

---
+++ packages/geo-parallel/geo_parallel-0.1.5.3.tar.gz/geo_parallel-0.1.5.3/geo_parallel/__init__.py
@@ -0,0 +1,26 @@
+import rasterio
+import numpy
+
+
+# Open raster file
+def open_raster(raster_file):
+    return rasterio.open(raster_file)
+
+
+# Save raster file
+def save_raster(data, raster_file, width, height, crs, transform):
+    raster = rasterio.open(raster_file, 'w', driver='Gtiff',
+                           width=width, height=height,
+                           count=1,
+                           crs=crs,
+                           transform=transform,
+                           dtype='float64')
+    raster.write(data, 1)
+    raster.close()
+
+
+# Calculate NDVI
+def ndvi(red, nir):
+    return numpy.where(nir + red == 0.,
+                       0,
+                       (nir - red) / (nir + red))

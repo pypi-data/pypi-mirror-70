02e5bbc038f04080dae2187dc01085357db18461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:54 +0000

    Add package gaia-sdk/1.2.0/gaia-sdk-1.2.0.tar.gz

---
+++ packages/gaia-sdk/gaia-sdk-1.2.0.tar.gz/gaia-sdk-1.2.0/gaia_sdk/api/crypto.py
@@ -0,0 +1,22 @@
+import base64
+import hashlib
+import hmac
+
+
+class HMAC:
+    """
+    This class is used to encrypt the data before sending it to G.A.I.A.
+    """
+
+    def __init__(self, secret):
+        self.secret = secret
+
+    def hash(self, data):
+        """
+        Hashes the given string data.
+        """
+        key = self.secret.encode("UTF-8")
+        msg = data.encode("UTF-8")
+
+        dig = hmac.new(key, msg=msg, digestmod=hashlib.sha256).digest()
+        return base64.b64encode(dig).decode()

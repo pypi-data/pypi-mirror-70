ca33b8d11976cff65cd3ac4890d7791b7ef4eb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:37 +0000

    Add package lightshare/0.8/lightshare-0.8.tar.gz

---
+++ packages/lightshare/lightshare-0.8.tar.gz/lightshare-0.8/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'lightshare',  
+  version = '0.8',   
+  license='MIT',     
+  description = 'Faster file sharing using sockets. Makes work easier.',  
+  author = 'Nannan',              
+  author_email = 'nanna7077@gmail.com',
+  keywords = ['Filesharing', 'File', 'Sharing'], 
+  py_modules=['lightshare'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)

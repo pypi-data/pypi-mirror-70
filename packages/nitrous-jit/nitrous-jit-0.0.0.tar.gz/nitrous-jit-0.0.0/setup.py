86bb29af8f31cf79ce1d3f6760c7d21e8618eb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package nitrous-jit/0.0.0/nitrous-jit-0.0.0.tar.gz

---
+++ packages/nitrous-jit/nitrous-jit-0.0.0.tar.gz/nitrous-jit-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="nitrous-jit",
+    version="0.0.0",
+    author="Kevin Modzelewski",
+    author_email="kevmod@gmail.com",
+    description="Nitrous JIT engine",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='==3.7',
+)

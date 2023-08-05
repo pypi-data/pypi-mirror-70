1147baaf8f6f9688cba28549d8ce0f74269be0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:24 +0000

    Add package cliquor/0.0.1/cliquor-0.0.1.tar.gz

---
+++ packages/cliquor/cliquor-0.0.1.tar.gz/cliquor-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cliquor',
+    version='0.0.1',
+    author="Koen Woortman",
+    author_email="koensw@outlook.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'cliquor = cliquor:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Environment :: Console',
+    ],
+)

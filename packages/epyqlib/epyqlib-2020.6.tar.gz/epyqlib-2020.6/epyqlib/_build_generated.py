d5282f20308bfed9385917ab3527a5600e3f774e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:46 +0000

    Add package epyqlib/2020.6/epyqlib-2020.6.tar.gz

---
+++ packages/epyqlib/epyqlib-2020.6.tar.gz/epyqlib-2020.6/epyqlib/_build_generated.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '33296174'
+build_number = '504'
+build_version = '1.0.504'
+job_id = 'wuk40h0yn62fxssg'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/stlib/build/job/wuk40h0yn62fxssg'

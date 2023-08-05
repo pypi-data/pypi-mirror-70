52669c91d6160b58f3b8e19845171ed7862b5021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:55 +0000

    Add package rseng/0.0.0/rseng-0.0.0.tar.gz

---
+++ packages/rseng/rseng-0.0.0.tar.gz/rseng-0.0.0/rseng/version.py
@@ -0,0 +1,29 @@
+"""
+
+Copyright (C) 2020 Vanessa Sochat.
+
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+
+__version__ = "0.0.0"
+AUTHOR = "Vanessa Sochat"
+AUTHOR_EMAIL = "vsochat@stanford.edu"
+NAME = "rseng"
+PACKAGE_URL = "http://www.github.com/rseng/rseng"
+KEYWORDS = "rseng,software,research software,database,taxonomy"
+DESCRIPTION = "tools for assessment and categorization of research software"
+LICENSE = "LICENSE"
+
+################################################################################
+# Global requirements
+
+
+INSTALL_REQUIRES = ()
+DATABASE_REQUIRES = (("sqlalchemy", {"min_version": None}),)
+TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
+
+
+ALL_REQUIRES = INSTALL_REQUIRES + DATABASE_REQUIRES

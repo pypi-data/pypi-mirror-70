ff43f53672dcd05e16c42f17ac9daee3488ec5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:32 +0000

    Add package wdhtools/1.0.1/wdhtools-1.0.1.tar.gz

---
+++ packages/wdhtools/wdhtools-1.0.1.tar.gz/wdhtools-1.0.1/README.md
@@ -0,0 +1,4 @@
+## wdhtools
+Tools used by wdh
+
+wangdehong934969
---
+++ packages/wdhtools/wdhtools-1.0.1.tar.gz/wdhtools-1.0.1/wdhtools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# @Author: dehong
+# @Date: 2020-06-04
+# @Time: 17:17
+# @Name: __init__.py
+
+from .hello import *
+
+name = 'wdhtools'

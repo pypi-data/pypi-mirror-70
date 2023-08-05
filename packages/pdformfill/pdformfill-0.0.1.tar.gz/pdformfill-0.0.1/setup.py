b9f2cbdb62371e8d66bf910c6567f2f81e17e0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:46 +0000

    Add package pdformfill/0.0.1/pdformfill-0.0.1.tar.gz

---
+++ packages/pdformfill/pdformfill-0.0.1.tar.gz/pdformfill-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdformfill",
+    version="0.0.1",
+    author="Tomislav Kucar",
+    author_email="kucar.tomislav@gmail.com",
+    url="http://github.com/frainfreeze/pdformfill",
+    description="library for filling pdf forms",
+    scripts = [],
+    license = "BSD-3-Clause",
+    platforms = ["any"],
+    zip_safe=False,
+    packages=find_packages()
+    )
+    

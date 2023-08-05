a71578ac9c13fccd95c519c03fe76a103758be03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:21 +0000

    Add package mlnd-GBP-distribution/0.1/mlnd_GBP_distribution-0.1.tar.gz

---
+++ packages/mlnd-GBP-distribution/mlnd_GBP_distribution-0.1.tar.gz/mlnd_GBP_distribution-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mlnd_GBP_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Supuli Jayaweera',
+      packages=['mlnd_GBP_distribution'],
+      zip_safe=False)

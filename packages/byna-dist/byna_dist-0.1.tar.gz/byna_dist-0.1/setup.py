5c89b46630febba76cd3be37158ac71e215ca44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:46 +0000

    Add package byna-dist/0.1/byna_dist-0.1.tar.gz

---
+++ packages/byna-dist/byna_dist-0.1.tar.gz/byna_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='byna_dist',
+      version='0.1',
+      author="Abubakar Abdusalam",
+      author_email="abdusalam@bynalab.com",
+      description='Gaussian distributions',
+      packages=['byna_dist'],
+      zip_safe=False)

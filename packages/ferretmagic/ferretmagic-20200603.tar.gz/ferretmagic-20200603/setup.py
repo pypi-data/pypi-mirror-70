8d3fbbf9a4802edea3d919f20981061058ac484a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package ferretmagic/20200603/ferretmagic-20200603.tar.gz

---
+++ packages/ferretmagic/ferretmagic-20200603.tar.gz/ferretmagic-20200603/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'ferretmagic',
+  packages = ['ferretmagic'],
+  py_modules = ['ferretmagic'],
+  version = '20200603',
+  description = 'ipython extension for pyferret',
+  author = 'Patrick Brockmann',
+  author_email = 'Patrick.Brockmann@lsce.ipsl.fr',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url = 'https://github.com/PBrockmann/ipython_ferretmagic',
+  download_url = 'https://github.com/PBrockmann/ipython_ferretmagic/tarball/master',
+  keywords = ['jupyter', 'ipython', 'ferret', 'pyferret', 'magic', 'extension'],
+  classifiers = [
+        'Programming Language :: Python :: 3',
+	'License :: OSI Approved :: MIT License',
+	'Operating System :: OS Independent',
+	'Framework :: Jupyter',
+	'Framework :: IPython'
+  ],
+)

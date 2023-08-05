a3605b39b9dcca286461f6149736f257ba59e945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package apkdownload/0.0.3/apkdownload-0.0.3-py3-none-any.whl

---
+++ packages/apkdownload/apkdownload-0.0.3-py3-none-any.whl/apkdownload/__init__.py
@@ -0,0 +1,15 @@
+import requests, webbrowser
+from bs4 import BeautifulSoup
+
+def dl(app_name):
+	res = requests.get(f"https://m.apkpure.com/search?q={app_name}")
+	soup = BeautifulSoup(res.text, 'html.parser')
+	result = soup.select('.dd')
+	for link in result[:1]:
+		res2 = requests.get("https://m.apkpure.com" + link.get('href') + "/download?from=details")
+		soup2 = BeautifulSoup(res2.text, 'html.parser')
+		result = soup2.select('.ga')
+		for link in result:
+			print(link.get('href'))
+
+dl("cpuz")

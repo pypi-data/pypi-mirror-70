35fb697919ae22258c05ec1d27c06aba8ee30f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:14 +0000

    Add package nummer-games/0.1/nummer_games-0.1.tar.gz

---
+++ packages/nummer-games/nummer_games-0.1.tar.gz/nummer_games-0.1/nummer_games/__init__.py
@@ -0,0 +1,3 @@
+from .Classifygeneration import Classify
+from .Primegeneration import Prime
+from .Genericgeneration import Generation
---
+++ packages/nummer-games/nummer_games-0.1.tar.gz/nummer_games-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='nummer_games',
+      version='0.1',
+      description='Guess random numbers and prime numbers',
+      packages=['nummer_games'],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Akande Imisioluwa',
+      author_email='imizezek84@gmail.com',
+      url='https://github.com/imisi-akande/number-game',
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+      python_requires='>=3.6',
+    )

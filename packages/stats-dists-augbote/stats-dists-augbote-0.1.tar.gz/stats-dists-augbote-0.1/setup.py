41e595bc7ffc75583ad3ada23b1e724b1568cad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package stats-dists-augbote/0.1/stats-dists-augbote-0.1.tar.gz

---
+++ packages/stats-dists-augbote/stats-dists-augbote-0.1.tar.gz/stats-dists-augbote-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats-dists-augbote',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['stats-dists-augbote'],
+      author='Augusto Bottelli',
+      author_email='augustobottelli@hotmail.com',
+      zip_safe=False)

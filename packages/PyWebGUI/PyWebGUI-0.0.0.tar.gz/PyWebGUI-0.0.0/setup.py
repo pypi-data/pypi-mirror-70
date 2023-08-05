619757d673f01a40adc8920000eb62440dd4b909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:57 +0000

    Add package PyWebGUI/0.0.0/PyWebGUI-0.0.0.tar.gz

---
+++ packages/PyWebGUI/PyWebGUI-0.0.0.tar.gz/PyWebGUI-0.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="PyWebGUI",
+    version="0.0.0",
+    author="Mister7F",
+    author_email="mister7f@gmail.com",
+    description="Future WebGUI",
+)

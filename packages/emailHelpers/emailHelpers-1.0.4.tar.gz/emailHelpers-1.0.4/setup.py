e50f7ee5675f508cb46336823066dc099b632f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:24 +0000

    Add package emailHelpers/1.0.4/emailHelpers-1.0.4.tar.gz

---
+++ packages/emailHelpers/emailHelpers-1.0.4.tar.gz/emailHelpers-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+version = "1.0.4"
+with open("PYPI.md", "r") as fh:
+    long_description = fh.read()
+setup(name="emailHelpers",
+                 version=version,
+                 author="Kendell R.",
+                 author_email="kidscodingplace@gmail.com",
+                 license="MIT",
+                 description="emailHelpers is a wrapper for the smtplib and email packages.",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://family-richards.github.io/emailHelpers/",
+                 packages=find_packages(),
+                 classifiers=["Topic :: Communications :: Email", "Natural Language :: English", "Operating System :: OS Independent", "Programming Language :: Python :: 3 :: Only", "Topic :: Software Development :: Libraries :: Python Modules", "Development Status :: 5 - Production/Stable"],
+                 python_requires='>=3.0')

3f3285b6bc51795a79c6314a49726039bd684108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:44 +0000

    Add package conviso-flowcli/1.1.0/conviso_flowcli-1.1.0-py3-none-any.whl

---
+++ packages/conviso-flowcli/conviso_flowcli-1.1.0-py3-none-any.whl/convisoappsec/flow/version_searchers/timebased_version_seacher.py
@@ -0,0 +1,23 @@
+from .version_searcher_result import VersionSearcherResult
+
+
+class TimeBasedVersionSearcher(object):
+    def __init__(self, version_control_system_adapter):
+        self.version_control_system_adapter = version_control_system_adapter
+
+
+    def find_current_and_previous_version(self):
+        tags = self.version_control_system_adapter.tags()
+
+        current_tag = None
+        previous_tag = None
+        tags = tags[:2]
+
+        if len(tags) >= 2:
+           (current_tag, previous_tag) = tags
+        elif len(tags) == 1:
+            current_tag = tags[0]
+        else:
+            raise Exception("Was not possible find the current tag")
+
+        return VersionSearcherResult(current_tag, previous_tag)

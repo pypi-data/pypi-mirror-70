e9da9ba904017ea055bcaf4a38d92fafbec4761c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:42 +0000

    Add package gcsutils/0.0.1/gcsutils-0.0.1.tar.gz

---
+++ packages/gcsutils/gcsutils-0.0.1.tar.gz/gcsutils-0.0.1/setup_gcsutils.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="gcsutils",
+    version="0.0.1",
+    packages=['gcsutils'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)

701242dcca211593831ef4a336d27b33ea6f01cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:27 +0000

    Add package pjisp-diff/0.1.5/pjisp_diff-0.1.5.tar.gz

---
+++ packages/pjisp-diff/pjisp_diff-0.1.5.tar.gz/pjisp_diff-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pjisp_diff']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+setup_kwargs = {
+    'name': 'pjisp-diff',
+    'version': '0.1.5',
+    'description': 'PJISP diff check',
+    'long_description': None,
+    'author': 'Jelena Dokic',
+    'author_email': 'jrubics@hacke.rs',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)

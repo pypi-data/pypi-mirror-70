65f5628d3022582fb789cccff1c0451f284d027b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:39 +0000

    Add package bin-gaus-dist/1.1/bin_gaus_dist-1.1.tar.gz

---
+++ packages/bin-gaus-dist/bin_gaus_dist-1.1.tar.gz/bin_gaus_dist-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bin_gaus_dist',
+      version='1.1',
+      description='Binomial Gaussian distributions',
+      packages=['bin_gaus_dist'],
+      author = "Avirup Mondal",
+      author_email = "xyz@example.com",
+      zip_safe=False)

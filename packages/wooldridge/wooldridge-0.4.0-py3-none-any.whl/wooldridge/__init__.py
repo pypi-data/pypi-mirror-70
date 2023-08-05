df78a9a5e64dbfdf0c091574af8d0075d1d002bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:37 +0000

    Add package wooldridge/0.4.0/wooldridge-0.4.0-py3-none-any.whl

---
+++ packages/wooldridge/wooldridge-0.4.0-py3-none-any.whl/wooldridge/__init__.py
@@ -0,0 +1,12 @@
+"""
+Github page: https://github.com/spring-haru/wooldridge
+"""
+
+from .load_data import *
+
+__all__ = ['data', 'dataWoo']
+
+__author__ = 'Tetsu Haruyama'
+__version__ = '0.4.0'
+__copyright__ = 'Copyright (c) 2020 Tetsu Haruyama'
+__license__ = 'GPL-3.0'

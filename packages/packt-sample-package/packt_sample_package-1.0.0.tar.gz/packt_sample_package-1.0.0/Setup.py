b4bbc72fbe1c7dbe1eccb10aa12917bd10a7aba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:09 +0000

    Add package packt-sample-package/1.0.0/packt_sample_package-1.0.0.tar.gz

---
+++ packages/packt-sample-package/packt_sample_package-1.0.0.tar.gz/packt_sample_package-1.0.0/Setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name = "packt_sample_package",
+    version = "1.0.0",
+    author = "Authors name",
+    authors_email = "saikapdi11@gmail.com",
+    long_description = "This is the longer description and will appear in the web.",
+    py_modules = ["packt"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+
+)

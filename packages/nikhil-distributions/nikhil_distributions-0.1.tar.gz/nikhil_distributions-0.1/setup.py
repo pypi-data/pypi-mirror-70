312776b6d4c36a6a044f7aec74734e022f962ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package nikhil-distributions/0.1/nikhil_distributions-0.1.tar.gz

---
+++ packages/nikhil-distributions/nikhil_distributions-0.1.tar.gz/nikhil_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nikhil_distributions',
+      version='0.1',
+      description='Gaussian distributions,Binomial distributions',
+      packages=['nikhil_distributions'],
+      zip_safe=False)

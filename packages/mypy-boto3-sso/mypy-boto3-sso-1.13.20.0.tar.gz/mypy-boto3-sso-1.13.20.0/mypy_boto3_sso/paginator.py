cc6ec6fa425cb618ac7882645ab767c15feddfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package mypy-boto3-sso/1.13.20.0/mypy-boto3-sso-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-sso/mypy-boto3-sso-1.13.20.0.tar.gz/mypy-boto3-sso-1.13.20.0/mypy_boto3_sso/paginator.py
@@ -0,0 +1,53 @@
+"""
+Main interface for sso service client paginators.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.sso import (
+        ListAccountRolesPaginator,
+        ListAccountsPaginator,
+    )
+
+    client: SSOClient = boto3.client("sso")
+
+    list_account_roles_paginator: ListAccountRolesPaginator = client.get_paginator("list_account_roles")
+    list_accounts_paginator: ListAccountsPaginator = client.get_paginator("list_accounts")
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_sso.type_defs import (
+    ListAccountRolesResponseTypeDef,
+    ListAccountsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("ListAccountRolesPaginator", "ListAccountsPaginator")
+
+
+class ListAccountRolesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListAccountRoles documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/sso.html#SSO.Paginator.ListAccountRoles)
+    """
+
+    def paginate(
+        self, accessToken: str, accountId: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListAccountRolesResponseTypeDef]:
+        """
+        [ListAccountRoles.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/sso.html#SSO.Paginator.ListAccountRoles.paginate)
+        """
+
+
+class ListAccountsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListAccounts documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/sso.html#SSO.Paginator.ListAccounts)
+    """
+
+    def paginate(
+        self, accessToken: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListAccountsResponseTypeDef]:
+        """
+        [ListAccounts.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/sso.html#SSO.Paginator.ListAccounts.paginate)
+        """

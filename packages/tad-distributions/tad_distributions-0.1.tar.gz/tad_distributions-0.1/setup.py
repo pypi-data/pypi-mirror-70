4201af8c5c7d3da254e00ca7b3225b51ae8b0981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:43 +0000

    Add package tad-distributions/0.1/tad_distributions-0.1.tar.gz

---
+++ packages/tad-distributions/tad_distributions-0.1.tar.gz/tad_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tad_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tad_distributions'],
+      zip_safe=False)

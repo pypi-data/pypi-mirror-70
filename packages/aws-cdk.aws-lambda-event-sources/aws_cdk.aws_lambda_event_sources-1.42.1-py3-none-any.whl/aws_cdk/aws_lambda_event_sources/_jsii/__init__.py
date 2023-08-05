023e90b153fa32cf45433fcfba5ae31a5c7393d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package aws-cdk.aws-lambda-event-sources/1.42.1/aws_cdk.aws_lambda_event_sources-1.42.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-lambda-event-sources/aws_cdk.aws_lambda_event_sources-1.42.1-py3-none-any.whl/aws_cdk/aws_lambda_event_sources/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_apigateway
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_kinesis
+import aws_cdk.aws_lambda
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_notifications
+import aws_cdk.aws_sns
+import aws_cdk.aws_sns_subscriptions
+import aws_cdk.aws_sqs
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-lambda-event-sources", "1.42.1", __name__[0:-6], "aws-lambda-event-sources@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

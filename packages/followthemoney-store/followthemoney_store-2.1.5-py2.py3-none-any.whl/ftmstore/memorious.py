617423909c26d724e82002c50d72148a92649c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:37 +0000

    Add package followthemoney-store/2.1.5/followthemoney_store-2.1.5-py2.py3-none-any.whl

---
+++ packages/followthemoney-store/followthemoney_store-2.1.5-py2.py3-none-any.whl/ftmstore/memorious.py
@@ -0,0 +1,46 @@
+from banal import ensure_list
+from memorious.settings import DATASTORE_URI
+
+from ftmstore.dataset import Dataset
+from ftmstore.settings import DATABASE_URI, DEFAULT_DATABASE_URI
+
+ORIGIN = 'memorious'
+
+
+def get_dataset(context, origin=ORIGIN):
+    name = context.get('dataset', context.crawler.name)
+    # Either use a database URI that has been explicitly set as a
+    # backend, or default to the memorious datastore.
+    database_uri = DATABASE_URI
+    if DATABASE_URI == DEFAULT_DATABASE_URI:
+        database_uri = DATASTORE_URI
+    return Dataset(name, database_uri=database_uri, origin=origin)
+
+
+def ftm_store(context, data):
+    # This is a simplistic implementation of a balkhash memorious operation.
+    # It is meant to serve the use of OCCRP where we pipe data into postgresql.
+    writer = get_dataset(context)
+    entities = ensure_list(data.get('entities', data))
+    for entity in entities:
+        context.log.debug("Store entity [%(schema)s]: %(id)s", entity)
+        writer.put(entity, entity.pop('fragment', None))
+        context.emit(rule='fragment', data=data, optional=True)
+    context.emit(data=data, optional=True)
+    writer.close()
+
+
+def ftm_load_aleph(context, data):
+    try:
+        from alephclient.memorious import get_api
+    except ImportError:
+        context.log.error("alephclient not installed. Skipping...")
+        return
+    api = get_api(context)
+    if api is not None:
+        foreign_id = context.params.get('foreign_id', context.crawler.name)
+        collection = api.load_collection_by_foreign_id(foreign_id, {})
+        collection_id = collection.get('id')
+        unsafe = context.params.get('unsafe', False)
+        entities = get_dataset(context)
+        api.write_entities(collection_id, entities, unsafe=unsafe)

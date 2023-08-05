63f0d15e6a67bd6b70b2b626da9b9fd03e4f6bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:45 +0000

    Add package esperclient/0.0.14/esperclient-0.0.14-py3-none-any.whl

---
+++ packages/esperclient/esperclient-0.0.14-py3-none-any.whl/esperclient/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from esperclient.api.application_api import ApplicationApi
+from esperclient.api.commands_api import CommandsApi
+from esperclient.api.commands_v2_api import CommandsV2Api
+from esperclient.api.device_api import DeviceApi
+from esperclient.api.device_group_api import DeviceGroupApi
+from esperclient.api.enterprise_api import EnterpriseApi
+from esperclient.api.group_commands_api import GroupCommandsApi
+from esperclient.api.token_api import TokenApi
---
+++ packages/esperclient/esperclient-0.0.14-py3-none-any.whl/test/test_script.py
@@ -0,0 +1,3 @@
+import esperclient
+from esperclient.rest import ApiException
+

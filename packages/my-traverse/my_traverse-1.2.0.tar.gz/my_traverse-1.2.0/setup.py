6d940e201e0e5c784e7101b85e2d1dfbd8ee639b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:09 +0000

    Add package my-traverse/1.2.0/my_traverse-1.2.0.tar.gz

---
+++ packages/my-traverse/my_traverse-1.2.0.tar.gz/my_traverse-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='my_traverse',
+    version='1.2.0',
+    py_modules=['nester'],
+    author='Bill',
+    author_email='bill@gmail.com',
+    url='http://www.python.org',
+    description='A simple printer of nested lists',
+)

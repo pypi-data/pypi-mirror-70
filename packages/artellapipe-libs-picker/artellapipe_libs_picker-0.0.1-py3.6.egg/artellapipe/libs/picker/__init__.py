5565ece6cb33a5de39f3b7165f185dfc10d5fc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:40 +0000

    Add package artellapipe-libs-picker/0.0.1/artellapipe_libs_picker-0.0.1-py3.6.egg

---
+++ packages/artellapipe-libs-picker/artellapipe_libs_picker-0.0.1-py3.6.egg/artellapipe/libs/picker/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization module for artellapipe-libs-picker
+"""
+
+
+def init():
+    """
+    Initializes module
+    """
+
+    pass

f1278b5142a63608c0dcc923cfbd97cd8591fb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package hpogrid/0.0.2/hpogrid-0.0.2.tar.gz

---
+++ packages/hpogrid/hpogrid-0.0.2.tar.gz/hpogrid-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hpogrid", # Replace with your own username
+    version="0.0.2",
+    author="Alkaid Cheng",
+    author_email="chi.lung.cheng@cern.ch",
+    description="A workflow for hyperparamter optimization using the ATLAS grid resources",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.cern.ch/aml/hyperparameter-optimization/alkaid-qt/hpogrid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[   
+          'ray[tune]',
+          'pandas',
+          'tqdm',
+          'tabulate',
+          'numpy',
+          'psutil'
+      ],
+    scripts=['bin/hpogrid'],
+    python_requires='>=3.6',
+)

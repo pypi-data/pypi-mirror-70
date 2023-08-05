5549ccd28244187837f57afc4d2a3d6b7c2b1ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package mypy-boto3-elb/1.13.20.0/mypy-boto3-elb-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-elb/mypy-boto3-elb-1.13.20.0.tar.gz/mypy-boto3-elb-1.13.20.0/mypy_boto3_elb/paginator.py
@@ -0,0 +1,53 @@
+"""
+Main interface for elb service client paginators.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.elb import (
+        DescribeAccountLimitsPaginator,
+        DescribeLoadBalancersPaginator,
+    )
+
+    client: ElasticLoadBalancingClient = boto3.client("elb")
+
+    describe_account_limits_paginator: DescribeAccountLimitsPaginator = client.get_paginator("describe_account_limits")
+    describe_load_balancers_paginator: DescribeLoadBalancersPaginator = client.get_paginator("describe_load_balancers")
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, List, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_elb.type_defs import (
+    DescribeAccessPointsOutputTypeDef,
+    DescribeAccountLimitsOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("DescribeAccountLimitsPaginator", "DescribeLoadBalancersPaginator")
+
+
+class DescribeAccountLimitsPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeAccountLimits documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/elb.html#ElasticLoadBalancing.Paginator.DescribeAccountLimits)
+    """
+
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[DescribeAccountLimitsOutputTypeDef]:
+        """
+        [DescribeAccountLimits.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/elb.html#ElasticLoadBalancing.Paginator.DescribeAccountLimits.paginate)
+        """
+
+
+class DescribeLoadBalancersPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeLoadBalancers documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/elb.html#ElasticLoadBalancing.Paginator.DescribeLoadBalancers)
+    """
+
+    def paginate(
+        self, LoadBalancerNames: List[str] = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[DescribeAccessPointsOutputTypeDef]:
+        """
+        [DescribeLoadBalancers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/elb.html#ElasticLoadBalancing.Paginator.DescribeLoadBalancers.paginate)
+        """

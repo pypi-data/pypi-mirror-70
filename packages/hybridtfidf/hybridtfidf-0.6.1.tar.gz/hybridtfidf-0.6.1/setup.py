00a04539c0e4ff6b602b4869484eacdf121862f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package hybridtfidf/0.6.1/hybridtfidf-0.6.1.tar.gz

---
+++ packages/hybridtfidf/hybridtfidf-0.6.1.tar.gz/hybridtfidf-0.6.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
+setup(
+  name = 'hybridtfidf',         
+  packages = ['hybridtfidf'],   
+  version = '0.6.1',      
+  license='MIT',        
+  description = 'An implementation of the Hybrid TF-IDF microblog summarisation algorithm as proposed by David Ionuye and Jugal K. Kalita. Hybrid TF-IDF is designed with twitter data in mind, where document lengths are short. It is an approach to generating Multiple Post Summaries of a collection of documents.',
+  long_description = README,
+  long_description_content_type='text/markdown',
+  author = 'Jamal Rahman',
+  author_email = 'jamalrahman95@gmail.com', 
+  url = 'https://github.com/jamalrahman/hybridtfidf',   
+  download_url = 'https://github.com/JamalRahman/hybridtfidf/archive/v0.5.tar.gz',
+  keywords = ['TFIDF', 'Text Summarization','NLP'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',    
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)

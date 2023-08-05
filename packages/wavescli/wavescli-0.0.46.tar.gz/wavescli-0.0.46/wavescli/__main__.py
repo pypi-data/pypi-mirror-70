6c81402b9021120b484b53b839b423f92a6631c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package wavescli/0.0.46/wavescli-0.0.46.tar.gz

---
+++ packages/wavescli/wavescli-0.0.46.tar.gz/wavescli-0.0.46/wavescli/__main__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import click
+from .cli import main
+from .apiclient import ApiClient
+
+
+class CliObject(dict):
+    """Classe auxiliar criada para fazer lazy-initialization do
+    client da API sem mudar muito a estrutura do CLI"""
+
+    def __init__(self):
+        self._client = None
+        super(CliObject, self).__init__()
+
+    @property
+    def client(self):
+        if self._client is None:
+            cfg = self['config']
+            url = cfg['WAVES_URL']
+            self._client = ApiClient(url)
+        return self._client
+
+
+def run():
+    default_context = CliObject()
+
+    try:
+        return main(obj=default_context)
+    except Exception as exc:
+        click.secho(str(exc), fg='red')
+
+
+if __name__ == '__main__':
+    run()

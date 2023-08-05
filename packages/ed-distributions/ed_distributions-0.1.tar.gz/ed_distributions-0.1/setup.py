020bd0aa941f5c9377090eade13bb9a6ef4f66cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package ed-distributions/0.1/ed_distributions-0.1.tar.gz

---
+++ packages/ed-distributions/ed_distributions-0.1.tar.gz/ed_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ed_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ed_distributions'],
+      author='Edie738',
+      author_email='dredielam@gmail.com',
+      zip_safe=False)

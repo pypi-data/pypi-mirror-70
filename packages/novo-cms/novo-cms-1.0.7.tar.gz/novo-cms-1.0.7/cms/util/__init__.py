606eda8aabe7be693ec14e1015656c0b69035fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:46 +0000

    Add package novo-cms/1.0.7/novo-cms-1.0.7.tar.gz

---
+++ packages/novo-cms/novo-cms-1.0.7.tar.gz/novo-cms-1.0.7/cms/util/__init__.py
@@ -0,0 +1,71 @@
+#-*- encoding: utf8 -*-
+import os
+import logging
+import getpass
+
+try:
+    from ConfigParser import ConfigParser
+except ImportError:
+    from configparser import ConfigParser
+
+
+logging.getLogger("requests.packages.urllib3").propagate = 0
+
+
+def get_logger(name=None, verbose=False):
+    logging.basicConfig(
+        format='[%(asctime)s %(funcName)s %(levelname)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.INFO)
+    logger = logging.getLogger(name)
+
+    if verbose:
+        logger.level = logging.DEBUG
+
+    return logger
+
+
+class User(object):
+    def __init__(self, username=None, password=None, configfile=None, **kwargs):
+        self.username = username
+        self.password = password
+        self.configfile = configfile
+        self.conf = ConfigParser()
+
+    def get_user_pass(self):
+        if self.username and self.password:
+            username, password = self.username, self.password
+        elif self.configfile and os.path.exists(self.configfile):
+            username, password = self.read()
+        else:
+            username, password = None, None
+
+        if not all([username, password]):
+            print('Please input your username and password')
+            username = raw_input('> username: ')
+            password = getpass.getpass('> password: ')
+
+        return username, password
+
+    def read(self):
+        self.conf.read(self.configfile)
+        password = None
+
+        if self.conf.has_section(self.username):
+            username = self.username
+            password = self.conf.get(self.username, 'password')
+        elif len(self.conf.sections()) == 1:
+            username = self.conf.sections()[0]
+            password = self.conf.get(username, 'password')
+
+        return username, password
+
+    def save(self, username, password):
+        if not self.conf.has_section(username):
+            self.conf.add_section(username)
+        self.conf.set(username, 'password', password)
+
+        with open(self.configfile, 'w') as out:
+            self.conf.write(out)
+
+
---
+++ packages/novo-cms/novo-cms-1.0.7.tar.gz/novo-cms-1.0.7/cms/util/lims/lims.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- encoding: utf8 -*-
+
+class LIMS(object):
+    base_url = 'http://lims.novogene.com/STARLIMS11.novogene'
+    def __init__(self):
+        pass
+
+    def login(self, username, password):
+        pass
+
+    def search_project(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()

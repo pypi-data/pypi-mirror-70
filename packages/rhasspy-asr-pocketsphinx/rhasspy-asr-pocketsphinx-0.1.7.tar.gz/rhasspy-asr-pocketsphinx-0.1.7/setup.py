7a2ea130e9a57d9d74cec8e78d98391dde23e838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:36 +0000

    Add package rhasspy-asr-pocketsphinx/0.1.7/rhasspy-asr-pocketsphinx-0.1.7.tar.gz

---
+++ packages/rhasspy-asr-pocketsphinx/rhasspy-asr-pocketsphinx-0.1.7.tar.gz/rhasspy-asr-pocketsphinx-0.1.7/setup.py
@@ -0,0 +1,40 @@
+"""Setup for rhasspyasr_pocketsphinx"""
+import os
+
+import setuptools
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, "README.md"), "r") as readme_file:
+    long_description = readme_file.read()
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
+
+
+setuptools.setup(
+    name="rhasspy-asr-pocketsphinx",
+    version=version,
+    author="Michael Hansen",
+    author_email="hansen.mike@gmail.com",
+    url="https://github.com/rhasspy/rhasspy-asr-pocketsphinx",
+    packages=setuptools.find_packages(),
+    package_data={"rhasspyasr_pocketsphinx": ["py.typed"]},
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "rhasspy-asr-pocketsphinx-hermes = rhasspyasr_pocketsphinx_hermes.__main__:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+)

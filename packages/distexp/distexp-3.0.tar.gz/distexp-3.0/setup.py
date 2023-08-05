95ed9642871a0c8a86eed06c60e0f513b6755ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package distexp/3.0/distexp-3.0.tar.gz

---
+++ packages/distexp/distexp-3.0.tar.gz/distexp-3.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distexp',
+      version='3.0',
+      description='Gaussian and Binomial distributions',
+      author = 'Ali Rahman',
+      packages=['distributions_test'],
+      author_email='alirman02@gmail.com',
+      zip_safe=False)

3b0b6de576da9601df5b810875e6302691f4d79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package jsonschema-cli/0.6.2/jsonschema_cli-0.6.2-py3-none-any.whl

---
+++ packages/jsonschema-cli/jsonschema_cli-0.6.2-py3-none-any.whl/jsonschema_cli/resolvers.py
@@ -0,0 +1,9 @@
+from jsonschema_cli.handlers import handle_file_uri
+from jsonschema import RefResolver
+import os
+
+
+def relative_path_resolver(schema, base_path=os.getcwd()):
+    handlers = {"": handle_file_uri(base_path)}
+    return RefResolver.from_schema(schema, handlers=handlers)
+

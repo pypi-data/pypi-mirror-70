ea378c9c4d34f9c79f57b942d8efaf14cbe46232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:45 +0000

    Add package flyvar/1.0.4/flyvar-1.0.4.tar.gz

---
+++ packages/flyvar/flyvar-1.0.4.tar.gz/flyvar-1.0.4/setup.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup  
+
+setup(
+        name='flyvar',     # 包名字
+        version='1.0.4',   # 包版本
+        description='A database that is convenient for version iteration',   # 简单描述
+        keywords='A database that is convenient for version iteration',
+        author='snbckcode',  # 作者
+        author_email='snbckcode@gmail.com',  # 作者邮箱
+        packages=['flyvar'],                 # 包
+        license='Mozilla',
+        url='https://github.com/snbck/flyvar',
+
+)

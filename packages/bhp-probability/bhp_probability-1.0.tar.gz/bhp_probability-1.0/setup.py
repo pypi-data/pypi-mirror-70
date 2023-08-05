ce25e72ba0c64f605562f9525b313a8e03981338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package bhp-probability/1.0/bhp_probability-1.0.tar.gz

---
+++ packages/bhp-probability/bhp_probability-1.0.tar.gz/bhp_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bhp_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bhp_probability'],
+      author='Bach Pham',
+      author_email='phuybach@gmail.com',
+      zip_safe=False)

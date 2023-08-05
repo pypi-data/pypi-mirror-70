d851811d17ab491826b7e077d423f860a07a3f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:51 +0000

    Add package pyDH/0.1.4/pyDH-0.1.4.tar.gz

---
+++ packages/pyDH/pyDH-0.1.4.tar.gz/pyDH-0.1.4/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup
+
+setup(
+  name = 'pyDH',
+  packages = ['pyDH'],
+  version = '0.1.4',
+  license = 'Apache',
+  description = 'Pure Python Implementation of Diffie-Hellman Key Exchange',
+  author = 'Amirali Sanatinia',
+  author_email = 'amirali@ccs.neu.edu',
+  url = 'https://github.com/amiralis/pyDH',
+  keywords = ['crypto', 'Diffie Hellman', 'Key Exchange'],
+  classifiers = [
+    	'License :: OSI Approved :: Apache Software License',
+    	'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+		'Topic :: Security :: Cryptography',
+		],
+)

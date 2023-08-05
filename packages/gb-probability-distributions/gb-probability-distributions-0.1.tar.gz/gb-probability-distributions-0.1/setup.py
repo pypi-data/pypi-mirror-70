e592545508348120cd10fb100c67ac0c386ae2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package gb-probability-distributions/0.1/gb-probability-distributions-0.1.tar.gz

---
+++ packages/gb-probability-distributions/gb-probability-distributions-0.1.tar.gz/gb-probability-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gb-probability-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gb-probability-distributions'],
+      zip_safe=False)

cebb20e2b4c47c8517459472b827c46a11e38077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:25 +0000

    Add package torchattacks/1.5/torchattacks-1.5-py3-none-any.whl

---
+++ packages/torchattacks/torchattacks-1.5-py3-none-any.whl/torchattacks/__init__.py
@@ -0,0 +1,11 @@
+from .attacks.fgsm import FGSM
+from .attacks.bim import BIM
+from .attacks.stepll import StepLL
+from .attacks.rfgsm import RFGSM 
+from .attacks.cw import CW
+from .attacks.pgd import PGD
+from .attacks.apgd import APGD
+from .attacks.deepfool import DeepFool
+from .attacks.multiattack import MultiAttack
+
+__version__ = 1.5

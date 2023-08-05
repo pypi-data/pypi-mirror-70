3d143fef313b4401fea3f7b767784569c673476f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package gitmantool/0.3/gitmantool-0.3.tar.gz

---
+++ packages/gitmantool/gitmantool-0.3.tar.gz/gitmantool-0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from gitmanager.utils import get_version
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gitmantool",
+    version=str(get_version()),
+    author='katmai',
+    author_email='katmai.mobil@gmail.com',
+    description='Console tool for manage projects on github/gitlab',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/katmai1/gitmanager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'gm=gitmanager:main',
+        ],
+    },
+    install_requires=[
+        'docopt', 'pygithub', 'gitlab', 'pyyaml', 'colorama'
+    ],
+)

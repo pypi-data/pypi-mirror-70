7f7eafeca80c03eb977df9b50188e851c41ad84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:11 +0000

    Add package openpifpaf/0.11.3/openpifpaf-0.11.3.tar.gz

---
+++ packages/openpifpaf/openpifpaf-0.11.3.tar.gz/openpifpaf-0.11.3/openpifpaf/__init__.py
@@ -0,0 +1,9 @@
+"""An open implementation of PifPaf."""
+
+__version__ = '0.11.3'
+
+from .annotation import Annotation, AnnotationDet
+from . import datasets
+from . import decoder
+from . import network
+from . import optimize

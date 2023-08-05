c6cde75f34236688fca70ad0ce10d3fdb02637ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:27 +0000

    Add package catacomb-ai/0.0.11/catacomb_ai-0.0.11-py3-none-any.whl

---
+++ packages/catacomb-ai/catacomb_ai-0.0.11-py3-none-any.whl/catacomb/systems.py
@@ -0,0 +1,7 @@
+class System():
+    def output(self, input_object):
+        return input_object
+
+    def format(self, input_type, output_type):
+        self.input_type = input_type
+        self.output_type = output_type

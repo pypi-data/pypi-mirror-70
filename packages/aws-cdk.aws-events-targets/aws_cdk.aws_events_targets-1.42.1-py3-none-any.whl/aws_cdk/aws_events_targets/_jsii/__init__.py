dd8c21969f026d67cb598b79bdcea60613dfa6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package aws-cdk.aws-events-targets/1.42.1/aws_cdk.aws_events_targets-1.42.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-events-targets/aws_cdk.aws_events_targets-1.42.1-py3-none-any.whl/aws_cdk/aws_events_targets/_jsii/__init__.py
@@ -0,0 +1,32 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_batch
+import aws_cdk.aws_codebuild
+import aws_cdk.aws_codepipeline
+import aws_cdk.aws_ec2
+import aws_cdk.aws_ecs
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_lambda
+import aws_cdk.aws_sns
+import aws_cdk.aws_sns_subscriptions
+import aws_cdk.aws_sqs
+import aws_cdk.aws_stepfunctions
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-events-targets", "1.42.1", __name__[0:-6], "aws-events-targets@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

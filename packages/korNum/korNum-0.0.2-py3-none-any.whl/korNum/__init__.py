3a1e0ef3b148cf7c4ce56cf75ed36f8e49cc1994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:40 +0000

    Add package korNum/0.0.2/korNum-0.0.2-py3-none-any.whl

---
+++ packages/korNum/korNum-0.0.2-py3-none-any.whl/korNum/__init__.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__name__ = 'korNum'
+__description__ = 'change Korean tex number <---> Arabian number'
+__version__ = '0.0.2'
+__url__ = 'https://github.com/study-ai-data/korNum'
+__download_url__ = 'https://github.com/study-ai-data/korNum'
+__install_requires__ = [
+],
+__author__ = 'Kyeongnam Kim',
+__author_email__ = 'devokkn@gmail.com'
+__license__ = 'MIT'
+
+from korNum.chgList import *
+from korNum.chgFormat import *

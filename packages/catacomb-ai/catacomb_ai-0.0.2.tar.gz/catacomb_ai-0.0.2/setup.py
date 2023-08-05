01249300c7312d58b52c8daa089bbc3bda5d49eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:18 +0000

    Add package catacomb-ai/0.0.2/catacomb_ai-0.0.2.tar.gz

---
+++ packages/catacomb-ai/catacomb_ai-0.0.2.tar.gz/catacomb_ai-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='catacomb_ai',
+    version='0.0.2',
+    description='Build tools for Catacomb model hosting.',
+    py_modules=['catacomb'],
+    package_dir={'': 'src'}   
+)

8059b94cba91c7b69a00dc8228a3c0409f2a5fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:24 +0000

    Add package geopack/1.0.8/geopack-1.0.8.tar.gz

---
+++ packages/geopack/geopack-1.0.8.tar.gz/geopack-1.0.8/setup.py
@@ -0,0 +1,30 @@
+# python geopack  setup.py
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='geopack',
+    version='1.0.8',
+    author='Sheng Tian',
+    author_email='tianx138@umn.edu',
+    description='Python version of geopack and Tsyganenko models, compatible with geopack05 and geopack08',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url= 'https://github.com/tsssss/geopack',
+    requires= ['numpy','scipy'],
+    platforms= ['Mac OS'],
+    license= 'MIT',
+    keywords= ['geopack','space physics','Tsyganenko model'],
+    packages= setuptools.find_packages(),
+    package_data={'':['*.txt','*.md']},
+    classifiers= [
+        'Programming Language :: Python :: 3',
+        'Operating System :: MacOS',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+)

77d3356438c1afcd6133b707a5fc2a04ccfe7671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:59 +0000

    Add package horsebot/0.0.1/horsebot-0.0.1.tar.gz

---
+++ packages/horsebot/horsebot-0.0.1.tar.gz/horsebot-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='horsebot',
+    version='0.0.1',
+    author="Koen Woortman",
+    author_email="koensw@outlook.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Environment :: Console',
+    ],
+)

8b2875e3dea0c5790455e34de3988cb19396149f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:08 +0000

    Add package kolaBitMEXBot/1.1.8/kolaBitMEXBot-1.1.8.tar.gz

---
+++ packages/kolaBitMEXBot/kolaBitMEXBot-1.1.8.tar.gz/kolaBitMEXBot-1.1.8/kolaBitMEXBot/kola/utils/constantes.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Constantes"""
+
+# some default cols
+EXECOLS = [
+    "orderID",
+    "clOrdID",
+    "side",
+    "orderQty",
+    "price",
+    "stopPx",
+    "execType",
+    "ordType",
+    "execInst",
+    "ordStatus",
+    "triggered",
+    "transactTime",
+]
+
+EXECOLS_L = EXECOLS + ["lastQty", "lastPx", "lastMkt", "commission"]
+
+# to get price in bargain.price
+SETTLEMENTPRICES = {"XBTUSD": ".BXBT", "ADAM20": ".BADAXBT30M"}
+
+# used to round price before passing orders
+PRICE_PRECISION = {"XBTUSD": 0.5, "ADAM20": 1e-8}

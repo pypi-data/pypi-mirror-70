2a4c988d847f67a3165d7aa419b6e7fa9c75c13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:03 +0000

    Add package testflows.core/1.5.200604.1230636/testflows.core-1.5.200604.1230636.tar.gz

---
+++ packages/testflows.core/testflows.core-1.5.200604.1230636.tar.gz/testflows.core-1.5.200604.1230636/testflows/core/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2019 Katteli Inc.
+# TestFlows Test Framework (http://testflows.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from testflows._core.test import TestBase
+from testflows._core.test import Module, Suite, Test, Step, NullStep, run
+from testflows._core.test import TestStep, TestCase, TestSuite, TestModule
+from testflows._core.test import Attributes, Requirements, Examples
+from testflows._core.test import Name, Description, Uid, Tags, TestClass, Context
+from testflows._core.test import Feature, Cleanup, Background, Scenario
+from testflows._core.test import Given, When, Then, And, But, By, Finally
+from testflows._core.test import Steps
+from testflows._core.test import TestFeature, TestScenario
+from testflows._core.funcs import *
+from testflows._core.filters import the
+from testflows._core.objects import *
+from testflows._core.name import *
+from testflows._core.flags import *
+from testflows._core import __author__, __version__, __license__
+from testflows._core import threading
+
+import testflows._core.utils as utils

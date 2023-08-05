1a7daa09100a9adc9342cd138b7bcf0a14bc9540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:49 +0000

    Add package diff-hilite/0.0/diff-hilite-0.0.tar.gz

---
+++ packages/diff-hilite/diff-hilite-0.0.tar.gz/diff-hilite-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='diff-hilite',
+      version='0.0',
+      description="Diff wrapper/filter for intra-line changes highlighting.",
+      url='https://github.com/pfalcon/diff-hilite',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['diff_hilite'])

c7757cc7ae94b326171aba7a06c14ed428b9af85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:33 +0000

    Add package hekate/0.0.1/hekate-0.0.1.tar.gz

---
+++ packages/hekate/hekate-0.0.1.tar.gz/hekate-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hekate',
+      version='0.0.1',
+      description='Dummy package for hekate package name usage',
+      url='',
+      author='mtaifi',
+      author_email='moutai10@gmail.com',
+      license='BSD',
+      packages=['hekate'],
+      zip_safe=False)

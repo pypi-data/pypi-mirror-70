8d3fcbbfddff76037c1103b4f906ffebbc04e083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:04 +0000

    Add package freshlybuiltimagebol/0.0.1a/freshlybuiltimagebol-0.0.1a.tar.gz

---
+++ packages/freshlybuiltimagebol/freshlybuiltimagebol-0.0.1a.tar.gz/freshlybuiltimagebol-0.0.1a/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'freshlybuiltimagebol',         
+  packages = ['freshlybuiltimagebol'],   
+  version = '0.0.1a',     
+  license='MIT',        
+  description = 'Image Bhi Bol Uthegi',   
+  author = 'Vishal Sharma',                   
+  author_email = 'vishalsharma.gbpecdelhi@gmail.com',      
+  url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol',   
+  download_url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol/archive/v0.0.1a.tar.gz',  
+  keywords = ['Image', 'Audio', 'Text'],   
+  install_requires=[            
+          'hyper',
+          'googletrans',
+          'gTTS',
+          'Pillow',
+          'pytesseract'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

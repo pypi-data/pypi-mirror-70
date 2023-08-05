ae4e53e1b09f502a2130095015c705de12c599e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:29 +0000

    Add package teim-distributions/0.1/teim_distributions-0.1.tar.gz

---
+++ packages/teim-distributions/teim_distributions-0.1.tar.gz/teim_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='teim_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['teim_distributions'],
+      author = 'Nick Teim',
+      auther_email = 'NickTeim@something.com',
+      zip_safe=False)

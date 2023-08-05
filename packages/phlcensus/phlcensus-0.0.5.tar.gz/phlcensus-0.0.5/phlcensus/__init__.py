7f60b2d7e5c8262a36f5d3d0ac89d169a3f8ea11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package phlcensus/0.0.5/phlcensus-0.0.5.tar.gz

---
+++ packages/phlcensus/phlcensus-0.0.5.tar.gz/phlcensus-0.0.5/phlcensus/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.0.5"
+
+from pathlib import Path
+
+data_dir = Path(__file__).parent / "data"
+
+EPSG = 2272
+
+from .core import DATASETS
+from . import acs
+from . import economic
+from . import external
+from .regions import *
+
+
+__all__ = sorted(DATASETS)
+
+
+def available_datasets():
+    """
+    Return a list of the names of the available datasets.
+    """
+    return [DATASETS[cls] for cls in sorted(DATASETS)]

9d6eea7e1b986dd2ff5759c938251e67acd4a1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package crdatamgt/2.0.10/crdatamgt-2.0.10.tar.gz

---
+++ packages/crdatamgt/crdatamgt-2.0.10.tar.gz/crdatamgt-2.0.10/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crdatamgt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.3.1,<6.0.0',
+ 'openpyxl>=3.0.3,<4.0.0',
+ 'pandas>=1.0.3,<2.0.0',
+ 'simplelogging>=0.10.0,<0.11.0',
+ 'toolz>=0.10.0,<0.11.0',
+ 'xlsxwriter>=1.2.8,<2.0.0']
+
+setup_kwargs = {
+    'name': 'crdatamgt',
+    'version': '2.0.10',
+    'description': 'Chemistry research data management application',
+    'long_description': None,
+    'author': 'Michael Jaquier',
+    'author_email': 'michael.jaquier@contracted.pmi.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

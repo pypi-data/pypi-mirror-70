450f01e750fabc87c5bfb6aeae83324de5e5a63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package platform-agent/0.0.25/platform-agent-0.0.25.tar.gz

---
+++ packages/platform-agent/platform-agent-0.0.25.tar.gz/platform-agent-0.0.25/platform_agent/lib/get_info.py
@@ -0,0 +1,39 @@
+import os
+import logging
+import requests
+
+import docker
+from platform_agent.docker_api.helpers import format_networks_result
+
+logger = logging.getLogger()
+
+
+def get_ip_addr():
+    resp = requests.get("https://ip.noia.network/")
+    return {
+        "external_ip": resp.json()
+    }
+
+
+def get_location():
+    return {
+        "location_lat": os.environ.get('NOIA_LAT'),
+        "location_lon": os.environ.get('NOIA_LON'),
+        "location_city": os.environ.get('NOIA_CITY')
+    }
+
+
+def get_network_info():
+    docker_client = docker.from_env()
+    networks = docker_client.networks()
+    network_info = format_networks_result(networks)
+    return {
+        "network_info": network_info
+    }
+
+
+def gather_initial_info():
+    result = {}
+    result.update(get_ip_addr())
+    result.update(get_network_info())
+    return result

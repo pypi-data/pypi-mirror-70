8660567f2d7210e3cd50ae9d4f0619192e0da382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package AS-Object-models/0.6.4/AS_Object_models-0.6.4-py3-none-any.whl

---
+++ packages/AS-Object-models/AS_Object_models-0.6.4-py3-none-any.whl/as_models/rest/sales_inv.py
@@ -0,0 +1,36 @@
+from .. import GrowWeek
+from .. import ItemReserve
+
+
+class SalesInventoryAPI(object):
+
+    def __init__(self, logger):
+        self.logger = logger
+
+    def createReserveAPI(self,reserveDate, customer_id, location_id, product_id, reserved):
+        gw = GrowWeek.GetGrowWeekByDate(reserveDate)
+        newReserve = gw.create_reserve(customer_id,location_id, product_id,reserved,reserveDate)
+        return newReserve.get_dict()
+
+    def updateReserveAPI(self,reserve_id, customer_id, location_id, product_id, reserved, reserveDate):
+        gw = GrowWeek.GetGrowWeekByDate(reserveDate)
+        updReserve = gw.update_reserve(reserve_id, customer_id, location_id, product_id, reserved, reserveDate)
+        return updReserve.get_dict()
+
+    def deleteReserveAPI(self,reserve_id):
+        ir = ItemReserve.getItemReserveInstance(reserve_id)
+        return ir.delete_resp()
+
+    def getAllReservesAPI(self,reserveDate):
+        gw = GrowWeek.GetGrowWeekByDate(reserveDate)
+        irList = gw.reserves
+        return [resv.get_dict() for resv in irList]
+
+    def getSummReservesAPI(self,reserveDate):
+        gw = GrowWeek.GetGrowWeekByDate(reserveDate)
+        irList = gw.getSummary().summary
+        return irList
+
+    def getReserveAPI(self,reserve_id):
+        ir = ItemReserve.getItemReserveInstance(reserve_id)
+        return ir.get_dict()

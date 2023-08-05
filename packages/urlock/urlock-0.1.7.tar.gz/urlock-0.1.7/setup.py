4f4b010a9a4f125decbdb5ce98bc2f0b6bc17c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:05 +0000

    Add package urlock/0.1.7/urlock-0.1.7.tar.gz

---
+++ packages/urlock/urlock-0.1.7.tar.gz/urlock-0.1.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="urlock", # Replace with your own username
+    version="0.1.7",
+    author='David Kerschner',
+    author_email='dkerschner@gmail.com',
+    description="Library for talking to a running Urbit ship over http",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/baudtack/urlock',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

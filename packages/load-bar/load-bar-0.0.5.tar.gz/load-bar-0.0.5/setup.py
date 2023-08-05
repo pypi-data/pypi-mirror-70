17d41df70918cec1a73dee1f762811039ed0fc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:36 +0000

    Add package load-bar/0.0.5/load-bar-0.0.5.tar.gz

---
+++ packages/load-bar/load-bar-0.0.5.tar.gz/load-bar-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="load-bar",
+    version="0.0.5",
+    author="Valentin Vignal",
+    license='MIT',
+    author_email="valentin.vignal.dev@outlook.fr",
+    description="Python Librairy for a loading bar in the console",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ValentinVignal/LoadBar",
+    packages=setuptools.find_packages(exclude=['trash', 'tests', 'help', 'doc']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

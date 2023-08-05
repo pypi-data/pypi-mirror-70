ff459ad22b3b794b7340043162a5bba78dd83f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package both-probability/1.1/both_probability-1.1.tar.gz

---
+++ packages/both-probability/both_probability-1.1.tar.gz/both_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='both_probability',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['both_probability'],
+      author='Ishita Arora',
+      author_email='ishita.arora28@gmail.com',
+      zip_safe=False)

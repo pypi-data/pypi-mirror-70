19b7bee312d9f4e4052b8e6c1a350348cd3770fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package Shenma/0.0.3/Shenma-0.0.3.tar.gz

---
+++ packages/Shenma/Shenma-0.0.3.tar.gz/Shenma-0.0.3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Shenma',
+    version='0.0.3',
+    description=(
+        'A custom library for teaching, maintained by CherryXuan'
+    ),
+    long_description=open('README.md', encoding="UTF-8").read(),
+    long_description_content_type="text/markdown",
+    author='CherryXuan',
+    author_email='shenzexuan1994@foxmail.com',
+    maintainer='CherryXuan',
+    maintainer_email='shenzexuan1994@foxmail.com',
+    license='MIT Licence',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://gitee.com/cherryxuan/SpeechRecognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'numpy>=1.14.0',
+        'matplotlib>=2.1.2',
+        'PyAudio>=0.2.11',
+    ],
+)

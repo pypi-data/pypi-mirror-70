d0b74b231bb44bcae4f30e76469d94f6b4ebd25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:36 +0000

    Add package robotframework-csvlib/1.0.1/robotframework_csvlib-1.0.1-py3-none-any.whl

---
+++ packages/robotframework-csvlib/robotframework_csvlib-1.0.1-py3-none-any.whl/CSVLib/__init__.py
@@ -0,0 +1,43 @@
+import csv
+
+
+class CSVLib(object):
+
+    """ Reads a given CSV file and returns it as a dictionary. """
+    def read_csv_as_dictionary(self, filename, key_column, value_columns, delimiter='\n'):
+        file = open(filename, 'r', encoding='utf-8-sig')
+        csvfile = csv.DictReader(file, delimiter=delimiter)
+        output = {}
+        for row in csvfile:
+            if type(value_columns) == str:
+                output[row[key_column]] = row[value_columns]
+            elif type(value_columns) == list:
+                valueList = []
+                for value in value_columns:
+                    valueList.append(row[value])
+                output[row[key_column]] = valueList
+
+        file.close()
+        return output
+
+    """ Reads a given CSV file and returns it as a list containing all rows as list. """
+    def read_csv_as_list(self, filename, delimiter='\n'):
+        file = open(filename, 'r', encoding='utf-8-sig')
+        csvfile = csv.reader(file, delimiter=delimiter)
+        output = []
+        for row in csvfile:
+            row = row[0].split(',')
+            output.append(row)
+        file.close()
+        return output
+
+    """ Reads a given CSV file and returns it as a single list containing all values. """
+    def read_csv_as_single_list(self, filename, delimiter='\n'):
+        file = open(filename, 'r', encoding='utf-8-sig')
+        csvfile = csv.reader(file, delimiter=delimiter)
+        output = []
+        for row in csvfile:
+            for value in row:
+                output.append(value)
+        file.close()
+        return output

638caa60d191a3558bb01040c14fe2b78613b9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package GEMLibrary-linux/0.3.0/GEMLibrary-linux-0.3.0.tar.gz

---
+++ packages/GEMLibrary-linux/GEMLibrary-linux-0.3.0.tar.gz/GEMLibrary-linux-0.3.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GEMLibrary-linux",
+    version="0.3.0",
+    include_package_data=True,
+    author="Kevin Morgan",
+    description="Derivatives Library for Teleios",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: Other/Proprietary License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.7",
+)

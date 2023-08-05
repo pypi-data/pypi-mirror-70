84c2274543a05be1e690d53d12c915847e9e562b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package blessing-distributions/0.1/blessing_distributions-0.1.tar.gz

---
+++ packages/blessing-distributions/blessing_distributions-0.1.tar.gz/blessing_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='blessing_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['blessing_distributions'],
+      author = 'Guembe Blessing',
+      author_email = 'blessingodede@gmail.com',
+      zip_safe=False)
+
+

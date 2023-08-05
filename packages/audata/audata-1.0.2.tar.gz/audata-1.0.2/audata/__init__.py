b2ec18ec977bc1dbd3174102ef71e5926cf308fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:05 +0000

    Add package audata/1.0.2/audata-1.0.2.tar.gz

---
+++ packages/audata/audata-1.0.2.tar.gz/audata-1.0.2/audata/__init__.py
@@ -0,0 +1,17 @@
+"""Define version and import high-level objects."""
+__VERSION_MAJOR__ = '1'
+__VERSION_MINOR__ = '0'
+__VERSION_BUILD__ = '2'
+
+__DEBUG = False
+if __DEBUG:
+    import datetime as dt
+    __VERSION_TAGS__ = f"d{dt.datetime.now().strftime('%Y%m%d_%H%M')}"
+else:
+    __VERSION_TAGS__ = ''
+
+__VERSION__ = f'{__VERSION_MAJOR__}.{__VERSION_MINOR__}.{__VERSION_BUILD__}{__VERSION_TAGS__}'
+
+__DATA_VERSION__ = 1
+
+from audata.file import File
---
+++ packages/audata/audata-1.0.2.tar.gz/audata-1.0.2/setup.py
@@ -0,0 +1,32 @@
+"""audata setup."""
+import os
+from setuptools import setup, find_packages
+
+import numpy as np
+
+from audata import __VERSION__
+
+NAME = 'audata'
+VERSION = __VERSION__
+
+
+def read(filename):
+    """Readlines of filename relative to setup file path."""
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='A python package for reading and writing data for AUView.',
+    long_description=read('readme.md'),
+    author='Anthony Wertz',
+    author_email='awertz@cmu.edu',
+    license='GNU LGPL 3',
+    entry_points={'console_scripts': ['csv2audata=audata.bin.csv2audata:main']},
+    install_requires=[
+        'numpy', 'pandas', 'h5py', 'jsbeautifier', 'lorem', 'tzlocal', 'h5py'
+    ],
+    python_requires='>=3.8',
+    include_dirs=[np.get_include()],
+    packages=find_packages())

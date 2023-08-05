ecbaf3e4897037852e7a8bfa1db897c209711dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:33 +0000

    Add package librair/2020.6.3/librair-2020.6.3.tar.gz

---
+++ packages/librair/librair-2020.6.3.tar.gz/librair-2020.6.3/librair/services/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import cerl
+from . import viaf
+from . import gnd
+from . import zdb
+from . import dnb
+from . import entityfacts
+from . import oclc
+from . import seealso
+from . import kalliope

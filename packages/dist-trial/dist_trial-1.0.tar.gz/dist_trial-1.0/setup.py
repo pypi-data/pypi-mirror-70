ef91da4aabdc56b41615e5effe2f63403c9ce2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:10 +0000

    Add package dist-trial/1.0/dist_trial-1.0.tar.gz

---
+++ packages/dist-trial/dist_trial-1.0.tar.gz/dist_trial-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_trial',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      author = 'Ilham Naharudinsyah',
+      packages=['dist_trial'],
+      author_email='ilham.naharudinsyah@gmail.com',
+      zip_safe=False)

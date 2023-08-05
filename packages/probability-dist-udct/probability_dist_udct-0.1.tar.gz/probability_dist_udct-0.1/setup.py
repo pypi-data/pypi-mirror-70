e9c53b70064298e9b6cae11d77a584eafff28bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:16 +0000

    Add package probability-dist-udct/0.1/probability_dist_udct-0.1.tar.gz

---
+++ packages/probability-dist-udct/probability_dist_udct-0.1.tar.gz/probability_dist_udct-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_dist_udct',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['probability_dist_udct'],
+      zip_safe=False)

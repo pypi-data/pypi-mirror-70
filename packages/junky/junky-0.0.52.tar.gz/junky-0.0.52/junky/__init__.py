d14cd71c4632871057851e9ca9ed2a17837964bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package junky/0.0.52/junky-0.0.52.tar.gz

---
+++ packages/junky/junky-0.0.52.tar.gz/junky-0.0.52/junky/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# junky lib
+#
+# Copyright (C) 2020-present by Sergei Ternovykh, Anastasiya Nikiforova
+# License: BSD, see LICENSE for details
+"""
+Provides a bunch of tools and utilities to use with PyTorch.
+"""
+from junky._version import __version__
+from junky.autotrain import *
+from junky.layers import *
+from junky.plotter import *
+from junky.supplements import *
+from junky.utils import *

db31c1813a3bf4c8010efa06d2add964156a2481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package cvectors/0.0.1/cvectors-0.0.1.tar.gz

---
+++ packages/cvectors/cvectors-0.0.1.tar.gz/cvectors-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="cvectors",
+    version="0.0.1",
+    author="Tom Fryers",
+    description="A simple complex vector package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

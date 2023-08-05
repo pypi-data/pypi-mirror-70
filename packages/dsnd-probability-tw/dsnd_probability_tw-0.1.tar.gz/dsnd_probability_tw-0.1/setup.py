df579e9737e316a178da4261dfeaa96f3455365e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:54 +0000

    Add package dsnd-probability-tw/0.1/dsnd_probability_tw-0.1.tar.gz

---
+++ packages/dsnd-probability-tw/dsnd_probability_tw-0.1.tar.gz/dsnd_probability_tw-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_tw',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_tw'],
+      author = 'Tanise Williams',
+      author_email = 'tanise.williams@icloud.com',
+      zip_safe=False)

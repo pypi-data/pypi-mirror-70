558001ad8143e01d7bed0dccafd8af8d115a78ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package emqx-extension-sdk/0.2/emqx-extension-sdk-0.2.tar.gz

---
+++ packages/emqx-extension-sdk/emqx-extension-sdk-0.2.tar.gz/emqx-extension-sdk-0.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+INSTALL_REQUIRES = []
+
+setup(
+    name='emqx-extension-sdk',
+    version='0.2',
+    author='EMQ X',
+    author_email='taodk@emqx.io',
+    url='https://github.com/emqx/emqx-python-sdk',
+    description='The Python SDK For EMQ X Extension',
+    long_description=open("README.md").read(),
+    platforms=['any'],
+    packages=[
+        "emqx_extension"
+    ],
+    license='Apache License 2.0',
+    install_requires=INSTALL_REQUIRES,
+    python_requires='>=3.6.0',
+    long_description_content_type='text/markdown',
+    keywords=[
+        'emqx',
+        'IoT',
+        'MQTT',
+        'MQTT SDK',
+        'emqx-extension'
+    ]
+)

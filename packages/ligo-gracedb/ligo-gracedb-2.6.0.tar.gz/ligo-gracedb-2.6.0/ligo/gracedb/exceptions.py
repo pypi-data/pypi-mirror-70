e97347379893943fcc94b5502d1db85be525e3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:38 +0000

    Add package ligo-gracedb/2.6.0/ligo-gracedb-2.6.0.tar.gz

---
+++ packages/ligo-gracedb/ligo-gracedb-2.6.0.tar.gz/ligo-gracedb-2.6.0/ligo/gracedb/exceptions.py
@@ -0,0 +1,9 @@
+# Custom exceptions
+
+
+class HTTPError(Exception):
+    def __init__(self, status, reason, message):
+        self.status = status
+        self.reason = reason
+        self.message = message
+        Exception.__init__(self, status, reason, message)

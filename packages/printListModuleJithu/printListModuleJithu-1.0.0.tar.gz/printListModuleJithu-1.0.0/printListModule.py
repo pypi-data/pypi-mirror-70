7443a76822680afa60d0f1c0ad2c97435b3ac473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package printListModuleJithu/1.0.0/printListModuleJithu-1.0.0.tar.gz

---
+++ packages/printListModuleJithu/printListModuleJithu-1.0.0.tar.gz/printListModuleJithu-1.0.0/printListModule.py
@@ -0,0 +1,9 @@
+
+
+
+def rec_print_list(lst):
+    for i in lst:
+        if(isinstance(i,list)):
+            rec_print_list(i)
+        else:
+            print(i)
---
+++ packages/printListModuleJithu/printListModuleJithu-1.0.0.tar.gz/printListModuleJithu-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name = "printListModuleJithu", version = "1.0.0", description = "an API to \
+        print list recursilvely", author = "Jithu", author_email = "jithu.ranjith@ignitarium.com", py_modules = ["printListModule"])

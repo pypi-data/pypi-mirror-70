8394d580bcadb48cfbb2222ff11ffba35ce1b976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package distributions-try/1.0/distributions_try-1.0.tar.gz

---
+++ packages/distributions-try/distributions_try-1.0.tar.gz/distributions_try-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_try',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      author = 'Ali Rahman',
+      packages=['distributions_try'],
+      author_email='alirman02@gmail.com',
+      zip_safe=False)

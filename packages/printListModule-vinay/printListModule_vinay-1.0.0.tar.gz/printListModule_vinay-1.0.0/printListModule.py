54b4d840688e23a6fa22c9686311a2eb0412b8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:49 +0000

    Add package printListModule-vinay/1.0.0/printListModule_vinay-1.0.0.tar.gz

---
+++ packages/printListModule-vinay/printListModule_vinay-1.0.0.tar.gz/printListModule_vinay-1.0.0/printListModule.py
@@ -0,0 +1,6 @@
+def rec_print_list(lst):
+    for i in lst:
+        if(isinstance(i,list)):
+            rec_print_list(i)
+        else:
+            print(i)
---
+++ packages/printListModule-vinay/printListModule_vinay-1.0.0.tar.gz/printListModule_vinay-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name="printListModule_vinay",version="1.0.0",description="an API for recursive print",author="Vinay",author_email="vinay.kulkarni@ignitarium.com",\
+      py_modules=["printListModule"])

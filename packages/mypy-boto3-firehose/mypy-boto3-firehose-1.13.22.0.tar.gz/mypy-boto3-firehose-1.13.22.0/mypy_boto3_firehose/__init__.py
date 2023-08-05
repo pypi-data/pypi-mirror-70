9a790f8c75ecffd8ae08f54ece84a54dd345cadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:30 +0000

    Add package mypy-boto3-firehose/1.13.22.0/mypy-boto3-firehose-1.13.22.0.tar.gz

---
+++ packages/mypy-boto3-firehose/mypy-boto3-firehose-1.13.22.0.tar.gz/mypy-boto3-firehose-1.13.22.0/mypy_boto3_firehose/__init__.py
@@ -0,0 +1,20 @@
+"""
+Main interface for firehose service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.firehose import (
+        Client,
+        FirehoseClient,
+        )
+
+    session = boto3.Session()
+
+    client: FirehoseClient = boto3.client("firehose")
+    session_client: FirehoseClient = session.client("firehose")
+"""
+from mypy_boto3_firehose.client import FirehoseClient, FirehoseClient as Client
+
+
+__all__ = ("Client", "FirehoseClient")

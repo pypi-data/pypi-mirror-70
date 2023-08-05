8231823043bf327cf62efd39d8832c9cea0f7cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:45 +0000

    Add package printListModuleKhalid/0.0.1/printListModuleKhalid-0.0.1.tar.gz

---
+++ packages/printListModuleKhalid/printListModuleKhalid-0.0.1.tar.gz/printListModuleKhalid-0.0.1/setup.py
@@ -0,0 +1,5 @@
+#pkg the module
+
+from distutils.core import setup
+# from setuptools import setup  # This can also be installed
+setup(name = 'printListModuleKhalid', version = '0.0.1', description = 'an API to print a list recursively', author = 'Ibinas', author_email = 'ibinasch@gmail.com', py_modules = ['src/printListModule'])

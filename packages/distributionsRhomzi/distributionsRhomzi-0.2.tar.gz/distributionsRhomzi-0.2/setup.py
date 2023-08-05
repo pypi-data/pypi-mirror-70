bd22230ef41252169a6946b9584f3b27c07d17b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:56 +0000

    Add package distributionsRhomzi/0.2/distributionsRhomzi-0.2.tar.gz

---
+++ packages/distributionsRhomzi/distributionsRhomzi-0.2.tar.gz/distributionsRhomzi-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsRhomzi',
+      version='0.2',
+      description='Gaussian & Binomial distributions',
+      packages=['distributionsRhomzi'],
+      author = 'Eromonsele O',
+      zip_safe=False)

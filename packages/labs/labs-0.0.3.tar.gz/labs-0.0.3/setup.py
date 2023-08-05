5449fb2910de8f686ffbe5d12d2d389e43b5a085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:39 +0000

    Add package labs/0.0.3/labs-0.0.3.tar.gz

---
+++ packages/labs/labs-0.0.3.tar.gz/labs-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import labs
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="labs",
+    version=labs.__version__,
+    author="Eran Brill",
+    author_email="brillonedev@gmail.com",
+    description="labs - a dask-distributed experiments manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Brillone/labs",
+    download_url='https://github.com/Brillone/labs/archive/v0.0.3.tar.gz',
+    packages=['labs'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+                        'numpy==1.18.4',
+                        'dask==2.16.0',
+                        'distributed==2.16.0',
+                        'slacker==0.14.0',
+                        'scipy==1.4.1',
+                        'scikit-learn==0.23.0',
+                        'scikit-optimize==0.7.4'
+                    ]
+)

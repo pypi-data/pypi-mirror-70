8ba5568cdc741b33f9e310c5c5e4ca6eaa56236f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:57 +0000

    Add package edx-django-utils/3.2.3/edx-django-utils-3.2.3.tar.gz

---
+++ packages/edx-django-utils/edx-django-utils-3.2.3.tar.gz/edx-django-utils-3.2.3/edx_django_utils/__init__.py
@@ -0,0 +1,11 @@
+"""
+EdX utilities for Django Application development..
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+__version__ = "3.2.3"
+
+default_app_config = (
+    "edx_django_utils.apps.EdxDjangoUtilsConfig"
+)

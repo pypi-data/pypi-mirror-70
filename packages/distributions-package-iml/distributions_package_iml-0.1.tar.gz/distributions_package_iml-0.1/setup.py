fc6d5b7cd18f4f380bf3b245e210f5ef5608e1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:50 +0000

    Add package distributions-package-iml/0.1/distributions_package_iml-0.1.tar.gz

---
+++ packages/distributions-package-iml/distributions_package_iml-0.1.tar.gz/distributions_package_iml-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_package_iml',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_package_iml'],
+      author='Isaac Looremeta',
+      author_email='isakm3@gmail.com',
+      zip_safe=False)

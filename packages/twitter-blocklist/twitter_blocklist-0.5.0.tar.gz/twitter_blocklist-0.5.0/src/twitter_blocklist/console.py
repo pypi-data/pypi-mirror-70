ae071b28c8571117736a52e78a68cfcbbc63dcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:06 +0000

    Add package twitter-blocklist/0.5.0/twitter_blocklist-0.5.0.tar.gz

---
+++ packages/twitter-blocklist/twitter_blocklist-0.5.0.tar.gz/twitter_blocklist-0.5.0/src/twitter_blocklist/console.py
@@ -0,0 +1,42 @@
+import click
+import toml
+from progressbar import progressbar
+import twitter
+import sys
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+@click.option("--export/--no-export", default=False)
+@click.option("--list/--no-list", default=False)
+@click.option("--unblock/--no-unblock", default=False)
+@click.argument("filename")
+def main(export, list, unblock, filename):
+
+    api = twitter.Api(**toml.load("twitter_keys.toml"))
+
+    if export:
+        with open(filename, "w") as f:
+            for user in api.GetBlocks():
+                f.write(str(user.id) + "\n")
+        sys.exit(0)
+
+    if list:
+        user_ids = [
+            user.id
+            for user in api.GetListMembersPaged(
+                list_id=int(filename), count=10000, skip_status=True
+            )[2]
+        ]
+    else:
+        with open(filename, "r") as f:
+            user_ids = [l.strip() for l in f.readlines()]
+
+    block = api.CreateBlock if not unblock else api.DestroyBlock
+    for user_id in progressbar(user_ids, redirect_stdout=True):
+        try:
+            block(user_id=int(user_id))
+        except Exception as e:
+            print(e)

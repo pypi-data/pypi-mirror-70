c3e770a84a5cac0b88dc920b3c88e544d2fd9fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package eddiefroufrou-test123/0.1/eddiefroufrou_test123-0.1.tar.gz

---
+++ packages/eddiefroufrou-test123/eddiefroufrou_test123-0.1.tar.gz/eddiefroufrou_test123-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eddiefroufrou_test123',
+      version='0.1',
+      description='eddiefroufrou_test123',
+      packages=['eddiefroufrou_test123'],
+      zip_safe=False)

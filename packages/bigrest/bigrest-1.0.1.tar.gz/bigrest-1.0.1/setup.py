122b6fe546c5b06d79c2615588b87dd87218d077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package bigrest/1.0.1/bigrest-1.0.1.tar.gz

---
+++ packages/bigrest/bigrest-1.0.1.tar.gz/bigrest-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bigrest",
+    version="1.0.1",
+    author="Leonardo Souza",
+    description="A Python SDK for F5 iControl REST API",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/leonardobdes/BIGREST",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

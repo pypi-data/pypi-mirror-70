c036a4cc64bae6663259735a38f438e9bfd397ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package hmmlogo/0.0.1/hmmlogo-0.0.1.tar.gz

---
+++ packages/hmmlogo/hmmlogo-0.0.1.tar.gz/hmmlogo-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hmmlogo",
+    version="0.0.1",
+    author="Inti Manuel Yabar-Pagaza",
+    author_email="intipagaza@live.dk",
+    description="Package to plot logos for profile hidden Markov models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imyp/hmmlogo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+

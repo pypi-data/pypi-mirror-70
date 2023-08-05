87783da409a28d5b428b99e0885bc1a2ce32a149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:37 +0000

    Add package bosdyn-client/1.1.2/bosdyn_client-1.1.2-481288766-py2.py3-none-any.whl

---
+++ packages/bosdyn-client/bosdyn_client-1.1.2-481288766-py2.py3-none-any.whl/bosdyn/client/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2019 Boston Dynamics, Inc.  All rights reserved.
+#
+# Downloading, reproducing, distributing or otherwise using the SDK Software
+# is subject to the terms and conditions of the Boston Dynamics Software
+# Development Kit License (20191101-BDSDK-SL).
+
+"""The client library package.
+Sets up some convenience imports for commonly used classes.
+"""
+from .common import BaseClient
+# yapf: disable
+from .exceptions import (Error,
+                             ResponseError,
+                                 InvalidRequestError,
+                                 LeaseUseError,
+                                 ServerError,
+                                     InternalServerError,
+                                     UnsetStatusError,
+                             RpcError,
+                                ClientCancelledOperationError,
+                                InvalidAppTokenError,
+                                InvalidClientCertificateError,
+                                NonexistentAuthorityError,
+                                ProxyConnectionError,
+                                ServiceUnavailableError,
+                                ServiceFailedDuringExecutionError,
+                                TimedOutError,
+                                UnableToConnectToRobotError,
+                                UnauthenticatedError,
+                                UnknownDnsNameError,
+                                UnimplementedError)
+# yapf: enable
+from .auth import AuthClient, InvalidLoginError, InvalidTokenError
+from .robot import Robot
+from .sdk import Sdk, create_standard_sdk, BOSDYN_RESOURCE_ROOT

f5d69b9b6a23cff95b19da1c39bf6dc306e3fb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:57 +0000

    Add package grand/0.18/grand-0.18-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/grand/grand-0.18-cp38-cp38-manylinux1_x86_64.whl/grand/libs/__init__.py
@@ -0,0 +1,14 @@
+'''Manage shared libs for the GRAND package
+'''
+
+from typing_extensions import Final
+
+import os
+from pathlib import Path
+
+__all__ = ['DATADIR']
+
+
+# Initialise the package globals
+DATADIR: Final = Path(__file__).parent / 'data'
+'''Path to the package data'''

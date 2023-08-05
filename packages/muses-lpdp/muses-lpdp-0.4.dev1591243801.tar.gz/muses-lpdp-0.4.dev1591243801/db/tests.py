17dfbb7d72179e1133e64bc14e1850c1f1d98a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package muses-lpdp/0.4.dev1591243801/muses-lpdp-0.4.dev1591243801.tar.gz

---
+++ packages/muses-lpdp/muses-lpdp-0.4.dev1591243801.tar.gz/muses-lpdp-0.4.dev1591243801/db/tests.py
@@ -0,0 +1,24 @@
+# Create your tests here.
+import unittest
+
+from db.models import Member
+
+
+class TestThemesList(unittest.TestCase):
+    themes = {
+        "THEME_DEFAULT": "muses_default",
+        "THEME_CLASSIC": "muses_classic",
+        "THEME_ROSE": "muses_rose",
+        "THEME_BLUE": "muses_blue",
+        "THEME_GREEN": "muses_green",
+        "THEME_ORANGE": "muses_orange",
+    }
+
+    def test_themes_in_choices(self):
+        m = Member()
+        for k in self.themes:
+            self.assertEquals(self.themes[k], getattr(m, k), msg=f"Member not contains {self.themes[k]}")
+
+
+if __name__ == '__main__':
+    unittest.main()

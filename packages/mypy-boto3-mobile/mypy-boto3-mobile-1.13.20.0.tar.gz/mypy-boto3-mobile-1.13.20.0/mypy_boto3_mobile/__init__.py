13909cc42db836f5e04278e6d400e80e56b45355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package mypy-boto3-mobile/1.13.20.0/mypy-boto3-mobile-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-mobile/mypy-boto3-mobile-1.13.20.0.tar.gz/mypy-boto3-mobile-1.13.20.0/mypy_boto3_mobile/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for mobile service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.mobile import (
+        Client,
+        ListBundlesPaginator,
+        ListProjectsPaginator,
+        MobileClient,
+        )
+
+    session = boto3.Session()
+
+    client: MobileClient = boto3.client("mobile")
+    session_client: MobileClient = session.client("mobile")
+
+    list_bundles_paginator: ListBundlesPaginator = client.get_paginator("list_bundles")
+    list_projects_paginator: ListProjectsPaginator = client.get_paginator("list_projects")
+"""
+from mypy_boto3_mobile.client import MobileClient as Client, MobileClient
+from mypy_boto3_mobile.paginator import ListBundlesPaginator, ListProjectsPaginator
+
+
+__all__ = ("Client", "ListBundlesPaginator", "ListProjectsPaginator", "MobileClient")

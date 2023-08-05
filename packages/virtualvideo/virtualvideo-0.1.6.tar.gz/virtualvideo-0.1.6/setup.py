783ab15877455ecf43da4949c658e8c47972fd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package virtualvideo/0.1.6/virtualvideo-0.1.6.tar.gz

---
+++ packages/virtualvideo/virtualvideo-0.1.6.tar.gz/virtualvideo-0.1.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="virtualvideo", # Replace with your own username
+    version="0.1.6",
+    author="Flashs",
+    description="""virtualvideo allows you to write simple programs
+                   that feed images to a v4l2loopback device""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GPLv3",
+    url="https://github.com/Flashs/virtualvideo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Topic :: System :: Operating System Kernels :: Linux",
+        "Topic :: Multimedia :: Video :: Display",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+    install_requires=[
+          'ffmpeg-python',
+          "numpy"
+    ],
+    python_requires='>=3.6',
+)

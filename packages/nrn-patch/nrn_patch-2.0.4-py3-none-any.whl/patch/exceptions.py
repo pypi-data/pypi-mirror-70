5b6b4946c225ec6914752abfc17956f3886dd847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:20 +0000

    Add package nrn-patch/2.0.4/nrn_patch-2.0.4-py3-none-any.whl

---
+++ packages/nrn-patch/nrn_patch-2.0.4-py3-none-any.whl/patch/exceptions.py
@@ -0,0 +1,46 @@
+class PatchError(Exception):
+    pass
+
+
+class NotConnectableError(PatchError):
+    pass
+
+
+class NotConnectedError(PatchError):
+    pass
+
+
+class TransformError(PatchError):
+    pass
+
+
+class HocError(PatchError):
+    pass
+
+
+class HocConnectError(HocError):
+    pass
+
+
+class HocSectionAccessError(HocError):
+    pass
+
+
+class SimulationError(PatchError):
+    pass
+
+
+class UninitializedError(SimulationError):
+    pass
+
+
+class ErrorHandlingError(PatchError):
+    pass
+
+
+class ParallelError(PatchError):
+    pass
+
+
+class ParallelConnectError(PatchError):
+    pass

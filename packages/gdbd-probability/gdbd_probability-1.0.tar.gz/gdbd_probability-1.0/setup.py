93982540fcf7f6c8f237d59562cc6f20d37e98e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:21 +0000

    Add package gdbd-probability/1.0/gdbd_probability-1.0.tar.gz

---
+++ packages/gdbd-probability/gdbd_probability-1.0.tar.gz/gdbd_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gdbd_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gdbd_probability'],
+      author = 'Abhi G',
+      author_email = 'abhi@yahoo.com',
+      zip_safe=False)

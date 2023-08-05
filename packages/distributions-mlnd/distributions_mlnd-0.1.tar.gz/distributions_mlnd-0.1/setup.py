786a1c3aca20d7c8124af3f4eb89c461924acf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:23 +0000

    Add package distributions-mlnd/0.1/distributions_mlnd-0.1.tar.gz

---
+++ packages/distributions-mlnd/distributions_mlnd-0.1.tar.gz/distributions_mlnd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_mlnd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mlnd'],
+      zip_safe=False)

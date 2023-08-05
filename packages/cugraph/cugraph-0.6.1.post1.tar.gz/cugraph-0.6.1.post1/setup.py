51c812ceaca4dfe395701f2be5c7d5953ebf6a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package cugraph/0.6.1.post1/cugraph-0.6.1.post1.tar.gz

---
+++ packages/cugraph/cugraph-0.6.1.post1.tar.gz/cugraph-0.6.1.post1/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools.command.install import install
+from setuptools import setup
+
+pkg = 'cugraph'
+version = '0.6.1.post1'
+name = pkg.split('-')[0] if 'cuda' in pkg else pkg
+
+long_description = 'Please install {} via the rapidsai conda channel. See https://rapids.ai/start.html for instructions.'.format(name)
+
+
+class InstallWrapper(install):
+
+    def run(self):
+        raise Exception(long_description)
+
+
+setup(name=pkg,
+      version=version,
+      description='',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/rapidsai/dask-cuda',
+      author='RAPIDS development team',
+      author_email='pypi@rapids.ai',
+      license='Apache 2.0',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Database',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      cmdclass={'install': InstallWrapper},
+      )
+

7cca74b02993dfe68a151b8a23b138cbd1a467a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:03 +0000

    Add package parsl/1.0.0a0/parsl-1.0.0a0.tar.gz

---
+++ packages/parsl/parsl-1.0.0a0.tar.gz/parsl-1.0.0a0/parsl/launchers/__init__.py
@@ -0,0 +1,14 @@
+from parsl.launchers.launchers import SimpleLauncher, SingleNodeLauncher, \
+    SrunLauncher, AprunLauncher, SrunMPILauncher, WrappedLauncher, \
+    GnuParallelLauncher, MpiExecLauncher, MpiRunLauncher, JsrunLauncher
+
+__all__ = ['SimpleLauncher',
+           'WrappedLauncher',
+           'SingleNodeLauncher',
+           'SrunLauncher',
+           'AprunLauncher',
+           'SrunMPILauncher',
+           'JsrunLauncher',
+           'GnuParallelLauncher',
+           'MpiExecLauncher',
+           'MpiRunLauncher']

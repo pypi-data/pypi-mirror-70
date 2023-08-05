deee9ce89883dc88ce0417621dd5b7dc8b6b90e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:08 +0000

    Add package distrib-probabilidad/1.0/distrib_probabilidad-1.0.tar.gz

---
+++ packages/distrib-probabilidad/distrib_probabilidad-1.0.tar.gz/distrib_probabilidad-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distrib_probabilidad',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distrib_probabilidad'],
+      author = 'Hairo Ulises Miranda Belmonte',
+      author_email = 'hairo1421@gmail.com',
+      zip_safe=False)

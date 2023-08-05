bbc2846d0d5cca3d19eb39fe9076f8f5ca3d863f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package gb5-distributions/0.1/gb5-distributions-0.1.tar.gz

---
+++ packages/gb5-distributions/gb5-distributions-0.1.tar.gz/gb5-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gb5-distributions',
+      version='0.1',
+      description='Gaussian/Binomial distributions',
+      packages=['gb5-distributions'],
+      zip_safe=False)

cf69dc2c28b067ff9b27ebee809ac9991d37d7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:55 +0000

    Add package gollama/0.0.2/gollama-0.0.2.tar.gz

---
+++ packages/gollama/gollama-0.0.2.tar.gz/gollama-0.0.2/gollama/frontend/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http.response import HttpResponsePermanentRedirect
+
+from backend.models import ShortHand
+
+
+def index(request):
+    if request.method == 'POST' and (request.POST.get('shorthand') and request.POST.get('url')):
+        ShortHand.objects.create(
+            label=request.POST.get('shorthand'),
+            url=request.POST.get('url')
+        )
+        return redirect('index')
+
+    return render(request, 'index.html', context={'shorthands': ShortHand.objects.all()})
+
+
+def reroute(request, shorthand):
+    obj = get_object_or_404(ShortHand, label=shorthand)
+    return HttpResponsePermanentRedirect(obj.url)

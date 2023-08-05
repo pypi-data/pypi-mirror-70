c40001ea341eb8eca173ee6f04ced94356d667d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:09 +0000

    Add package tello-binom/1.0/tello_binom-1.0.tar.gz

---
+++ packages/tello-binom/tello_binom-1.0.tar.gz/tello_binom-1.0/setup.py
@@ -0,0 +1,8 @@
+﻿from setuptools import setup
+
+setup(name='tello_binom',
+      version='1.0',
+      description='Tello Edu SDK library',
+      packages=['tello_binom'],
+      author_email='dimitriz@mail.ru',
+      zip_safe=False)

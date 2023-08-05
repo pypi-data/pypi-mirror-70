8fdbf1eadd4aed726b411974bc490f52de93f5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:14 +0000

    Add package simple-bank-korea/0.2.13/simple_bank_korea-0.2.13.tar.gz

---
+++ packages/simple-bank-korea/simple_bank_korea-0.2.13.tar.gz/simple_bank_korea-0.2.13/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='simple_bank_korea',
+      version='0.2.13',
+      url='https://github.com/beomi/simple_bank_korea',
+      license='MIT',
+      author='Junbum Lee',
+      author_email='jun@beomi.net',
+      description='Crawling Korea bank transactions',
+      packages=find_packages(),
+      long_description="""\
+      Crawler with requests/bs4/selenium/PhantomJS for Korea Bank Transctions.
+      Currently supports Kookmin Bank""",
+      zip_safe=False,
+      setup_requires=['python-dateutil', 'requests', 'bs4', 'selenium', 'pillow'],
+      install_requires=['python-dateutil', 'requests', 'bs4', 'selenium', 'pillow'],
+      download_url='https://github.com/beomi/simple_bank_korea/archive/0.2.13.tar.gz',
+      include_package_data = True,
+)

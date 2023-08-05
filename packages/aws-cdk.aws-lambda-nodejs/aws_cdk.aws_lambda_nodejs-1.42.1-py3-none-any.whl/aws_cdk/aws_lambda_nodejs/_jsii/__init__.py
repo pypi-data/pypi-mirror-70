a00d97d3a5be6298d0723668c99c1c7451779c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package aws-cdk.aws-lambda-nodejs/1.42.1/aws_cdk.aws_lambda_nodejs-1.42.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-lambda-nodejs/aws_cdk.aws_lambda_nodejs-1.42.1-py3-none-any.whl/aws_cdk/aws_lambda_nodejs/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_lambda
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-lambda-nodejs", "1.42.1", __name__[0:-6], "aws-lambda-nodejs@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

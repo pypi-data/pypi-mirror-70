371c49f09341babec200403b0ca7910abf2aba78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package mypy-boto3-cur/1.13.20.0/mypy-boto3-cur-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-cur/mypy-boto3-cur-1.13.20.0.tar.gz/mypy-boto3-cur-1.13.20.0/mypy_boto3_cur/__init__.py
@@ -0,0 +1,27 @@
+"""
+Main interface for cur service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.cur import (
+        Client,
+        CostandUsageReportServiceClient,
+        DescribeReportDefinitionsPaginator,
+        )
+
+    session = boto3.Session()
+
+    client: CostandUsageReportServiceClient = boto3.client("cur")
+    session_client: CostandUsageReportServiceClient = session.client("cur")
+
+    describe_report_definitions_paginator: DescribeReportDefinitionsPaginator = client.get_paginator("describe_report_definitions")
+"""
+from mypy_boto3_cur.client import (
+    CostandUsageReportServiceClient,
+    CostandUsageReportServiceClient as Client,
+)
+from mypy_boto3_cur.paginator import DescribeReportDefinitionsPaginator
+
+
+__all__ = ("Client", "CostandUsageReportServiceClient", "DescribeReportDefinitionsPaginator")

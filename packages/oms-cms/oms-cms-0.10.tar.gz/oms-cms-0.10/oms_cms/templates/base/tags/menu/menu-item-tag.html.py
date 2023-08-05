d2a1fa42f58a2f291022e1fad3dfb0bdd1aa6162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package oms-cms/0.10/oms-cms-0.10.tar.gz

---
+++ packages/oms-cms/oms-cms-0.10.tar.gz/oms-cms-0.10/oms_cms/templates/base/tags/menu/menu-item-tag.html.py
@@ -0,0 +1,19 @@
+BBBB BBBBBBBBB
+XXXX
+    BBBBBBBBBBB BBBBB
+        XXXX
+            BB BBBBBBBB
+                XX XXXXXXXX XXXXXXXXXXXX
+            BBBB BBBBBBBBBBB
+                XX XXXXXXXX XXXXXXXXXXXX
+            BBBB
+                XX XXXXXXXX XXXXXXXXXXXX
+            BBBBB
+            BB BBB BBBBBBBBBBBBBBBBB
+                XXX XXXXXXXXXXXXXXXXX
+                    
+                XXXXX
+            BBBBB
+        XXXXX
+    BBBBBBBBBBBBBB
+XXXXX

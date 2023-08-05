88d4533ae219f43733f3ba551dcaccb4cc486458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:29 +0000

    Add package isc-py-common/0.0.149/isc_py_common-0.0.149-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.149-py3-none-any.whl/isc_common/fields/description_field.py
@@ -0,0 +1,17 @@
+from django.db.models import TextField
+
+from isc_common import delAttr
+from isc_common.fields import Field
+
+
+class DescriptionField(TextField, Field):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('null', True)
+        kwargs.setdefault('blank', True)
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        delAttr(kwargs, 'null')
+        delAttr(kwargs, 'blank')
+        return name, path, args, kwargs

518fee57a646da46e881be6b87804bf7c54c2d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:04 +0000

    Add package lykmmcv/0.5.6/lykmmcv-0.5.6.tar.gz

---
+++ packages/lykmmcv/lykmmcv-0.5.6.tar.gz/lykmmcv-0.5.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='lykmmcv',
+      version='0.5.6',
+      description='from mmcv 0.5.6',
+      author='liuyuankai',
+      author_email='2452378121@qq.com',
+      packages=['mmCV'],
+      )

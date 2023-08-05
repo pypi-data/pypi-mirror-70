e046115e3c964f7e8063f8cc5e10b973f75b25ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:47 +0000

    Add package StringCalculator/0.0.3/StringCalculator-0.0.3.tar.gz

---
+++ packages/StringCalculator/StringCalculator-0.0.3.tar.gz/StringCalculator-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="StringCalculator",
+    version="0.0.3",
+    author="Ido Zahavy",
+    author_email="idozahavy@gmail.com",
+    description="Calculates string math expressions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/idozahavy/StringCalculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # 3 - Alpha , 4 - Beta , 5 - Production/Stable
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
+# https://packaging.python.org/tutorials/packaging-projects/

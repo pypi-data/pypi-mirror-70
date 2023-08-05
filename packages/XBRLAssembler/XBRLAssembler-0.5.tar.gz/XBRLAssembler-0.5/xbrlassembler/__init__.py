8269696897d220ed71404a8724cdd1f9e873550d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package XBRLAssembler/0.5/XBRLAssembler-0.5.tar.gz

---
+++ packages/XBRLAssembler/XBRLAssembler-0.5.tar.gz/XBRLAssembler-0.5/xbrlassembler/__init__.py
@@ -0,0 +1,9 @@
+from xbrlassembler.assembler import XBRLAssembler, XBRLElement
+from xbrlassembler.enums import XBRLType, FinancialStatement, DateParser
+from xbrlassembler.error import (XBRLError,
+                                 XBRLCellsError,
+                                 XBRLLabelError,
+                                 XBRLSchemaError,
+                                 XBRLIndexError,
+                                 XBRLDirectoryError,
+                                 XBRLRefDocError)

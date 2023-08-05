293632f4e4014ba643807a135a3b05ff2a9e9bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package aricma-pretty-print/0.0.3/aricma_pretty_print-0.0.3.tar.gz

---
+++ packages/aricma-pretty-print/aricma_pretty_print-0.0.3.tar.gz/aricma_pretty_print-0.0.3/README.md
@@ -0,0 +1,11 @@
+# Pretty Print
+
+This is a simple printer package. 
+You can use it to print more useful information during development and also more readable prints for any production cli.  
+
+The package got inspired by the [jest] testing cli for javascript. Its colors, indents and order is very user friendly. 
+
+
+
+[jest]: (https://jestjs.io/)
+ 
---
+++ packages/aricma-pretty-print/aricma_pretty_print-0.0.3.tar.gz/aricma_pretty_print-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aricma_pretty_print",  # Replace with your own username
+    version="0.0.3",
+    author="Adrian Mindak",
+    description="A small printer package "
+                "to get some better looking print statements for any cli.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aricma/pretty-print.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

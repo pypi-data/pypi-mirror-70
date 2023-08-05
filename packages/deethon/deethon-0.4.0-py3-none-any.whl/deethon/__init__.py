913f543ba224927715666116b198c4e576c92b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package deethon/0.4.0/deethon-0.4.0-py3-none-any.whl

---
+++ packages/deethon/deethon-0.4.0-py3-none-any.whl/deethon/__init__.py
@@ -0,0 +1,13 @@
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
+
+from . import types, utils, consts, errors, session
+from .session import Session
+from .types import Album, Track
+
+__all__ = ['Session', 'Album', 'Track', 'errors',
+           'utils', 'consts', 'types', 'session']
---
+++ packages/deethon/deethon-0.4.0-py3-none-any.whl/deethon/errors.py
@@ -0,0 +1,34 @@
+"""The errors module contains all custom error classes"""
+
+
+class DeezerLoginError(Exception):
+    """Occurs when login to Deezer fails."""
+
+    def __init__(self):
+        super().__init__("Failed to login to Deezer. Please check your arltoken.")
+
+
+class DeezerApiError(Exception):
+    """Occurs when a Deezer API request replies with an error"""
+
+    def __init__(self, error: str, message: str, code: int):
+        self.error: str = error
+        self.code: int = code
+        self.message: str = message
+        super().__init__(f"Error {code}: {error} - {message}")
+
+
+class InvalidUrlError(Exception):
+    """Occurs when an invalid URL is passed"""
+
+    def __init__(self, url: str):
+        self.url: str = url
+        super().__init__(f'"{url}" is not a valid Deezer url.')
+
+
+class ActionNotSupported(Exception):
+    """Occurs when an invalid action is called"""
+
+    def __init__(self, mode: str):
+        self.mode: str = mode
+        super().__init__(f"Action is not (yet) supported: {self.mode}")

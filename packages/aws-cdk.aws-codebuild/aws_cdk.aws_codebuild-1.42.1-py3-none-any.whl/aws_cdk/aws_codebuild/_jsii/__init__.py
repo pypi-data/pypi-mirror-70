92aa7d75024a7503fbbca3fe7e5f1c64bc514282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package aws-cdk.aws-codebuild/1.42.1/aws_cdk.aws_codebuild-1.42.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-codebuild/aws_cdk.aws_codebuild-1.42.1-py3-none-any.whl/aws_cdk/aws_codebuild/_jsii/__init__.py
@@ -0,0 +1,32 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.assets
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_codecommit
+import aws_cdk.aws_ec2
+import aws_cdk.aws_ecr
+import aws_cdk.aws_ecr_assets
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_assets
+import aws_cdk.aws_secretsmanager
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-codebuild", "1.42.1", __name__[0:-6], "aws-codebuild@1.42.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

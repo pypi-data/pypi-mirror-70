d59f8ccb00222c46dfa28205751e8eec5c7b7658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:49 +0000

    Add package bunga/0.8.0/bunga-0.8.0.tar.gz

---
+++ packages/bunga/bunga-0.8.0.tar.gz/bunga-0.8.0/bunga/__init__.py
@@ -0,0 +1,262 @@
+import asyncio
+import sys
+import os
+import argparse
+import threading
+import logging
+
+from prompt_toolkit import prompt
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+
+from .version import __version__
+from .bunga_client import BungaClient
+
+
+LOGGER = logging.getLogger(__file__)
+
+
+class Client(BungaClient):
+
+    def __init__(self, uri, loop):
+        super().__init__(uri)
+        self._is_connected = False
+        self._connected_event = asyncio.Event(loop=loop)
+        self._complete_event = asyncio.Event(loop=loop)
+        self._fout = None
+        self._command_output = []
+        self._error = ''
+
+    async def on_connected(self):
+        print(f'Connected.')
+        self._is_connected = True
+        self._connected_event.set()
+
+    async def on_disconnected(self):
+        print(f'Disconnected.')
+        self._connected_event.clear()
+        self._is_connected = False
+        self._complete_event.set()
+
+    def print_result_and_signal(self, error):
+        if error:
+            print(f'ERROR({error})')
+        else:
+            print('OK')
+
+        self._error = error
+        self._complete_event.set()
+
+    async def on_execute_command_rsp(self, message):
+        if message.output:
+            output = message.output.decode('utf-8', 'replace')
+            print(output, end='', flush=True)
+            self._command_output.append(output)
+        else:
+            self.print_result_and_signal(message.error)
+
+    async def on_log_entry_ind(self, message):
+        print(''.join(message.text))
+
+    async def on_get_file_rsp(self, message):
+        if message.data:
+            self._fout.write(message.data)
+        else:
+            self.print_result_and_signal(message.error)
+
+    async def on_put_file_rsp(self, message):
+        self.print_result_and_signal(message.error)
+
+    async def execute_command(self, command):
+        if not self._is_connected:
+            await self._connected_event.wait()
+
+        self._command_output = []
+        message = self.init_execute_command_req()
+        message.command = command
+        self._complete_event.clear()
+        self.send()
+        await self._complete_event.wait()
+
+        return (''.join(self._command_output), self._error)
+
+    async def get(self, remote_path, local_path):
+        if not self._is_connected:
+            await self._connected_event.wait()
+
+        with open(local_path, 'wb') as self._fout:
+            message = self.init_get_file_req()
+            message.path = remote_path
+            self._complete_event.clear()
+            self.send()
+            await self._complete_event.wait()
+
+        return self._error
+
+    async def put(self, local_path, remote_path):
+        if not self._is_connected:
+            await self._connected_event.wait()
+
+        self._complete_event.clear()
+
+        with open(local_path, 'rb') as fin:
+            message = self.init_put_file_req()
+            message.path = remote_path
+            self.send()
+
+            while True:
+                message = self.init_put_file_req()
+                message.data = fin.read(64)
+                self.send()
+
+                if not message.data:
+                    break
+
+        await self._complete_event.wait()
+
+        return self._error
+
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, uri):
+        super().__init__()
+        LOGGER.info('Server URI: %s', uri)
+        self._loop = asyncio.new_event_loop()
+        self._client = Client(uri, self._loop)
+        self.daemon = True
+
+    def run(self):
+        asyncio.set_event_loop(self._loop)
+        self._loop.run_until_complete(self._start())
+        self._loop.run_forever()
+
+    def execute_command(self, command):
+        asyncio.run_coroutine_threadsafe(self._client.execute_command(command),
+                                         self._loop).result()
+
+    def get(self, remote_path, local_path):
+        asyncio.run_coroutine_threadsafe(self._client.get(remote_path, local_path),
+                                         self._loop).result()
+
+    def put(self, local_path, remote_path):
+        asyncio.run_coroutine_threadsafe(self._client.put(local_path, remote_path),
+                                         self._loop).result()
+
+    async def _start(self):
+        self._client.start()
+
+
+def is_comment(line):
+    """Lines starting with "#" are comments.
+
+    """
+
+    return line.strip().startswith('#')
+
+
+def shell(client):
+    commands = [command[1:] for command in []]
+    commands.append('exit')
+    user_home = os.path.expanduser('~')
+    history = FileHistory(os.path.join(user_home, '.bunga-history.txt'))
+
+    while True:
+        try:
+            line = prompt('$ ',
+                          complete_while_typing=True,
+                          auto_suggest=AutoSuggestFromHistory(),
+                          enable_history_search=True,
+                          history=history)
+        except EOFError:
+            break
+
+        if line:
+            if is_comment(line):
+                continue
+
+            if line == 'exit':
+                break
+
+            client.execute_command(line)
+
+
+def do_shell(args):
+    client = ClientThread(args.uri)
+    client.start()
+    shell(client)
+
+
+def do_put(args):
+    client = ClientThread(args.uri)
+    client.start()
+    client.put(args.localfile, args.remotefile)
+
+
+def do_get(args):
+    client = ClientThread(args.uri)
+    client.start()
+    client.get(args.remotefile, args.localfile)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('-l', '--log-level',
+                        default='error',
+                        choices=[
+                            'debug', 'info', 'warning', 'error', 'critical'
+                        ],
+                        help='Set the logging level (default: %(default)s).')
+    parser.add_argument('--version',
+                        action='version',
+                        version=__version__,
+                        help='Print version information and exit.')
+
+    # Workaround to make the subparser required in Python 3.
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       dest='subcommand')
+    subparsers.required = True
+
+    # The shell subparser.
+    shell_parser = subparsers.add_parser('shell')
+    shell_parser.add_argument('-u' ,'--uri',
+                              default='tcp://127.0.0.1:28000',
+                              help='URI of the server (default: %(default)s)')
+    shell_parser.set_defaults(func=do_shell)
+
+    # The put subparser.
+    put_parser = subparsers.add_parser('put')
+    put_parser.add_argument('-u' ,'--uri',
+                            default='tcp://127.0.0.1:28000',
+                            help='URI of the server (default: %(default)s)')
+    put_parser.add_argument('localfile', help='The local file path.')
+    put_parser.add_argument('remotefile', help='The remote file path.')
+    put_parser.set_defaults(func=do_put)
+
+    # The get subparser.
+    get_parser = subparsers.add_parser('get')
+    get_parser.add_argument('-u' ,'--uri',
+                            default='tcp://127.0.0.1:28000',
+                            help='URI of the server (default: %(default)s)')
+    get_parser.add_argument('remotefile', help='The remote file path.')
+    get_parser.add_argument('localfile', help='The local file path.')
+    get_parser.set_defaults(func=do_get)
+
+    args = parser.parse_args()
+
+    level = logging.getLevelName(args.log_level.upper())
+    logging.basicConfig(level=level, format='%(asctime)s %(message)s')
+
+    if args.debug:
+        args.func(args)
+    else:
+        try:
+            args.func(args)
+        except BaseException as e:
+            sys.exit(str(e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/bunga/bunga-0.8.0.tar.gz/bunga-0.8.0/tests/test_client.py
@@ -0,0 +1,157 @@
+import logging
+import asyncio
+import unittest
+
+import bunga
+
+
+def create_tcp_uri(listener):
+    address, port = listener.sockets[0].getsockname()
+
+    return f'tcp://{address}:{port}'
+
+
+async def server_main(listener):
+    async with listener:
+        try:
+            await listener.serve_forever()
+        except asyncio.CancelledError:
+            pass
+
+
+class Client(bunga.Client):
+
+    def __init__(self, listener):
+        super().__init__(create_tcp_uri(listener), asyncio.get_event_loop())
+        self.log_entries = asyncio.Queue()
+
+    async def on_log_entry_ind(self, message):
+        await super().on_log_entry_ind(message)
+        await self.log_entries.put(''.join(message.text))
+
+
+class ClientTest(unittest.TestCase):
+
+    def test_execute_command(self):
+        asyncio.run(self.execute_command())
+
+    async def execute_command(self):
+        async def on_client_connected(reader, writer):
+            date_req = await reader.readexactly(12)
+            self.assertEqual(date_req, b'\x01\x00\x00\x08\n\x06\n\x04date')
+            writer.write(b'\x02\x00\x00\x08\x0a\x06\x0a\x04\x32\x30\x32\x30')
+            writer.write(b'\x02\x00\x00\x02\x0a\x00')
+
+            bad_req = await reader.readexactly(11)
+            self.assertEqual(bad_req, b'\x01\x00\x00\x07\n\x05\n\x03bad')
+            writer.write(b'\x02\x00\x00\x0d\x0a\x0b\x12\x09\x6e\x6f\x74\x20\x66'
+                         b'\x6f\x75\x6e\x64')
+
+            writer.close()
+
+        listener = await asyncio.start_server(on_client_connected, 'localhost', 0)
+
+        async def client_main():
+            client = Client(listener)
+            client.start()
+
+            output, error = await client.execute_command('date')
+            self.assertEqual(output, '2020')
+            self.assertEqual(error, '')
+
+            output, error = await client.execute_command('bad')
+            self.assertEqual(output, '')
+            self.assertEqual(error, 'not found')
+
+            client.stop()
+            listener.close()
+
+        await asyncio.wait_for(
+            asyncio.gather(server_main(listener), client_main()), 2)
+
+    def test_log_entry(self):
+        asyncio.run(self.log_entry())
+
+    async def log_entry(self):
+        async def on_client_connected(reader, writer):
+            writer.write(b'\x02\x00\x00\x0f\x12\r\n\x03123\n\x06Hello!')
+            writer.write(b'\x02\x00\x00\x15\x12\x13\n\x03123\n\x06Hello!\n\x04 Hi!')
+            writer.close()
+
+        listener = await asyncio.start_server(on_client_connected, 'localhost', 0)
+
+        async def client_main():
+            client = Client(listener)
+            client.start()
+            log_entry = await client.log_entries.get()
+            self.assertEqual(log_entry, '123Hello!')
+            log_entry = await client.log_entries.get()
+            self.assertEqual(log_entry, '123Hello! Hi!')
+            client.stop()
+            listener.close()
+
+        await asyncio.wait_for(
+            asyncio.gather(server_main(listener), client_main()), 2)
+
+    def test_get(self):
+        asyncio.run(self.get())
+
+    async def get(self):
+        async def on_client_connected(reader, writer):
+            req = await reader.readexactly(13)
+            self.assertEqual(req, b'\x01\x00\x00\x09\x12\x07\n\x05/init')
+            writer.write(b'\x02\x00\x00\x08\x1a\x06\n\x041234')
+            writer.write(b'\x02\x00\x00\x08\x1a\x06\n\x045678')
+            writer.write(b'\x02\x00\x00\x02\x1a\x00')
+
+            writer.close()
+
+        listener = await asyncio.start_server(on_client_connected, 'localhost', 0)
+
+        async def client_main():
+            client = Client(listener)
+            client.start()
+
+            error = await client.get('/init', 'get.txt')
+            self.assertEqual(error, '')
+
+            with open('get.txt', 'rb') as fin:
+                self.assertEqual(fin.read(), b'12345678')
+
+            client.stop()
+            listener.close()
+
+        await asyncio.wait_for(
+            asyncio.gather(server_main(listener), client_main()), 2)
+
+    def test_put(self):
+        asyncio.run(self.put())
+
+    async def put(self):
+        async def on_client_connected(reader, writer):
+            req = await reader.readexactly(22)
+            self.assertEqual(
+                req,
+                b'\x01\x00\x00\x12\x1a\x10\n\x0eput_remote.txt')
+            req = await reader.readexactly(17)
+            self.assertEqual(req, b'\x01\x00\x00\r\x1a\x0b\x12\t12345678\n')
+            req = await reader.readexactly(6)
+            self.assertEqual(req, b'\x01\x00\x00\x02\x1a\x00')
+            writer.write(b'\x02\x00\x00\x02"\x00')
+
+            writer.close()
+
+        listener = await asyncio.start_server(on_client_connected, 'localhost', 0)
+
+        async def client_main():
+            client = Client(listener)
+            client.start()
+
+            error = await client.put('tests/put.txt', 'put_remote.txt')
+            self.assertEqual(error, '')
+
+            client.stop()
+            listener.close()
+
+        await asyncio.wait_for(
+            asyncio.gather(server_main(listener), client_main()), 2)

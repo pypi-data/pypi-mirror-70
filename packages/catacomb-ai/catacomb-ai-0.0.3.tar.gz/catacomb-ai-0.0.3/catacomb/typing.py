96025da3a1bbc0209ad14c3c4de2810d85cdb438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:19 +0000

    Add package catacomb-ai/0.0.3/catacomb-ai-0.0.3.tar.gz

---
+++ packages/catacomb-ai/catacomb-ai-0.0.3.tar.gz/catacomb-ai-0.0.3/catacomb/typing.py
@@ -0,0 +1,4 @@
+class Types:
+    def __init__(self):
+        self.input = 'TEXT'
+        self.output = 'NUMBER'
---
+++ packages/catacomb-ai/catacomb-ai-0.0.3.tar.gz/catacomb-ai-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name='catacomb-ai',
+    version='0.0.3',
+    description='Build tools for Catacomb model hosting.',
+    packages=['catacomb'],
+)

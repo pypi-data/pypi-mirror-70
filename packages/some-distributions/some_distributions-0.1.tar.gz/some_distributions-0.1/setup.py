adb0fac2c1318860ac49a2e78693cc74554a53c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package some-distributions/0.1/some_distributions-0.1.tar.gz

---
+++ packages/some-distributions/some_distributions-0.1.tar.gz/some_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='some_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['some_distributions'],
+      author = 'Nawal Yala',
+      zip_safe=False)

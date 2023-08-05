225c9fa686eef30890d915fd653ed52690757342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:43 +0000

    Add package sdtables/1.0.1/sdtables-1.0.1.tar.gz

---
+++ packages/sdtables/sdtables-1.0.1.tar.gz/sdtables-1.0.1/sdtables/__init__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+
+"""
+    sdtables - Load table data from various sources
+    into python dictionary structures
+
+    cunningr - 2020
+
+    Requires:
+     - openpyxl >= 2.6.2
+     - jsonschema
+
+
+"""
+
+__version__ = "1.0.1"
+
+from sdtables.xlTables import *

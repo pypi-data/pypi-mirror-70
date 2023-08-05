4b7131f8271fcc818b0c7a41de8931f96d3c5dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package mypy-boto3-mediastore-data/1.13.20.0/mypy-boto3-mediastore-data-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-mediastore-data/mypy-boto3-mediastore-data-1.13.20.0.tar.gz/mypy-boto3-mediastore-data-1.13.20.0/mypy_boto3_mediastore_data/paginator.py
@@ -0,0 +1,34 @@
+"""
+Main interface for mediastore-data service client paginators.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.mediastore_data import (
+        ListItemsPaginator,
+    )
+
+    client: MediaStoreDataClient = boto3.client("mediastore-data")
+
+    list_items_paginator: ListItemsPaginator = client.get_paginator("list_items")
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_mediastore_data.type_defs import ListItemsResponseTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListItemsPaginator",)
+
+
+class ListItemsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListItems documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/mediastore-data.html#MediaStoreData.Paginator.ListItems)
+    """
+
+    def paginate(
+        self, Path: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListItemsResponseTypeDef]:
+        """
+        [ListItems.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/mediastore-data.html#MediaStoreData.Paginator.ListItems.paginate)
+        """

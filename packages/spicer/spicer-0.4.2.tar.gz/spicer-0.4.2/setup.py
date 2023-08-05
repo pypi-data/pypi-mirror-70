625cb4d31a346518e5a19e68eba13ed87592afad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:43 +0000

    Add package spicer/0.4.2/spicer-0.4.2.tar.gz

---
+++ packages/spicer/spicer-0.4.2.tar.gz/spicer-0.4.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+test_requirements = ["pytest"]
+setup_requirements = ["spiceypy", "tqdm", "planetarypy"]
+setup(
+    name="spicer",
+    version="0.4.2",
+    packages=find_packages(),
+    description="Library to make SPICE a bit easier",
+    long_description=readme + "\n\n",
+    # metadata
+    author="K.-Michael Aye",
+    author_email="kmichael.aye@gmail.com",
+    license="MIT license",
+    keywords="Solarsystem, planetaryscience, planets",
+    zip_safe=True,
+    url="https://github.com/michaelaye/spicer",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    test_suite="tests",
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)

c5566a9bc04acd1f0298b1ad275494e0382f4ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:09 +0000

    Add package eltsnap/0.0.8/eltsnap-0.0.8.tar.gz

---
+++ packages/eltsnap/eltsnap-0.0.8.tar.gz/eltsnap-0.0.8/test/test1.py
@@ -0,0 +1,14 @@
+from eltsnap.eltPackage import *
+create_dataflow_package('DESKTOP-D7VB61Q\SQLEXPRESS', 'eltsnap_v2',
+'1 Maja OleDB',
+' \* source query *\ ',
+'is_expression(default 0)',
+'11test',
+'1 Maja OleDB',
+'destination_schema',
+'destination_table',
+'destination truncate default(0)',
+'keep identity default(0)',
+'use bulk copy default(0)',
+'test_test_ADO',
+'batch size default(0)')

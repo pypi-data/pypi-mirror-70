fa27f5ba3352059642e01766a6d3360a2afac61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:21 +0000

    Add package dsgb-probability/1.1/dsgb_probability-1.1.tar.gz

---
+++ packages/dsgb-probability/dsgb_probability-1.1.tar.gz/dsgb_probability-1.1/dsgb_probability/__init__.py
@@ -0,0 +1,2 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution_kc import Binomial
---
+++ packages/dsgb-probability/dsgb_probability-1.1.tar.gz/dsgb_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsgb_probability',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsgb_probability'],
+      author = 'Kelvin Chong',
+      author_email = 'ch0030in@e.ntu.edu.sg',
+      zip_safe=False)

bc939f825773aad809dd818c2cb55905f49e2d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:58 +0000

    Add package covid19dh/1.0.0/covid19dh-1.0.0.tar.gz

---
+++ packages/covid19dh/covid19dh-1.0.0.tar.gz/covid19dh-1.0.0/covid19dh/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Unified data hub for a better understanding of COVID-19.
+
+For more information check README.md.
+ 
+Reference: https://covid19datahub.io/
+Todo:
+    * caching
+"""
+
+import pkg_resources
+from .main import *
+from .cite import *
+
+__version__ = pkg_resources.get_distribution("covid19dh").version
+

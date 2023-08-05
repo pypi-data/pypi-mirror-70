4ac277574d28fb4a71abc3d5651e825d385bafb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:05 +0000

    Add package smbadi-distributions/0.1/smbadi_distributions-0.1.tar.gz

---
+++ packages/smbadi-distributions/smbadi_distributions-0.1.tar.gz/smbadi_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='smbadi_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['smbadi_distributions'],
+      author='Sheila Mbadi',
+      author_email='mbadidev@gmail.com',
+     zip_safe=False)

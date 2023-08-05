cab7a8bb06ce16fc26206a7cc4afcbef31f289c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package distributions-eddiefroufrou/0.1/distributions_eddiefroufrou-0.1.tar.gz

---
+++ packages/distributions-eddiefroufrou/distributions_eddiefroufrou-0.1.tar.gz/distributions_eddiefroufrou-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_eddiefroufrou',
+      version='0.1',
+      description='distributions_eddiefroufrou',
+      author='eddie froufrou',
+      author_email='edwinjmeyers@gmail.com',
+      packages=['distributions_eddiefroufrou'],
+      zip_safe=False)

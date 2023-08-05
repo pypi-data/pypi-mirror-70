f645cb5dc4e73316d77a1751cd236aad1b9e1dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:53 +0000

    Add package ikabot/6.0.1/ikabot-6.0.1.tar.gz

---
+++ packages/ikabot/ikabot-6.0.1.tar.gz/ikabot-6.0.1/ikabot/helpers/process.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+import psutil
+import json
+from ikabot.config import *
+from ikabot.helpers.signals import deactivate_sigint
+
+def set_child_mode(s):
+	s.padre = False
+	deactivate_sigint()
+	s.login()
+
+def run(command):
+	return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()
+
+def updateProcessList(s, programprocesslist = []):
+	# read from file
+	fileData = s.getFileData()
+	try:
+		fileList = fileData['processList']
+	except KeyError:
+		fileList = []
+
+	# check it's still running
+	runningIkabotProcessList = []
+	ika_process = 'python.exe' if isWindows else 'ikabot'
+	for process in fileList:
+		try:
+			processName = psutil.Process(pid = process['pid']).name()
+		except:
+			continue
+
+		if processName == ika_process:
+			runningIkabotProcessList.append(process)
+
+	# add new to the list and write to file only if it's given
+	for process in programprocesslist:
+		if process not in runningIkabotProcessList:
+			runningIkabotProcessList.append(process)
+
+	# write to file
+	fileData['processList'] = runningIkabotProcessList
+	s.setFileData(fileData)
+	
+	return runningIkabotProcessList

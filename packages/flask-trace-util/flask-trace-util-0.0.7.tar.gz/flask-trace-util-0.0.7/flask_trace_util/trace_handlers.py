082de562c0be9c5b69c562d91673ee154041b445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package flask-trace-util/0.0.7/flask-trace-util-0.0.7.tar.gz

---
+++ packages/flask-trace-util/flask-trace-util-0.0.7.tar.gz/flask-trace-util-0.0.7/flask_trace_util/trace_handlers.py
@@ -0,0 +1,54 @@
+"""
+Contains common trace extractors and delegators
+"""
+from flask import request, g
+from flask_trace_util.gunicorn_logger import GunicornLogger
+
+
+def gcloud_trace_extractor(project_id):
+    """Trace extractor for google cloud GKE
+
+    Arguments:
+        project_id {str} -- project id
+    """
+
+    def extractor():
+        trace = request.headers.get("X-Cloud-Trace-Context")
+        if not trace:
+            return None
+        return f"projects/{project_id}/traces/{trace.split('/')[0]}"
+
+    return extractor
+
+
+def gcloud_trace_delegator():
+    """Trace delegator for google cloud GKE
+
+    Arguments:
+        trace {str} -- trace
+    """
+
+    def delegator():
+        if g.trace:
+            header = g.trace.split("/")[-1]
+            return "X-Cloud-Trace-Context", header
+        else:
+            return "X-Cloud-Trace-Context", ""
+
+    return delegator
+
+
+class GCloudGunicornAccesLogger(GunicornLogger):
+    """ Acces logger for flask, gunicorn and google cloud
+
+    Arguments:
+        GunicornLogger {type} -- flask trace util GunicornLogger
+    """
+
+    def get_custom_variables(self, *args):
+        """Returns custom variables for acces logging
+
+        Returns:
+            dict -- custom env variables
+        """
+        return {"logging.googleapis.com/trace": g.trace}

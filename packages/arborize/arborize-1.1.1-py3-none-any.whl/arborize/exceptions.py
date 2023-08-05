7e1a95aca05bf30bca8dd3cd6b969a838bd1fea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:14 +0000

    Add package arborize/1.1.1/arborize-1.1.1-py3-none-any.whl

---
+++ packages/arborize/arborize-1.1.1-py3-none-any.whl/arborize/exceptions.py
@@ -0,0 +1,29 @@
+class ArborizeError(Exception):
+    pass
+
+class ModelError(ArborizeError):
+    pass
+
+class ModelClassError(ModelError):
+    pass
+
+class MorphologyBuilderError(ModelError):
+    pass
+
+class MechanismNotPresentError(ModelClassError):
+    pass
+
+class SectionAttributeError(ModelClassError):
+    pass
+
+class ConnectionError(ArborizeError):
+    pass
+
+class AmbiguousSynapseError(ConnectionError):
+    pass
+
+class SynapseNotPresentError(ConnectionError):
+    pass
+
+class SynapseNotDefinedError(ConnectionError):
+    pass

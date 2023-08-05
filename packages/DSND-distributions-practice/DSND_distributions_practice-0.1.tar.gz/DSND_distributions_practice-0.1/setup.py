5c7defe45ba7bcb8a6bab299ca4625488cd2a5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:05 +0000

    Add package DSND-distributions-practice/0.1/DSND_distributions_practice-0.1.tar.gz

---
+++ packages/DSND-distributions-practice/DSND_distributions_practice-0.1.tar.gz/DSND_distributions_practice-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DSND_distributions_practice',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['DSND_distributions_practice'],
+      author= 'Francisco Patino',
+      email = 'francisco.patino@gmail.com',
+      zip_safe=False)

23b11fd8700107b9f0c7ad5a8d78fa3b6b0f1011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:40 +0000

    Add package score.ctx/0.6.2/score.ctx-0.6.2.tar.gz

---
+++ packages/score.ctx/score.ctx-0.6.2.tar.gz/score.ctx-0.6.2/score/ctx/cli.py
@@ -0,0 +1,42 @@
+# Copyright © 2019-2020 Necdet Can Ateşman, Vienna, Austria
+#
+# This file is part of the The SCORE Framework.
+#
+# The SCORE Framework and all its parts are free software: you can redistribute
+# them and/or modify them under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation which is in
+# the file named COPYING.LESSER.txt.
+#
+# The SCORE Framework and all its parts are distributed without any WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. For more details see the GNU Lesser General Public
+# License.
+#
+# If you have not received a copy of the GNU Lesser General Public License see
+# http://www.gnu.org/licenses/.
+#
+# The License-Agreement realised between you as Licensee and STRG.AT GmbH as
+# Licenser including the issue of its valid conclusion and its pre- and
+# post-contractual effects is governed by the laws of Austria. Any disputes
+# concerning this License-Agreement including the issue of its valid conclusion
+# and its pre- and post-contractual effects are exclusively decided by the
+# competent court, in whose district STRG.AT GmbH has its registered seat, at
+# the discretion of STRG.AT GmbH also the competent court, in whose district
+# the # Licensee has his registered seat, an establishment or assets.
+
+import functools
+
+
+def init_cli_ctx(command):
+    """
+    Decorator for click commands that provides a Context object.
+    """
+
+    from score.cli import init_score
+
+    @functools.wraps(command)
+    @init_score
+    def wrapped(score, *args, **kwargs):
+        with score.ctx.Context() as ctx:
+            return command(ctx, *args, **kwargs)
+    return wrapped

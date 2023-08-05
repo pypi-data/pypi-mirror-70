02c569ec911d8af3bd30548b9abbc248e2dfba14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:22 +0000

    Add package VisualiseKerasLayers/1.0/VisualiseKerasLayers-1.0.tar.gz

---
+++ packages/VisualiseKerasLayers/VisualiseKerasLayers-1.0.tar.gz/VisualiseKerasLayers-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='VisualiseKerasLayers',
+      version='1.0',
+      description='Package to generating plots with visualised activations of model layers',
+      author='Matt Kozlowski',
+      author_email='mateusz.a.kozlowski@gmail.com',
+      license='MIT',
+      packages=['VisualiseKerasLayers'],
+      zip_safe=False)

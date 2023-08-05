909e9868a65bc1270d164dc30e8aff15948f2073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:56 +0000

    Add package GaussianBinomialDistributions/0.1/GaussianBinomialDistributions-0.1.tar.gz

---
+++ packages/GaussianBinomialDistributions/GaussianBinomialDistributions-0.1.tar.gz/GaussianBinomialDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GaussianBinomialDistributions',
+      version='0.1',
+      description='Gaussian Binomial distributions',
+      packages=['GaussianBinomialDistributions'],
+      zip_safe=False)

ed1545299550fa8492d6cca0878f73599099f2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:53 +0000

    Add package mypy-boto3-macie/1.13.21.0/mypy-boto3-macie-1.13.21.0.tar.gz

---
+++ packages/mypy-boto3-macie/mypy-boto3-macie-1.13.21.0.tar.gz/mypy-boto3-macie-1.13.21.0/mypy_boto3_macie/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for macie service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.macie import (
+        Client,
+        ListMemberAccountsPaginator,
+        ListS3ResourcesPaginator,
+        MacieClient,
+        )
+
+    session = boto3.Session()
+
+    client: MacieClient = boto3.client("macie")
+    session_client: MacieClient = session.client("macie")
+
+    list_member_accounts_paginator: ListMemberAccountsPaginator = client.get_paginator("list_member_accounts")
+    list_s3_resources_paginator: ListS3ResourcesPaginator = client.get_paginator("list_s3_resources")
+"""
+from mypy_boto3_macie.client import MacieClient, MacieClient as Client
+from mypy_boto3_macie.paginator import ListMemberAccountsPaginator, ListS3ResourcesPaginator
+
+
+__all__ = ("Client", "ListMemberAccountsPaginator", "ListS3ResourcesPaginator", "MacieClient")

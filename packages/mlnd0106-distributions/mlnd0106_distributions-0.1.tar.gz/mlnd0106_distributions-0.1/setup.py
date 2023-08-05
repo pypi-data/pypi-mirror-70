09cddd14245d3ce52888a05500e9dab4d0730fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package mlnd0106-distributions/0.1/mlnd0106_distributions-0.1.tar.gz

---
+++ packages/mlnd0106-distributions/mlnd0106_distributions-0.1.tar.gz/mlnd0106_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mlnd0106_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mlnd0106_distributions'],
+      zip_safe=False)

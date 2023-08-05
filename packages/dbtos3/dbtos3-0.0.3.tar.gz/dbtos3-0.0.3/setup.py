a315a5660bbae663a52ccb7952f162239326e12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package dbtos3/0.0.3/dbtos3-0.0.3.tar.gz

---
+++ packages/dbtos3/dbtos3-0.0.3.tar.gz/dbtos3-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = [
+    'boto3',
+    'numpy',
+    'psycopg2',
+    'pandas',
+    'mysql-connector-python',
+    'flask'
+]
+
+setuptools.setup(
+    name='dbtos3',
+    version='0.0.3',
+    description='Replication & Full Load Application for multiple databases to s3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='DirkSCGM',
+    author_email='dirkscgm@gmail.com',
+    url='https://github.com/DirksCGM/DBtoS3',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    packages=setuptools.find_packages(),
+    install_requires=requires,
+    python_requires='>=3.6',
+    keywords=['postgresql', 's3', 'aws', 'mysql', 'sentry', 'replication', 'sql']
+)

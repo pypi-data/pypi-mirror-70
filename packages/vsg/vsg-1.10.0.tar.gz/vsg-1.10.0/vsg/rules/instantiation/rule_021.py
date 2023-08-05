e8f4e063f955448114dd80161544a0989708117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:57 +0000

    Add package vsg/1.10.0/vsg-1.10.0.tar.gz

---
+++ packages/vsg/vsg-1.10.0.tar.gz/vsg-1.10.0/vsg/rules/instantiation/rule_021.py
@@ -0,0 +1,41 @@
+
+from vsg import rule
+
+import re
+import copy
+
+
+class rule_021(rule.rule):
+    '''
+    Instantiation rule 021 checks multiple port assignments on the same line.
+    '''
+
+    def __init__(self):
+        rule.rule.__init__(self)
+        self.name = 'instantiation'
+        self.identifier = '021'
+        self.solution = 'Move multiple port assignments to their own lines.'
+        self.phase = 1
+
+    def _analyze(self, oFile, oLine, iLineNumber):
+        if oLine.isInstantiationPortAssignment:
+            if re.match('^\s*\S+\s*=>\s*.*\s*,\s*\S+\s*=>', oLine.line):
+                sTemp = re.match('(^\s*\S+\s*=>\s*.*\s*),\s*\S+\s*=>', oLine.line).group(0)
+                if sTemp.count('(') == sTemp.count(')'):
+                    self.add_violation(iLineNumber)
+
+    def _fix_violations(self, oFile):
+        for iLineNumber in self.violations[::-1]:
+            oLine = oFile.lines[iLineNumber]
+            iNumberOfPorts = oLine.line.count(',')
+            # Replicate ports ###
+            for iIndex in range(1, iNumberOfPorts):
+                oFile.lines.insert(iLineNumber, copy.deepcopy(oLine))
+            # Split ports
+            for iIndex in range(0, iNumberOfPorts):
+                oLine = oFile.lines[iLineNumber + iIndex]
+                lPorts = oLine.line.split(',')
+                if iIndex == iNumberOfPorts - 1 and oFile.lines[iLineNumber + iIndex + 1].isInstantiationPortEnd:
+                    oLine.update_line(lPorts[iIndex])
+                else:
+                    oLine.update_line(lPorts[iIndex] + ',')
---
+++ packages/vsg/vsg-1.10.0.tar.gz/vsg-1.10.0/vsg/rules/port/rule_013.py
@@ -0,0 +1,34 @@
+
+from vsg import rule
+
+import re
+import copy
+
+
+class rule_013(rule.rule):
+    '''
+    Port rule 013 checks for multiple ports declared on single line.
+    '''
+
+    def __init__(self):
+        rule.rule.__init__(self, 'port', '013')
+        self.solution = 'Place multiple ports on their own lines.'
+        self.phase = 1
+
+    def _analyze(self, oFile, oLine, iLineNumber):
+        if oLine.isPortDeclaration and re.match('^.*,.*:', oLine.lineNoComment):
+            self.add_violation(iLineNumber)
+
+    def _fix_violations(self, oFile):
+        for iLineNumber in self.violations[::-1]:
+            oLine = oFile.lines[iLineNumber]
+            iNumberOfPorts = oLine.line.split(':')[0].count(',') + 1
+            # Replicate ports
+            for iIndex in range(1, iNumberOfPorts):
+                oFile.lines.insert(iLineNumber, copy.deepcopy(oLine))
+            # Split ports
+            for iIndex in range(0, iNumberOfPorts):
+                oLine = oFile.lines[iLineNumber + iIndex]
+                lLine = oLine.line.split(':')
+                lPorts = lLine[0].split(',')
+                oLine.update_line(lPorts[iIndex] + ' :' + lLine[1])

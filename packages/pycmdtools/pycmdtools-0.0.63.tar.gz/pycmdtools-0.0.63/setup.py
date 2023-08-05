46caa16a593f55295d040c84bc86a9c17aeb8fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:01 +0000

    Add package pycmdtools/0.0.63/pycmdtools-0.0.63.tar.gz

---
+++ packages/pycmdtools/pycmdtools-0.0.63.tar.gz/pycmdtools-0.0.63/setup.py
@@ -0,0 +1,56 @@
+import setuptools
+
+setuptools.setup(
+    # the first three fields are a must according to the documentation
+    name='pycmdtools',
+    version='0.0.63',
+    packages=[
+        'pycmdtools',
+        'pycmdtools.endpoints',
+    ],
+    # from here all is optional
+    description='pycmdtools is set of useful command line tools written in python',
+    long_description='pycmdtools is set of useful command line tools written in python',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
+    keywords=[
+        'utils',
+        'command line',
+        'python',
+        'shell',
+        'utilities',
+    ],
+    url='https://veltzer.github.io/pycmdtools',
+    download_url='https://github.com/veltzer/pycmdtools',
+    license='MIT',
+    platforms=[
+        'python3',
+    ],
+    install_requires=[
+        'pylogconf',
+        'pytconf',
+        'requests',
+        'tqdm',
+        'numpy',
+        'pandas',
+        'unidecode',
+        'pyyaml',
+    ],
+    extras_require={
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    data_files=[
+    ],
+    entry_points={'console_scripts': [
+        'pycmdtools=pycmdtools.endpoints.main:main',
+    ]},
+    python_requires='>=3.5',
+)

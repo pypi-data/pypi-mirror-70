89648a5e36d1a3eaf8b089828278bd32fc50f922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:36 +0000

    Add package pysprint/0.9.0/pysprint-0.9.0-py2.py3-none-any.whl

---
+++ packages/pysprint/pysprint-0.9.0-py2.py3-none-any.whl/pysprint/core/bases/dataset_base.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod, ABCMeta
+
+__all__ = ['DatasetBase']
+
+C_LIGHT = 299.792458
+
+class DatasetBase(metaclass=ABCMeta):
+	"""Base metaclass that defines the interface
+	for any interferogram."""
+
+	@abstractmethod
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def GD_lookup(self, *args, **kwargs):
+		''' Quick GD lookup'''
+
+	@property
+	@abstractmethod
+	def data(self):
+		'''The stored dataset.'''
+		return None
+
+	@abstractmethod
+	def show(self):
+		'''Plot the stored dataset. '''
+		pass

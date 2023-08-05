7f83acc68c4026a0753a6c49f24eb699a3fab526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:26 +0000

    Add package pygamp/0.11/PyGAMP-0.11-py3-none-any.whl

---
+++ packages/pygamp/PyGAMP-0.11-py3-none-any.whl/PyGAMP/transport.py
@@ -0,0 +1,30 @@
+import requests
+import random
+
+endpoint = 'https://www.google-analytics.com/collect'
+user_agent = 'PyGAMP'
+
+
+def send(payload, property_id):
+    """Send a payload to Google Analytics using the Measurement Protocol API.
+
+    :param payload: Python dictionary of URL key value pairs
+    :param property_id: Universal Analytics property ID, i.e. UA-123456-1
+    :return: HTTP response status
+    """
+
+    required_payload = {
+        'v': 1,
+        'tid': property_id,
+        'aip': 1,
+        'z': random.random()
+    }
+
+    final_payload = {**required_payload, **payload}
+    response = requests.post(url=endpoint,
+                             data=final_payload,
+                             headers={'User-Agent': user_agent},
+                             timeout=5.0)
+    print(response.request.body)
+    return response
+

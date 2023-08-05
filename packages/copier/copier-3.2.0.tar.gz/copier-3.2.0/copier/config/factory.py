49449325d783e6b109c75c42428664af5107b46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:52 +0000

    Add package copier/3.2.0/copier-3.2.0.tar.gz

---
+++ packages/copier/copier-3.2.0.tar.gz/copier-3.2.0/copier/config/factory.py
@@ -0,0 +1,118 @@
+from typing import Tuple
+
+import semver
+from plumbum import local
+from plumbum.cmd import git
+
+from .. import vcs
+from ..types import AnyByStrDict, OptAnyByStrDict, OptBool, OptStr, OptStrSeq
+from .objects import DEFAULT_DATA, ConfigData, EnvOps, NoSrcPathError, UserMessageError
+from .user_data import load_answersfile_data, load_config_data, query_user_data
+
+__all__ = ("make_config",)
+
+
+def filter_config(data: AnyByStrDict) -> Tuple[AnyByStrDict, AnyByStrDict]:
+    """Separates config and questions data."""
+    conf_data: AnyByStrDict = {"secret_questions": set()}
+    questions_data = {}
+    for k, v in data.items():
+        if k == "_secret_questions":
+            conf_data["secret_questions"].update(v)
+        elif k.startswith("_"):
+            conf_data[k[1:]] = v
+        else:
+            # Transform simplified questions format into complex
+            if not isinstance(v, dict):
+                v = {"default": v}
+            questions_data[k] = v
+            if v.get("secret"):
+                conf_data["secret_questions"].add(k)
+    return conf_data, questions_data
+
+
+def verify_minimum_version(version: str) -> None:
+    """Raise an error if the current Copier version is less than the given version."""
+    # Importing __version__ at the top of the module creates a circular import
+    # ("cannot import name '__version__' from partially initialized module 'copier'"),
+    # so instead we do a lazy import here
+    from .. import __version__
+
+    if semver.VersionInfo.parse(__version__).compare(version) == -1:
+        raise UserMessageError(
+            f"This template requires Copier version >= {version}, "
+            f"while your version of Copier is {__version__}."
+        )
+
+
+def make_config(
+    src_path: OptStr = None,
+    dst_path: str = ".",
+    *,
+    answers_file: OptStr = None,
+    data: OptAnyByStrDict = None,
+    exclude: OptStrSeq = None,
+    skip_if_exists: OptStrSeq = None,
+    tasks: OptStrSeq = None,
+    envops: OptAnyByStrDict = None,
+    extra_paths: OptStrSeq = None,
+    pretend: OptBool = None,
+    force: OptBool = None,
+    skip: OptBool = None,
+    quiet: OptBool = None,
+    cleanup_on_error: OptBool = None,
+    vcs_ref: OptStr = None,
+    subdirectory: OptStr = None,
+    **kwargs,
+) -> ConfigData:
+    """Provides the configuration object, merged from the different sources.
+
+    The order of precedence for the merger of configuration objects is:
+    function_args > user_data > defaults.
+    """
+    # Merge answer sources in the order of precedence
+    answers_data = DEFAULT_DATA.copy()
+    answers_data.update(load_answersfile_data(dst_path, answers_file))
+    answers_data.update(data or {})
+
+    _metadata = {}
+    if "_commit" in answers_data:
+        _metadata["old_commit"] = answers_data["_commit"]
+    # Detect original source if running in update mode
+    if src_path is None:
+        try:
+            src_path = answers_data["_src_path"]
+        except KeyError:
+            raise NoSrcPathError(
+                "No copier answers file found, or it didn't include "
+                "original template information (_src_path). "
+                "Run `copier copy` instead."
+            )
+    _metadata["original_src_path"] = src_path
+    if src_path:
+        repo = vcs.get_repo(src_path)
+        if repo:
+            src_path = vcs.clone(repo, vcs_ref or "HEAD")
+            vcs_ref = vcs_ref or vcs.checkout_latest_tag(src_path)
+            with local.cwd(src_path):
+                _metadata["commit"] = git("describe", "--tags", "--always").strip()
+    # Obtain config and query data, asking the user if needed
+    file_data = load_config_data(src_path, quiet=True)
+
+    try:
+        verify_minimum_version(file_data["_min_copier_version"])
+    except KeyError:
+        pass
+
+    config_data, questions_data = filter_config(file_data)
+    config_data.update(_metadata)
+    del _metadata
+    args = {k: v for k, v in locals().items() if v is not None and v != []}
+    env_ops = EnvOps(**config_data.get("envops", {}))
+    config_data["data"] = query_user_data(
+        questions_data, answers_data, not force, env_ops
+    )
+    args = {**config_data, **args}
+    args["envops"] = env_ops
+    args["data"].update(config_data["data"])
+    return ConfigData(**args)
---
+++ packages/copier/copier-3.2.0.tar.gz/copier-3.2.0/copier/config/objects.py
@@ -0,0 +1,113 @@
+import datetime
+from hashlib import sha512
+from os import urandom
+from pathlib import Path
+from typing import Any, Sequence, Tuple, Union
+
+from pydantic import BaseModel, Extra, StrictBool, validator
+
+from ..types import AnyByStrDict, OptStr, PathSeq, StrOrPathSeq, StrSeq
+
+# Default list of files in the template to exclude from the rendered project
+DEFAULT_EXCLUDE: Tuple[str, ...] = (
+    "copier.yaml",
+    "copier.yml",
+    "~*",
+    "*.py[co]",
+    "__pycache__",
+    ".git",
+    ".DS_Store",
+    ".svn",
+)
+
+DEFAULT_DATA: AnyByStrDict = {
+    "now": datetime.datetime.utcnow,
+    "make_secret": lambda: sha512(urandom(48)).hexdigest(),
+}
+
+DEFAULT_TEMPLATES_SUFFIX = ".tmpl"
+
+
+class UserMessageError(Exception):
+    """Exit the program giving a message to the user."""
+
+    pass
+
+
+class NoSrcPathError(UserMessageError):
+    pass
+
+
+class EnvOps(BaseModel):
+    autoescape: StrictBool = False
+    block_start_string: str = "[%"
+    block_end_string: str = "%]"
+    comment_start_string: str = "[#"
+    comment_end_string: str = "#]"
+    variable_start_string: str = "[["
+    variable_end_string: str = "]]"
+    keep_trailing_newline: StrictBool = True
+
+    class Config:
+        allow_mutation = False
+        extra = Extra.allow
+
+
+class Migrations(BaseModel):
+    version: str
+    before: Sequence[Union[str, StrSeq]] = ()
+    after: Sequence[Union[str, StrSeq]] = ()
+
+
+class ConfigData(BaseModel):
+    src_path: Path
+    subdirectory: OptStr
+    dst_path: Path
+    data: AnyByStrDict = {}
+    extra_paths: PathSeq = ()
+    exclude: StrOrPathSeq = DEFAULT_EXCLUDE
+    skip_if_exists: StrOrPathSeq = ()
+    tasks: Sequence[Union[str, StrSeq]] = ()
+    envops: EnvOps = EnvOps()
+    templates_suffix: str = DEFAULT_TEMPLATES_SUFFIX
+    original_src_path: OptStr
+    commit: OptStr
+    old_commit: OptStr
+    cleanup_on_error: StrictBool = True
+    force: StrictBool = False
+    only_diff: StrictBool = True
+    pretend: StrictBool = False
+    quiet: StrictBool = False
+    skip: StrictBool = False
+    vcs_ref: OptStr
+    migrations: Sequence[Migrations] = ()
+    secret_questions: StrSeq = ()
+    answers_file: Path = Path(".copier-answers.yml")
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.data.update(dict(DEFAULT_DATA, _folder_name=self.dst_path.name))
+
+    @validator("skip", always=True)
+    def mutually_exclusive_flags(cls, v, values):  # noqa: B902
+        if v and values["force"]:
+            raise ValueError(f"Flags `force` and `skip` are mutually exclusive.")
+        return v
+
+    # sanitizers
+    @validator("src_path", "dst_path", "extra_paths", pre=True, each_item=True)
+    def resolve_path(cls, v: Path) -> Path:  # noqa: B902
+        return Path(v).expanduser().resolve()
+
+    @validator("src_path", "extra_paths", pre=True, each_item=True)
+    def dir_must_exist(cls, v: Path) -> Path:  # noqa: B902
+        if not v.exists():
+            raise ValueError("Project template not found.")
+        if not v.is_dir():
+            raise ValueError("Project template not a folder.")
+        return v
+
+    # configuration
+    class Config:
+        allow_mutation = False
+        anystr_strip_whitespace = True

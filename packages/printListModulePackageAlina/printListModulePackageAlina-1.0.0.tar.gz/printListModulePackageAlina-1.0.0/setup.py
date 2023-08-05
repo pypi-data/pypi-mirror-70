2349bd6b2428d38c435178cc07221ffc9f323318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:47 +0000

    Add package printListModulePackageAlina/1.0.0/printListModulePackageAlina-1.0.0.tar.gz

---
+++ packages/printListModulePackageAlina/printListModulePackageAlina-1.0.0.tar.gz/printListModulePackageAlina-1.0.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 13:44:02 2020
+
+@author: Ignitarium
+"""
+
+
+from distutils.core import setup
+setup(name = "printListModulePackageAlina", version = "1.0.0", description = "an API to print\
+              a list recursively", author = "Alina",\
+                  author_email = "alina.jose@ignitarium.com",\
+                      py_modules = ["printListModulePackageAlina"])

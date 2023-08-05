7b89d1c096829e9e929ef378cf2b827f4cf228bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:57 +0000

    Add package python-sonarqube-api/1.0.1/python-sonarqube-api-1.0.1.tar.gz

---
+++ packages/python-sonarqube-api/python-sonarqube-api-1.0.1.tar.gz/python-sonarqube-api-1.0.1/sonarqube/project_links.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from .config import *
+
+
+class SonarQubeProject_Links(object):
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def create_project_links(self, projectKey, name, url):
+        """
+        Create a new project link.
+        :param projectKey:
+        :param name:
+        :param url:
+        :return:
+        """
+        params = {
+            'projectKey': projectKey,
+            'name': name,
+            'url': url
+        }
+        self.sonarqube._make_call('post', API_PROJECT_LINKS_CREATE_ENDPOINT, **params)
+
+    def delete_project_links(self, id):
+        """
+        Delete existing project link.
+        :param id:
+        :return:
+        """
+        params = {
+            'id': id
+        }
+        self.sonarqube._make_call('post', API_PROJECT_LINKS_DELETE_ENDPOINT, **params)
+
+    def search_project_links(self, projectKey):
+        """
+        List links of a project.
+        :param projectKey:
+        :return:
+        """
+        params = {
+            'projectKey': projectKey
+        }
+        resp = self.sonarqube._make_call('get', API_PROJECT_LINKS_SEARCH_ENDPOINT, **params)
+        data = resp.json()
+        return data["links"]

9cc3cb27a7dc43f434a3e7838fc104d07d829bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:52 +0000

    Add package production_rest_client/2.2.6/production_rest_client-2.2.6.tar.gz

---
+++ packages/production_rest_client/production_rest_client-2.2.6.tar.gz/production_rest_client-2.2.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+#python setup.py sdist upload  
+setup(
+    name = 'production_rest_client',
+    version = '2.2.6',
+    keywords = ['runner', 'client'],
+    description = 'REST Runner',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+      ],
+    license = 'MIT License',
+	url = 'https://pypi.org/project/production_rest_client',
+    install_requires = ['simplejson>=1.1'],
+ 
+    author = 'weili.han',
+    author_email = '826338726@qq.com',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)

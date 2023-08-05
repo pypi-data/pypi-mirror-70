10c62d11c2f90d2aae2d46d997b37bc19077992a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:17 +0000

    Add package pywilight/0.0.32/pywilight-0.0.32.tar.gz

---
+++ packages/pywilight/pywilight-0.0.32.tar.gz/pywilight-0.0.32/pywilight/__init__.py
@@ -0,0 +1,3 @@
+"""Lightweight Python module to discover and control WiLight devices."""
+from .discovery import discover_devices  # noqa F401
+from .subscribe import SubscriptionRegistry  # noqa F401

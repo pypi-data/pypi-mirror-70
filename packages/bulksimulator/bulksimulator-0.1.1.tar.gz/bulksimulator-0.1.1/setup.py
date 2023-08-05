8ab171f4daf8ad647cb2c1ad591495c6d0032828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package bulksimulator/0.1.1/bulksimulator-0.1.1.tar.gz

---
+++ packages/bulksimulator/bulksimulator-0.1.1.tar.gz/bulksimulator-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='bulksimulator',
+    version='0.1.1',
+    description='Module to run a simulation for many parameter choices',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    author='Jeremy Worsfold',
+    author_email='jw3286@bath.ac.uk',
+    url='https://github.bath.ac.uk/jw3286/bulksimulator',
+    license=license,
+    packages=find_packages(exclude=('tests'))
+)

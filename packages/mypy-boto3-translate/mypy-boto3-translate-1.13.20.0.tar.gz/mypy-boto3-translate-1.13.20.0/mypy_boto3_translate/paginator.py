05cb3affa57dca5561be224a9530257c0c75c1bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package mypy-boto3-translate/1.13.20.0/mypy-boto3-translate-1.13.20.0.tar.gz

---
+++ packages/mypy-boto3-translate/mypy-boto3-translate-1.13.20.0.tar.gz/mypy-boto3-translate-1.13.20.0/mypy_boto3_translate/paginator.py
@@ -0,0 +1,34 @@
+"""
+Main interface for translate service client paginators.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.translate import (
+        ListTerminologiesPaginator,
+    )
+
+    client: TranslateClient = boto3.client("translate")
+
+    list_terminologies_paginator: ListTerminologiesPaginator = client.get_paginator("list_terminologies")
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_translate.type_defs import ListTerminologiesResponseTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListTerminologiesPaginator",)
+
+
+class ListTerminologiesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListTerminologies documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/translate.html#Translate.Paginator.ListTerminologies)
+    """
+
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListTerminologiesResponseTypeDef]:
+        """
+        [ListTerminologies.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.20/reference/services/translate.html#Translate.Paginator.ListTerminologies.paginate)
+        """

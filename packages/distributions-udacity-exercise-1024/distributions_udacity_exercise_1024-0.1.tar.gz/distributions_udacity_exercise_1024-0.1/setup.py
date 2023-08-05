5e26502dbfe32e26355cc3707090f1f6be75ee7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package distributions-udacity-exercise-1024/0.1/distributions_udacity_exercise_1024-0.1.tar.gz

---
+++ packages/distributions-udacity-exercise-1024/distributions_udacity_exercise_1024-0.1.tar.gz/distributions_udacity_exercise_1024-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_exercise_1024',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_exercise_1024'],
+      zip_safe=False)

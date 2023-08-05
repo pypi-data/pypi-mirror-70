3a08398ad63d17d5d195a113325ee9b5fd8cc0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:39 +0000

    Add package textnets/0.3.6/textnets-0.3.6-py2.py3-none-any.whl

---
+++ packages/textnets/textnets-0.3.6-py2.py3-none-any.whl/textnets/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Textnets."""
+
+from .corpus import Corpus  # noqa: F401
+from .network import Textnet  # noqa: F401
+
+
+__author__ = """John D. Boy"""
+__email__ = "jboy@bius.moe"
+__version__ = "0.3.6"

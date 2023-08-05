5e91c458b1a3b1e96b25646d02da37bd02f303f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:18 +0000

    Add package binarytree/5.1.0/binarytree-5.1.0.tar.gz

---
+++ packages/binarytree/binarytree-5.1.0.tar.gz/binarytree-5.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open("./binarytree/version.py") as fp:
+    exec(fp.read(), version)
+
+with open('./README.rst') as fp:
+    description = fp.read()
+
+setup(
+    name='binarytree',
+    description='Python Library for Studying Binary Trees',
+    long_description=description,
+    version=version['__version__'],
+    author='Joohwan Oh',
+    author_email='joohwan.oh@outlook.com',
+    url='https://github.com/joowani/binarytree',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    tests_require=['pytest', 'flake8'],
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Education'
+    ]
+)

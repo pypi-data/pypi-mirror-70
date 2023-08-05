83d2290866844aad84d8649792c7aa70efd2c5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:07 +0000

    Add package deepray/0.1.1/deepray-0.1.1.tar.gz

---
+++ packages/deepray/deepray-0.1.1.tar.gz/deepray-0.1.1/deepray/base/metrics.py
@@ -0,0 +1,38 @@
+#  Copyright © 2020-2020 Hailin Fu All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ==============================================================================
+import tensorflow as tf
+
+
+class CategoricalTruePositives(tf.keras.metrics.Metric):
+
+    def __init__(self, name='categorical_true_positives', **kwargs):
+        super(CategoricalTruePositives, self).__init__(name=name, **kwargs)
+        self.true_positives = self.add_weight(name='tp', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.reshape(tf.argmax(y_pred, axis=1), shape=(-1, 1))
+        values = tf.cast(y_true, 'int32') == tf.cast(y_pred, 'int32')
+        values = tf.cast(values, 'float32')
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, 'float32')
+            values = tf.multiply(values, sample_weight)
+        self.true_positives.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.true_positives
+
+    def reset_states(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.true_positives.assign(0.)

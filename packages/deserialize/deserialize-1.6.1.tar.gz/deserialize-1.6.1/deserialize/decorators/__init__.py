545e916704d1ef4642dc548a85c22a1ca52d793c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:00 +0000

    Add package deserialize/1.6.1/deserialize-1.6.1.tar.gz

---
+++ packages/deserialize/deserialize-1.6.1.tar.gz/deserialize-1.6.1/deserialize/decorators/__init__.py
@@ -0,0 +1,14 @@
+"""Decorators used for adding functionality to the library."""
+
+from deserialize.decorators.constructed import constructed, _call_constructed
+from deserialize.decorators.default import default, _get_default, _has_default
+from deserialize.decorators.downcasting import (
+    downcast_field,
+    _get_downcast_field,
+    downcast_identifier,
+    _get_downcast_class,
+)
+from deserialize.decorators.ignore import ignore, _should_ignore
+from deserialize.decorators.key import key, _get_key
+from deserialize.decorators.parser import parser, _get_parser
+from deserialize.decorators.unhandled import allow_unhandled, _should_allow_unhandled

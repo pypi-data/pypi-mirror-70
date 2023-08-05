b3afe3c38a51f3f7baed9988307167de00fdbca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package rpa-ocr/0.0.6/rpa_ocr-0.0.6-py3-none-any.whl

---
+++ packages/rpa-ocr/rpa_ocr-0.0.6-py3-none-any.whl/rpa_ocr/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @author :adolf
+__name__ = 'rpa_ocr'
+from rpa_ocr.Identify_English.train_tools import Train
+from rpa_ocr.verification_service.verification_main import ocr_pipeline_main

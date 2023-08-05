7e49586b64dea8bf1e7251088fef61bbdecf551b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:08 +0000

    Add package distr-12/0.1/distr_12-0.1.tar.gz

---
+++ packages/distr-12/distr_12-0.1.tar.gz/distr_12-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distr_12',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Erwin',
+      author_email='erwinhuang16@gmail.com',
+      zip_safe=False)

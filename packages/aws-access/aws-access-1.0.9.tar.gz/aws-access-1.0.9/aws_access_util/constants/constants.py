0f8ba3e32ca51784204c0dea3e1735871bd817c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package aws-access/1.0.9/aws-access-1.0.9.tar.gz

---
+++ packages/aws-access/aws-access-1.0.9.tar.gz/aws-access-1.0.9/aws_access_util/constants/constants.py
@@ -0,0 +1,25 @@
+# EXIT CODES
+success = 0
+failure = 1
+# MESSAGES
+support_contact = "Please connect with the support team"
+# ALL DUO CONSTANTS
+sslverification = True
+auth_url = "https://{duo_host}/frame/web/v1/auth"
+prompt_url = "https://{duo_host}/frame/prompt"
+status_url = "https://{duo_host}/frame/status"
+result_url = "https://{duo_host}{duo_result_url}"
+call_back_url = "{cisco_url}{duo_callback}?sig_response={sig_response}"
+user_type = 'cco'
+# AWS Params
+awsfolder = '/.aws/'
+awsconfigfile = '/.aws/credentials'
+outputformat = 'json'
+region = 'us-west-2'
+default_access_duration = 43200
+# DUO OPTIONS
+options = [
+    'Send Me a Push,Duo+Push',
+    'Call Me,Phone+Call',
+    'SMS me a code,sms',
+    'Passcode,Passcode']

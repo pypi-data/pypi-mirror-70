851ec0206e475f684931b9a704da90bfaf06199f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:54 +0000

    Add package octool/0.0.11/octool-0.0.11-py3-none-any.whl

---
+++ packages/octool/octool-0.0.11-py3-none-any.whl/octool/__main__.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2020 jumanjiman (Paul Morgan) <jumanjiman@gmail.com>
+#
+# This file is part of octool.
+#
+# octool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# octool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with octool.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""CLI component for OCTool."""
+import logging
+import sys
+
+from .cli import Cli
+
+
+def main():
+    """Invoke octool as a CLI utility."""
+    try:
+        Cli().main()
+    except KeyboardInterrupt:
+        logging.error("Exit on keyboard interrupt")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()

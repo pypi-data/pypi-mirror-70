b899d77ce75c95abdb8375506cb4f8157e6e096a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:42 +0000

    Add package cidan/0.1.12/CIDAN-0.1.12.tar.gz

---
+++ packages/cidan/CIDAN-0.1.12.tar.gz/CIDAN-0.1.12/CIDAN/GUI/Data_Interaction/loadDataset.py
@@ -0,0 +1,86 @@
+import logging
+import os
+
+from CIDAN.GUI.Data_Interaction.DataHandler import DataHandler
+from CIDAN.GUI.Inputs.FileInput import createFileDialog
+
+logger1 = logging.getLogger("CIDAN.loadDataset")
+
+
+# def loadImageWrapper(main_widget):
+#     def loadImage():
+#         path_to_file = createFileDialog("~/Desktop", forOpen=True, fmt='', isFolder=0)
+#         path_to_save_dir = createFileDialog("~/Desktop", forOpen=False, fmt='',
+#                                             isFolder=1)
+#         if hasattr(main_widget, "data_handler"):
+#             main_widget.data_handler.__del__()
+#         main_widget.data_handler = DataHandler(data_path=path_to_file,
+#                                                save_dir_path=path_to_save_dir,
+#                                                save_dir_already_created=False)
+#         main_widget.image_view.setImage(main_widget.data_handler.calculate_filters())
+#
+#     return loadImage
+
+
+def load_new_dataset(main_widget, file_input, save_dir_input, trials=None, single=False):
+    print(trials)
+
+    file_path = file_input.current_state()
+    save_dir_path = save_dir_input.current_state()
+    # if hasattr(main_widget, "data_handler"):
+    #     main_widget.data_handler.__del__()
+    if single:
+        try:
+            dir_path = os.path.dirname(file_path)
+
+            main_widget.data_handler = DataHandler(data_path=dir_path,
+                                                   trials=[os.path.basename(file_path)],
+                                                   save_dir_path=save_dir_path,
+                                                   save_dir_already_created=False)
+            main_widget.init_w_data()
+        except IndentationError as e:
+            logger1.error(e)
+            print("Loading Failed please make sure it is a valid file")
+    elif not trials:
+            try:
+                dir_path = os.path.dirname(file_path)
+
+                main_widget.data_handler = DataHandler(data_path=dir_path,
+                                                       trials=[os.path.basename(file_path)],
+                                                       save_dir_path=save_dir_path,
+                                                       save_dir_already_created=False)
+                main_widget.init_w_data()
+            except IndentationError as e:
+                logger1.error(e)
+                print("Loading Failed please make sure it is a valid file")
+    elif trials:
+        logger1.debug("Trials:" + str(trials))
+        if len(trials) == 0:
+            print("Please select at least one trial")
+        try:
+            main_widget.data_handler = DataHandler(data_path=file_path, trials=trials,
+                                                   save_dir_path=save_dir_path,
+                                                   save_dir_already_created=False)
+            main_widget.init_w_data()
+        except IndentationError as e:
+            logger1.error(e)
+            print("Loading Failed please make sure it is a valid folder and all trials"
+                  + " are valid files")
+
+
+def load_prev_session(main_widget, save_dir_input):
+    # TODO add error handling here to ensure valid inputs
+    save_dir_path = save_dir_input.current_state()
+    try:
+        main_widget.data_handler = DataHandler(data_path="",
+                                               save_dir_path=save_dir_path,
+                                               save_dir_already_created=True)
+        main_widget.init_w_data()
+    except Exception as e:
+        logger1.error(e)
+        print("Loading Failed please try again, if problem persists save directory " +
+              "is corrupted")
+
+
+def export_timetraces(main_widget):
+    createFileDialog(forOpen=False, isFolder=2)
---
+++ packages/cidan/CIDAN-0.1.12.tar.gz/CIDAN-0.1.12/CIDAN/GUI/ImageView/ImageViewModule.py
@@ -0,0 +1,50 @@
+from pyqtgraph import ImageView
+from qtpy.QtWidgets import *
+
+
+class ImageViewModule(QFrame):
+    def __init__(self, main_widget, histogram=True, roi=True):
+        super().__init__()
+
+        self.main_window = main_widget
+        # self.setMinimumWidth(600)
+        # self.setMinimumHeight(300)
+        # self.setStyleSheet("ImageViewModule {margin:5px; border:1px solid rgb(50, 65, "
+        #                    "75);} ")
+        self.setStyleSheet("ImageViewModule {margin:0px; border:0px  solid rgb(50, 65, "
+                           "75); padding: 0px;} ")
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        # self.layout.setAlignment(Qt.AlignHCenter)
+
+        self.setLayout(self.layout)
+        # self.already_loaded = True
+        # self.no_image_message = QPushButton("Please open a dataset first")
+        # self.no_image_message.clicked.connect(main_widget.open_file_dialog)
+        # self.no_image_message.setStyleSheet("QPushButton {font-size:80;}")
+        self.image_view = ImageView()
+        self.image_view.ui.layoutWidget.setContentsMargins(0, 0, 0, 0)
+        # self.image_view.ui.roiBtn.hide()
+        self.image_view.ui.menuBtn.hide()
+        if not histogram:
+            self.image_view.ui.histogram.hide()
+        if not roi:
+            self.image_view.ui.roiBtn.hide()
+        # self.image_view.getRoiPlot().hide()
+
+        self.layout.addWidget(self.image_view)
+
+    def setImage(self, data):
+
+        # if self.already_loaded == False:
+        #     print("changed image")
+        #     self.already_loaded = True
+        #     self.layout.removeWidget(self.no_image_message)
+        #     self.no_image_message.deleteLater()
+        #     # self.layout.setAlignment(Qt.AlignLeft)
+        #     self.image_view = ImageView()
+        #
+        #     self.layout.addWidget(self.image_view)
+        self.image_view.setImage(data, levelMode='mono', autoRange=True,
+                                 autoLevels=True, autoHistogramRange=True)

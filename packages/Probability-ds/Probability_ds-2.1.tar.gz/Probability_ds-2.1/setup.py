86fee3f5172510f26fe14a43396c3d8820d601d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:35 +0000

    Add package Probability-ds/2.1/Probability_ds-2.1.tar.gz

---
+++ packages/Probability-ds/Probability_ds-2.1.tar.gz/Probability_ds-2.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Probability_ds',
+      version='2.1',
+      description='Gaussian distributions',
+      packages=['probability_ds'],
+      zip_safe=False)

5d4de075d15c8efdbda22f0bd44a306b0fb0fb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package diff-cover/3.0.1/diff_cover-3.0.1.tar.gz

---
+++ packages/diff-cover/diff_cover-3.0.1.tar.gz/diff_cover-3.0.1/diff_cover/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+# This file is imported in setup.py and cannot use any external dependencies
+
+VERSION = '3.0.1'
+DESCRIPTION = 'Automatically find diff lines that need test coverage.'
+QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'

77b8276bb15b1f1b3d49224c46850cc26c709ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package livestyled/0.0.1.dev121/livestyled-0.0.1.dev121.tar.gz

---
+++ packages/livestyled/livestyled-0.0.1.dev121.tar.gz/livestyled-0.0.1.dev121/livestyled/models/__init__.py
@@ -0,0 +1,44 @@
+from livestyled.models.booking import Booking
+from livestyled.models.cohort import Cohort
+from livestyled.models.competition import Competition
+from livestyled.models.device import Device
+from livestyled.models.device_preference import DevicePreference
+from livestyled.models.device_push_consent import DevicePushConsent
+from livestyled.models.device_token import DeviceToken
+from livestyled.models.event import Event
+from livestyled.models.fixture import Fixture
+from livestyled.models.league_table import LeagueTable, LeagueTableGroup
+from livestyled.models.magic_field import MagicField
+from livestyled.models.news import News
+from livestyled.models.push_broadcast import PushBroadcast
+from livestyled.models.push_consent import PushConsent
+from livestyled.models.season import Season
+from livestyled.models.sport_venue import SportVenue
+from livestyled.models.team import Team
+from livestyled.models.ticket import Ticket
+from livestyled.models.user import User, UserInfo, UserSSO
+
+__all__ = [
+    Booking,
+    Cohort,
+    Competition,
+    Device,
+    DevicePreference,
+    DevicePushConsent,
+    DeviceToken,
+    Event,
+    Fixture,
+    LeagueTable,
+    LeagueTableGroup,
+    MagicField,
+    News,
+    PushBroadcast,
+    PushConsent,
+    Season,
+    SportVenue,
+    Team,
+    Ticket,
+    UserInfo,
+    User,
+    UserSSO
+]
---
+++ packages/livestyled/livestyled-0.0.1.dev121.tar.gz/livestyled-0.0.1.dev121/livestyled/schemas/event_stage.py
@@ -0,0 +1,21 @@
+from marshmallow import EXCLUDE, fields, Schema
+
+from livestyled.schemas.utils import get_id_from_url
+
+
+class EventStageSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+        api_type = 'event_stages'
+        url = 'v4/event_stages'
+
+    id = fields.Int()
+    status = fields.String()
+    venue = fields.Function(get_id_from_url)
+    name = fields.String()
+    description = fields.String()
+    color = fields.String()
+    sort_id = fields.Int(data_key='sortId')
+    updated_at = fields.AwareDateTime(data_key='updatedAt', allow_none=True)
+    created_at = fields.AwareDateTime(data_key='createdAt', allow_none=True)
+    app = fields.Function(get_id_from_url)

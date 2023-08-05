c86fd2dba7e506929c584dd466c9fed690faa4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package alpha1p/0.1.5/alpha1p-0.1.5-py3.7.egg

---
+++ packages/alpha1p/alpha1p-0.1.5-py3.7.egg/alpha1p/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .version import version as __version__
+from .version import show_versions
+from . import robot  # pylint: disable=wildcard-import
+from . import dance # pylint: disable=wildcard-import

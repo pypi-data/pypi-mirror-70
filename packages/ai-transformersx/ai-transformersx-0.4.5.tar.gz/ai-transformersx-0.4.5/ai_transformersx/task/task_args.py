0d3432dd788129307c8ce9f5a08b8849119af423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:58 +0000

    Add package ai-transformersx/0.4.5/ai-transformersx-0.4.5.tar.gz

---
+++ packages/ai-transformersx/ai-transformersx-0.4.5.tar.gz/ai-transformersx-0.4.5/ai_transformersx/task/task_args.py
@@ -0,0 +1,22 @@
+from ..model.model_args import ModelArguments
+from ..data.data_args import DataArguments
+from ..train.training_args import TrainingArguments
+from ..transformersx_base import *
+
+
+@configclass
+class TaskArguments:
+    model_args: ModelArguments = ModelArguments()
+    data_args: DataArguments = DataArguments()
+    training_args: TrainingArguments = TrainingArguments()
+
+
+def parse_tasks_args(argsObjOrClass=None):
+    if argsObjOrClass is None:
+        task_args = TaskArguments()
+    elif argsObjOrClass is not None and type(argsObjOrClass) != type:
+        task_args = argsObjOrClass
+    else:
+        task_args = argsObjOrClass()
+
+    return Arguments(task_args).parse()

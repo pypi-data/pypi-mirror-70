1173f9fa94394f49034da817e39b28a05f6fd102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package pdf-text-overlay/0.4.3/pdf_text_overlay-0.4.3-py2-none-any.whl

---
+++ packages/pdf-text-overlay/pdf_text_overlay-0.4.3-py2-none-any.whl/pdf_text_overlay/__init__.py
@@ -0,0 +1,16 @@
+"""Pdf writer."""
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+
+    :copyright: (c) 2018 by Zerodha Technology.
+    :license: see LICENSE for details.
+"""
+__author__ = 'Shridhar Patil'
+__email__ = 'shridharpatil2792@gmail.com'
+__version__ = '0.4.3'
+
+from .pdfWriter import pdf_writer, pdf_from_template, ConditionalCoordinatesNotFound # noqa
+
+
+__all__ = ["pdf_writer", "pdf_from_template", "ConditionalCoordinatesNotFound"]

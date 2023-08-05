024bc79e34c5d8db698f69459f21944bdc752cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:28 +0000

    Add package time-series-predictor/0.1.5/time_series_predictor-0.1.5.tar.gz

---
+++ packages/time-series-predictor/time_series_predictor-0.1.5.tar.gz/time_series_predictor-0.1.5/setup.py
@@ -0,0 +1,57 @@
+"""
+Setup
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="time_series_predictor",
+    version="0.1.5",
+    author="Daniel Kaminski de Souza",
+    author_email="daniel@kryptonunite.com",
+    description="Time Series Predictor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://timeseriespredictor.readthedocs.io/",
+    packages=['time_series_predictor'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'torch',
+        'psutil',
+        'tqdm'
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'autopep8',
+            'sphinx',
+            'rstcheck',
+            'rinohtype',
+            'pytest',
+            'pandas',
+            'seaborn',
+            'sklearn',
+            'bumpversion',
+            'twine',
+            'jupyterlab',
+            'matplotlib'
+        ],
+        'test': [
+            'pytest',
+            'pytest-cov',
+            'pandas',
+            'seaborn',
+            'sklearn'
+        ],
+        'docs': [
+            'rinohtype'
+        ]
+    }
+)
---
+++ packages/time-series-predictor/time_series_predictor-0.1.5.tar.gz/time_series_predictor-0.1.5/time_series_predictor/time_series_dataset.py
@@ -0,0 +1,52 @@
+"""
+time_series_dataset
+"""
+
+import abc
+from torch.utils.data import Dataset
+from .min_max_scaler import MinMaxScaler
+
+class TimeSeriesDataset(Dataset):
+    """
+    TimeSeriesDataset
+
+    :param _x: input predictor
+    :param _y: output predictor
+    """
+    def __init__(self, _x, _y, labels):
+        super().__init__()
+        self.labels = labels
+        # Normalize x
+        self.scaler_x = MinMaxScaler()
+        self._x = self.scaler_x.fit_transform(_x)
+        # Normalize y
+        self.scaler_y = MinMaxScaler()
+        self._y = self.scaler_y.fit_transform(_y)
+
+    def __getitem__(self, idx):
+        return (self._x[idx], self._y[idx])
+
+    def __len__(self):
+        return self._x.shape[0]
+
+    def get_x_shape(self):
+        """get_x_shape
+
+        :returns: input predictor shape
+        """
+        return self._x.shape
+
+    def get_y_shape(self):
+        """get_y_shape
+
+        :returns: output predictor shape
+        """
+        return self._y.shape
+
+    @abc.abstractmethod
+    def make_future_dataframe(self, *args, **kwargs):
+        """make_future_dataframe
+
+        :param *args: variable length unnamed args list
+        :param **kwargs: variable length named args list
+        """

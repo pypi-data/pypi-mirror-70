6f4e643ab5f08e975fee9362b1584ea7bd4c80d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:44 +0000

    Add package geeutils/0.0.1/geeutils-0.0.1.tar.gz

---
+++ packages/geeutils/geeutils-0.0.1.tar.gz/geeutils-0.0.1/setup_geeutils.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="geeutils",
+    version="0.0.1",
+    packages=['geeutils'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)

87886573c23439538f5de76f9d9e5106cb90ba33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:18 +0000

    Add package randan/0.0.1/randan-0.0.1.tar.gz

---
+++ packages/randan/randan-0.0.1.tar.gz/randan-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randan", # Replace with your own username
+    version="0.0.1",
+    author="Svetlana Zhuchkova",
+    author_email="lana_lob@mail.ru",
+    description="A python package for the analysis of social data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LanaLob/randan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

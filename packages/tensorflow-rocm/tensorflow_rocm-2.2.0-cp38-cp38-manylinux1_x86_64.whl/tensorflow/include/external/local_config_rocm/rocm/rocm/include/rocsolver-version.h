b9bf8e9f38216fe5486e9503018e4590b442f377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:27 +0000

    Add package tensorflow-rocm/2.2.0/tensorflow_rocm-2.2.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.2.0-cp38-cp38-manylinux1_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/rocsolver-version.h
@@ -0,0 +1,17 @@
+/* ************************************************************************
+ * Copyright 2019-2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+/* the configured version and settings
+ */
+#ifndef ROCSOLVER_VERSION_H_
+#define ROCSOLVER_VERSION_H_
+
+// clang-format off
+#define ROCSOLVER_VERSION_MAJOR 3
+#define ROCSOLVER_VERSION_MINOR 5
+#define ROCSOLVER_VERSION_PATCH 0
+#define ROCSOLVER_VERSION_TWEAK 106-rocm-rel-3.5-30-2e45cd8
+// clang-format on
+
+#endif

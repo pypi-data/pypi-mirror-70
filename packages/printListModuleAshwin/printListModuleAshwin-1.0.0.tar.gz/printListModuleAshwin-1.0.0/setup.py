46cdd78f45c875da95e100b6d5d8e9b23b3f7f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package printListModuleAshwin/1.0.0/printListModuleAshwin-1.0.0.tar.gz

---
+++ packages/printListModuleAshwin/printListModuleAshwin-1.0.0.tar.gz/printListModuleAshwin-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(name="printListModuleAshwin", version ="1.0.0", description="an API to print list recursively", author = "Ashwin", author_email="ashwin.joseph@ignitarium.com", py_modules=["printListModule"])

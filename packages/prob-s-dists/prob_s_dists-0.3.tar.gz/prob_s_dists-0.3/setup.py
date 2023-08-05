c69760246b852460bd9048261f00f5f99fe08979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package prob-s-dists/0.3/prob_s_dists-0.3.tar.gz

---
+++ packages/prob-s-dists/prob_s_dists-0.3.tar.gz/prob_s_dists-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_s_dists',
+      version='0.3',
+      description='Gaussian distribution, Binomial distribution',
+      packages=['prob_s_dists'],
+      author = 'Saswata Mishra',
+      author_email = 'mysaswatamishra@gmail.com',
+      zip_safe=False)

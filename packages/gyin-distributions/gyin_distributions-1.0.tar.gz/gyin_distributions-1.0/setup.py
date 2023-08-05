a98f02e9bfe74679647eac8e67456443f331a860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package gyin-distributions/1.0/gyin_distributions-1.0.tar.gz

---
+++ packages/gyin-distributions/gyin_distributions-1.0.tar.gz/gyin_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gyin_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gyin_distributions'],
+      author = 'Guopeng Yin',
+      author_email = 'yinguopeng@gmail.com',
+      zip_safe=False)

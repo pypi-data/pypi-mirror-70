aada3728cbc5d72707a80b9765d7995290b302e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:47 +0000

    Add package printModuleMostafiz1/1.0.0/printModuleMostafiz1-1.0.0.tar.gz

---
+++ packages/printModuleMostafiz1/printModuleMostafiz1-1.0.0.tar.gz/printModuleMostafiz1-1.0.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name = "printModuleMostafiz1",version="1.0.0",description="bla bla",author="Mostafiz",author_email="mostafiz.h@ignitarium.com",py_modules=["printListModule"])

ab11640704128653291096b738edf500b61bc4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:42 +0000

    Add package smalld/0.1.1/smalld-0.1.1-py3-none-any.whl

---
+++ packages/smalld/smalld-0.1.1-py3-none-any.whl/smalld/__init__.py
@@ -0,0 +1,8 @@
+from .exceptions import (
+    ConnectionError,
+    GatewayClosedException,
+    HttpError,
+    RateLimitException,
+    SmallDException,
+)
+from .smalld import Intent, SmallD, __version__

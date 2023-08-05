c35e583333659702c35adc6814d396c5988a16e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:44 +0000

    Add package mlnd-distributions-excercise/0.1/mlnd_distributions_excercise-0.1.tar.gz

---
+++ packages/mlnd-distributions-excercise/mlnd_distributions_excercise-0.1.tar.gz/mlnd_distributions_excercise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mlnd_distributions_excercise',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mlnd_distributions_excercise'],
+      zip_safe=False)

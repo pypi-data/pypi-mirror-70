24c7bdd0b3c645a516e15eff242438de5bb449a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package ai-transformersx/0.4.6/ai-transformersx-0.4.6.tar.gz

---
+++ packages/ai-transformersx/ai-transformersx-0.4.6.tar.gz/ai-transformersx-0.4.6/ai_transformersx/examples/__init__.py
@@ -0,0 +1,40 @@
+from ..transformersx_base import *
+from .tasks import *
+from .. import *
+
+TASKS = dict([
+    ('news', NewsSegmentTask),
+    ('sentiment', SentimentTask)
+])
+
+
+@configclass
+class TaskRunArguments:
+    action: str = field('train', 'specified the action name: train,eval,predict')
+
+
+@configclass
+class ExampleTasksArguments():
+    runArgs: TaskRunArguments = TaskRunArguments()
+    taskArgs = None
+
+
+def _build_arguments():
+    arguments = {}
+    for task_name, task_class in TASKS.items():
+        arg_obj = ExampleTasksArguments()
+        arg_obj.taskArgs = task_class.args_class()
+        arguments[task_name] = arg_obj
+    return arguments
+
+
+def start_example_task(test=False, args=None):
+    argument_objs = _build_arguments()
+
+    task_name, arguments = ComplexArguments(argument_objs).parse()
+    arguments.taskArgs.model_args.unit_test = test
+    log.info("Start Example Task:{}, with arguments:{}".format(task_name, str(arguments.taskArgs)))
+    task_action = arguments.runArgs.action
+    task_instance = TASKS[task_name](arguments.taskArgs)
+
+    eval('task_instance.' + task_action + '()')

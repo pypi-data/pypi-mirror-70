7b5dad87d4f0e987efe5c140438f6efc9c4d0962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:37 +0000

    Add package gb-ali-distributions/0.1/gb_ali_distributions-0.1.tar.gz

---
+++ packages/gb-ali-distributions/gb_ali_distributions-0.1.tar.gz/gb_ali_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_ali_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['gb_ali_distributions'],
+      autho='Liaquat Ali',
+      author_email = 'liaquat85@gmail.com',
+      zip_safe=False)

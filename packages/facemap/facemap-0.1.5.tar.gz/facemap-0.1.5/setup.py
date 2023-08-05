12bda46d4797e3ba328fa734755a2703dc541cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:39 +0000

    Add package facemap/0.1.5/facemap-0.1.5.tar.gz

---
+++ packages/facemap/facemap-0.1.5.tar.gz/facemap-0.1.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="facemap",
+    version="0.1.5",
+    author="Carsen Stringer",
+    author_email="carsen.stringer@gmail.com",
+    description="Processing motion SVDs of videos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MouseLand/FaceMap",
+    packages=setuptools.find_packages(),
+    install_requires = ['ffmpeg','pyqtgraph==0.11.0rc0', 'PyQt5', 'PyQt5.sip', 'numpy>=1.13.0', 'scipy','matplotlib','pims','natsort'],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)

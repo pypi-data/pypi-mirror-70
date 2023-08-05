259a0249693eee834d29b0bd1d84fddbff5372c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package bsmedit/3.2.3/bsmedit-3.2.3.tar.gz

---
+++ packages/bsmedit/bsmedit-3.2.3.tar.gz/bsmedit-3.2.3/bsmedit/bsm/__init__.py
@@ -0,0 +1,2 @@
+
+auto_load_module = ['shell', 'editor', 'graph', 'sim', 'misctools', 'debugtool']
---
+++ packages/bsmedit/bsmedit-3.2.3.tar.gz/bsmedit-3.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+exec(open("bsmedit/version.py").read())
+
+long_description="""
+**bsmedit** is a C/C++/SystemC Visualizer.
+- [Documentation](http://bsmedit.feiyilin.com)
+"""
+setup(name='bsmedit',
+      version=BSM_VERSION,
+      description='C/C++/SystemC Visualizer',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Tianzhu Qiao',
+      author_email='tq@feiyilin.com',
+      url='http://bsmedit.feiyilin.com',
+      license="MIT",
+      platforms=["any"],
+      packages=find_packages(),
+      include_package_data = True,
+      entry_points={
+          'gui_scripts': [
+              'bsmedit = bsmedit.main:main'
+          ]
+      },
+      install_requires=['wxpython', 'matplotlib', 'numpy', 'click', 'PyOpenGL',
+                        'PyOpenGL_accelerate']
+     )

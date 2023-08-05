7e8b7a10fd90a9e2e817bfe41fd16e5c632c5dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:30 +0000

    Add package Oleksii-2394/9.1/Oleksii_2394-9.1.tar.gz

---
+++ packages/Oleksii-2394/Oleksii_2394-9.1.tar.gz/Oleksii_2394-9.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Oleksii_2394',
+      version='9.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)

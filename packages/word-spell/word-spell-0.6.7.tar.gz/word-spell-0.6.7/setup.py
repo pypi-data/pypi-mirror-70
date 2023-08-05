edf119252e1c247924675d6119ff3649f102eb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:12 +0000

    Add package word-spell/0.6.7/word-spell-0.6.7.tar.gz

---
+++ packages/word-spell/word-spell-0.6.7.tar.gz/word-spell-0.6.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+import word_spell
+
+setup(
+        name='word-spell',
+        version='0.6.7',
+        py_modules=['word_spell'],
+        scripts=['word_spell.py'],
+        url='',
+        license='MIT',
+        author='Kelly',
+        author_email='',
+        description=''
+)

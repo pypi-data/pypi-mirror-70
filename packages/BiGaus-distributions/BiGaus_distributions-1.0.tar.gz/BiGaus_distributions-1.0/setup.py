e81dcce5fae89912a841bb2b1dd870df105e2449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package BiGaus-distributions/1.0/BiGaus_distributions-1.0.tar.gz

---
+++ packages/BiGaus-distributions/BiGaus_distributions-1.0.tar.gz/BiGaus_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BiGaus_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['BiGaus_distributions'],
+      zip_safe=False)

2f515e46db763fb3e6681ded2626f0ac13cba20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:13 +0000

    Add package fariman-distributions/0.1/fariman_distributions-0.1.tar.gz

---
+++ packages/fariman-distributions/fariman_distributions-0.1.tar.gz/fariman_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='fariman_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['fariman_distributions'],
+      zip_safe=False)

aa59b9966136ee3367191a578ddcdf0dd92fb73f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package probDist-test/0.1/probDist_test-0.1.tar.gz

---
+++ packages/probDist-test/probDist_test-0.1.tar.gz/probDist_test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'probDist_test',
+    version = '0.1',
+    description = 'Gaussian and binomial distributions',
+    packages = ['probDist_test'],
+    zip_safe = False
+)

7f3f6fcc4d1d9240feedb9fca7e6b6d3d2de86a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package dist-pypi-ex/1.0/dist_pypi_ex-1.0.tar.gz

---
+++ packages/dist-pypi-ex/dist_pypi_ex-1.0.tar.gz/dist_pypi_ex-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_pypi_ex',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      author = 'Ilham Naharudinsyah',
+      packages=['distributions'],
+      author_email='ilham.naharudinsyah@gmail.com',
+      zip_safe=False)

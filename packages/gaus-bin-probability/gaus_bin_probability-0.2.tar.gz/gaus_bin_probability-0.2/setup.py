0b33c1f884cd1963d4d5ba769c9c8b236e965074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:55 +0000

    Add package gaus-bin-probability/0.2/gaus_bin_probability-0.2.tar.gz

---
+++ packages/gaus-bin-probability/gaus_bin_probability-0.2.tar.gz/gaus_bin_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_bin_probability',
+      version='0.2',
+      author='Patrick Walukagga',
+      author_email='pwalukagga@gmail.com',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_bin_probability'],
+      zip_safe=False)

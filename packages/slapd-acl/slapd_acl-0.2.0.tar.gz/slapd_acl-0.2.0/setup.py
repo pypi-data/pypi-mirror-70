4fb58766f86eb2eeca638d3fe88bb6e604ae7476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:07 +0000

    Add package slapd-acl/0.2.0/slapd_acl-0.2.0.tar.gz

---
+++ packages/slapd-acl/slapd_acl-0.2.0.tar.gz/slapd_acl-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='slapd_acl',
+      version='0.2.0',
+      description="Slapd olcAccess (ACL) utility",
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                  'License :: OSI Approved :: Apache Software License',
+                  'Programming Language :: Python :: 3'],
+      url='https://github.com/peppelinux/slapd_acl',
+      author='Giuseppe De Marco',
+      author_email='giuseppe.demarco@unical.it',
+      license='Apache Software License',
+      scripts=['slapd_acl/slapd_acl'],
+      packages=['slapd_acl'],
+      install_requires=[],
+     )

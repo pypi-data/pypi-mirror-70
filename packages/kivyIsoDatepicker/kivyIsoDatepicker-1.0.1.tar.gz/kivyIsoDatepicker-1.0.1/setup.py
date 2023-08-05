9584b70929b3738c57302e24d8b81345b3bec540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:36 +0000

    Add package kivyIsoDatepicker/1.0.1/kivyIsoDatepicker-1.0.1.tar.gz

---
+++ packages/kivyIsoDatepicker/kivyIsoDatepicker-1.0.1.tar.gz/kivyIsoDatepicker-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kivyIsoDatepicker",  # Replace with your own username
+    version="1.0.1",
+    author="Ricardo Vogel",
+    author_email="info@ricardovogel.nl",
+    description="Python 3.3+ version of Oleg Kozlov KivyCalandar in ISO 8601 date format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ricardovogel/datepicker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+)

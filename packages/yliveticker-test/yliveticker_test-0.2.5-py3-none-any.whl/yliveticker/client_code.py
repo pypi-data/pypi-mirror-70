f043fd68513399187e8e2bb60e6a300be1bacf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:32 +0000

    Add package yliveticker-test/0.2.5/yliveticker_test-0.2.5-py3-none-any.whl

---
+++ packages/yliveticker-test/yliveticker_test-0.2.5-py3-none-any.whl/yliveticker/client_code.py
@@ -0,0 +1,17 @@
+import yliveticker
+
+
+def printRes(res):
+    print(res)
+
+
+def on_close():
+    print("bye")
+
+
+print("Test output:", yliveticker.yliveticker.test())
+
+yliveticker.YLiveTicker(on_ticker=printRes,
+                                  on_close=on_close,
+                                  ticker_names=[
+                                      "BTC=X", "^GSPC", "^DJI", "^IXIC", "^RUT", "CL=F", "GC=F", "SI=F", "EURUSD=X", "^TNX", "^VIX", "GBPUSD=X", "JPY=X", "BTC-USD", "^CMC200", "^FTSE", "^N225"])

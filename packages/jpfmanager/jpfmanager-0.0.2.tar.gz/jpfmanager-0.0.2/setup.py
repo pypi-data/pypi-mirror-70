76d39da0ead2bc333c2fabb5d1e344ce0a154258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:34 +0000

    Add package jpfmanager/0.0.2/jpfmanager-0.0.2.tar.gz

---
+++ packages/jpfmanager/jpfmanager-0.0.2.tar.gz/jpfmanager-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jpfmanager",
+    version="0.0.2",
+    author="Ibrahim Abbas",
+    author_email="i.abbas85@gmail.com",
+    description="File manager to deal with json and pickle files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IbrahimABBAS85/jpf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pathlib",
+        "jsonpickle"
+    ],
+)

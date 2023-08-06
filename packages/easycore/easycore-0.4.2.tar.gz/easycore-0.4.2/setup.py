7bd8e3fabb81dc376832b98ac9469b1c744b9410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:02 +0000

    Add package easycore/0.4.2/easycore-0.4.2.tar.gz

---
+++ packages/easycore/easycore-0.4.2.tar.gz/easycore-0.4.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'easycore',
+    version = '0.4.2',
+    author = 'Yuxin Zhao',
+    url = 'https://github.com/YuxinZhaozyx/easycore',
+    packages = find_packages(),
+    license = 'LICENSE',
+    description = 'a collection of tools to support research and development.',
+    long_description = open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "pyyaml",
+        "tqdm",
+        "portalocker>=1.6.0",
+    ]
+)

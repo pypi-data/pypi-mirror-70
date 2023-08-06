e1ac5b3efd23dad23780669ca62e151a7064ee30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:53 +0000

    Add package dsnd-probability-yrmadev/0.1/dsnd_probability_yrmadev-0.1.tar.gz

---
+++ packages/dsnd-probability-yrmadev/dsnd_probability_yrmadev-0.1.tar.gz/dsnd_probability_yrmadev-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_yrmadev',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_yrmadev'],
+      author = 'Yrma Dev',
+      author_email = 'yrmadev@outlook.com',
+      zip_safe=False)

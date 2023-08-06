48cf6d8c00ef5b5ebdb5a84cc13b8748a8d9a2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:44 +0000

    Add package binogaus/0.1/binogaus-0.1.tar.gz

---
+++ packages/binogaus/binogaus-0.1.tar.gz/binogaus-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binogaus',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      author = 'Niyaz B. Hashem',
+      packages=['binogaus'],
+      zip_safe=False)

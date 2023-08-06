4ad8186ce3b5bc7c49e41eb849fc59935f76bfc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:34 +0000

    Add package ntare-distributions/2.0/ntare-distributions-2.0.tar.gz

---
+++ packages/ntare-distributions/ntare-distributions-2.0.tar.gz/ntare-distributions-2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ntare-distributions',
+      version='2.0',
+      description='Gaussian and Binomial distributions',
+      author='Ntare Guy',
+      package = ['ntare-distributions'],
+      zip_safe=False)

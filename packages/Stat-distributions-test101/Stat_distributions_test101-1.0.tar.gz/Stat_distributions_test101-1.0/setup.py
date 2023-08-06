dd56df3bdb02d21e5d25d03f354efd1b8b80216c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:54 +0000

    Add package Stat-distributions-test101/1.0/Stat_distributions_test101-1.0.tar.gz

---
+++ packages/Stat-distributions-test101/Stat_distributions_test101-1.0.tar.gz/Stat_distributions_test101-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Stat_distributions_test101',
+      version='1.0',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['Stat_distributions_test101'],
+      zip_safe=False)

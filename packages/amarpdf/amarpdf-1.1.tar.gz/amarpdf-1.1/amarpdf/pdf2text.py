80d70649cd1550492caf2ec82b045222d6114e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:44 +0000

    Add package amarpdf/1.1/amarpdf-1.1.tar.gz

---
+++ packages/amarpdf/amarpdf-1.1.tar.gz/amarpdf-1.1/amarpdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module provide functions to convert a PDF to text """
+
+
+class Converter:
+    """ A simple convertor for converting PDFs to text. """
+
+    def convert(self, path):
+        """ 
+        Convert the given PDF to text.
+
+        Parameters:
+        path (str): The path to the PDF file.
+
+        Returns:
+        str: The content of the PDF file as text. 
+        """
+        print("pdf2text")

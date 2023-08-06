08edd71d09929d041377803062c275b2a85af264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:32 +0000

    Add package itk-anisotropicdiffusionlbr/1.1.2/itk_anisotropicdiffusionlbr-1.1.2-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/itk-anisotropicdiffusionlbr/itk_anisotropicdiffusionlbr-1.1.2-cp37-cp37m-manylinux1_x86_64.whl/itk/Configuration/AnisotropicDiffusionLBRConfig.py
@@ -0,0 +1,24 @@
+depends = ('ITKPyBase', 'ITKImageGradient', 'ITKIOSpatialObjects', 'ITKIOImageBase', 'ITKCommon', )
+templates = (
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterISS2', True, 'itk::Image< signed short,2 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterISS3', True, 'itk::Image< signed short,3 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterIUC2', True, 'itk::Image< unsigned char,2 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterIUC3', True, 'itk::Image< unsigned char,3 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterIUS2', True, 'itk::Image< unsigned short,2 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterIUS3', True, 'itk::Image< unsigned short,3 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterIF2', True, 'itk::Image< float,2 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterIF3', True, 'itk::Image< float,3 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterID2', True, 'itk::Image< double,2 >'),
+  ('AnisotropicDiffusionLBRImageFilter', 'itk::AnisotropicDiffusionLBRImageFilter', 'itkAnisotropicDiffusionLBRImageFilterID3', True, 'itk::Image< double,3 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterISS2', True, 'itk::Image< signed short,2 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterISS3', True, 'itk::Image< signed short,3 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterIUC2', True, 'itk::Image< unsigned char,2 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterIUC3', True, 'itk::Image< unsigned char,3 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterIUS2', True, 'itk::Image< unsigned short,2 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterIUS3', True, 'itk::Image< unsigned short,3 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterIF2', True, 'itk::Image< float,2 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterIF3', True, 'itk::Image< float,3 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterID2', True, 'itk::Image< double,2 >'),
+  ('CoherenceEnhancingDiffusionImageFilter', 'itk::CoherenceEnhancingDiffusionImageFilter', 'itkCoherenceEnhancingDiffusionImageFilterID3', True, 'itk::Image< double,3 >'),
+)
+snake_case_functions = ('coherence_enhancing_diffusion_image_filter', 'anisotropic_diffusion_lbr_image_filter', )

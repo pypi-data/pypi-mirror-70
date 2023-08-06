ece1ace17a879da2bbee8525f4fe6b77174450d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:47 +0000

    Add package favorite-probability-distributions/0.1/favorite_probability_distributions-0.1.tar.gz

---
+++ packages/favorite-probability-distributions/favorite_probability_distributions-0.1.tar.gz/favorite_probability_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='favorite_probability_distributions',
+      version='0.1',
+      description='Includes all-time greatest hits like Gaussian and Binomial',
+      packages=['favorite_probability_distributions'],
+      zip_safe=False)

a476f759c0b7abdcf5bb3f86508a611272e1614e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:10 +0000

    Add package rrrs-distributions-p/0.1/rrrs_distributions_p-0.1.tar.gz

---
+++ packages/rrrs-distributions-p/rrrs_distributions_p-0.1.tar.gz/rrrs_distributions_p-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rrrs_distributions_p',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rrrs_distributions_p'],
+      author='Raul R.',
+      zip_safe=False)

17060d72a4156493c286972553d70508deb6d790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:16 +0000

    Add package xshinnosuke/0.0.2/xshinnosuke-0.0.2.tar.gz

---
+++ packages/xshinnosuke/xshinnosuke-0.0.2.tar.gz/xshinnosuke-0.0.2/xshinnosuke/utils/__init__.py
@@ -0,0 +1,9 @@
+from .activators import *
+from .initializers import *
+from .preprocess import *
+from .optimizers import *
+from .objectives import *
+from .toolkit import *
+
+
+name = 'xshinnosuke-utils'

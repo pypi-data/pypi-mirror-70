58418b86ab2773a48a451def1d45893c5671889f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package xsun-probability/0.1/xsun_probability-0.1.tar.gz

---
+++ packages/xsun-probability/xsun_probability-0.1.tar.gz/xsun_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xsun_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['xsun_probability'],
+      author = 'Xiake Sun',
+      author_email = 'xiakesun0711@gmail.com',
+      zip_safe=False)

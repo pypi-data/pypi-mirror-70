e78717c1d1d7a64aa9678f5f1a4de0b6267f8268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:13 +0000

    Add package bqmail/2.0/bqmail-2.0.tar.gz

---
+++ packages/bqmail/bqmail-2.0.tar.gz/bqmail-2.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+packages = find_packages()
+
+VERSION = "2.0"
+setup(name='bqmail',
+      version=VERSION,
+      author='Mijian Xu',
+      author_email='gomijianxu@gmail.com',
+      license='GPLv3',
+      packages=find_packages(),
+      package_dir={'bqmail': 'bqmail'},
+      package_data={'': ['data/*']},
+      install_requires=['obspy', 'pandas'],
+      entry_points={'console_scripts': ['get_stations=bqmail.query:get_stations',
+                                        'get_events=bqmail.query:get_events']},
+      include_package_data=True,
+      zip_safe=False
+      )

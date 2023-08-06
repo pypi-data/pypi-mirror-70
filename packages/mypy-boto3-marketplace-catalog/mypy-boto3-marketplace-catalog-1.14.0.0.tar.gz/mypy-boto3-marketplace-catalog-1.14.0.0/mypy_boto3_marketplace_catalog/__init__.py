34722df4f67d2f8bc639c6e66539e880dbed1727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:29 +0000

    Add package mypy-boto3-marketplace-catalog/1.14.0.0/mypy-boto3-marketplace-catalog-1.14.0.0.tar.gz

---
+++ packages/mypy-boto3-marketplace-catalog/mypy-boto3-marketplace-catalog-1.14.0.0.tar.gz/mypy-boto3-marketplace-catalog-1.14.0.0/mypy_boto3_marketplace_catalog/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for marketplace-catalog service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_marketplace_catalog import (
+        Client,
+        MarketplaceCatalogClient,
+    )
+
+    session = boto3.Session()
+
+    client: MarketplaceCatalogClient = boto3.client("marketplace-catalog")
+    session_client: MarketplaceCatalogClient = session.client("marketplace-catalog")
+    ```
+"""
+from mypy_boto3_marketplace_catalog.client import (
+    MarketplaceCatalogClient as Client,
+    MarketplaceCatalogClient,
+)
+
+
+__all__ = ("Client", "MarketplaceCatalogClient")

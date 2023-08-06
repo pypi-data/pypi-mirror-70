4ef03b4314e1b0265874e275540d35b2760a5e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:06 +0000

    Add package normally-dist/0.5/normally_dist-0.5.tar.gz

---
+++ packages/normally-dist/normally_dist-0.5.tar.gz/normally_dist-0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='normally_dist',
+      version='0.5',
+      description='Gaussian and Binomial distributions',
+      packages=['normally_dist'],
+      author = 'Ifenna Okoye',
+      zip_safe=False)

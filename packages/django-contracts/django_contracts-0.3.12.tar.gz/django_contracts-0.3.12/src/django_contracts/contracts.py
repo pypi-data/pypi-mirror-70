2ea4383d11421d5fd344ef99f60ca865fd3f6e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:05 +0000

    Add package django-contracts/0.3.12/django_contracts-0.3.12.tar.gz

---
+++ packages/django-contracts/django_contracts-0.3.12.tar.gz/django_contracts-0.3.12/src/django_contracts/contracts.py
@@ -0,0 +1,30 @@
+import logging
+from functools import wraps
+from .request import apply_request
+from .response import apply_response
+
+log = logging.getLogger(__name__)
+
+
+def request_contract(request_contracts, response_contract, pass_in_user=False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            new_func = func
+            if response_contract:
+                new_func = apply_response(contract=response_contract)(func=new_func)
+
+            if request_contracts:
+                method = request.method
+                contract = request_contracts[method]
+                new_func = apply_request(
+                    request_contract=contract,
+                    for_method=method,
+                    pass_in_user=pass_in_user
+                )(func=new_func)
+
+            return new_func(request, *args, **kwargs)
+
+        return wrapper
+
+    return decorator

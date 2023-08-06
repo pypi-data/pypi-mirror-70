d6610a418b487bab47fef455f183bba03df36720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package MouseTools/2.0.0b0/MouseTools-2.0.0b0-py3-none-any.whl

---
+++ packages/MouseTools/MouseTools-2.0.0b0-py3-none-any.whl/MouseTools/__init__.py
@@ -0,0 +1,15 @@
+from MouseTools.parks import *
+from MouseTools.attractions import *
+from MouseTools.entertainments import *
+from MouseTools.characters import *
+from MouseTools.facilities import *
+from MouseTools.pointsofinterest import *
+from MouseTools.destinations import *
+from MouseTools.database import *
+
+
+name = "MouseTools"
+
+__version__ = "2.0.0-beta"
+
+__all__ = ["Destination", "Park", "Attraction", "Entertainment", "Facility", "Character", "PointOfInterest"]

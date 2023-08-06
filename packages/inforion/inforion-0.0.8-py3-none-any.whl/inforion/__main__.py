53d0cf67c121abe3339ebab3b285d770ecf357ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:51 +0000

    Add package inforion/0.0.8/inforion-0.0.8-py3-none-any.whl

---
+++ packages/inforion/inforion-0.0.8-py3-none-any.whl/inforion/__main__.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import base64
+
+import sys, getopt
+
+from requests_oauthlib import OAuth2Session
+from requests.auth import HTTPBasicAuth
+from oauthlib.oauth2 import BackendApplicationClient
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('inforion -i InforIONFile -url url -methode')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('inforion -i InforIONFile -url url -methode')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print ('Input file is "', inputfile)
+    print ('Output file is "', outputfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

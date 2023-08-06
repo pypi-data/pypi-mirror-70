3e2eebc7a99f9423b50f635edf04a6e1e22df5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:16 +0000

    Add package mypy-boto3-iotevents-data/1.13.24.0/mypy-boto3-iotevents-data-1.13.24.0.tar.gz

---
+++ packages/mypy-boto3-iotevents-data/mypy-boto3-iotevents-data-1.13.24.0.tar.gz/mypy-boto3-iotevents-data-1.13.24.0/mypy_boto3_iotevents_data/__init__.py
@@ -0,0 +1,20 @@
+"""
+Main interface for iotevents-data service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.iotevents_data import (
+        Client,
+        IoTEventsDataClient,
+        )
+
+    session = boto3.Session()
+
+    client: IoTEventsDataClient = boto3.client("iotevents-data")
+    session_client: IoTEventsDataClient = session.client("iotevents-data")
+"""
+from mypy_boto3_iotevents_data.client import IoTEventsDataClient, IoTEventsDataClient as Client
+
+
+__all__ = ("Client", "IoTEventsDataClient")

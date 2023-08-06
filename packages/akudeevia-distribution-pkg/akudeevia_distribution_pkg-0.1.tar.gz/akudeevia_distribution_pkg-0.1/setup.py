7551b943a72deb6cb6f57319768ef0113a1f631c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:38 +0000

    Add package akudeevia-distribution-pkg/0.1/akudeevia_distribution_pkg-0.1.tar.gz

---
+++ packages/akudeevia-distribution-pkg/akudeevia_distribution_pkg-0.1.tar.gz/akudeevia_distribution_pkg-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='akudeevia_distribution_pkg',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['akudeevia_distribution_pkg'],
+      author='Akshay Kulkarni',
+      email='dontknow@mail.com',
+      zip_safe=False)

67972b954a59cc97b5117a12f1dcabfba208864b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package pic2sketch/0.1.0/pic2sketch-0.1.0-py3-none-any.whl

---
+++ packages/pic2sketch/pic2sketch-0.1.0-py3-none-any.whl/pic2sketch/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: pic2sketch.version
+.. moduleauthor:: Allen Shaw <lonsty@sina.com>
+"""
+
+__version__ = "0.1.0"  #: the working version
+__release__ = "0.1.0"  #: the release version

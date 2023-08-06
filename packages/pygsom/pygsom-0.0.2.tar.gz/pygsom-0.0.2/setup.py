bede640532421537d2e8237e377d2b1a463efa51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:05 +0000

    Add package pygsom/0.0.2/pygsom-0.0.2.tar.gz

---
+++ packages/pygsom/pygsom-0.0.2.tar.gz/pygsom-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygsom",
+    version="0.0.2",
+    author="Thimal Kempitiya",
+    author_email="t.kempitiya@gmail.com",
+    description="gsom clustering and dimensional reduction algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/thimalk/pygsom/src",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+            'numpy>=1.17.4',
+            'pandas>=0.25.3',
+            'scipy>=1.4.1',
+            'tqdm>=4.39.0',
+            'matplotlib>=3.1.2',
+    ],
+    python_requires='>=3.6',
+)

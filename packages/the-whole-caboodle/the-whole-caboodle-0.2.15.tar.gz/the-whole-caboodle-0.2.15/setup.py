149dc99f30fdd5b7bd98e3368ecc58606591e578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:06 +0000

    Add package the-whole-caboodle/0.2.15/the-whole-caboodle-0.2.15.tar.gz

---
+++ packages/the-whole-caboodle/the-whole-caboodle-0.2.15.tar.gz/the-whole-caboodle-0.2.15/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['caboodle']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiofiles>=0.5.0,<0.6.0',
+ 'gcloud-aio-storage>=5.4.0,<6.0.0',
+ 'google-cloud-storage>=1.25,<2.0',
+ 'tqdm>=4.41.1,<5.0.0',
+ 'uvloop>=0.14.0,<0.15.0']
+
+setup_kwargs = {
+    'name': 'the-whole-caboodle',
+    'version': '0.2.15',
+    'description': 'Utilities for artifact management for data science in the cloud.',
+    'long_description': None,
+    'author': 'Saad Khan',
+    'author_email': 'skhan8@mail.einstein.yu.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

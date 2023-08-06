da3c62bd8a2788c4b086c6d65258ffb0a7cd69f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:13 +0000

    Add package pgetopt-jyrgenn/2020.606.1328/pgetopt-jyrgenn-2020.606.1328.tar.gz

---
+++ packages/pgetopt-jyrgenn/pgetopt-jyrgenn-2020.606.1328.tar.gz/pgetopt-jyrgenn-2020.606.1328/setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import setuptools
+
+repository = "https://git.w21.org/python_modules/pgetopt"
+
+with open("README.md", "r") as fh:
+    # filter out the comment lines from the README
+    description_lines = [ l for l in fh if not l.startswith("#") ]
+
+setuptools.setup(
+    name="pgetopt-jyrgenn",
+    version=os.environ["PKG_VERSION"] or sys.exit("missing $PKG_VERSION env."),
+    author="Juergen Nickelsen",
+    author_email="ni@w21.org",
+    description=description_lines[0].strip(),
+    long_description="".join(description_lines),
+    long_description_content_type="text/markdown",
+    url=repository,
+    packages=setuptools.find_packages(),
+    project_urls=dict(
+        Documentation=repository+"/-/blob/master/README.md",
+        Source=repository,
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)

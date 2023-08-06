25d2902ca05269fbfb46739087cbe63d4770cac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:51 +0000

    Add package uproot4/0.0.8/uproot4-0.0.8.tar.gz

---
+++ packages/uproot4/uproot4-0.0.8.tar.gz/uproot4-0.0.8/uproot4/deserialization.py
@@ -0,0 +1,196 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import struct
+
+import numpy
+
+import uproot4.const
+import uproot4.model
+import uproot4._util
+
+
+scope = {
+    "struct": struct,
+    "numpy": numpy,
+    "VersionedModel": uproot4.model.VersionedModel,
+}
+
+
+def _actually_compile(class_code, new_scope):
+    exec(compile(class_code, "<dynamic>", "exec"), new_scope)
+
+
+def compile_class(file, classes, class_code, class_name):
+    new_scope = dict(scope)
+    for cls in classes.values():
+        new_scope[cls.__name__] = cls
+
+    def c(name, version=None):
+        cls = new_scope.get(uproot4.model.classname_encode(name, version))
+        if cls is None:
+            cls = new_scope.get(uproot4.model.classname_encode(name))
+        if cls is None:
+            cls = file.class_named(name, version)
+        return cls
+
+    new_scope["c"] = c
+
+    _actually_compile(class_code, new_scope)
+
+    out = new_scope[class_name]
+    out.class_code = class_code
+    out.__module__ = "<dynamic>"
+
+    behavior_cls = uproot4.behavior_of(uproot4.model.classname_decode(class_name)[0])
+    if behavior_cls is not None:
+        out.__bases__ = (behavior_cls,) + out.__bases__
+
+    return out
+
+
+_numbytes_version_1 = struct.Struct(">IH")
+_numbytes_version_2 = struct.Struct(">H")
+
+
+def numbytes_version(chunk, cursor, move=True):
+    num_bytes, version = cursor.fields(chunk, _numbytes_version_1, move=False)
+    num_bytes = numpy.int64(num_bytes)
+
+    if num_bytes & uproot4.const.kByteCountMask:
+        num_bytes = int(num_bytes & ~uproot4.const.kByteCountMask) + 4
+        if move:
+            cursor.skip(_numbytes_version_1.size)
+
+    else:
+        num_bytes = None
+        version = cursor.field(chunk, _numbytes_version_2, move=move)
+
+    return num_bytes, version
+
+
+def numbytes_check(start_cursor, stop_cursor, num_bytes, classname, file_path):
+    if num_bytes is not None:
+        observed = stop_cursor.displacement(start_cursor)
+        if observed != num_bytes:
+            if file_path is None:
+                in_file = ""
+            else:
+                in_file = "\nin file {0}".format(file_path)
+            raise ValueError(
+                """instance of ROOT class {0} has {1} bytes; expected {2}{3}""".format(
+                    classname, observed, num_bytes, in_file
+                )
+            )
+
+
+_map_string_string_format1 = struct.Struct(">I")
+
+
+def map_string_string(chunk, cursor):
+    cursor.skip(12)
+    size = cursor.field(chunk, _map_string_string_format1)
+    cursor.skip(6)
+    keys = [cursor.string(chunk) for i in range(size)]
+    cursor.skip(6)
+    values = [cursor.string(chunk) for i in range(size)]
+    return dict(zip(keys, values))
+
+
+scope["map_string_string"] = map_string_string
+
+
+_read_object_any_format1 = struct.Struct(">I")
+
+
+def read_object_any(chunk, cursor, context, file, parent, as_class=None):
+    # TBufferFile::ReadObjectAny()
+    # https://github.com/root-project/root/blob/c4aa801d24d0b1eeb6c1623fd18160ef2397ee54/io/io/src/TBufferFile.cxx#L2684
+    # https://github.com/root-project/root/blob/c4aa801d24d0b1eeb6c1623fd18160ef2397ee54/io/io/src/TBufferFile.cxx#L2404
+
+    beg = cursor.displacement()
+    bcnt = numpy.int64(cursor.field(chunk, _read_object_any_format1))
+
+    if (bcnt & uproot4.const.kByteCountMask) == 0 or (
+        bcnt == uproot4.const.kNewClassTag
+    ):
+        vers = 0
+        start = 0
+        tag = bcnt
+        bcnt = 0
+    else:
+        vers = 1
+        start = cursor.displacement()
+        tag = numpy.int64(cursor.field(chunk, _read_object_any_format1))
+        bcnt = int(bcnt)
+
+    if tag & uproot4.const.kClassMask == 0:
+        # reference object
+
+        if tag == 0:
+            return None  # return null
+
+        elif tag == 1:
+            return parent  # return parent
+
+        elif tag not in cursor.refs:
+            # jump past this object
+            cursor.move_to(cursor.origin + beg + bcnt + 4)
+            return None  # return null
+
+        else:
+            return cursor.refs[int(tag)]  # return object
+
+    elif tag == uproot4.const.kNewClassTag:
+        # new class and object
+
+        classname = cursor.classname(chunk)
+
+        cls = file.class_named(classname)
+
+        if vers > 0:
+            cursor.refs[start + uproot4.const.kMapOffset] = cls
+        else:
+            cursor.refs[len(cursor.refs) + 1] = cls
+
+        if as_class is None:
+            obj = cls.read(chunk, cursor, context, file, parent)
+        else:
+            obj = as_class.read(chunk, cursor, context, file, parent)
+
+        if vers > 0:
+            cursor.refs[beg + uproot4.const.kMapOffset] = obj
+        else:
+            cursor.refs[len(cursor.refs) + 1] = obj
+
+        return obj  # return object
+
+    else:
+        # reference class, new object
+
+        ref = int(tag & ~uproot4.const.kClassMask)
+
+        if as_class is None:
+            if ref not in cursor.refs:
+                if getattr(file, "file_path") is None:
+                    in_file = ""
+                else:
+                    in_file = "\nin file {0}".format(file.file_path)
+                raise OSError("""invalid class-tag reference{0}""".format(in_file))
+
+            cls = cursor.refs[ref]  # reference class
+            obj = cls.read(chunk, cursor, context, file, parent)
+
+        else:
+            obj = as_class.read(chunk, cursor, context, file, parent)
+
+        if vers > 0:
+            cursor.refs[beg + uproot4.const.kMapOffset] = obj
+        else:
+            cursor.refs[len(cursor.refs) + 1] = obj
+
+        return obj  # return object
+
+
+scope["read_object_any"] = read_object_any
---
+++ packages/uproot4/uproot4-0.0.8.tar.gz/uproot4-0.0.8/uproot4/interpretation/numerical.py
@@ -0,0 +1,241 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot4/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import uproot4.interpretation
+
+import numpy
+
+
+def _dtype_shape(dtype):
+    shape = ()
+    while dtype.subdtype is not None:
+        dtype, s = dtype.subdtype
+        shape = shape + s
+    return dtype, shape
+
+
+class Numerical(uproot4.interpretation.Interpretation):
+    @property
+    def itemsize(self):
+        raise AssertionError
+
+    @property
+    def to_dtype(self):
+        return self._to_dtype
+
+    @property
+    def numpy_dtype(self):
+        return self._to_dtype
+
+    @property
+    def awkward_form(self):
+        raise NotImplementedError
+
+    def final_array(
+        self, basket_arrays, entry_start, entry_stop, entry_offsets, library, branch
+    ):
+        self.hook_before_final_array(
+            basket_arrays=basket_arrays,
+            entry_start=entry_start,
+            entry_stop=entry_stop,
+            entry_offsets=entry_offsets,
+            library=library,
+            branch=branch,
+        )
+
+        if entry_start >= entry_stop:
+            output = library.empty((0,), self.to_dtype)
+
+        else:
+            length = 0
+            start = entry_offsets[0]
+            for basket_num, stop in enumerate(entry_offsets[1:]):
+                if start <= entry_start and entry_stop <= stop:
+                    length += entry_stop - entry_start
+                elif start <= entry_start < stop:
+                    length += stop - entry_start
+                elif start <= entry_stop <= stop:
+                    length += entry_stop - start
+                elif entry_start < stop and start <= entry_stop:
+                    length += stop - start
+                start = stop
+
+            output = library.empty((length,), self.to_dtype)
+
+            start = entry_offsets[0]
+            for basket_num, stop in enumerate(entry_offsets[1:]):
+                if start <= entry_start and entry_stop <= stop:
+                    local_start = entry_start - start
+                    local_stop = entry_stop - start
+                    basket_array = basket_arrays[basket_num]
+                    output[:] = basket_array[local_start:local_stop]
+
+                elif start <= entry_start < stop:
+                    local_start = entry_start - start
+                    local_stop = stop - start
+                    basket_array = basket_arrays[basket_num]
+                    output[: stop - entry_start] = basket_array[local_start:local_stop]
+
+                elif start <= entry_stop <= stop:
+                    local_start = 0
+                    local_stop = entry_stop - start
+                    basket_array = basket_arrays[basket_num]
+                    output[start - entry_start :] = basket_array[local_start:local_stop]
+
+                elif entry_start < stop and start <= entry_stop:
+                    basket_array = basket_arrays[basket_num]
+                    output[start - entry_start : stop - entry_start] = basket_array
+
+                start = stop
+
+        self.hook_before_library_finalize(
+            basket_arrays=basket_arrays,
+            entry_start=entry_start,
+            entry_stop=entry_stop,
+            entry_offsets=entry_offsets,
+            library=library,
+            branch=branch,
+            output=output,
+        )
+
+        output = library.finalize(output, branch)
+
+        self.hook_after_final_array(
+            basket_arrays=basket_arrays,
+            entry_start=entry_start,
+            entry_stop=entry_stop,
+            entry_offsets=entry_offsets,
+            library=library,
+            branch=branch,
+            output=output,
+        )
+
+        return output
+
+
+class AsDtype(Numerical):
+    def __init__(self, from_dtype, to_dtype=None):
+        self._from_dtype = numpy.dtype(from_dtype)
+        if to_dtype is None:
+            self._to_dtype = self._from_dtype.newbyteorder("=")
+        else:
+            self._to_dtype = numpy.dtype(to_dtype)
+
+    def __repr__(self):
+        if self._to_dtype == self._from_dtype.newbyteorder("="):
+            return "AsDtype({0})".format(repr(str(self._from_dtype)))
+        else:
+            return "AsDtype({0}, {1})".format(
+                repr(str(self._from_dtype)), repr(str(self._to_dtype))
+            )
+
+    @property
+    def from_dtype(self):
+        return self._from_dtype
+
+    @property
+    def itemsize(self):
+        return self._from_dtype.itemsize
+
+    _numpy_byteorder_to_cache_key = {
+        "!": "B",
+        ">": "B",
+        "<": "L",
+        "|": "L",
+        "=": "B" if numpy.dtype(">f8").isnative else "L",
+    }
+
+    @property
+    def cache_key(self):
+        def form(dtype, name):
+            d, s = _dtype_shape(dtype)
+            return "{0}{1}{2}({3}{4})".format(
+                self._numpy_byteorder_to_cache_key[d.byteorder],
+                d.kind,
+                d.itemsize,
+                ",".join(repr(x) for x in s),
+                name,
+            )
+
+        if self._from_dtype.names is None:
+            from_dtype = form(self._from_dtype, "")
+        else:
+            from_dtype = (
+                "["
+                + ",".join(
+                    form(self._from_dtype[n], "," + repr(n))
+                    for n in self._from_dtype.names
+                )
+                + "]"
+            )
+
+        if self._to_dtype.names is None:
+            to_dtype = form(self._to_dtype, "")
+        else:
+            to_dtype = (
+                "["
+                + ",".join(
+                    form(self._to_dtype[n], "," + repr(n)) for n in self._to_dtype.names
+                )
+                + "]"
+            )
+
+        return "{0}({1},{2})".format(type(self).__name__, from_dtype, to_dtype)
+
+    def basket_array(self, data, byte_offsets, basket, branch):
+        self.hook_before_basket_array(
+            data=data, byte_offsets=byte_offsets, basket=basket, branch=branch
+        )
+
+        assert byte_offsets is None
+
+        dtype, shape = _dtype_shape(self._from_dtype)
+        try:
+            output = data.view(dtype).reshape((-1,) + shape)
+        except ValueError:
+            raise ValueError(
+                """basket {0} in branch {1} has the wrong number of bytes ({2}) """
+                """for interpretation {3}
+in file {4}""".format(
+                    basket.basket_num,
+                    repr(branch.name),
+                    len(data),
+                    self,
+                    branch.file.file_path,
+                )
+            )
+
+        self.hook_before_basket_array(
+            data=data,
+            byte_offsets=byte_offsets,
+            basket=basket,
+            branch=branch,
+            output=output,
+        )
+
+        return output
+
+
+class AsArray(AsDtype):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class AsDouble32(Numerical):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class AsFloat16(Numerical):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class AsSTLBits(Numerical):
+    def __init__(self):
+        raise NotImplementedError
+
+    @property
+    def itemsize(self):
+        return self._num_bytes + 4

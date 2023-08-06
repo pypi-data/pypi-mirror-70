343fa87dba5f7b8d1479bd7c3e219058913c52b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package StringGenerator/0.3.4/StringGenerator-0.3.4.tar.gz

---
+++ packages/StringGenerator/StringGenerator-0.3.4.tar.gz/StringGenerator-0.3.4/setup.py
@@ -0,0 +1,31 @@
+#!python
+import io
+from distutils.core import setup
+
+description = 'Generate randomized strings of characters using a template'
+
+with io.open('README.txt', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(name='StringGenerator',
+      description=description,
+      url='https://github.com/paul-wolf/strgen',
+      author='Paul Wolf',
+      author_email='paul.wolf@yewleaf.com',
+      version='0.3.4',
+      # version = module.__version__,
+      packages=['strgen', ],
+      license='BSD',
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      )

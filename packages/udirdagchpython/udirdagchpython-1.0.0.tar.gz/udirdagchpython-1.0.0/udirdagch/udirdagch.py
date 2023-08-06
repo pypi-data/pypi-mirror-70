a0b1404b49298d38138260a774477e2c9f2d539b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:18 +0000

    Add package udirdagchpython/1.0.0/udirdagchpython-1.0.0.tar.gz

---
+++ packages/udirdagchpython/udirdagchpython-1.0.0.tar.gz/udirdagchpython-1.0.0/udirdagch/udirdagch.py
@@ -0,0 +1,12 @@
+import random
+
+def udirdagch_songo(ner_degshigchid):
+    '''
+    Удирдагчыг санамсаргүй байдлаар сонгон буцаана
+
+    Parameters:
+        ner_degshigchid(list): Нэр дэвшиж буй намууд
+    Return:
+        string: Сонгогдсон нэр
+    '''
+    return random.choice(ner_degshigchid)

1ade6754319b01a1fe8b2049ccf1bb87786ae4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:35 +0000

    Add package render-engine/2020.6.4/render_engine-2020.6.4.tar.gz

---
+++ packages/render-engine/render_engine-2020.6.4.tar.gz/render_engine-2020.6.4/setup.py
@@ -0,0 +1,126 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+# from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='render_engine',  # Required
+    version='2020.6.4',  # Required
+    description='Static Page Generation with Flask-like simplicity and flair ✨',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/kjaymiller/render_engine',  # Optional
+    author='Jay Miller',  # Optional
+    author_email='jay@productivityintech.com',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Text Processing :: Markup :: HTML',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Natural Language
+        'Natural Language :: English',
+
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='static site web framework',  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    python_requires='>=3.7',
+    install_requires=[
+    'pendulum',
+    'markdown',
+    'jinja2',
+    'more-itertools',
+    'typer',
+    ],  # Optional
+    extras_require={  # Optional
+        'dev': ['pipenv', 'check-manifest'],
+        'test': ['pytest', 'pytest-cov', 'black'],
+    },
+
+    package_data={  # Optional
+    #    'sample': ['package_data.dat'],
+         'render_engine': ['rss/*', 'templates/*'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    # entry_points={  # Optional
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Documentation': 'https://render-engine.readthedocs.io/en/latest',
+        'Bug Reports': 'https://github.com/kjaymiller/render_engine/issues',
+        'Funding': 'https://paypal.me/kjaymiller',
+        'Say Thanks!': 'http://saythanks.io/to/kjaymiller',
+        'Source': 'https://github.com/kjaymiller/render_engine/',
+    },
+)

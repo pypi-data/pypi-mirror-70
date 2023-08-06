c362e49752812f90962e32fe9b8283be138f8a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package scimple/1.11.2/scimple-1.11.2-py3-none-any.whl

---
+++ packages/scimple/scimple-1.11.2-py3-none-any.whl/scimple/__init__.py
@@ -0,0 +1,10 @@
+# Scimple package
+# Author: Enzo Bonnal (enzobonnal@gmail.com)
+__version__ = '1.11.1'
+print('using scimple', __version__)
+
+
+from .plot import *
+from .utils import *
+from .stats import *
+from .graph import *

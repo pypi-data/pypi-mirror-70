75acefac761e76330a39e7fabcc075d379aff8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:05 +0000

    Add package creme/0.6.0/creme-0.6.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/creme/creme-0.6.0-cp38-cp38-macosx_10_9_x86_64.whl/creme/time_series/__init__.py
@@ -0,0 +1,11 @@
+"""Time series forecasting."""
+from .detrender import Detrender
+from .detrender import GroupDetrender
+from .snarimax import SNARIMAX
+
+
+__all__ = [
+    'Detrender',
+    'GroupDetrender',
+    'SNARIMAX'
+]

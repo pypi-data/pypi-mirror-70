f2c21eb784e8d54a9cfcf4e5956476e7d3b78049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:45 +0000

    Add package distributions-udacity-course123/0.1/distributions_udacity_course123-0.1.tar.gz

---
+++ packages/distributions-udacity-course123/distributions_udacity_course123-0.1.tar.gz/distributions_udacity_course123-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_course123',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_course123'],
+      author='Leonardo Nerone',
+      zip_safe=False)

1fd40c29d85c0f25c5fbed15ef1d3baefe007466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:33 +0000

    Add package prettyqt/0.53.0/prettyqt-0.53.0-py2.py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-0.53.0-py2.py3-none-any.whl/prettyqt/widgets/widgetaction.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtWidgets
+
+from prettyqt import widgets
+
+
+QtWidgets.QWidgetAction.__bases__ = (widgets.Action,)
+
+
+class WidgetAction(QtWidgets.QWidgetAction):
+    pass
+    # def __init__(self, text="", icon=None, parent=None, shortcut="", tooltip=""):
+    #     super().__init__(parent=parent)
+    #     self.set_text(text)
+    #     self.set_icon(icon)
+    #     self.set_shortcut(shortcut)
+    #     self.set_tooltip(tooltip)

3d58906ba167603b39b02ab87130ea4c29e4e875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:01 +0000

    Add package tdds/0.0.0/tdds-0.0.0.tar.gz

---
+++ packages/tdds/tdds-0.0.0.tar.gz/tdds-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='tdds',
+    version='0.0.0',
+    description='Typed declarative data structures',
+    author='Hervé Saint-Amand',
+    author_email='tdds@saintamh.org',
+    url='https://github.com/saintamh/tdds',
+    packages=['tdds'],
+)
+
+#----------------------------------------------------------------------------------------------------------------------------------

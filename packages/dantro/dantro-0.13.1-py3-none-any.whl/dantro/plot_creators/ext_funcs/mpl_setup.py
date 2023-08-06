ba147940d36e11ca2ca98139047376851df94286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:52 +0000

    Add package dantro/0.13.1/dantro-0.13.1-py3-none-any.whl

---
+++ packages/dantro/dantro-0.13.1-py3-none-any.whl/dantro/plot_creators/ext_funcs/mpl_setup.py
@@ -0,0 +1,14 @@
+"""This module sets up matplotlib to be used in the other modules."""
+
+import logging
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+# Local constants
+log = logging.getLogger(__name__)
+
+
+# -----------------------------------------------------------------------------
+# Can define helper functions here

fe579c98f4330bd8d86388c562af1bbf52c47411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package mypy-boto3-signer/1.14.0.0/mypy_boto3_signer-1.14.0.0-py3-none-any.whl

---
+++ packages/mypy-boto3-signer/mypy_boto3_signer-1.14.0.0-py3-none-any.whl/mypy_boto3_signer/waiter.py
@@ -0,0 +1,36 @@
+"""
+Main interface for signer service client waiters.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_signer import SignerClient
+    from mypy_boto3_signer import (
+        SuccessfulSigningJobWaiter,
+    )
+
+    client: SignerClient = boto3.client("signer")
+
+    successful_signing_job_waiter: SuccessfulSigningJobWaiter = client.get_waiter("successful_signing_job")
+    ```
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import TYPE_CHECKING
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_signer.type_defs import WaiterConfigTypeDef
+
+
+__all__ = ("SuccessfulSigningJobWaiter",)
+
+
+class SuccessfulSigningJobWaiter(Boto3Waiter):
+    """
+    [Waiter.SuccessfulSigningJob documentation](https://boto3.amazonaws.com/v1/documentation/api/1.14.0/reference/services/signer.html#Signer.Waiter.SuccessfulSigningJob)
+    """
+
+    def wait(self, jobId: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [SuccessfulSigningJob.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.14.0/reference/services/signer.html#Signer.Waiter.SuccessfulSigningJob.wait)
+        """

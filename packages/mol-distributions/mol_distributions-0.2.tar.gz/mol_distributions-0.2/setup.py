211b12fff93dd6253acd5a5c6cf4ac24aeed80df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package mol-distributions/0.2/mol_distributions-0.2.tar.gz

---
+++ packages/mol-distributions/mol_distributions-0.2.tar.gz/mol_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mol_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['mol_distributions'],
+      author= 'Alex Poulain',
+      zip_safe=False)

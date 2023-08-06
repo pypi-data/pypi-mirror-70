1c81d0dd93963d299d141bdbeeb6ce9392f8c7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package mypy-boto3-quicksight/1.14.0.0/mypy-boto3-quicksight-1.14.0.0.tar.gz

---
+++ packages/mypy-boto3-quicksight/mypy-boto3-quicksight-1.14.0.0.tar.gz/mypy-boto3-quicksight-1.14.0.0/mypy_boto3_quicksight/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for quicksight service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_quicksight import (
+        Client,
+        QuickSightClient,
+    )
+
+    session = boto3.Session()
+
+    client: QuickSightClient = boto3.client("quicksight")
+    session_client: QuickSightClient = session.client("quicksight")
+    ```
+"""
+from mypy_boto3_quicksight.client import QuickSightClient as Client, QuickSightClient
+
+
+__all__ = ("Client", "QuickSightClient")

5a1ca787189058a5d3df16cf0e0216445153373f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:14 +0000

    Add package RoboMentor-Client/1.1.57/RoboMentor_Client-1.1.57.tar.gz

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.1.57.tar.gz/RoboMentor_Client-1.1.57/robomentor_client/device/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .gpio import GPIO
+from .camera import Camera

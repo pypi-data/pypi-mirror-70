62cd3f8cdbffd440d462a1276662c98f932c82ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:26 +0000

    Add package nexoclom/2.0.21/nexoclom-2.0.21.tar.gz

---
+++ packages/nexoclom/nexoclom-2.0.21.tar.gz/nexoclom-2.0.21/nexoclom/__init__.py
@@ -0,0 +1,16 @@
+from .Input import Input
+from .Output import Output
+from .LOSResult import LOSResult
+from .produce_image import ModelImage
+from .configure_model import configure_model
+from .LossInfo import LossInfo
+from .LOSResult import LOSResult
+from .IDLout import IDLout
+
+
+name = 'nexoclom'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '2.0.21'
+
+configure_model()

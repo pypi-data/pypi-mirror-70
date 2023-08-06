5a73e8cd8050f47f96d057e86c227b93689a5cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:54 +0000

    Add package migri/0.2.0/migri-0.2.0.tar.gz

---
+++ packages/migri/migri-0.2.0.tar.gz/migri-0.2.0/migri/__init__.py
@@ -0,0 +1,1 @@
+from migri.main import run_initialization, run_migrations
---
+++ packages/migri/migri-0.2.0.tar.gz/migri-0.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='migri',
+    version='0.2.0',
+    install_requires=[
+        'aiofiles==0.4.*',
+        'asyncpg>=0.18.*',
+        'click==7.*',
+        'sqlparse==0.3.*',
+    ],
+    entry_points={
+        'console_scripts': [
+            'migri = migri.main:main'
+        ]
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Database',
+    ]
+)

924e14b03f7461d6a35aa648bb74f0b7c86fc212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:41 +0000

    Add package printlistmodulebymohitbangale/1.0.0/printlistmodulebymohitbangale-1.0.0.tar.gz

---
+++ packages/printlistmodulebymohitbangale/printlistmodulebymohitbangale-1.0.0.tar.gz/printlistmodulebymohitbangale-1.0.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="printlistmodulebymohitbangale",version="1.0.0",description="API for recurise printing of list", author="mohit", author_email="mohit.bangale@ignitarium.com", py_modules=["print_list_module"])

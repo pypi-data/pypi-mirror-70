7ea81c22231db1b30174bf34c2dc15fb5f3d5aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package vl-public-aws-ssm/0.1.1/vl_public_aws_ssm-0.1.1.tar.gz

---
+++ packages/vl-public-aws-ssm/vl_public_aws_ssm-0.1.1.tar.gz/vl_public_aws_ssm-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='vl_public_aws_ssm',
+      version='0.1.1',
+      description='Wrapper for reading cfg from aws ssm',
+      url='',
+      license='MIT',
+      packages=['vl_public_aws_ssm'],
+      zip_safe=False,
+      setup_requires=['wheel'])

0266145c7061bc89eff00ba07e58609c84e7d09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:45 +0000

    Add package kafkaconnectsync/0.0.3/kafkaconnectsync-0.0.3.tar.gz

---
+++ packages/kafkaconnectsync/kafkaconnectsync-0.0.3.tar.gz/kafkaconnectsync-0.0.3/kafkaconnectsync/utils.py
@@ -0,0 +1,29 @@
+""" utils """
+import time
+from requests import exceptions
+from jsondiff import diff
+
+
+def isjsonequal(base, obj):
+    """ Compare two json objects and return true if they are deeply equal """
+    result = diff(base, obj)
+    keys = len(result.keys())
+    return keys == 0
+
+
+def wait_for_client_info(client, timeout=600):
+    """
+    Function to wait until the kafka client is accesible.
+    Do not return until it can get the client info.
+    """
+    start = time.time()
+    while time.time() - start < timeout:
+        time.sleep(1)
+        try:
+            response = client.get_cluster_info()
+            if response['kafka_cluster_id']:
+                return response['kafka_cluster_id']
+        except exceptions.RequestException:
+            print('[+] Waiting for Kafka Connect to be ready...')
+
+    raise RuntimeError('[-] Kafka Connect client timeout exceeded')

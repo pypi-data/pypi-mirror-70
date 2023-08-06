7113ce238f18ccad4ae167e598eca49b67fe71de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:34 +0000

    Add package data-rdb/0.1.1/data_rdb-0.1.1.tar.gz

---
+++ packages/data-rdb/data_rdb-0.1.1.tar.gz/data_rdb-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='data_rdb',
+      version='0.1.1',
+      description='DataDBS for RethinkDB',
+      url='http://www.gitlab.com/dpineda/data_rdb',
+      author='David Pineda Osorio',
+      author_email='dpineda@csn.uchile.cl',
+      license='MIT',
+      packages=['data_rdb'],
+      install_requires=['datadbs',"rethinkdb"],
+      include_package_data=True,      
+      package_dir={'data_rdb': 'data_rdb'},
+      package_data={
+          'data_rdb': ['../doc', '../docs', '../requeriments.txt', '../tests']},
+      zip_safe=False)

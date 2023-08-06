8a775dc057e4c6de750deb9ad3900a45c84692f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:35 +0000

    Add package normalbinomialsampledistributions/0.1/normalbinomialsampledistributions-0.1.tar.gz

---
+++ packages/normalbinomialsampledistributions/normalbinomialsampledistributions-0.1.tar.gz/normalbinomialsampledistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='normalbinomialsampledistributions',
+      version='0.1',
+      author='Guillermo Ampie',
+      author_email='ga.ampie@gmail.com',
+      description='Sample - Normal and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)

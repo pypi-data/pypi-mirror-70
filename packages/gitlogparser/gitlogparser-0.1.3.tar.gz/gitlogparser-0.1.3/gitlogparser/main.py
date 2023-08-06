eb04b8fcb7f1aac78114f1f5ea1739a07128f8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:44 +0000

    Add package gitlogparser/0.1.3/gitlogparser-0.1.3.tar.gz

---
+++ packages/gitlogparser/gitlogparser-0.1.3.tar.gz/gitlogparser-0.1.3/gitlogparser/main.py
@@ -0,0 +1,17 @@
+import argparse
+from . import parser
+
+
+def main():
+    # create the -dir and -mDir arguments
+    arg_parser = argparse.ArgumentParser(description='Parse a git repos log into json')
+    group = arg_parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-dir', '--directory', metavar='', help='Extracts the specified directory as a repo')
+    group.add_argument('-mDir', '--multiple_directories', metavar='', help='Extracts every subdirectory as a repo')
+    arg_parser.add_argument('-GHT', '--github_token', default=None ,metavar='', help='Your github access token, if specified, the parser will get the line/file changes of each commit')
+    args = arg_parser.parse_args()
+    parser.get_log(args)
+
+
+if __name__ == '__main__':
+    main()

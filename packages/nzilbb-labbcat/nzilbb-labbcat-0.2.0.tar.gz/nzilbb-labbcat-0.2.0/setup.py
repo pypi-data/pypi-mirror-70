31d993f5d54dd5f2a465c721fbae65351334b4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:34 +0000

    Add package nzilbb-labbcat/0.2.0/nzilbb-labbcat-0.2.0.tar.gz

---
+++ packages/nzilbb-labbcat/nzilbb-labbcat-0.2.0.tar.gz/nzilbb-labbcat-0.2.0/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="nzilbb-labbcat",
+    version="0.2.0",
+    description="Client library for communicating with LaBB-CAT servers",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nzilbb/labbcat-py/",
+    author="Robert Fromont",
+    author_email="robert@fromont.net.nz",
+    license="GPL-3.0-or-later",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["labbcat"],
+    include_package_data=False,
+    install_requires=["requests"],
+)

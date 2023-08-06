a995b470b28b20767cc0fb39098c39f0b2ad20f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package dsnd-distributions-yh/0.1/dsnd_distributions_yh-0.1.tar.gz

---
+++ packages/dsnd-distributions-yh/dsnd_distributions_yh-0.1.tar.gz/dsnd_distributions_yh-0.1/README.md
@@ -0,0 +1,3 @@
+# dsnd_distributions_yh - Readme 
+
+This is a test project for Gaussian and Binomial distributions.
---
+++ packages/dsnd-distributions-yh/dsnd_distributions_yh-0.1.tar.gz/dsnd_distributions_yh-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_yh',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions_yh'],
+      zip_safe=False)

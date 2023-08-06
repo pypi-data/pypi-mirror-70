e8d7eea3464055c5c8b8dcb3ac4a73ecaccc48a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:02 +0000

    Add package novelWriter/0.7.1/novelWriter-0.7.1-py3-none-any.whl

---
+++ packages/novelWriter/novelWriter-0.7.1-py3-none-any.whl/nw/gui/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from nw.gui.about import GuiAbout
+from nw.gui.build import GuiBuildNovel
+from nw.gui.docbars import GuiDocTitleBar
+from nw.gui.docbars import GuiNoticeBar
+from nw.gui.docbars import GuiSearchBar
+from nw.gui.docdetails import GuiDocViewDetails
+from nw.gui.doceditor import GuiDocEditor
+from nw.gui.docmerge import GuiDocMerge
+from nw.gui.docsplit import GuiDocSplit
+from nw.gui.docviewer import GuiDocViewer
+from nw.gui.itemdetails import GuiItemDetails
+from nw.gui.itemeditor import GuiItemEditor
+from nw.gui.mainmenu import GuiMainMenu
+from nw.gui.outline import GuiProjectOutline
+from nw.gui.preferences import GuiPreferences
+from nw.gui.projload import GuiProjectLoad
+from nw.gui.projsettings import GuiProjectSettings
+from nw.gui.projtree import GuiProjectTree
+from nw.gui.sessionlog import GuiSessionLogView
+from nw.gui.statusbar import GuiMainStatus
+from nw.gui.theme import GuiIcons
+from nw.gui.theme import GuiTheme
+
+__all__ = [
+    "GuiAbout",
+    "GuiBuildNovel",
+    "GuiDocTitleBar",
+    "GuiNoticeBar",
+    "GuiSearchBar",
+    "GuiDocViewDetails",
+    "GuiDocEditor",
+    "GuiDocMerge",
+    "GuiDocSplit",
+    "GuiDocViewer",
+    "GuiItemDetails",
+    "GuiItemEditor",
+    "GuiMainMenu",
+    "GuiProjectOutline",
+    "GuiPreferences",
+    "GuiProjectLoad",
+    "GuiProjectSettings",
+    "GuiProjectTree",
+    "GuiSessionLogView",
+    "GuiMainStatus",
+    "GuiIcons",
+    "GuiTheme",
+]

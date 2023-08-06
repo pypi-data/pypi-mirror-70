09fa70bccafdf7f7c82ca73bf0fb5bb61e13698f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:57 +0000

    Add package MailerLiteSDK/0.0.4/MailerLiteSDK-0.0.4.tar.gz

---
+++ packages/MailerLiteSDK/MailerLiteSDK-0.0.4.tar.gz/MailerLiteSDK-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MailerLiteSDK',
+    version='0.0.4',
+    keywords='MailerLite',
+    description='a simple MailerLite SDK',
+    license='MIT License',
+    url='',
+    author='Ben',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        # 'requests>=2.23.0'
+    ],
+)

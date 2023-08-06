761968851464e2c8227901ff310e08591c2fa8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package twomartens.allrisscraper/0.3.7/twomartens.allrisscraper-0.3.7.tar.gz

---
+++ packages/twomartens.allrisscraper/twomartens.allrisscraper-0.3.7.tar.gz/twomartens.allrisscraper-0.3.7/src/twomartens/allrisscraper/definitions.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright 2020 Jim Martens
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+ABBREVIATIONS = {
+    "Altona": {
+        "Haupt": "HA",
+    },
+    "Bergedorf": {
+        "Haupt": "HA",
+    },
+    "Eimsbüttel": {
+        "Haupt":      "HA",
+        "Kerngebiet": "KGA",
+        "RaLNS":      "RaLoNiS",
+        "HKS":        "HaKuS",
+        "GNUVWD":     "GNUVWDi",
+        "SAIBGGSG":   "SR",
+        "AS":         "StaPla",
+        "AU":         "Uni"
+    },
+    "Hamburg-Mitte": {
+        "Haupt": "HA",
+        "Stadtplanungs": "StaPla"
+    },
+    "Hamburg-Nord": {
+        "Haupt": "HA",
+    },
+    "Harburg": {
+        "Haupt": "HA",
+    },
+    "Wandsbek": {
+        "Haupt": "HA",
+    }
+}
+
+BASE_LINKS = {
+    "Altona": "https://sitzungsdienst-altona.hamburg.de/ri",
+    "Bergedorf": "https://sitzungsdienst-bergedorf.hamburg.de/ri",
+    "Eimsbüttel": "https://sitzungsdienst-eimsbuettel.hamburg.de/ri",
+    "Hamburg-Mitte": "https://sitzungsdienst-hamburg-mitte.hamburg.de/ri",
+    "Hamburg-Nord": "https://sitzungsdienst-hamburg-nord.hamburg.de/ri",
+    "Harburg": "https://sitzungsdienst-harburg.hamburg.de/ri",
+    "Wandsbek": "https://sitzungsdienst-wandsbek.hamburg.de/ri",
+}

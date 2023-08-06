f27e76acc12f1a17655b568b00facc38acfaf739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:36 +0000

    Add package isoquant-diff/0.0.14/isoquant_diff-0.0.14.win32.zip

---
+++ packages/isoquant-diff/isoquant_diff-0.0.14.win32.zip/Users/Thilo/AppData/Local/Programs/Python/Python38-32/Scripts/isoquant_diff-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\Thilo\AppData\Local\Programs\Python\Python38-32\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'isoquant-diff==0.0.14','console_scripts','isoquant_diff'
+__requires__ = 'isoquant-diff==0.0.14'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('isoquant-diff==0.0.14', 'console_scripts', 'isoquant_diff')()
+    )

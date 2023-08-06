1f334e3058eac7bf60d8b9529f318862cec9d0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:21 +0000

    Add package RoboMentor-Client/1.2.8/RoboMentor_Client-1.2.8.tar.gz

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.2.8.tar.gz/RoboMentor_Client-1.2.8/robomentor_client/__config__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "1.2.8"
+__apiUrl__ = "https://api.robomentor.cn"
+__messageUrl__ = "47.111.96.253"
---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.2.8.tar.gz/RoboMentor_Client-1.2.8/robomentor_client/device/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .gpio import GPIO

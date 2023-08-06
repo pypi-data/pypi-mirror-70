e1fe262a1bca2cf78e0464718bddabc706ca08e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:43 +0000

    Add package ldscripts/1.6/ldscripts-1.6.tar.gz

---
+++ packages/ldscripts/ldscripts-1.6.tar.gz/ldscripts-1.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ldscripts',
+      version='1.06',
+      description='Scripts',
+      author='Jack Vallis',
+      author_email='jack@lachlandickson.com',
+      packages=['ldscripts'],
+      zip_safe=False)

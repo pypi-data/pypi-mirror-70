f41780c279562497a4f3b83c0a667b35859de120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:48 +0000

    Add package soumyasomanpackage/1.0.0/soumyasomanpackage-1.0.0.tar.gz

---
+++ packages/soumyasomanpackage/soumyasomanpackage-1.0.0.tar.gz/soumyasomanpackage-1.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name="soumyasomanpackage",
+    version="1.0.0",
+    author="Soumya Somasundaran",
+    author_email="soumyas567@gmail.com",
+    description="packt example package",
+    long_description="This is the longer description and will appear in the web.",
+    py_modules=["packt"],classifiers=["Programming Language :: Python :: 3","Operating System :: OS Independent",],)

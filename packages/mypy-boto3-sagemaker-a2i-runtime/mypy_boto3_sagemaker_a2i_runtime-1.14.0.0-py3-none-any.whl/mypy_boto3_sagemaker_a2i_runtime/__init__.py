4c0014db3ab350157cac1e9355f19aeb8185e734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:41 +0000

    Add package mypy-boto3-sagemaker-a2i-runtime/1.14.0.0/mypy_boto3_sagemaker_a2i_runtime-1.14.0.0-py3-none-any.whl

---
+++ packages/mypy-boto3-sagemaker-a2i-runtime/mypy_boto3_sagemaker_a2i_runtime-1.14.0.0-py3-none-any.whl/mypy_boto3_sagemaker_a2i_runtime/__init__.py
@@ -0,0 +1,29 @@
+"""
+Main interface for sagemaker-a2i-runtime service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_sagemaker_a2i_runtime import (
+        AugmentedAIRuntimeClient,
+        Client,
+        ListHumanLoopsPaginator,
+    )
+
+    session = boto3.Session()
+
+    client: AugmentedAIRuntimeClient = boto3.client("sagemaker-a2i-runtime")
+    session_client: AugmentedAIRuntimeClient = session.client("sagemaker-a2i-runtime")
+
+    list_human_loops_paginator: ListHumanLoopsPaginator = client.get_paginator("list_human_loops")
+    ```
+"""
+from mypy_boto3_sagemaker_a2i_runtime.client import (
+    AugmentedAIRuntimeClient as Client,
+    AugmentedAIRuntimeClient,
+)
+from mypy_boto3_sagemaker_a2i_runtime.paginator import ListHumanLoopsPaginator
+
+
+__all__ = ("AugmentedAIRuntimeClient", "Client", "ListHumanLoopsPaginator")

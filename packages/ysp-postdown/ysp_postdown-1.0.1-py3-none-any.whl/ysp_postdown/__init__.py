3e49c95154ad317c6064ded007edbdabe4d80866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:55 +0000

    Add package ysp-postdown/1.0.1/ysp_postdown-1.0.1-py3-none-any.whl

---
+++ packages/ysp-postdown/ysp_postdown-1.0.1-py3-none-any.whl/ysp_postdown/__init__.py
@@ -0,0 +1,11 @@
+import sys
+from .ctor import MDDoc
+from .parser import parse
+
+if sys.version_info.major == 2:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    
+__author__ = 'YangShuiPing'
+__version__ = '1.0.1'
+

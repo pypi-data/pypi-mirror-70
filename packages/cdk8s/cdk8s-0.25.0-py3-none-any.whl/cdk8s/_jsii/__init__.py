a6bee408b56a940b901b4db43ac9117dd8e9f05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:44 +0000

    Add package cdk8s/0.25.0/cdk8s-0.25.0-py3-none-any.whl

---
+++ packages/cdk8s/cdk8s-0.25.0-py3-none-any.whl/cdk8s/_jsii/__init__.py
@@ -0,0 +1,14 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import constructs
+__all__ = []
+
+publication.publish()

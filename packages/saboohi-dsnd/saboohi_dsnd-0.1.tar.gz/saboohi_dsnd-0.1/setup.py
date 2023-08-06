ed7308730936760935f9cc05bb3d2d5658a690c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package saboohi-dsnd/0.1/saboohi_dsnd-0.1.tar.gz

---
+++ packages/saboohi-dsnd/saboohi_dsnd-0.1.tar.gz/saboohi_dsnd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='saboohi_dsnd',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['saboohi_dsnd'],
+	  author = 'Saboohi Dhawan',
+	  author_email = 'saboohi.dhawan@gmail.com',
+      zip_safe=False)

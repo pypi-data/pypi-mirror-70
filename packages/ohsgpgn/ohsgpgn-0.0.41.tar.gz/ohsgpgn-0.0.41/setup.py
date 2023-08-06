3841b44c65ee2cc1b80fdd636acd46b1a285ea46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package ohsgpgn/0.0.41/ohsgpgn-0.0.41.tar.gz

---
+++ packages/ohsgpgn/ohsgpgn-0.0.41.tar.gz/ohsgpgn-0.0.41/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='ohsgpgn',
+    version='0.0.41',
+    author='OhYoungJooung',
+    author_email='wnapdlf05@gmail.com',
+    packages=[''],
+    #packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url='http://pypi.python.org/pypi/ohsgpgn/',
+    license='LICENSE.txt',
+    description='oh Simple good password generator(ohsgpgn)',
+    long_description=open('README.txt').read(),
+    #install_requires=[
+    #],
+    python_requires='>=3.6',
+)

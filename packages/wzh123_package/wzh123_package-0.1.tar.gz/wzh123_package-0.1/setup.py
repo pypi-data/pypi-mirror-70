5b8e03c9bcf2d6e05d475000e5ec979c98bf1b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package wzh123_package/0.1/wzh123_package-0.1.tar.gz

---
+++ packages/wzh123_package/wzh123_package-0.1.tar.gz/wzh123_package-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='wzh123_package',
+      version='0.1',
+      description='clinical trial information retriver',
+      #url='http://github.com/tongling/clinicaltrial',
+      author='wangzehan',
+      author_email='2287359887@qq.com',
+      license='MIT',
+      packages=['my_package'],
+      zip_safe=False)

080f8bbc6bfa5f6d48d3cecd65656d08b3c0967c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:19 +0000

    Add package distributions-5996/0.1/distributions_5996-0.1.tar.gz

---
+++ packages/distributions-5996/distributions_5996-0.1.tar.gz/distributions_5996-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_5996',
+      version='0.1',
+      description='Probability distributions',
+      packages=['distributions_5996'],
+      zip_safe=False)

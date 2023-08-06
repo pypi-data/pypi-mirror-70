ac1bd8b2e2a9efbaeb452938fd0b074742a96688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:39 +0000

    Add package distributions-aws/0.1/distributions_aws-0.1.tar.gz

---
+++ packages/distributions-aws/distributions_aws-0.1.tar.gz/distributions_aws-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_aws',
+      author='sameh abouelsaad',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_aws'],
+      zip_safe=False)

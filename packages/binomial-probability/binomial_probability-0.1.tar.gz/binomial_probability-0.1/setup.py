aa60f2712d55152455f7ca8cda09c3b55bc7e374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package binomial-probability/0.1/binomial_probability-0.1.tar.gz

---
+++ packages/binomial-probability/binomial_probability-0.1.tar.gz/binomial_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binomial_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['binomial_probability'],
+      author='Tolulope Adegunle',
+      author_email='toluadegunle@gmail.com',
+      zip_safe=False)

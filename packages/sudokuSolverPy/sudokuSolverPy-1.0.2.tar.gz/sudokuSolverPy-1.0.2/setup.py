de77c0a7dfff9567a5078fee2c8e8b971622ec01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:03 +0000

    Add package sudokuSolverPy/1.0.2/sudokuSolverPy-1.0.2.tar.gz

---
+++ packages/sudokuSolverPy/sudokuSolverPy-1.0.2.tar.gz/sudokuSolverPy-1.0.2/setup.py
@@ -0,0 +1,16 @@
+import pathlib
+from setuptools import setup
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name="sudokuSolverPy",
+    version="1.0.2",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="",
+    author_email="",
+    license="MIT",
+    packages=["sudokuSolverPy"],
+    zip_safe=False
+)

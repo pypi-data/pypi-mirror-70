5ed3da35e44dbb0700794d33b1bd97e14bd4a8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:22 +0000

    Add package hdwallets/0.0.8/hdwallets-0.0.8.tar.gz

---
+++ packages/hdwallets/hdwallets-0.0.8.tar.gz/hdwallets-0.0.8/hdwallets/__init__.py
@@ -0,0 +1,10 @@
+from .bip32 import BIP32
+from .utils import BIP32DerivationError, HARDENED_INDEX
+
+__version__ = "0.0.8"
+
+__all__ = [
+    "BIP32",
+    "BIP32DerivationError",
+    "HARDENED_INDEX",
+]

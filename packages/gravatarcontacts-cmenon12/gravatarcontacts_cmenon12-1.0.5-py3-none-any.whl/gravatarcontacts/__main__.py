151535bf8ee53b9cd0c989a0f9400117e4d0a687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:33 +0000

    Add package gravatarcontacts-cmenon12/1.0.5/gravatarcontacts_cmenon12-1.0.5-py3-none-any.whl

---
+++ packages/gravatarcontacts-cmenon12/gravatarcontacts_cmenon12-1.0.5-py3-none-any.whl/gravatarcontacts/__main__.py
@@ -0,0 +1,171 @@
+"""
+Script to update a user's Google contacts with Gravatar photos.
+
+When run as a script (by running __main__.py) the program will first
+ask a user to grant the Google Cloud Platform app access to their
+contacts in their Google Account. It will then download all of their
+contacts and remove those that don't have an email address and a name.
+
+For each email address in each contact it will attempt to download the
+corresponding Gravatar photo. If a Gravatar photo is found for that
+contact then it will update the contact with that photo. If there are
+multiple possible Gravatar photos (due to a contact having multiple
+email addresses then a GUI will be presented to choose one. The user
+is notified which contacts have been updated, and those contacts have
+the custom Gravatar Photo field set to "True" (so that the program
+can safely update them in the future).
+
+This program will not update contacts that already have a user-supplied
+photo that didn't previously come from Gravatar.
+"""
+import base64
+import logging
+import tkinter
+from functools import partial
+from io import BytesIO
+
+from PIL import Image
+
+from gravatarcontacts.google_contacts import Contact
+from gravatarcontacts.gravatar import Gravatar, MAX_RATING
+
+__author__ = "Christopher Menon"
+__credits__ = "Christopher Menon"
+__license__ = "gpl-3.0"
+
+# Prepare the log
+logging.basicConfig(filename="gravatarcontacts.log",
+                    filemode="a",
+                    format="%(asctime)s | %(levelname)s : %(message)s",
+                    level=logging.DEBUG)
+
+
+def main() -> None:
+    """Updates a user's Google contacts with Gravatar photos.
+
+    :return: None
+    :rtype: None
+    """
+
+    # Connect to the service and fetch a list of the contacts
+    service = Contact.authorize()
+    contacts = Contact.list_contacts(service)
+
+    # Get a Gravatar image for each email address in each contact
+    # This will not save the image where it wasn't available
+    for contact in contacts:
+        if contact.has_user_photo is False or \
+                contact.is_gravatar is True:
+            for email in contact.emails:
+                gravatar_image = Gravatar(email). \
+                    download_image()
+                if gravatar_image:
+                    contact.gravatar_images.append(gravatar_image)
+
+        # Remove duplicates from list of images
+        contact.gravatar_images = list(dict.fromkeys(
+            contact.gravatar_images))
+
+        # Stop if there were no hits from Gravatar
+        if len(contact.gravatar_images) == 0:
+            logging.info("Contact has no Gravatars, "
+                         "continuing with next contact.")
+            continue
+
+        # Choose one if there are too many
+        if len(contact.gravatar_images) >= 2:
+            logging.info("Contact has 2+ Gravatars, need to choose.")
+            chosen_image = contact.gravatar_images[
+                choose_image(contact.name, contact.gravatar_images)]
+            contact.gravatar_images = [chosen_image]
+
+        # Update the photo
+        if contact.update_photo(service, contact.gravatar_images[0]) is True:
+            print("Gravatar added for", contact.name)
+        else:
+            print("Gravatar could not be added for", contact.name)
+
+
+def make_choice(root: tkinter.Tk, value: int):
+    """Accepts the user's choice from the GUI and saves it as a global.
+
+    :param root: the Tkinter GUI
+    :type root: class:`tkinter.Tk`
+    :param value: the chosen value
+    :type value: int
+    :return: None
+    :rtype: None
+    """
+
+    # Save the choice and close the window
+    global answer
+    answer = value
+    root.destroy()
+    logging.info("Answer chosen, Tkinter window destroyed.")
+    logging.debug("User chose %d", value)
+
+
+def choose_image(name: str, images: list) -> int:
+    """Creates a GUI to choose a photo.
+
+    This function creates a Tkinter GUI to prompt the user to choose a
+    photo from the images list. If the user doesn't choose one and
+    closes the window then the first is picked anyway (0 is returned).
+
+    :param name: the contact name
+    :type name: str
+    :param images: a list of the images as base64 strings
+    :type images: list
+    :return: the index of the chosen image
+    :rtype: int
+    """
+
+    # Using a default value of 0 in case they just close it
+    global answer
+    answer = 0
+
+    # Set up GUI
+    root = tkinter.Tk()
+    tkinter.Label(root, text="Choose a Gravatar profile picture for " +
+                  name).grid(row=0, column=0,
+                             columnspan=len(images))
+
+    # Prepare for loop
+    counter = 0
+    buttons = []
+
+    for image in images:
+
+        # Resize the image to 200x200
+        buffered = BytesIO()
+        Image.open(BytesIO(base64.b64decode(image))).\
+            resize((200, 200)).save(buffered, format="PNG")
+        image_gui_str = base64.b64encode(buffered.getvalue())
+        image_gui_obj = tkinter.PhotoImage(data=image_gui_str)
+
+        # Create a button with the image
+        buttons.append(tkinter.Button(root, image=image_gui_obj,
+                                      command=partial(make_choice,
+                                                      root, counter)))
+        buttons[counter].photo = image_gui_obj
+        buttons[counter].grid(row=2, column=counter)
+        logging.info("Button created for image %d", counter)
+        counter += 1
+
+    # Display the GUI and get an answer
+    # We delete the global to stay safe
+    print("Please choose a profile picture for " + name +
+          " in the GUI window.")
+    logging.info("Creating GUI.")
+    root.mainloop()
+    value = answer
+    del answer
+
+    assert 0 <= value < len(images)
+
+    return value
+
+
+if __name__ == "__main__":
+    logging.info("Running gravatarcontacts.__main__.main()")
+    main()
---
+++ packages/gravatarcontacts-cmenon12/gravatarcontacts_cmenon12-1.0.5-py3-none-any.whl/gravatarcontacts/gravatar.py
@@ -0,0 +1,68 @@
+"""
+A module that provides access to Gravatar APIs by extending libgravatar.
+"""
+
+import base64
+import logging
+from typing import Union
+import requests
+import libgravatar
+
+__author__ = "Christopher Menon"
+__credits__ = ["Christopher Menon",
+               "https://github.com/pabluk/libgravatar"]
+__license__ = "gpl-3.0"
+
+# The max rating of the Gravatar
+# One of g, pg, r, or x
+MAX_RATING = "g"
+
+# The resolution of the Gravatar that's downloaded (in px)
+RESOLUTION = 720
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Gravatar(libgravatar.Gravatar):
+    """Facilitates access to the Gravatar API.
+
+    This class extends the libgravatar.Gravatar class by providing an
+    instance method to download an image.
+    """
+
+    def download_image(self, rating: str = MAX_RATING) -> Union[bool, str]:
+        """Returns the image of the supplied Gravatar object.
+
+        This method will download the image for the specified Gravatar
+        and return it as a base64 string. It will return False on
+        failure.
+
+        :param rating: the max rating to accept
+        :type rating: str, optional
+        :return: The image as a base64 string or `False`on failure
+        :rtype: bool, str
+        """
+
+        # Get the URL of the image
+        image_url = super(Gravatar, self).get_image(RESOLUTION, "404", False,
+                                                    rating, True, True)
+        LOGGER.info("Gravatar URL calculated.")
+        LOGGER.debug("URL is %s", image_url)
+
+        try:
+            # Download the image
+            r = requests.get(image_url)
+            r.raise_for_status()
+            LOGGER.info("Image successfully downloaded.")
+
+            # Convert to base64 and return
+            image_bytes = base64.b64encode(r.content)
+            image_str = image_bytes.decode("utf-8")
+            assert isinstance(image_str, str)
+
+            return image_str
+
+        # Catch any exception and don't return the raw response
+        except requests.RequestException as err:
+            LOGGER.info(err)
+            return False

2ab5e27a7df3bc9e38b56695dae91a1ac37d009f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:18 +0000

    Add package my-first-ever-python-package/1.1/my_first_ever_python_package-1.1.tar.gz

---
+++ packages/my-first-ever-python-package/my_first_ever_python_package-1.1.tar.gz/my_first_ever_python_package-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='my_first_ever_python_package',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['my_first_ever_python_package'],
+      author = 'Nabanita Ganguly',
+      author_email = 'nabanita.rimpi@gmail.com',
+      zip_safe=False)

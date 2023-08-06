0e7877d8401a540f7c4402e271e8642afe93854e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:23 +0000

    Add package pdfconduit-modify/2.2.4/pdfconduit-modify-2.2.4.tar.gz

---
+++ packages/pdfconduit-modify/pdfconduit-modify-2.2.4.tar.gz/pdfconduit-modify-2.2.4/pdf/modify/canvas/__init__.py
@@ -0,0 +1,5 @@
+from pdf.modify.canvas.constructor import CanvasConstructor
+from pdf.modify.canvas.objects import CanvasImg, CanvasStr, CanvasObjects
+
+
+__all__ = ['CanvasConstructor', 'CanvasImg', 'CanvasStr', 'CanvasObjects']
---
+++ packages/pdfconduit-modify/pdfconduit-modify-2.2.4.tar.gz/pdfconduit-modify-2.2.4/pdf/modify/draw/__init__.py
@@ -0,0 +1,5 @@
+from PillowImage import PillowImage, img_adjust
+
+from pdf.modify.draw.pdf import DrawPDF, WatermarkDraw
+
+__all__ = ['DrawPDF', 'WatermarkDraw', 'PillowImage', 'img_adjust']

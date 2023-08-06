2d2cd083c86b4915f6ad71dbb357a49ad3f293f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:41 +0000

    Add package dataprotocols/0.2/dataprotocols-0.2.tar.gz

---
+++ packages/dataprotocols/dataprotocols-0.2.tar.gz/dataprotocols-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='dataprotocols',
+      version='0.2',
+      description='DataProtocols is a set of classes that implement protocols for data acquisition',
+      url='http://gitlab.csn.uchile.cl/dpineda/dataprotocols',
+      author='David Pineda Osorio',
+      author_email='dpineda@csn.uchile.cl',
+      install_requires=['Click','networktools', 'basic-logtools'],
+      scripts=[
+          'dataprotocols/gsof/test_gsof.py',
+          'dataprotocols/eryo/test_eryo.py'          
+      ],      
+      entry_points={
+        'console_scripts':["gsof = dataprotocols.gsof.test_gsof:run_gsof",
+                          "eryo = dataprotocols.eryo.test_eryo:run_eryo"]
+        },
+      license='MIT',
+      zip_safe=False)

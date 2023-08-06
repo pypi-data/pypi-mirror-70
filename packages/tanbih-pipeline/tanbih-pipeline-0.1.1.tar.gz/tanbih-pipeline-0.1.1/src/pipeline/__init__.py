5fc43135540e5e20125d11c46d43705d0f2d6fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:23 +0000

    Add package tanbih-pipeline/0.1.1/tanbih-pipeline-0.1.1.tar.gz

---
+++ packages/tanbih-pipeline/tanbih-pipeline-0.1.1.tar.gz/tanbih-pipeline-0.1.1/src/pipeline/__init__.py
@@ -0,0 +1,19 @@
+from .exception import PipelineError
+from .message import Message
+from .tap import DestinationTap, SourceTap, SourceOf, DestinationOf
+from .worker import Generator, Processor, Splitter
+from .utils import parse_kind
+
+
+__all__ = [
+    'PipelineError',
+    'SourceOf',
+    'DestinationOf',
+    'SourceTap',
+    'DestinationTap',
+    'Generator',
+    'Processor',
+    'Splitter',
+    'Message',
+    'parse_kind',
+]

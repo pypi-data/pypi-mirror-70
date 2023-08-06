30c9adc56f1787abeabbf075c4617e352fd13749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:19 +0000

    Add package cmaketools/0.1.3/cmaketools-0.1.3.tar.gz

---
+++ packages/cmaketools/cmaketools-0.1.3.tar.gz/cmaketools-0.1.3/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+
+# read the contents of your README file
+from os import path
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="cmaketools",
+    version="0.1.3",
+    author="Takeshi (Kesh) Ikuma",
+    author_email="tikuma@gmail.com",
+    description="Seamless integration of Cmake build system to setuptools/distutils",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    license="MIT License",
+    url="https://github.com/python-cmaketools/python-cmaketools",
+    packages=["cmaketools"],
+)

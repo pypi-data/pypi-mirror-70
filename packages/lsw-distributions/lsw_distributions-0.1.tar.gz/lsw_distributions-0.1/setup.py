ac96064459b158273a2f750e218abcbc2efa96ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package lsw-distributions/0.1/lsw_distributions-0.1.tar.gz

---
+++ packages/lsw-distributions/lsw_distributions-0.1.tar.gz/lsw_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lsw_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['lsw_distributions'],
+      author = 'Steve Wasik',
+      author_email = 'steven.l.wasik@gmail.com',
+      zip_safe=False)

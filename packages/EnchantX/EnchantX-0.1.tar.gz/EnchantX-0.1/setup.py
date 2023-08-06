6536eff113398240ea5bb8abe4ff68e20391bc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:40 +0000

    Add package EnchantX/0.1/EnchantX-0.1.tar.gz

---
+++ packages/EnchantX/EnchantX-0.1.tar.gz/EnchantX-0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EnchantX", # Replace with your own username
+    version="0.1",
+    author="AkashD",
+    author_email="akashdave555@gmail.com",
+    description="An extension of PyEnchant Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akashdve/enchantx",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)

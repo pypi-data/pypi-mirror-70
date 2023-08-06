4ee05725ef4a64550a90ca4fe100d243872d4b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:11 +0000

    Add package sclblpy/0.1.5/sclblpy-0.1.5-py3-none-any.whl

---
+++ packages/sclblpy/sclblpy-0.1.5-py3-none-any.whl/sclblpy/__init__.py
@@ -0,0 +1,21 @@
+"""
+The sclblpy package allows users to upload, monitor, and administer models and REST endpoints directly from python.
+"""
+# Ran on import of the package. Check version:
+import sys
+
+if sys.version_info < (3, 0):
+    print('Sclblpy requires Python 3, while Python ' + str(sys.version[0] + ' was detected. Terminating... '))
+    sys.exit(1)
+
+
+from .main import upload, update, update_docs, run, endpoints, delete_endpoint, remove_credentials, list_models, stop_print, \
+    start_print, _set_toolchain_URL, _set_usermanager_URL, _set_taskmanager_URL
+from .version import __version__
+
+# Simple welcome message:
+print("\n*** Thanks for importing sclblpy! ***")
+print("You can use the 'upload()' function to upload your models.")
+print("To inspect your currently uploaded models, use `endpoints()`.")
+print("Check the docs at https://pypi.org/project/sclblpy/ for more info. \n")
+

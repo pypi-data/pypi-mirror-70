3b2f4feb3716a8c48c99c40e4a449a2af83dd2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:48 +0000

    Add package gempy/2.2.2/gempy-2.2.2.tar.gz

---
+++ packages/gempy/gempy-2.2.2.tar.gz/gempy-2.2.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+version = '2.2.2'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gempy',
+    version=version,
+    packages=find_packages(exclude=('test', 'docs')),
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'Theano>=1.0.4',
+        'matplotlib',
+        'numpy',
+        'pytest',
+        'seaborn>=0.9',
+        'networkx',
+        'scikit-image>=0.17',
+        'pyvista==0.24.3',
+        'iPython'
+    ],
+    url='https://github.com/cgre-aachen/gempy',
+    license='LGPL v3',
+    author='Miguel de la Varga, Elisa Heim, Alexander Schaaf, Fabian Stamm, Florian Wellmann',
+    author_email='varga@aices.rwth-aachen.de',
+    description='An Open-source, Python-based 3-D structural geological modeling software.',
+    keywords=['geology', '3-D modeling', 'structural geology', 'uncertainty']
+)

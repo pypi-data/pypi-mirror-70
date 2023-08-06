e8e65c01121f92e98077867f17e704e0732b8db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:48 +0000

    Add package notipy-osx/0.0.3/notipy_osx-0.0.3.tar.gz

---
+++ packages/notipy-osx/notipy_osx-0.0.3.tar.gz/notipy_osx-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open('../README.md') as readme_file:
+  readme = readme_file.read()
+
+setuptools.setup(
+    name='notipy_osx',
+    version='0.0.3',
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    url='https://github.com/ninest/notipy_osx',
+    long_description_content_type="text/markdown",
+    description='Display native customizable Mac OS dialogs and notifications with ease',
+    long_description=readme,
+)

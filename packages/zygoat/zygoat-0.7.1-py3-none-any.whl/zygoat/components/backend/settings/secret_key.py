195b3692b4c09819aaa03adbf8a321a9b723cd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:11 +0000

    Add package zygoat/0.7.1/zygoat-0.7.1-py3-none-any.whl

---
+++ packages/zygoat/zygoat-0.7.1-py3-none-any.whl/zygoat/components/backend/settings/secret_key.py
@@ -0,0 +1,30 @@
+import logging
+
+from zygoat.components import SettingsComponent
+
+log = logging.getLogger()
+
+
+class SecretKey(SettingsComponent):
+    def create(self):
+        red = self.parse()
+        key_node = red.find("name", value="SECRET_KEY").parent
+
+        log.info("Retrieving default secret key")
+        default_key = key_node.value.to_python()
+
+        log.info("Relocating default secret key")
+        key_node.value = f"prod_required_env('DJANGO_SECRET_KEY', default='{default_key}')"
+
+        log.info("Dumping secret key node")
+        self.dump(red)
+
+    @property
+    def installed(self):
+        red = self.parse()
+        return (
+            "prod_required_env(" in red.find("name", value="SECRET_KEY").parent.value.dumps()
+        )
+
+
+secret_key = SecretKey()

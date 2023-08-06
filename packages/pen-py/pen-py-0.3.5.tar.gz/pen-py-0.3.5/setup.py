da48d1511b5aa76a9cb5676ec218e1d0b97acb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:26 +0000

    Add package pen-py/0.3.5/pen-py-0.3.5.tar.gz

---
+++ packages/pen-py/pen-py-0.3.5.tar.gz/pen-py-0.3.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pen-py",
+    version="0.3.5",
+    description="Penbox utils for python",
+    url="http://github.com/storborg/funniest",
+    author="Chris Castan",
+    author_email="chris@penbox.io",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "dotmap>=1.3",
+        "python-jose>=3",
+        "requests>=2",
+        "google-cloud-datastore",
+        "google-cloud-redis",
+        "flask",
+        "jmespath",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+)
+
+"""
+Code to upload to pypi
+
+python3 setup.py sdist
+
+twine upload dist/*
+"""

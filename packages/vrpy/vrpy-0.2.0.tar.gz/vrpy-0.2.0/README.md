65889f1efc9b2371ad35650ddf5cb03f3e9c87f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:18 +0000

    Add package vrpy/0.2.0/vrpy-0.2.0.tar.gz

---
+++ packages/vrpy/vrpy-0.2.0.tar.gz/vrpy-0.2.0/README.md
@@ -0,0 +1,87 @@
+[![CircleCI](https://circleci.com/gh/Kuifje02/vrpy.svg?style=svg)](https://circleci.com/gh/Kuifje02/vrpy)
+[![codecov](https://codecov.io/gh/Kuifje02/vrpy/branch/master/graph/badge.svg)](https://codecov.io/gh/Kuifje02/vrpy)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/6f27b9ccd1c2446aa1dba15e701aa9b0)](https://app.codacy.com/manual/Kuifje02/vrpy?utm_source=github.com&utm_medium=referral&utm_content=Kuifje02/vrpy&utm_campaign=Badge_Grade_Dashboard)
+[![Python 3.8](https://img.shields.io/badge/python-3.6|3.7|3.8-blue.svg)](https://www.python.org/downloads/release/python-360/)
+[![Documentation Status](https://readthedocs.org/projects/vrpy/badge/?version=latest)](https://vrpy.readthedocs.io/en/latest/?badge=master)
+
+# VRPy
+
+VRPy is a python framework for solving Vehicle Routing Problems (VRP) including:
+
+-   the Capacitated VRP (CVRP),
+-   the CVRP with resource constraints,
+-   the CVRP with time windows (CVRPTW),
+-   the CVRP with simultaneous distribution and collection (CVRPSDC),
+-   the CVRP with heterogeneous fleet (HFCVRP).
+
+Check out the [docs](https://vrpy.readthedocs.io/en/latest/) to find more variants and options.
+
+## Simple example
+
+```python
+from networkx import DiGraph
+from vrpy import VehicleRoutingProblem
+
+# Define the network
+G = DiGraph()
+G.add_edge("Source",1,cost=1,time=2)
+G.add_edge("Source",2,cost=2,time=1)
+G.add_edge(1,"Sink",cost=0,time=2)
+G.add_edge(2,"Sink",cost=2,time=3)
+G.add_edge(1,2,cost=1,time=1)
+G.add_edge(2,1,cost=1,time=1)
+
+# Define the customers demands
+G.nodes[1]["demand"] = 5
+G.nodes[2]["demand"] = 4
+
+# Define the Vehicle Routing Problem
+prob = VehicleRoutingProblem(G, load_capacity=10, duration=5)
+
+# Solve and display solution value
+prob.solve()
+print(prob.best_value)
+3
+print(prob.best_routes)
+{1: ["Source",2,1,"Sink"]}
+```
+
+## Install
+
+```sh
+pip install vrpy
+```
+
+## Requirements
+
+[cspy](https://pypi.org/project/cspy/)
+
+[NetworkX](https://pypi.org/project/networkx/)
+
+[numpy](https://pypi.org/project/numpy/)
+
+[PuLP](https://pypi.org/project/PuLP/)
+
+## Documentation
+
+Documentation is found [here](https://vrpy.readthedocs.io/en/latest/).
+
+## Running the tests
+
+### Unit Tests
+
+```sh
+cd tests
+pytest unittests/
+```
+
+### Benchmarks
+
+```sh
+cd tests
+pytest benchmarks/
+```
+
+## Bugs
+
+Please report any bugs that you find [here](https://github.com/Kuifje02/vrpy/issues). Or, even better, fork the repository on [GitHub](https://github.com/Kuifje02/vrpy) and create a pull request. Any contributions are welcome.
---
+++ packages/vrpy/vrpy-0.2.0.tar.gz/vrpy-0.2.0/examples/benchmarks/tests_augerat.py
@@ -0,0 +1,87 @@
+import time
+import os
+from pandas import DataFrame
+import sys
+
+sys.path.append("../../")
+sys.path.append("../../examples/")
+
+from benchmarks.cvrp_augerat import DataSet
+from vrpy.clarke_wright import ClarkeWright
+from vrpy import VehicleRoutingProblem
+
+initial_routes = [
+    ["Source", 21, 31, 19, 17, 13, 7, 26, "Sink"],
+    ["Source", 12, 1, 16, 30, "Sink"],
+    ["Source", 27, 24, "Sink"],
+    ["Source", 29, 18, 8, 9, 22, 15, 10, 25, 5, 20, "Sink"],
+    ["Source", 14, 28, 11, 4, 23, 3, 2, 6, "Sink"],
+]
+initial_routes = None
+
+if __name__ == "__main__":
+
+    TIME = 60 * 120
+    keys = [
+        "instance",
+        "nodes",
+        "algorithm",
+        "res",
+        "best known solution",
+        "gap",
+        "time (s)",
+        "vrp",
+        "time limit (s)",
+    ]
+    instance = []
+    nodes = []
+    alg = []
+    res = []
+    best_known_solution = []
+    gap = []
+    run_time = []
+    vrp = []
+    time_limit = []
+    for file_name in os.listdir("./data/"):
+        if file_name[-3:] == "vrp" and file_name == "A-n36-k5.vrp":
+            for cspy in [False]:
+                data = DataSet(path="./data/", instance_name=file_name)
+                if len(data.G.nodes()) < 105:
+                    print(file_name)
+                    print("lower bound =", data.best_known_solution)
+                    instance.append(file_name)
+                    nodes.append(len(data.G.nodes()) - 1)
+                    best_known_solution.append(data.best_known_solution)
+                    vrp.append("cvrp")
+                    time_limit.append(TIME)
+
+                    start = time.time()
+                    prob = VehicleRoutingProblem(data.G, load_capacity=data.max_load)
+                    prob.solve(cspy=cspy, solver="cplex", time_limit=TIME)
+                    res.append(prob.best_value)
+                    gap.append(
+                        (prob.best_value - data.best_known_solution)
+                        / data.best_known_solution
+                        * 100
+                    )
+
+                    run_time.append(float(time.time() - start))
+                    if cspy:
+                        alg.append("cspy")
+                    else:
+                        alg.append("lp cplex %s" % TIME)
+
+                    values = [
+                        instance,
+                        nodes,
+                        alg,
+                        res,
+                        best_known_solution,
+                        gap,
+                        run_time,
+                        vrp,
+                        time_limit,
+                    ]
+                    compar = dict(zip(keys, values))
+                    df = DataFrame(compar, columns=keys)
+                    df.to_csv("test.csv", sep=";", index=False)

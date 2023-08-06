e63d64c1ebee3d33e516d4d6819e73a4cb3f61f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:59 +0000

    Add package teddy-utils/0.1/teddy_utils-0.1.tar.gz

---
+++ packages/teddy-utils/teddy_utils-0.1.tar.gz/teddy_utils-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="teddy_utils",
+      version="0.01",
+      description="Just for learning packages",
+      author="Mannem Srinivas",
+      author_email="m.srinivas365@gmail.com",
+      packages=["teddy_utils"],
+      install_requires=["numpy"])

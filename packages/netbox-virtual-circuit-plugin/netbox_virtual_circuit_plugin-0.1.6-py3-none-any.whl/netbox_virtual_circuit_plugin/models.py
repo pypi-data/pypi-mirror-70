96291424d66ea8e8c786334239cdc20f6246fd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:18 +0000

    Add package netbox-virtual-circuit-plugin/0.1.6/netbox_virtual_circuit_plugin-0.1.6-py3-none-any.whl

---
+++ packages/netbox-virtual-circuit-plugin/netbox_virtual_circuit_plugin-0.1.6-py3-none-any.whl/netbox_virtual_circuit_plugin/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from ipam.models import VLAN
+from .choices import VirtualCircuitStatusChoices
+
+
+class VirtualCircuit(models.Model):
+    """Virtual Circuit model."""
+
+    vcid = models.PositiveSmallIntegerField(
+        primary_key=True,
+        verbose_name='ID'
+    )
+    name = models.CharField(
+        max_length=64
+    )
+    status = models.CharField(
+        max_length=30,
+        choices=VirtualCircuitStatusChoices,
+        default=VirtualCircuitStatusChoices.STATUS_PENDING_CONFIGURATION
+    )
+    context = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['vcid']
+
+    def __str__(self):
+        if self.vcid and self.name:
+            return f'{self.vcid} ({self.name})'
+        return None
+
+
+class VirtualCircuitVLAN(models.Model):
+    """Virtual Circuit to VLAN relationship."""
+
+    virtual_circuit = models.ForeignKey(
+        to=VirtualCircuit,
+        on_delete=models.CASCADE,
+        related_name='vlans'
+    )
+    vlan = models.OneToOneField(
+        to=VLAN,
+        on_delete=models.CASCADE,
+        related_name='vlan_of'
+    )
+
+    class Meta:
+        ordering = ['virtual_circuit']

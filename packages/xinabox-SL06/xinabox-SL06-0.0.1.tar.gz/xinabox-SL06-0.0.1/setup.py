bb4d71b4affbb7232908c5af3c5ec8b710b0ff7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:41 +0000

    Add package xinabox-SL06/0.0.1/xinabox-SL06-0.0.1.tar.gz

---
+++ packages/xinabox-SL06/xinabox-SL06-0.0.1.tar.gz/xinabox-SL06-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xinabox-SL06",
+    version="0.0.1",
+    author="Luqmaan Baboo",
+    author_email="luqmaanbaboo@gmail.com",
+    description="Proximity, gesture, light and colour sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinabox/Python-SL06",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

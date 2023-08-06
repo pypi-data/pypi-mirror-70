1fd074c48e0ef85cb65d61fbbb412791f1cc3797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:16 +0000

    Add package stock-data-downloader-pkg-windoro/0.0.4/stock_data_downloader_pkg_windoro-0.0.4-py3-none-any.whl

---
+++ packages/stock-data-downloader-pkg-windoro/stock_data_downloader_pkg_windoro-0.0.4-py3-none-any.whl/main/main.py
@@ -0,0 +1,16 @@
+from stockdatadownloader.fetcher.Idx import Idx
+from stockdatadownloader.fetcher.StockBitFetcher import StockBitFetcher
+
+from stockdatadownloader.query.StockBitQuery import StockBitQuery
+
+if __name__ == "__main__":
+    # Idx().download()
+    # stocks = Idx.get_stocks()
+    stocks = ['BBRI']
+
+    # fetcher = StockBitFetcher('', '')
+    # fetcher.download_fundamental(stocks)
+    # fetcher.download_price(stocks, '2010-01-01', '2020-01-01')
+    # print(StockBitQuery.query_data_fundamental(stocks[0], '2016-12-12', 'Laba Kotor'))
+    # print(StockBitQuery.query_data_price(stocks[0], '2019-12-17', 'high'))
+    print(StockBitQuery.get_trading_dates(stocks[0]))

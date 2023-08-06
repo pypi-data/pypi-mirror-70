ed8a3baa2a825b9c61705297fab32c499867133e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:34 +0000

    Add package tonic/0.0.3/tonic-0.0.3.tar.gz

---
+++ packages/tonic/tonic-0.0.3.tar.gz/tonic-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tonic",
+    version="0.0.3",
+    author="The Neuromorphs of Telluride",
+    author_email="event-data-augmentation@googlegroups.com",
+    description="Spike manipulation and augmentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neuromorphs/tonic",
+    packages=["tonic"],
+    install_requires=["numpy", "tqdm"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)

ce08eb658749baebf333967222f20dab0396a654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:32 +0000

    Add package roboself/0.0.1/roboself-0.0.1.tar.gz

---
+++ packages/roboself/roboself-0.0.1.tar.gz/roboself-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="roboself",
+    version="0.0.1",
+    author="Razvan Dinu",
+    author_email="razvan@roboself.com",
+    description="The roboself python client.",
+    long_description="The roboself python client.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/roboself/roboself-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

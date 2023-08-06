a49d7c54a4d8b1eae6b5bb08b41ce6c247cac444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:51 +0000

    Add package distributions-nik/1.2/distributions_nik-1.2.tar.gz

---
+++ packages/distributions-nik/distributions_nik-1.2.tar.gz/distributions_nik-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_nik',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_nik'],
+      author = 'niks_2708',
+      zip_safe = False)

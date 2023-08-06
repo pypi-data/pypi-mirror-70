3f9f14c660b1080e5845ac0f9eb75b2fe1192cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:59 +0000

    Add package Threadripper/0.0.1/Threadripper-0.0.1.tar.gz

---
+++ packages/Threadripper/Threadripper-0.0.1.tar.gz/Threadripper-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from setuptools import setup
+
+setup(
+	name = 'Threadripper',
+	version = '0.0.1',
+	author='gucheng',
+	author_email='1012228022@qq.com',
+	url = 'https://github.com/gongxiaoze/threadripper',
+	description='threadpool in processpool',
+	packages=['Threadripper'],
+	install_requires=[],
+)

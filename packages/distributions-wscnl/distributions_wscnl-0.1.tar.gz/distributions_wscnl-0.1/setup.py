7d05d7d881e61e6062c3e1fa79c293f99421f738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package distributions-wscnl/0.1/distributions_wscnl-0.1.tar.gz

---
+++ packages/distributions-wscnl/distributions_wscnl-0.1.tar.gz/distributions_wscnl-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_wscnl',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions_wscnl'],
+      zip_safe=False)

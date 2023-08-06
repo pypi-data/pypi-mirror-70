d2c2280514db7bdf7c1cc55f5af73b6440529c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package zabel/0.10.1/zabel-0.10.1.tar.gz

---
+++ packages/zabel/zabel-0.10.1.tar.gz/zabel-0.10.1/README.md
@@ -0,0 +1,17 @@
+# zabel
+
+## Overview
+
+The Zabel platform.
+
+## License
+
+```text
+Copyright (c) 2019-2020 Martin Lafaix (martin.lafaix@external.engie.com) and others
+
+This program and the accompanying materials are made
+available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+
+SPDX-License-Identifier: EPL-2.0
+```
---
+++ packages/zabel/zabel-0.10.1.tar.gz/zabel-0.10.1/setup.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2020 Martin Lafaix (martin.lafaix@external.engie.com)
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+from setuptools import setup, find_namespace_packages
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='zabel',
+    version='0.10.1',
+    description='Zabel',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/engie-group/zabel',
+    author='Martin Lafaix',
+    author_email='martin.lafaix@external.engie.com',
+    license='Eclipse Public License 2.0',
+    packages=find_namespace_packages(include=['zabel.*']),
+    package_data={'': ['*.yaml']},
+    install_requires=[
+        'requests>=2.23',
+        'zabel-commons>=1.0',
+        'zabel-fabric>=0.13',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>= 3.6.5',
+)

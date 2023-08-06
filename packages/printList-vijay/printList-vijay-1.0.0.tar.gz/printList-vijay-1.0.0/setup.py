cfa5e1d5af51aa2ef81af86ae3ed6bf83e34a573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:27 +0000

    Add package printList-vijay/1.0.0/printList-vijay-1.0.0.tar.gz

---
+++ packages/printList-vijay/printList-vijay-1.0.0.tar.gz/printList-vijay-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = 'printList-vijay',
+    version = '1.0.0',
+    py_modules = ['printList-vijay'],
+    author = 'vijay.tambe',
+    author_email = 'vijaydt89@gmail.com',
+    url = 'http://www.vijaytambe.com',
+    description = 'A simple printer of nested lines.'
+)

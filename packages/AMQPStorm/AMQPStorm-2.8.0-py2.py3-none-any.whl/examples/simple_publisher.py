fd812d5e4dd42804671b3ccb470df18a04e72df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package AMQPStorm/2.8.0/AMQPStorm-2.8.0-py2.py3-none-any.whl

---
+++ packages/AMQPStorm/AMQPStorm-2.8.0-py2.py3-none-any.whl/examples/simple_publisher.py
@@ -0,0 +1,29 @@
+import logging
+
+from amqpstorm import Connection
+from amqpstorm import Message
+
+logging.basicConfig(level=logging.INFO)
+
+
+def publish_message():
+    with Connection('127.0.0.1', 'guest', 'guest') as connection:
+        with connection.channel() as channel:
+            # Declare the Queue, 'simple_queue'.
+            channel.queue.declare('simple_queue')
+
+            # Message Properties.
+            properties = {
+                'content_type': 'text/plain',
+                'headers': {'key': 'value'}
+            }
+
+            # Create the message.
+            message = Message.create(channel, 'Hello World!', properties)
+
+            # Publish the message to a queue called, 'simple_queue'.
+            message.publish('simple_queue')
+
+
+if __name__ == '__main__':
+    publish_message()

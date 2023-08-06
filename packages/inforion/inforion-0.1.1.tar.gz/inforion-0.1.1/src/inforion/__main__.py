803b4c0e6c0865c764870fc78f851a8e3c644697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:17 +0000

    Add package inforion/0.1.1/inforion-0.1.1.tar.gz

---
+++ packages/inforion/inforion-0.1.1.tar.gz/inforion-0.1.1/src/inforion/__main__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import base64
+
+import logging
+
+import sys, getopt
+
+#from . import __version__
+
+from requests_oauthlib import OAuth2Session
+from requests.auth import HTTPBasicAuth
+from oauthlib.oauth2 import BackendApplicationClient
+
+
+
+def main(argv):
+    argv = sys.argv[1:]
+    #log.debug('inforion ' + __version__)
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('inforion -i InforIONFile -url url -methode')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('inforion -i InforIONFile -url url -methode')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print ('Input file is "', inputfile)
+    print ('Output file is "', outputfile)
+
+if __name__ == "__main__":
+    #sys.exit(run("-h"))
+    main(sys.argv[1:])
+

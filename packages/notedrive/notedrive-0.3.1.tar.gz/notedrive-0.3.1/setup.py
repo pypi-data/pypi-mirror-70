0199d063d2ebe970b058fdf36d439fabeb6b9b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:16 +0000

    Add package notedrive/0.3.1/notedrive-0.3.1.tar.gz

---
+++ packages/notedrive/notedrive-0.3.1.tar.gz/notedrive-0.3.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/03/30 12:10
+# @Author  : niuliangtao
+# @Site    :
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+
+install_requires = ['requests', 'demjson', 'numpy', 'tqdm', 'cryptography', 'pycurl', 'notetool']
+
+setup(name='notedrive',
+      version='0.3.1',
+      description='notedrive',
+      author='niuliangtao',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+
+      packages=find_packages(),
+      install_requires=install_requires
+      )

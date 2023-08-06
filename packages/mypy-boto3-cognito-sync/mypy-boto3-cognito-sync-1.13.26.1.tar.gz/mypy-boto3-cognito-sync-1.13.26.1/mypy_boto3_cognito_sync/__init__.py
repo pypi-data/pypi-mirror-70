ef3042c1feddadcd382ac0d132eb18c0c76e64f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:21 +0000

    Add package mypy-boto3-cognito-sync/1.13.26.1/mypy-boto3-cognito-sync-1.13.26.1.tar.gz

---
+++ packages/mypy-boto3-cognito-sync/mypy-boto3-cognito-sync-1.13.26.1.tar.gz/mypy-boto3-cognito-sync-1.13.26.1/mypy_boto3_cognito_sync/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for cognito-sync service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_cognito_sync import (
+        Client,
+        CognitoSyncClient,
+    )
+
+    session = boto3.Session()
+
+    client: CognitoSyncClient = boto3.client("cognito-sync")
+    session_client: CognitoSyncClient = session.client("cognito-sync")
+    ```
+"""
+from mypy_boto3_cognito_sync.client import CognitoSyncClient, CognitoSyncClient as Client
+
+
+__all__ = ("Client", "CognitoSyncClient")

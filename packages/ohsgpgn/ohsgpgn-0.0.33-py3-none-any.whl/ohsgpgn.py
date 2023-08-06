4d86aa67e00f371d9d1b4258a91625ef9a6944f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:34 +0000

    Add package ohsgpgn/0.0.33/ohsgpgn-0.0.33-py3-none-any.whl

---
+++ packages/ohsgpgn/ohsgpgn-0.0.33-py3-none-any.whl/ohsgpgn.py
@@ -0,0 +1,24 @@
+import random
+import string
+def genGoodpass(n):
+    '''
+      This ohsgpgn is a simple but good password generator. The generated password includes at least one number,one ascii,one capatalized ascii and one special chars(‘!@$%^&*()+’).With argument, length of password can be set. If the argument(n) is less than 10(n < 10), the generated password length will be at least 10.
+    '''
+    if n < 10:
+        n = 10
+    goodPass=[]
+    rc=random.choice
+    sal=string.ascii_lowercase
+    sau=string.ascii_uppercase
+    spchar='!@#$%^&*()+'
+    sd=string.digits
+    passins=[sal,sau,spchar,sd]
+    for i in range(n-4):
+        passins.append(rc(passins))
+    for i in passins:
+        goodPass.append(rc(i))
+
+    
+    random.shuffle(goodPass)
+    goodPass=''.join(goodPass)
+    return goodPass

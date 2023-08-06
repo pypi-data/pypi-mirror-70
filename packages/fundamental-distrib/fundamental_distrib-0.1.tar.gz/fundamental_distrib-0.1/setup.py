49a4928e6089ef13e7350eb7f92cd5406833d00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:35 +0000

    Add package fundamental-distrib/0.1/fundamental_distrib-0.1.tar.gz

---
+++ packages/fundamental-distrib/fundamental_distrib-0.1.tar.gz/fundamental_distrib-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fundamental_distrib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['fundamental_distrib'],
+      author = 'AWS Course',
+      zip_safe=False)

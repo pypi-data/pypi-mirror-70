a547b058660944c80ea9c724e4e7629337ba7fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:06 +0000

    Add package gnb-distributions-varun/0.1/gnb_distributions_varun-0.1.tar.gz

---
+++ packages/gnb-distributions-varun/gnb_distributions_varun-0.1.tar.gz/gnb_distributions_varun-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gnb_distributions_varun',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gnb_distributions_varun'],
+      zip_safe=False)

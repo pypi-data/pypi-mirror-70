0bba6fa8d15bdd47c9627e3fc30ec4c8a1d917ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package ecmtool/0.1.6/ecmtool-0.1.6.tar.gz

---
+++ packages/ecmtool/ecmtool-0.1.6.tar.gz/ecmtool-0.1.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import numpy as np
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+setup(
+    name='ecmtool',
+    version='0.1.6',
+    packages=['ecmtool'],
+    install_requires=['numpy',
+                      'scipy',
+                      'sympy',
+                      'python-libsbml',
+                      'cbmpy',
+                      'psutil',
+                      'sklearn'],
+    ext_modules=cythonize(Extension("_bglu_dense", ["ecmtool/_bglu_dense.pyx"], include_dirs=[np.get_include()]), compiler_directives={'language_level' : "3"}),
+    include_dirs=[np.get_include()],
+    url='https://github.com/tjclement/ecmtool',
+    license='MIT',
+    author='Tom Clement',
+    author_email='mr@tomclement.nl',
+    description='Calculates elementary conversion modes (Urbanczik & Wagner, 2005) of metabolic networks.',
+    include_package_data=True
+)

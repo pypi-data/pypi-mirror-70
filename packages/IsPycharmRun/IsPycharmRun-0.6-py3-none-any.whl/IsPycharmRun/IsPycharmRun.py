5a68a24b71a7189f54dc44a2e2a83510fe0e426c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:55 +0000

    Add package IsPycharmRun/0.6/IsPycharmRun-0.6-py3-none-any.whl

---
+++ packages/IsPycharmRun/IsPycharmRun-0.6-py3-none-any.whl/IsPycharmRun/IsPycharmRun.py
@@ -0,0 +1,21 @@
+from functools import wraps
+import os
+
+class IsPycharmRun:
+    def __init__(self, deviceUrl, logDir):
+        self.deviceUrl = deviceUrl
+        self.logDir = logDir
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(*args):
+            if self.isPycharmRun():
+                print(func.__name__ + " was called")
+                return func(basedir=args[0], devices=self.deviceUrl, logdir=self.logDir)
+            else:
+                return func(*args)
+        return wrapped_function
+
+    def isPycharmRun(self):
+        isRunningInPyCharm = "PYCHARM_HOSTED" in os.environ
+        return isRunningInPyCharm

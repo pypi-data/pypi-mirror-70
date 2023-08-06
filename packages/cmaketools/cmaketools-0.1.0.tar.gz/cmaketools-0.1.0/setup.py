5abe9857eeaa9982485e27ac08c30ba5e476a888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:05 +0000

    Add package cmaketools/0.1.0/cmaketools-0.1.0.tar.gz

---
+++ packages/cmaketools/cmaketools-0.1.0.tar.gz/cmaketools-0.1.0/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="cmaketools",
+    version="0.1.0",
+    author="Takeshi (Kesh) Ikuma",
+    author_email="tikuma@gmail.com",
+    description="Seamless integration of Cmake build system to setuptools/distutils",
+    url="https://github.com/python-cmaketools/python-cmaketools",
+    packages=["cmaketools"]
+)

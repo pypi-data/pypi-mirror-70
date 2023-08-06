258f3542c570d58a264e7773c7e0ce8b7605caf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package xtract-sdk/0.0.1/xtract-sdk-0.0.1.tar.gz

---
+++ packages/xtract-sdk/xtract-sdk-0.0.1.tar.gz/xtract-sdk-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xtract-sdk", # Replace with your own username
+    version="0.0.1",
+    author="Tyler J. Skluzacek",
+    author_email="skluzacek@uchicago.edu",
+    description="A package used for downloading and processing files from multiple habitats.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xtracthub/xtract-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

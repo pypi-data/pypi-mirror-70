8ffa1dd5315c350c4071d7b876c41cafc90e01b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:41 +0000

    Add package OASYS1-ShadowOui/1.5.66/OASYS1-ShadowOui-1.5.66.tar.gz

---
+++ packages/OASYS1-ShadowOui/OASYS1-ShadowOui-1.5.66.tar.gz/OASYS1-ShadowOui-1.5.66/orangecontrib/shadow/widgets/loop_management/__init__.py
@@ -0,0 +1,22 @@
+"""
+=========
+Associate
+=========
+
+Widgets for association rules.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "Loop Management"
+
+DESCRIPTION = "Widgets for Shadow - Loop Management."
+
+#BACKGROUND = "#f0d3f3"
+BACKGROUND = "#A9D0F5"
+
+ICON = "icons/loop_management.png"
+
+PRIORITY = 107
+

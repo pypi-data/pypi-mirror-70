a1d88f1f06d023b7874d1d5a412bf9923c90cfab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package eririn/0.0.2/eririn-0.0.2.tar.gz

---
+++ packages/eririn/eririn-0.0.2.tar.gz/eririn-0.0.2/src/eririn/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Usage: arguments_example.py [-vqrh] [FILE] ...
+          arguments_example.py (--left | --right) CORRECTION FILE
+Process FILE and optionally apply correction to either left-hand side or
+right-hand side.
+Arguments:
+  FILE        optional input file
+  CORRECTION  correction angle, needs FILE, --left or --right to be present
+Options:
+  -h --help
+  -v       verbose mode
+  -q       quiet mode
+  -r       make report
+  --left   use left-hand side
+  --right  use right-hand side
+"""
+from docopt import docopt
+
+def main():
+    arguments = docopt(__doc__)
+    print(arguments)   
+
+
+if __name__ == '__main__':
+    main()

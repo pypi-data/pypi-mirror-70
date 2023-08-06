83c0ff6040d6e2cc11e10a2581d39ffcb2796c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:11 +0000

    Add package kmerexplor/0.4.2/kmerexplor-0.4.2.tar.gz

---
+++ packages/kmerexplor/kmerexplor-0.4.2.tar.gz/kmerexplor-0.4.2/kmerexplor/info.py
@@ -0,0 +1,8 @@
+# info.py
+
+APPNAME = "KmerExploR"
+SHORTDESC = "What else in these samples ?"
+LICENCE = "GPL3"
+VERSION = "0.4.2"
+AUTHOR = "Benoit Guibert"
+AUTHOR_EMAIL = "benoit.guibert@free.fr"

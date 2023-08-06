3651704eabb7013c36ef4a0a7f5aad4d7669339f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:41 +0000

    Add package basic-logtools/0.0.1/basic_logtools-0.0.1.tar.gz

---
+++ packages/basic-logtools/basic_logtools-0.0.1.tar.gz/basic_logtools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='basic_logtools',
+      version='0.0.1',
+      description='Some useful to create log-rotate files, register events',
+      url='https://logtools.readthedocs.io/en/latest/',
+      author='David Pineda Osorio',
+      author_email='dpineda@uchile.cl',
+      license='GPL3',
+      packages=['basic_logtools'],
+      zip_safe=False)

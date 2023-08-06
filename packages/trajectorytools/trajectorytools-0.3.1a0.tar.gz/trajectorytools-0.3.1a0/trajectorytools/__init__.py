b0b9119c37530a8cd31298c00ceaf6035578c599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package trajectorytools/0.3.1a0/trajectorytools-0.3.1a0.tar.gz

---
+++ packages/trajectorytools/trajectorytools-0.3.1a0.tar.gz/trajectorytools-0.3.1a0/trajectorytools/__init__.py
@@ -0,0 +1,7 @@
+from .geometry import *
+from .interpolate import *
+from .trajectories import Trajectories, FishTrajectories, TrajectoriesWithPoints
+#from . import animation
+from . import socialcontext
+from . import collective
+#from . import plot

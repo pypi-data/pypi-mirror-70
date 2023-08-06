01da870fcef96cb7374c5d4c0ada28b8cf59bdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package keract/4.0.0/keract-4.0.0.tar.gz

---
+++ packages/keract/keract-4.0.0.tar.gz/keract-4.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='keract',
+    version='4.0.0',
+    description='Keract - Tensorflow Keras Activations and Gradients',
+    author='Philippe Remy',
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=['keract'],
+    install_requires=[
+        'numpy>=1.18.5',
+    ]
+)

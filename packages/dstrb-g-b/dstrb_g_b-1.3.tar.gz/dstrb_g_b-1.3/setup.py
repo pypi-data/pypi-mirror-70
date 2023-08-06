c6ef8c5238639e34ea445a9ddf71d48d20cb845e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package dstrb-g-b/1.3/dstrb_g_b-1.3.tar.gz

---
+++ packages/dstrb-g-b/dstrb_g_b-1.3.tar.gz/dstrb_g_b-1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dstrb_g_b',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['dstrb_g_b'],
+      author = 'ariche Salma', 
+      zip_safe=False)

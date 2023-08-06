6eb46d01af6697d4085645948f3940230a20002f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:46 +0000

    Add package mf-horizon-client/2.3.0/mf_horizon_client-2.3.0.tar.gz

---
+++ packages/mf-horizon-client/mf_horizon_client-2.3.0.tar.gz/mf_horizon_client-2.3.0/src/mf_horizon_client/data_structures/configs/stage_config_enums.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+
+class CorrelationMethod(Enum):
+    mutual_info = "mutual_info"
+    spearman = "spearman"
+    pearson = "pearson"
+    kendall = "kendall"
+
+
+class StationarisationStrategy(Enum):
+    keep_fail = "keep_fail"
+    discard_fail = "discard_fail"
+    none = "none"
+
+
+class TargetTransformType(Enum):
+    DoNothing = "DoNothingTargetTransform"
+    HorizonLagDiff = "HorizonDiff"
+    HorizonLagDiffRatio = "HorizonDiffRatio"
+
+
+class FeatureGeneratorType(Enum):
+    autolag = "autolag"
+    ewma = "ewma"
+    logarithm = "logarithm"
+    lag = "lag"
+    num_peaks = "num_peaks"
+    rolling_average = "rolling_average"
+    perc_change = "perc_change"
+
+
+class RegressorType(Enum):
+    RandomForest = "RandomForest"
+    Martingale = "Martingale"
+    VBLinReg = "VBLinReg"
+    MondrianForest = "MondrianForest"
+    XGBoost = "XGBoost"

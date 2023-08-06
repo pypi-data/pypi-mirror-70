655a53a9d28cae2cda7d4c4fc28329a39e8c76f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:46 +0000

    Add package scikit-network/0.18.0/scikit_network-0.18.0-cp37-cp37m-win32.whl

---
+++ packages/scikit-network/scikit_network-0.18.0-cp37-cp37m-win32.whl/sknetwork/utils/tests/test_kmeans.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on October 2019
+@author: Nathan de Lara <ndelara@enst.fr>
+"""
+
+import unittest
+
+import numpy as np
+
+from sknetwork.utils import KMeansDense
+
+
+class TestKMeans(unittest.TestCase):
+
+    def test_kmeans(self):
+        x = np.random.randn(10, 3)
+        kmeans = KMeansDense(n_clusters=2)
+        kmeans.fit(x)
+        self.assertEqual(kmeans.labels_.shape, (x.shape[0],))
+        self.assertEqual(kmeans.cluster_centers_.shape, (kmeans.n_clusters, x.shape[1]))

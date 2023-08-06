f874a68a734347deae8b6498f33f5232bb2ef264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:27 +0000

    Add package rhasspy-fuzzywuzzy-hermes/0.1.1/rhasspy-fuzzywuzzy-hermes-0.1.1.tar.gz

---
+++ packages/rhasspy-fuzzywuzzy-hermes/rhasspy-fuzzywuzzy-hermes-0.1.1.tar.gz/rhasspy-fuzzywuzzy-hermes-0.1.1/setup.py
@@ -0,0 +1,38 @@
+"""Setup script for rhasspy-fuzzywuzzy-hermes package"""
+import os
+
+import setuptools
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, "README.md"), "r") as readme_file:
+    long_description = readme_file.read()
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
+
+setuptools.setup(
+    name="rhasspy-fuzzywuzzy-hermes",
+    version=version,
+    author="Michael Hansen",
+    author_email="hansen.mike@gmail.com",
+    url="https://github.com/rhasspy/rhasspy-fuzzywuzzy-hermes",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "rhasspy-fuzzywuzzy-hermes = rhasspyfuzzywuzzy_hermes.__main__:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+)

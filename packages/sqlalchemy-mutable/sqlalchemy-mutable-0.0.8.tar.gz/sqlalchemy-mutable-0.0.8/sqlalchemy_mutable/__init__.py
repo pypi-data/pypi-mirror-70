3f99c0f45703ff533cb0c2b0e877220dcb023e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:05 +0000

    Add package sqlalchemy-mutable/0.0.8/sqlalchemy-mutable-0.0.8.tar.gz

---
+++ packages/sqlalchemy-mutable/sqlalchemy-mutable-0.0.8.tar.gz/sqlalchemy-mutable-0.0.8/sqlalchemy_mutable/__init__.py
@@ -0,0 +1,5 @@
+from .model_shell import Query
+from .mutable import Mutable, MutableType, MutableModelBase
+from .mutable_dict import MutableDict, MutableDictType
+from .mutable_list import MutableList, MutableListType
+from .coerced_types import *

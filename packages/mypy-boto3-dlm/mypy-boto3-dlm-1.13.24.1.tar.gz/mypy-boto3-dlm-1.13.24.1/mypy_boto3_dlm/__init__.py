fa118d08ae225e23df785601bf559ec8fcf39e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:57 +0000

    Add package mypy-boto3-dlm/1.13.24.1/mypy-boto3-dlm-1.13.24.1.tar.gz

---
+++ packages/mypy-boto3-dlm/mypy-boto3-dlm-1.13.24.1.tar.gz/mypy-boto3-dlm-1.13.24.1/mypy_boto3_dlm/__init__.py
@@ -0,0 +1,20 @@
+"""
+Main interface for dlm service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.dlm import (
+        Client,
+        DLMClient,
+        )
+
+    session = boto3.Session()
+
+    client: DLMClient = boto3.client("dlm")
+    session_client: DLMClient = session.client("dlm")
+"""
+from mypy_boto3_dlm.client import DLMClient, DLMClient as Client
+
+
+__all__ = ("Client", "DLMClient")

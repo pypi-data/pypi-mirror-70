b3c65e22538e489d5d73ac1787ac6c64d1aea5cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package ephysiopy/1.6.14/ephysiopy-1.6.14-py3-none-any.whl

---
+++ packages/ephysiopy/ephysiopy-1.6.14-py3-none-any.whl/ephysiopy/__init__.py
@@ -0,0 +1,7 @@
+from ephysiopy import __about__
+kk_path = '/home/robin/klustakwik/KlustaKwik'
+# used to bandpass filter the continuous raw openephys data when converting to
+# Axona eeg/ egf format
+lfp_lowcut = 1
+lfp_highcut = 500
+fs = 30000 # sampling rate of continuous data in openephys

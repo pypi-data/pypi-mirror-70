9efd432ce71dff19de6cc57065295785379f0b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:44 +0000

    Add package model_template/0.0.9/model_template-0.0.9.tar.gz

---
+++ packages/model_template/model_template-0.0.9.tar.gz/model_template-0.0.9/model_template/__init__.py
@@ -0,0 +1,9 @@
+"""
+A package to run any machine learning model in production
+"""
+
+from .model_template import ModelTemplate
+from .model_template import ModelPieces
+from .model_template import ModelThemes
+
+__version__ = "0.0.9"

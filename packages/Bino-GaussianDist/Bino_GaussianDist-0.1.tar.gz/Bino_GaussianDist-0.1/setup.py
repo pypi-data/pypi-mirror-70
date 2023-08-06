cafa41075dda9252401ec8a0f2ba75989ececba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:08 +0000

    Add package Bino-GaussianDist/0.1/Bino_GaussianDist-0.1.tar.gz

---
+++ packages/Bino-GaussianDist/Bino_GaussianDist-0.1.tar.gz/Bino_GaussianDist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Bino_GaussianDist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Bino_GaussianDist'],
+      author= 'Namratha Sri Mateti',
+      zip_safe=False)

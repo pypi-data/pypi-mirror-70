9772017846d676cb516a39a7b949982ed52b0d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:04 +0000

    Add package wearablevar/0.4/wearablevar-0.4.tar.gz

---
+++ packages/wearablevar/wearablevar-0.4.tar.gz/wearablevar-0.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='wearablevar',
+      packages = ['wearablevar'],
+      version='0.4',
+      description='Variability Metrics from Longitudinal Wearable Sensors',
+      url='https://github.com/brinnaebent/wearablevar',
+      download_url = 'https://github.com/brinnaebent/wearablevar/archive/0.4.tar.gz',
+      author='Brinnae Bent',
+      author_email='runsdata@gmail.com',
+      keywords = ['wearables', 'features', 'feature engineering'],
+      license='MIT',
+
+      install_requires=['pandas','numpy','datetime',
+            ],
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',      
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
+)

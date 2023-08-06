ec35245dbb899cd8db5ee35c1712e7b95eff127e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:36 +0000

    Add package yplate/0.0.1/yplate-0.0.1-py3-none-any.whl

---
+++ packages/yplate/yplate-0.0.1-py3-none-any.whl/yplate/ops.py
@@ -0,0 +1,19 @@
+""" Functions imported in commands  """
+
+## Command line Display UI ##
+def display_top():
+    print("\n#################################################")
+    print("                   Plate-Scan                     ")
+    print("#################################################\n")
+
+
+## Show Model Config ##
+def model_config():
+    print("#################################################")
+    print("               Model Configuration               ")
+    print("#################################################\n")
+    print("model: YOLO v3")
+    print("Model cfg: plate.cfg")
+    print("Model weights: plate.weights")
+    print("Last training Iteration: 4234")
+    print("\n################################################\n")

e842d4941562d2afe026d09d2941e6fb0bcd7947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:37 +0000

    Add package RoboMentor-Client/1.1.15/RoboMentor_Client-1.1.15-py2.py3-none-any.whl

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.1.15-py2.py3-none-any.whl/robomentor_client/function/serial.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+import serial
+from ..utils import Log
+
+
+class Serial:
+
+    def __init__(self, port, baudrate, stopbits, timeout: float = 0.2):
+
+        self.port = port
+        self.baudrate = baudrate
+        self.stopbits = stopbits
+        self.timeout = timeout
+        self.conn = None
+        self.readData = ""
+
+    def open(self):
+
+        status = False
+
+        try:
+            self.conn = serial.Serial(self.port, self.baudrate, self.timeout)
+            if self.conn.is_open:
+                status = True
+        except Exception as e:
+            Log.error(e)
+
+        return self.conn, status
+
+    def write(self, command):
+
+        try:
+            self.conn.write(command)
+        except Exception as e:
+            Log.error(e)
+
+    def close(self):
+
+        self.conn.close()
+

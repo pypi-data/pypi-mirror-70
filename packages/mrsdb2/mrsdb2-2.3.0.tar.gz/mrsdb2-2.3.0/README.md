0215b475191d2d6708081d076a8d685bdd96a7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package mrsdb2/2.3.0/mrsdb2-2.3.0.tar.gz

---
+++ packages/mrsdb2/mrsdb2-2.3.0.tar.gz/mrsdb2-2.3.0/README.md
@@ -0,0 +1,10 @@
+# MRSDB
+![Upload Python Package](https://github.com/Netriza/mrsdb/workflows/Upload%20Python%20Package/badge.svg) ![Run on Repl.it](https://repl.it/badge/github/Netriza/mrsdb)
+
+The quick, rich, serverless, and efficient json-like Python 3 document database.
+
+[Check out our Wiki for documentation/tutorials](https://github.com/netriza/mrsdb/wiki)
+
+```bash
+pip -m install mrsdb2
+```
---
+++ packages/mrsdb2/mrsdb2-2.3.0.tar.gz/mrsdb2-2.3.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+packages = setuptools.find_packages()
+
+
+
+setuptools.setup(
+    name="mrsdb2",
+    version="2.3.0",
+    author="Netriza",
+    author_email="info@netriza.ml",
+    description="The quick, rich, serverless, and efficient json-like Python 3 document database.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/netriza/mrsdb",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'bson',
+        'bcrypt',
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers"
+    ],
+    python_requires='>=3.6',
+)

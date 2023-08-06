985be52c1df2bb05502277059b943d860b028695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:33 +0000

    Add package Distribution-HSM/0.2/Distribution_HSM-0.2.tar.gz

---
+++ packages/Distribution-HSM/Distribution_HSM-0.2.tar.gz/Distribution_HSM-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Distribution_HSM',
+      version='0.2',
+      description='distributions',
+      packages=['Distribution_HSM'],
+      author = "Hrushikesh Mulavekar",
+      author_email = "hmulavekar@gmail.com",
+      zip_safe=False)

727715f60fbb306f7f5963d86fbd5cbd01a1aaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:41 +0000

    Add package RoboMentor-Client/1.1.24/RoboMentor_Client-1.1.24-py2.py3-none-any.whl

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.1.24-py2.py3-none-any.whl/robomentor_client/service/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .message import Message
+from .service import Service
+from .system import System

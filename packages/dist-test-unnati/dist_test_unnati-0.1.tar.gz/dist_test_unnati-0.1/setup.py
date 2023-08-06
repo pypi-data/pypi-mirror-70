b2e1884737dd601afcb2e62fefaa242a524088a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:41 +0000

    Add package dist-test-unnati/0.1/dist_test_unnati-0.1.tar.gz

---
+++ packages/dist-test-unnati/dist_test_unnati-0.1.tar.gz/dist_test_unnati-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_test_unnati',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_test_unnati'],
+      zip_safe=False)

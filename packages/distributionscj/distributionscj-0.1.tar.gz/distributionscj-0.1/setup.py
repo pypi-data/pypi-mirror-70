a8920047c886a95b29decfeb62d9c7be12a6e2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:09 +0000

    Add package distributionscj/0.1/distributionscj-0.1.tar.gz

---
+++ packages/distributionscj/distributionscj-0.1.tar.gz/distributionscj-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionscj',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionscj'],
+      zip_safe=False)

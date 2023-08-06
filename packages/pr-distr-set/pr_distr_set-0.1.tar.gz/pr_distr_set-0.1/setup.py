003183a63147fae418d684af367b7c8554412f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:22 +0000

    Add package pr-distr-set/0.1/pr_distr_set-0.1.tar.gz

---
+++ packages/pr-distr-set/pr_distr_set-0.1.tar.gz/pr_distr_set-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pr_distr_set',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pr_distr_set'],
+      author = 'Sushant',
+      zip_safe=False)

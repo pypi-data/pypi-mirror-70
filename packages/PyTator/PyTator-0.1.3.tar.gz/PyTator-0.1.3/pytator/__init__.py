85e54cf625d8112844960928eabef187417840fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:30 +0000

    Add package PyTator/0.1.3/PyTator-0.1.3.tar.gz

---
+++ packages/PyTator/PyTator-0.1.3.tar.gz/PyTator-0.1.3/pytator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tator Python Bindings REST API
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+from .tator import Tator
+from .tator import Auth
+from .version import __version__ as __version__
+
---
+++ packages/PyTator/PyTator-0.1.3.tar.gz/PyTator-0.1.3/pytator/version.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+__version__ = "0.1.3"
+
+if __name__=="__main__":
+    print(__version__)

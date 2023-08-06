d012d0810950d3d4e076cc13ea97fefe25528b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package ap-perf/0.1.1/ap_perf-0.1.1.tar.gz

---
+++ packages/ap-perf/ap_perf-0.1.1.tar.gz/ap_perf-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='ap_perf',
+      version='0.1.1',
+      description='AP-Perf: Incorporating Generic Performance Metrics in Differentiable Learning',
+      url='http://github.com/rizalzaf/ap_perf',
+      author='Rizal Fathony',
+      author_email='rizal@fathony.com',
+      license='MIT',
+      packages=['ap_perf'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'torch',
+          'numba'
+      ],
+      zip_safe=False)

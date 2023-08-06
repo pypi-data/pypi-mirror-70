eeb29913ccafb4048c9afa52df875dda0c68a8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:00 +0000

    Add package mypy-boto3-kinesis-video-archived-media/1.13.26.0/mypy_boto3_kinesis_video_archived_media-1.13.26.0-py3-none-any.whl

---
+++ packages/mypy-boto3-kinesis-video-archived-media/mypy_boto3_kinesis_video_archived_media-1.13.26.0-py3-none-any.whl/mypy_boto3_kinesis_video_archived_media/__init__.py
@@ -0,0 +1,29 @@
+"""
+Main interface for kinesis-video-archived-media service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_kinesis_video_archived_media import (
+        Client,
+        KinesisVideoArchivedMediaClient,
+        ListFragmentsPaginator,
+    )
+
+    session = boto3.Session()
+
+    client: KinesisVideoArchivedMediaClient = boto3.client("kinesis-video-archived-media")
+    session_client: KinesisVideoArchivedMediaClient = session.client("kinesis-video-archived-media")
+
+    list_fragments_paginator: ListFragmentsPaginator = client.get_paginator("list_fragments")
+    ```
+"""
+from mypy_boto3_kinesis_video_archived_media.client import (
+    KinesisVideoArchivedMediaClient as Client,
+    KinesisVideoArchivedMediaClient,
+)
+from mypy_boto3_kinesis_video_archived_media.paginator import ListFragmentsPaginator
+
+
+__all__ = ("Client", "KinesisVideoArchivedMediaClient", "ListFragmentsPaginator")

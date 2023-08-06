59795fcc6f69739f7cbcd93e72ec69cf9a732903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:07 +0000

    Add package perf-timer/0.1.1/perf_timer-0.1.1-py3-none-any.whl

---
+++ packages/perf-timer/perf_timer-0.1.1-py3-none-any.whl/perf_timer/__init__.py
@@ -0,0 +1,18 @@
+from ._impl import PerfTimer, ThreadPerfTimer
+try:
+    from ._trio import trio_perf_counter, TrioPerfTimer
+except ImportError:
+    pass
+from ._version import __version__
+
+def _metadata_fix():
+    # don't do this for Sphinx case because it breaks "bysource" member ordering
+    import sys  # pylint: disable=import-outside-toplevel
+    if 'sphinx' in sys.modules:
+        return
+
+    for name, value in globals().items():
+        if not name.startswith('_'):
+            value.__module__ = __name__
+
+_metadata_fix()

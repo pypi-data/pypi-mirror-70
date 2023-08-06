e519e92d0ea10dcdd72413c270f78467c2c36d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:53 +0000

    Add package distributions-DH/0.5/distributions_DH-0.5.tar.gz

---
+++ packages/distributions-DH/distributions_DH-0.5.tar.gz/distributions_DH-0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_DH',
+      version='0.5',
+      description='Gaussian distributions',
+      packages=['distributions_DH'],
+      zip_safe=False)

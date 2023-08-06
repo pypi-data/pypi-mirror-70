30f3a5e4cdcc898ff9b241f88745fc41ae05920d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:10 +0000

    Add package distributions-sri/0.2/distributions_sri-0.2.tar.gz

---
+++ packages/distributions-sri/distributions_sri-0.2.tar.gz/distributions_sri-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_sri',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_sri'],
+      author = 'Sridhar Sundaraju',
+      author_email = 'test@something.com',
+      zip_safe=False)

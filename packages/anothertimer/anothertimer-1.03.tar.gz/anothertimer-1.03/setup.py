1999c798ca9a2aa42e8e27f6c69e3e1888ccac49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:34 +0000

    Add package anothertimer/1.03/anothertimer-1.03.tar.gz

---
+++ packages/anothertimer/anothertimer-1.03.tar.gz/anothertimer-1.03/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'anothertimer',
+  packages = ['anothertimer'],
+  version = '1.03',     
+  license='MIT',
+  description = 'Literally another code timer (with fancy plotting)',
+  author = 'Dang Pham',
+  author_email = 'dang.c.pham@hotmail.com',
+  url = 'https://github.com/dangcpham/anothertimer',
+  download_url = 'https://github.com/dangcpham/anothertimer/archive/v1.0.tar.gz',
+  keywords = ['time', 'timer', 'timing'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)

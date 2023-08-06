b4646aaa0cb0374619b7200cb8558c1e25e60c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:24 +0000

    Add package kosmorro/0.8.1/kosmorro-0.8.1.tar.gz

---
+++ packages/kosmorro/kosmorro-0.8.1.tar.gz/kosmorro-0.8.1/test/ephemerides.py
@@ -0,0 +1,121 @@
+import unittest
+from .testutils import expect_assertions
+from kosmorrolib import ephemerides
+from kosmorrolib.data import EARTH, Position, MoonPhase
+from datetime import date
+from kosmorrolib.exceptions import OutOfRangeDateError
+
+
+class EphemeridesTestCase(unittest.TestCase):
+    def test_get_ephemerides_for_aster_returns_correct_hours(self):
+        position = Position(0, 0, EARTH)
+        eph = ephemerides.get_ephemerides(date=date(2019, 11, 18),
+                                          position=position)
+
+        @expect_assertions(self.assertRegex, num=3)
+        def do_assertions(assert_regex):
+            for ephemeris in eph:
+                if ephemeris.object.skyfield_name == 'SUN':
+                    assert_regex(ephemeris.rise_time.isoformat(), '^2019-11-18T05:41:')
+                    assert_regex(ephemeris.culmination_time.isoformat(), '^2019-11-18T11:45:')
+                    assert_regex(ephemeris.set_time.isoformat(), '^2019-11-18T17:48:')
+                    break
+
+        do_assertions()
+
+    ###################################################################################################################
+    ###                                             MOON PHASE TESTS                                                ###
+    ###################################################################################################################
+
+    def test_moon_phase_new_moon(self):
+        phase = ephemerides.get_moon_phase(date(2019, 11, 25))
+        self.assertEqual('WANING_CRESCENT', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-26T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 26))
+        self.assertEqual('NEW_MOON', phase.identifier)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-12-04T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 27))
+        self.assertEqual('WAXING_CRESCENT', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-12-04T')
+
+    def test_moon_phase_first_crescent(self):
+        phase = ephemerides.get_moon_phase(date(2019, 11, 3))
+        self.assertEqual('WAXING_CRESCENT', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-04T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 4))
+        self.assertEqual('FIRST_QUARTER', phase.identifier)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-12T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 5))
+        self.assertEqual('WAXING_GIBBOUS', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-12T')
+
+    def test_moon_phase_full_moon(self):
+        phase = ephemerides.get_moon_phase(date(2019, 11, 11))
+        self.assertEqual('WAXING_GIBBOUS', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-12T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 12))
+        self.assertEqual('FULL_MOON', phase.identifier)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-19T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 13))
+        self.assertEqual('WANING_GIBBOUS', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-19T')
+
+    def test_moon_phase_last_quarter(self):
+        phase = ephemerides.get_moon_phase(date(2019, 11, 18))
+        self.assertEqual('WANING_GIBBOUS', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-19T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 19))
+        self.assertEqual('LAST_QUARTER', phase.identifier)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-26T')
+
+        phase = ephemerides.get_moon_phase(date(2019, 11, 20))
+        self.assertEqual('WANING_CRESCENT', phase.identifier)
+        self.assertIsNone(phase.time)
+        self.assertRegexpMatches(phase.next_phase_date.isoformat(), '^2019-11-26T')
+
+    def test_moon_phase_prediction(self):
+        phase = MoonPhase('NEW_MOON', None, None)
+        self.assertEqual('First Quarter', phase.get_next_phase_name())
+        phase = MoonPhase('WAXING_CRESCENT', None, None)
+        self.assertEqual('First Quarter', phase.get_next_phase_name())
+
+        phase = MoonPhase('FIRST_QUARTER', None, None)
+        self.assertEqual('Full Moon', phase.get_next_phase_name())
+        phase = MoonPhase('WAXING_GIBBOUS', None, None)
+        self.assertEqual('Full Moon', phase.get_next_phase_name())
+
+        phase = MoonPhase('FULL_MOON', None, None)
+        self.assertEqual('Last Quarter', phase.get_next_phase_name())
+        phase = MoonPhase('WANING_GIBBOUS', None, None)
+        self.assertEqual('Last Quarter', phase.get_next_phase_name())
+
+        phase = MoonPhase('LAST_QUARTER', None, None)
+        self.assertEqual('New Moon', phase.get_next_phase_name())
+        phase = MoonPhase('WANING_CRESCENT', None, None)
+        self.assertEqual('New Moon', phase.get_next_phase_name())
+
+    def test_get_ephemerides_raises_exception_on_out_of_date_range(self):
+        with self.assertRaises(OutOfRangeDateError):
+            ephemerides.get_ephemerides(date(1789, 5, 5), Position(0, 0, EARTH))
+
+    def test_get_moon_phase_raises_exception_on_out_of_date_range(self):
+        with self.assertRaises(OutOfRangeDateError):
+            ephemerides.get_moon_phase(date(1789, 5, 5))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/kosmorro/kosmorro-0.8.1.tar.gz/kosmorro-0.8.1/test/events.py
@@ -0,0 +1,71 @@
+import unittest
+
+from datetime import date, datetime
+
+from kosmorrolib import events
+from kosmorrolib.data import Event, ASTERS
+from kosmorrolib.core import get_timescale
+from unittest_data_provider import data_provider
+from kosmorrolib.exceptions import OutOfRangeDateError
+
+
+class EventTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.maxDiff = None
+
+    def test_event_only_accepts_valid_values(self):
+        with self.assertRaises(ValueError):
+            Event('SUPERNOVA', None, get_timescale().now())
+
+    expected_events_provider = lambda: (
+        (date(2020, 2, 7), []),
+
+        (date(2020, 10, 13), [Event('OPPOSITION', [ASTERS[4]], datetime(2020, 10, 13, 23, 25))]),
+
+        (date(2022, 12, 8), [Event('CONJUNCTION', [ASTERS[1], ASTERS[4]], datetime(2022, 12, 8, 4, 18)),
+                             Event('OPPOSITION', [ASTERS[4]], datetime(2022, 12, 8, 5, 41))]),
+
+        (date(2025, 1, 16), [Event('OPPOSITION', [ASTERS[4]], datetime(2025, 1, 16, 2, 38))]),
+
+        (date(2027, 2, 19), [Event('OPPOSITION', [ASTERS[4]], datetime(2027, 2, 19, 15, 50))]),
+
+        (date(2020, 1, 2), [Event('CONJUNCTION', [ASTERS[2], ASTERS[5]], datetime(2020, 1, 2, 16, 41))]),
+
+        (date(2020, 1, 12), [Event('CONJUNCTION', [ASTERS[2], ASTERS[6]], datetime(2020, 1, 12, 9, 51)),
+                             Event('CONJUNCTION', [ASTERS[2], ASTERS[9]], datetime(2020, 1, 12, 10, 13)),
+                             Event('CONJUNCTION', [ASTERS[6], ASTERS[9]], datetime(2020, 1, 12, 16, 57))]),
+
+        (date(2020, 2, 10), [Event('MAXIMAL_ELONGATION', [ASTERS[2]], datetime(2020, 2, 10, 13, 46), details='18.2°')]),
+
+        (date(2020, 3, 24), [Event('MAXIMAL_ELONGATION', [ASTERS[2]], datetime(2020, 3, 24, 1, 56), details='27.8°'),
+                             Event('MAXIMAL_ELONGATION', [ASTERS[3]], datetime(2020, 3, 24, 21, 58), details='46.1°')]),
+
+        (date(2005, 6, 16), [Event('OCCULTATION', [ASTERS[1], ASTERS[5]], datetime(2005, 6, 16, 6, 31))])
+    )
+
+    @data_provider(expected_events_provider)
+    def test_search_events(self, d: date, expected_events: [Event]):
+        actual_events = events.search_events(d)
+        self.assertEqual(len(expected_events), len(actual_events),
+                         'Expected %d elements, got %d for date %s.' % (len(expected_events),
+                                                                        len(actual_events),
+                                                                        d.isoformat()))
+
+        for i, expected_event in enumerate(expected_events):
+            actual_event = actual_events[i]
+            # Remove unnecessary precision (seconds and microseconds)
+            actual_event.start_time = datetime(actual_event.start_time.year,
+                                               actual_event.start_time.month,
+                                               actual_event.start_time.day,
+                                               actual_event.start_time.hour,
+                                               actual_event.start_time.minute)
+
+            self.assertEqual(expected_event.__dict__, actual_event.__dict__)
+
+    def test_get_events_raises_exception_on_out_of_date_range(self):
+        with self.assertRaises(OutOfRangeDateError):
+            events.search_events(date(1789, 5, 5))
+
+
+if __name__ == '__main__':
+    unittest.main()

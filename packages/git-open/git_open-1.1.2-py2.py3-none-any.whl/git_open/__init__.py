4b1057a954e260668eca6579aa07c5f328ea8d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package git-open/1.1.2/git_open-1.1.2-py2.py3-none-any.whl

---
+++ packages/git-open/git_open-1.1.2-py2.py3-none-any.whl/git_open/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "1.1.2"
+
+__title__ = "git_open"
+__author__ = """Carl-Fredrik Pettersson"""
+__email__ = "cfp@highball.se"
+__description__ = "Open a git repo in the browser from the terminal."
+__url__ = "https://github.com/cfp2000/git-open"
+__license__ = "GPLv3"
---
+++ packages/git-open/git_open-1.1.2-py2.py3-none-any.whl/git_open/cli.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import click
+import sys
+from .git_open import GitOpen
+from . import __version__
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    """Open a git repo in the browser from the terminal.
+
+    Running the command without any sub-command will open the main page of the repository.
+    """
+    if ctx.invoked_subcommand is None:
+        git_repo = GitOpen()
+        git_repo.open_from_terminal()
+
+
+@cli.command()
+def commit():
+    """Open the current commit"""
+    git_repo = GitOpen()
+    click.echo("Opening commit...")
+    git_repo.add_commit_to_url()
+    git_repo.open_from_terminal()
+
+
+@cli.command()
+def branch():
+    """Open the current branch"""
+    git_repo = GitOpen()
+    click.echo("Opening branch...")
+    git_repo.add_branch_to_url()
+    git_repo.open_from_terminal()
+
+
+@cli.command()
+def version():
+    """Show the current git-open version"""
+    click.echo("version: {}".format(__version__))
+    sys.exit()
+
+
+if __name__ == "__main__":
+    cli(obj={})

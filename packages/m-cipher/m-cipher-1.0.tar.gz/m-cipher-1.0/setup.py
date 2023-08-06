105f8c15dab699a3fe3f1196c0fc8dea6d49fda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package m-cipher/1.0/m-cipher-1.0.tar.gz

---
+++ packages/m-cipher/m-cipher-1.0.tar.gz/m-cipher-1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Author: ChungNT
+    Company: MobioVN
+    Date created: 05/06/2020
+"""
+
+from distutils.core import setup
+from Cython.Build import cythonize
+
+SOURCE_PATH = './mobio/'
+
+setup(
+    name='m-cipher',
+    version='1.0',
+    packages=[''],
+    author='Mobio Company',
+    author_email='contact@mobio.vn',
+    ext_modules=cythonize([SOURCE_PATH + '/**/*.py'], compiler_directives=dict(always_allow_keywords=True)),
+    install_requires=[
+                      'Crypto',
+                      'numpy',
+                      ]
+    )

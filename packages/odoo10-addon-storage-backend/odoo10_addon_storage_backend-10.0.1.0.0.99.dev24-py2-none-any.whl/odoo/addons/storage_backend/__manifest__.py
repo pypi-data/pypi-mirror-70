7e799fa62c68476b67642ac614733be0445afcbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:40 +0000

    Add package odoo10-addon-storage-backend/10.0.1.0.0.99.dev24/odoo10_addon_storage_backend-10.0.1.0.0.99.dev24-py2-none-any.whl

---
+++ packages/odoo10-addon-storage-backend/odoo10_addon_storage_backend-10.0.1.0.0.99.dev24-py2-none-any.whl/odoo/addons/storage_backend/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Storage Bakend",
+    "summary": "Implement the concept of Storage with amazon S3, sftp...",
+    "version": "10.0.1.0.0",
+    "category": "Storage",
+    "website": "https://www.github.com/OCA/storage",
+    "author": " Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "external_dependencies": {
+        "python": [
+            # "paramiko",
+            # "boto",
+        ]
+    },
+    "depends": ["base", "keychain", "component"],
+    "data": [
+        "views/backend_storage_view.xml",
+        "data/data.xml",
+        "security/ir.model.access.csv",
+    ],
+}

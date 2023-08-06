b7c507604c6394aad883f8fb238d90012cd6a13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:14 +0000

    Add package pyppl-report/0.6.2/pyppl_report-0.6.2-py3-none-any.whl

---
+++ packages/pyppl-report/pyppl_report-0.6.2-py3-none-any.whl/pyppl_report/__init__.py
@@ -0,0 +1,274 @@
+"""A report generating system for PyPPL"""
+import sys
+from time import time
+from hashlib import sha256
+from pathlib import Path
+import textwrap
+import toml
+from pyppl.plugin import hookimpl
+from pyppl.logger import Logger
+from pyppl.jobmgr import STATES
+from pyppl.config import config
+from pyppl.utils import fs, format_secs, filesig
+from pyppl.exception import ProcessAttributeError
+from diot import Diot, OrderedDiot
+from cmdy import CmdyReturnCodeError
+from .report import Report
+
+__version__ = "0.6.2"
+
+logger = Logger(plugin='report') # pylint: disable=invalid-name
+
+config.config.report_template = ''
+config.config.report_envs = Diot(level=1, pre='', post='')
+
+def report_template_converter(value):
+    """Convert relative path of a template to absolute"""
+    if value and value.startswith('file:'):
+        scriptpath = Path(value[5:])
+        if not scriptpath.is_absolute():
+            from inspect import getframeinfo, stack
+
+            # 0: .../pyppl_report/pyppl_report/__init__.py
+            # 1: .../PyPPL/pyppl/plugin.py
+            # 2: .../site-packages/diot.py
+            # 3: .../pyppl_report/tests/test_report.py
+            # if it fails in the future, check if the callstacks
+            # changed from pluggy
+            caller = getframeinfo(stack()[3][0])
+            scriptdir = Path(caller.filename).parent.resolve()
+            scriptpath = scriptdir / scriptpath
+        if not scriptpath.is_file():
+            raise ProcessAttributeError(
+                'Report template file does not exist: %s' % scriptpath)
+        return "file:%s" % scriptpath
+    return value
+
+@hookimpl
+def logger_init(logger):  # pylint: disable=redefined-outer-name
+    """Add log level"""
+    logger.add_level('report')
+
+
+@hookimpl
+def proc_init(proc):
+    """Add config"""
+    proc.add_config('report_template',
+                    default='',
+                    converter=report_template_converter)
+    proc.add_config('report_envs',
+                    default=Diot(),
+                    converter=lambda envs: envs or Diot())
+
+
+@hookimpl
+def proc_postrun(proc, status):
+    """Generate report for the process"""
+    # skip if process failed or cached
+    # pylint: disable=too-many-locals,too-many-branches,too-many-statements
+    report_file = proc.workdir.joinpath('proc.report.md')
+    template = proc.config.report_template
+    template_file = None
+    if template and template.startswith('file:'):
+        template_file = Path(template[5:])
+        logger.debug("Using report template: %s", template_file, proc=proc.id)
+
+    if not template and report_file.is_file():
+        report_file.unlink()
+
+    if not template or status == 'failed':
+        return
+
+    signature = OrderedDiot([(key, value)
+                             for key, value in sorted(proc.config.items())
+                             if key.startswith('report_')])
+    if template_file and template_file.is_file():
+        signature.template = filesig(template_file)
+    signature = sha256(toml.dumps(signature).encode()).hexdigest()
+
+    if status == 'cached' and report_file.is_file():
+        with report_file.open() as frpt:
+            if frpt.readline().strip() == '<!--- %s -->' % signature:
+                logger.debug("Report markdown file cached, skip.", proc=proc.id)
+                return
+
+    fs.remove(report_file)
+    logger.debug('Rendering report template ...', proc=proc.id)
+    if template_file:
+        template = template_file.read_text()
+
+    template = proc.template(textwrap.dedent(template), **proc.envs)
+    rptdata = dict(jobs=[None] * proc.size, proc=proc, args=proc.args)
+    for i, job in enumerate(proc.jobs):
+        rptdata['jobs'][i] = job.data.job.copy()
+        rptdata['jobs'][i]['i'] = job.data.i
+        rptdata['jobs'][i]['o'] = job.data.o
+
+        datafile = job.dir / 'output/job.report.data.toml'
+        if datafile.is_file():
+            with datafile.open() as fdata:
+                rptdata['jobs'][i].update(toml.load(fdata))
+
+    rptenvs = Diot(level=1, pre='', post='', title=proc.desc)
+    rptenvs.update(proc.config.report_envs)
+    rptdata['report'] = rptenvs
+    try:
+        reportmd = template.render(rptdata)
+    except Exception as exc:
+        raise RuntimeError('Failed to render report markdown for process: %s' %
+                           (proc)) from exc
+    reportmd = reportmd.splitlines()
+
+    codeblock = False
+    for i, line in enumerate(reportmd):
+        if line.startswith('#') and not codeblock:
+            reportmd[i] = '#' * (rptenvs.level - 1) + line
+        elif codeblock:
+            if line.startswith('```') and len(line) - len(
+                    line.lstrip('`')) == codeblock:
+                codeblock = False
+        elif line.startswith('```'):
+            codeblock = len(line) - len(line.lstrip('`'))
+
+    report_file.write_text(
+        '<!--- %s -->' % signature +
+        proc.template(textwrap.dedent(rptenvs.pre),
+                      **proc.envs).render(rptdata) + '\n\n' +
+        '\n'.join(reportmd) + '\n\n' +
+        proc.template(textwrap.dedent(rptenvs.post),
+                      **proc.envs).render(rptdata) + '\n'
+    )
+
+def report(ppl, # pylint: disable=too-many-arguments
+           outfile=None,
+           title=None,
+           standalone=True,
+           template=False,
+           filters=False,
+           toc=3):
+    """@API
+    Generate report for the pipeline.
+    Currently only HTML format supported.
+    @params:
+        outfile (file): The report file, could be a directory,
+            then the report file will be:
+            - `<pipeline name>.report.html`
+        title (str): The title of the report.
+            - Default: 'A report generated by pipeline powered by PyPPL'
+        standalone (bool): A standalone html file? Default: True
+            - Otherwise, static files, including js/css/images will be
+              store in a separated directory.
+    """
+    timer = time()
+    default_basename = '%s.report.html' % ppl.name
+    if not outfile:
+        outfile = Path('.').joinpath(default_basename)
+    elif Path(outfile).is_dir():
+        outfile = Path(outfile).joinpath(default_basename)
+    else:
+        outfile = Path(outfile)
+
+    logger.report('Generating report using pandoc ...')
+    reports = [
+        proc.workdir.joinpath('proc.report.md') for proc in ppl.procs
+        if proc.workdir.joinpath('proc.report.md').exists()
+    ]
+
+    # see if it is cached:
+    if (outfile.is_file()
+            and reports
+            and outfile.stat().st_mtime >= max(rptmd.stat().st_mtime
+                                               for rptmd in reports)):
+        logger.report('Report cached: %s', outfile)
+    else:
+        # force to add a title.
+        title = title or 'Reports for ' + ppl.name + ' pipeline'
+        cmd = Report(reports, outfile, title).generate(standalone,
+                                                       template,
+                                                       filters,
+                                                       toc)
+        try:
+            logger.debug('Running: ' + cmd.strcmd)
+            cmd.run(wait=True)
+            logger.report('Report generated: %s', outfile)
+            logger.report('Time elapsed: %s', format_secs(time() - timer))
+        except CmdyReturnCodeError as ex:
+            ex = str(ex)
+            if 'Stack space overflow' not in ex:
+                logger.error(str(ex))
+                sys.exit(1)
+
+            logger.warning('Error "Stack space overflow" from pandoc, '
+                           'try to use "+RTS -K512M -RTS"')
+            cmds = cmd.cmd.split(' ', 1)
+            cmds.insert(1, '+RTS -K512M -RTS')
+            cmd.cmd = ' '.join(cmds)
+            try:
+                logger.debug('Running: ' + cmd.cmd)
+                cmd.done = False
+                cmd.run()
+                logger.report('Report generated: %s', outfile)
+                logger.report('Time elapsed: %s', format_secs(time() - timer))
+            except CmdyReturnCodeError as ex2:
+                logger.error(str(ex2))
+                sys.exit(1)
+
+@hookimpl
+def pyppl_init(ppl):
+    """Add method to PyPPL instance"""
+    ppl.add_method(report, require='run')
+
+
+@hookimpl
+def cli_addcmd(commands):
+    """Add subcommand to pyppl"""
+    commands.report = 'Convert a Markdown file to report.'
+    params = commands.report
+    params['in'].required = True
+    params['in'].desc = 'The input file.'
+    params.i = params['in']
+    params.i.type = list
+    params.out.desc = 'The output file. Default: <in>.html'
+    params.out.callback = lambda opt, ps: (
+        opt.set_value(Path(ps.i.value[0]).with_suffix('.html'))
+        if not opt.value
+        else None
+    )
+    params.o = params.out
+    params.nonstand = False
+    params.nonstand.desc = ('Non-standalone mode. '
+                            'Save static files in `<filename of --out>.files` '
+                            'separately.')
+    params.n = params.nonstand
+    params.filter = []
+    params.filter.desc = 'The filters for pandoc'
+    params.toc = 3
+    params.toc.desc = 'The depth of heading levels to put in TOC. 0 to disable.'
+    params.title = 'Untitled document'
+    params.title.desc = ['The title of the document.',
+                         'If the first element of the document is H1 (#), '
+                         'this will be ignored and the text of H1 will be '
+                         'used as title.',
+                         'If the title is specified as "# Title", '
+                         'then a title will be added anyway.']
+    params.template = 'bootstrap'
+    params.template.desc = ('The template to use. '
+                            'Either standard template name or '
+                            'full path to template file.')
+
+
+@hookimpl
+def cli_execcmd(command, opts):
+    """Run the command"""
+    if command == 'report':
+        cmd = Report(opts.i, opts.o,
+                     opts.title).generate(not opts.nonstand, opts.template,
+                                          opts.filter, opts.toc)
+
+        try:
+            logger.info('Running: ' + ' | '.join(cmd.piped_strcmds))
+            cmd.run(wait=True)
+            logger.info('Report generated: ' + str(opts.o))
+        except CmdyReturnCodeError as ex:
+            logger.error(str(ex))
+            sys.exit(1)
---
+++ packages/pyppl-report/pyppl_report-0.6.2-py3-none-any.whl/pyppl_report/report.py
@@ -0,0 +1,184 @@
+"""Report generating system using pandoc"""
+import re
+from pathlib import Path
+from functools import partial
+from base64 import b64encode, b64decode
+from shutil import rmtree
+from cmdy import pandoc, wkhtmltopdf
+from .filters.utils import copy_to_media
+
+HERE = Path(__file__).resolve().parent
+DEFAULT_FILTERS = ['table', 'link', 'image']
+DEFAULT_TEMPLATE = 'layui'
+
+
+def _replace_all(regex, callback, string):
+    matches = re.finditer(regex, string)
+    ret = ''
+    start = 0
+    for match in matches:
+        ret += string[start:match.start(0)]
+        start = match.end(0)
+        ret += callback(match.group(), *match.groups())
+    return ret + string[start:]
+
+def _replace_ref_index(matched, citations):
+    """citations: index => ref"""
+    index = matched[2:-2]
+    if index in citations:
+        return '[#REF: %s #]' % citations[index]
+    return matched
+
+def _replace_ref(matched, citations):
+    """citations: ref => index"""
+    cite = matched[7:-3] # [#REF: ... #]
+    if cite not in citations:
+        citations[cite] = len(citations) + 1
+    return '[[{i}]](#PYPPL-REF-{i}){{.pyppl-report-refindex}}'.format(
+        i=citations[cite]
+    )
+
+
+class ProcReport:  # pylint: disable=too-few-public-methods
+    """Report for each process"""
+    def __init__(self, rptfile):
+        self.rptfile = Path(rptfile)
+        self.source = None
+
+    def prepare(self):
+        """Get the citations"""
+        lines = self.rptfile.read_text().splitlines()
+        # get the references first
+        # references should be placed at the bottom
+        source = []
+        citations = {}
+        for line in lines:
+            line = line.rstrip('\n')
+            # [1]: reference 1
+            # [2]: reference 2
+            matched = re.match(r'\[\[(\d+)\]\]: (.+)', line)
+            if not matched:
+                source.append(line)
+                continue
+            citations[matched.group(1)] = b64encode(
+                matched.group(2).strip().encode()
+            ).decode()
+
+        source = '\n'.join(source) if source else ''
+        self.source = _replace_all(r'\[\[\d+\]\]',
+                                   partial(_replace_ref_index,
+                                           citations=citations),
+                                   source)
+
+class Report:
+    """The Report class"""
+    def __init__(self, rptfiles, outfile, title='Untitled document'):
+        self.reports = [ProcReport(rptfile) for rptfile in rptfiles]
+        # // convert to relative path
+        try:
+            self.outfile = Path(outfile).resolve().relative_to(Path.cwd())
+        except ValueError:
+            self.outfile = Path(outfile)
+        self.mdfile = self.outfile.with_suffix('.tmp.md')
+        self.title = title
+
+    def cleanup(self):
+        """Cleanup and generate markdown for each process"""
+        citations = {}
+
+        with self.mdfile.open('w') as fmd:
+            for report in self.reports:
+                report.prepare()
+                # replace reference to citation indexes
+                source = _replace_all(r'\[#REF: .+? #\]',
+                                      partial(_replace_ref,
+                                              citations=citations),
+                                      report.source)
+
+                fmd.write(source + '\n\n')
+
+            if citations:
+                fmd.write('# Reference\n')
+                for cite, index in sorted(citations.items(),
+                                          key=lambda item: int(item[1])):
+                    fmd.write('[[{i}]: {cite}](){{.pyppl-report-reference '
+                              'name=PYPPL-REF-{i}}}'.format(
+                                  i=index, cite=b64decode(cite).decode()
+                              ))
+
+    def generate(self, standalone, template, filters, toc):
+        """Generate the reports"""
+        from pyppl import __version__ as pyppl_version
+        from . import __version__ as report_version
+
+        self.cleanup()
+        template = template or DEFAULT_TEMPLATE
+        if template and '/' not in template:
+            template = HERE / 'templates' / template / 'template.html'
+
+        ext = self.outfile.suffix.lower()
+        if 'htm' not in ext and 'pdf' not in ext:
+            raise ValueError('Only html and pdf format supported currently.')
+        if not standalone and 'pdf' in ext:
+            raise ValueError('pdf format has to be standalone.')
+
+        metadata = [
+            'pagetitle=%s' % self.title,
+            'pyppl_version=%s' % pyppl_version,
+            'report_version=%s' % report_version,
+            'standalone=%s' % int(standalone),
+            'indir=%s' % self.reports[0].rptfile.parent if self.reports else '',
+            'outdir=%s' % self.outfile.parent,
+            'pdf=%s' % bool('pdf' in ext)
+        ]
+        if toc > 0:
+            metadata.append('toc=%d' % toc)
+        mediadir = self.outfile.with_suffix('.files')
+        srcpath = ['.', str(self.outfile.parent), str(mediadir),
+                   str(Path(template).parent)]
+        # add input directory
+        srcpath += [str(self.reports[0].rptfile.parent)] if self.reports else []
+        if mediadir.is_dir():
+            rmtree(mediadir)
+        metadata.extend(['mediadir=%s' % mediadir, 'template=%s' % template])
+
+        if not standalone:
+            # copy files to dir relative to the output file
+            dest_staticdir = self.outfile.parent.joinpath('static')
+            src_staticdir = Path(template).parent.joinpath('static')
+            if not src_staticdir.is_dir():
+                raise ValueError(
+                    'Directory "static" not found in the template directory'
+                )
+            dest_staticdir.mkdir(parents=True, exist_ok=True)
+            for staticfile in src_staticdir.rglob('*'):
+                if staticfile.is_file():
+                    copy_to_media(staticfile,
+                                  dest_staticdir.joinpath(
+                                      staticfile.relative_to(src_staticdir)
+                                  ))
+
+        kwargs = {
+            'metadata': metadata,
+            'read': 'markdown',
+            'write': 'html5',
+            'template': template,
+            'filter': [HERE.joinpath('filters', filt + '.py')
+                       if '_' not in filt
+                       else filt
+                       for filt in DEFAULT_FILTERS + (filters or [])],
+            'self-contained': standalone,
+            'resource-path': ':'.join(srcpath),
+            '_raise': True,
+            '_sep': 'auto',
+            '_dupkey': True,
+        }
+        if 'pdf' not in ext:
+            kwargs['output'] = self.outfile
+            return pandoc(self.mdfile, **kwargs).h
+
+        return pandoc(self.mdfile, **kwargs).p | wkhtmltopdf(
+            "-",
+            _=self.outfile,
+            _raise=True
+        ).h

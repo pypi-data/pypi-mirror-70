2d7c7fefe1a448d28aba8543c037c1d5ce3731fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:02 +0000

    Add package distributions-by-chito/0.1/distributions_by_chito-0.1.tar.gz

---
+++ packages/distributions-by-chito/distributions_by_chito-0.1.tar.gz/distributions_by_chito-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_by_chito',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_by_chito'],
+      zip_safe=False)

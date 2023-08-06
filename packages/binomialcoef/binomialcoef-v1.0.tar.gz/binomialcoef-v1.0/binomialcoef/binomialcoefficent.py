5d84384342f24c144d874d8eb79cd60243b1eef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:12 +0000

    Add package binomialcoef/v1.0/binomialcoef-v1.0.tar.gz

---
+++ packages/binomialcoef/binomialcoef-v1.0.tar.gz/binomialcoef-v1.0/binomialcoef/binomialcoefficent.py
@@ -0,0 +1,14 @@
+import math
+
+def binomialcoef(a,b):
+	if b == 1 or a == b:
+	    return 1
+	if y > x:
+	    print("Error ... the 2nd number is higher than the 1st.")
+	    return 0   
+	else:
+	    fact_a = math.factorial(a)
+	    fact_b = math.factorial(b)
+	    div = fact_a // (fact_b*(a-b))
+	    return div
+	

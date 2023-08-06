7d5e3a50dde8285f67465669341aa9d99fed257a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:39 +0000

    Add package ai2thor/2.4.5/ai2thor-2.4.5-py2.py3-none-any.whl

---
+++ packages/ai2thor/ai2thor-2.4.5-py2.py3-none-any.whl/ai2thor/_builds.py
@@ -0,0 +1,11 @@
+# GENERATED FILE - DO NOT EDIT
+VERSION = '202006081330'
+BUILDS = {'Darwin': {'build_exception': '',
+            'log': 'thor-OSXIntel64-202006081330.log',
+            'sha256': '95b88ada67a952511f86738b68d5998aa77d31c7a5f703b4875e2b2627d48ac3',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-OSXIntel64-202006081330.zip'},
+ 'Docker': {'tag': '202006081330'},
+ 'Linux': {'build_exception': '',
+           'log': 'thor-Linux64-202006081330.log',
+           'sha256': 'b177fa819f34934caf4ecea25601e81b81b0d1e66c81455b4cf2c3e33905edba',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-Linux64-202006081330.zip'}}

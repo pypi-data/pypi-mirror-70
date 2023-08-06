80e4c523bcd50b580d432c41f6341088db40edfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:36 +0000

    Add package scimple/1.11.4/scimple-1.11.4.tar.gz

---
+++ packages/scimple/scimple-1.11.4.tar.gz/scimple-1.11.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='scimple',
+    version='1.11.4',
+    #py_modules=['scimple'],
+    install_requires=['matplotlib', 'numpy', 'pandas', 'pyspark', 'pyarrow', 'networkx'],
+    packages=['scimple/scimple_data', 'scimple'],
+    package_data={'scimple/scimple_data': ['*']},
+    url='http://github.com/EnzoBnl/Scimple',
+    license='',  # MIT
+    author='Enzo Bonnal',
+    author_email='enzobonnal@gmail.com',
+    description='Scimplify Ploting, Graph manipulation, Spark Streaming & Kafka and other tools'
+)

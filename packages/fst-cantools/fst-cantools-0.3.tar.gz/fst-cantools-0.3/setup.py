5b89df4eb96749c827f417ac5fa8fbb9e31efae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:12 +0000

    Add package fst-cantools/0.3/fst-cantools-0.3.tar.gz

---
+++ packages/fst-cantools/fst-cantools-0.3.tar.gz/fst-cantools-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fst-cantools",
+    description="CAN library for FST Lisboa",
+    version="0.3",
+    author="Joao Freitas",
+    author_email="joaj.freitas@gmail.com",
+    license="GPLv3",
+    url="https://github.com/joajfreitas/fcp",
+    scripts=["fst_cantools/fst_cantools.py"],
+    ##download_url="https://github.com/joajfreitas/fcp/archive/v0.1.tar.gz",
+    packages=find_packages(),
+    install_requires=[],
+)

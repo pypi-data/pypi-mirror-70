7e5488ed3b57f85a81a94ce34804e2dd22488332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:08 +0000

    Add package ehn/0.0.0/ehn-0.0.0.tar.gz

---
+++ packages/ehn/ehn-0.0.0.tar.gz/ehn-0.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='ehn',
+    version='0.0.0',
+    description='E-HowNet Utilities',
+    long_description='Use `ehownet <https://pypi.org/project/ehownet/>`__ instead.',
+    url='https://pypi.org/project/ehownet/',
+    install_requires=['ehownet'],
+)

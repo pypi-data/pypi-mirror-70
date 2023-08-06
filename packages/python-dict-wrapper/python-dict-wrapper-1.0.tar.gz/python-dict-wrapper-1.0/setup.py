88ab1456cedec1e1500f35e3a739cb87ebd2135e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:48 +0000

    Add package python-dict-wrapper/1.0/python-dict-wrapper-1.0.tar.gz

---
+++ packages/python-dict-wrapper/python-dict-wrapper-1.0.tar.gz/python-dict-wrapper-1.0/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'python-dict-wrapper',
+    py_modules = ['python_dict_wrapper'],
+    version = '1.0',
+    description = 'Wraps Python dictionary keys as attributes',
+    long_description=long_description,
+    author = 'Steve Brettschneider',
+    author_email = 'steve@bluehousefamily.com',
+    url = 'https://github.com/brettschneider/python_dict_wrapper',
+    keywords = ['dictionary', 'wrapper', 'attributes', 'enforce'],
+    classifiers = []
+)

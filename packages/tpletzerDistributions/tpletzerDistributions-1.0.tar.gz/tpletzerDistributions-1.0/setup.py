411469ba945ebecc4b5f266db6679dd751f62a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:32 +0000

    Add package tpletzerDistributions/1.0/tpletzerDistributions-1.0.tar.gz

---
+++ packages/tpletzerDistributions/tpletzerDistributions-1.0.tar.gz/tpletzerDistributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'tpletzerDistributions',
+      version = '1.0',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['tpletzerDistributions'],
+      author = 'Tamara Pletzer',
+      author_email = 'tamara.pletzer@gmail.com',
+      zip_safe=False)

100aef80e84906f8ea8031d1e4c543cc924210f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:52 +0000

    Add package ing-distributions/0.1/ing_distributions-0.1.tar.gz

---
+++ packages/ing-distributions/ing_distributions-0.1.tar.gz/ing_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ing_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ing_distributions'],
+      zip_safe=False)

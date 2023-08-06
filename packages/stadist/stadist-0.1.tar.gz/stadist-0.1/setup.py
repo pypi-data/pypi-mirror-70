24891473be7683d5f439ebd0a7cd16ab07827ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:15 +0000

    Add package stadist/0.1/stadist-0.1.tar.gz

---
+++ packages/stadist/stadist-0.1.tar.gz/stadist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stadist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stadist'],
+      author = 'Lucas Pontes',
+      author_email = 'lucas.pontesm@gmail.com',
+      zip_safe=False)

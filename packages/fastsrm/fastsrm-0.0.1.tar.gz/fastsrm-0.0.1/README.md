a894599646f840a9341658fd367938902749869a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:37 +0000

    Add package fastsrm/0.0.1/fastsrm-0.0.1.tar.gz

---
+++ packages/fastsrm/fastsrm-0.0.1.tar.gz/fastsrm-0.0.1/README.md
@@ -0,0 +1,13 @@
+Standalone implementation of FastSRM.
+
+See https://arxiv.org/pdf/1909.12537.pdf
+
+Install
+--------
+
+`pip install fastsrm`
+
+Docs
+-----
+
+Docs are available at https://hugorichard.github.io/FastSRM/generated/html/
---
+++ packages/fastsrm/fastsrm-0.0.1.tar.gz/fastsrm-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils import sysconfig
+from setuptools import setup, Extension, find_packages
+import os
+import sys
+import setuptools
+from copy import deepcopy
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fastsrm',
+    install_requires=[
+        'scikit-learn',
+        'joblib',
+        'picard'
+    ],
+    version = '0.0.1',
+    license='MIT',
+    author='Hugo RICHARD',
+    author_email='hugo.richard@inria.fr',
+    url='https://github.com/hugorichard/FastSRM',
+    long_description=long_description,
+    description='Fast Shared response model',
+    keywords='Component analysis, fMRI',
+    packages=find_packages(),
+    python_requires='>=3',
+)

6b508fa7d6b65d3b43da793a693a7c61e0d2c80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:22 +0000

    Add package mypy-boto3-connectparticipant/1.13.24.3/mypy_boto3_connectparticipant-1.13.24.3-py3-none-any.whl

---
+++ packages/mypy-boto3-connectparticipant/mypy_boto3_connectparticipant-1.13.24.3-py3-none-any.whl/mypy_boto3_connectparticipant/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for connectparticipant service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_connectparticipant import (
+        Client,
+        ConnectParticipantClient,
+    )
+
+    session = boto3.Session()
+
+    client: ConnectParticipantClient = boto3.client("connectparticipant")
+    session_client: ConnectParticipantClient = session.client("connectparticipant")
+    ```
+"""
+from mypy_boto3_connectparticipant.client import (
+    ConnectParticipantClient as Client,
+    ConnectParticipantClient,
+)
+
+
+__all__ = ("Client", "ConnectParticipantClient")

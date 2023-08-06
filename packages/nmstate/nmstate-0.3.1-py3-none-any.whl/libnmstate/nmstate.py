65aa99a689dc67bfdc60345bbd9a6ab8f681bced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:01 +0000

    Add package nmstate/0.3.1/nmstate-0.3.1-py3-none-any.whl

---
+++ packages/nmstate/nmstate-0.3.1-py3-none-any.whl/libnmstate/nmstate.py
@@ -0,0 +1,61 @@
+#
+# Copyright (c) 2020 Red Hat, Inc.
+#
+# This file is part of nmstate
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+from contextlib import contextmanager
+import logging
+
+from libnmstate import validator
+from libnmstate.nm import NetworkManagerPlugin
+from libnmstate.schema import DNS
+from libnmstate.schema import Interface
+from libnmstate.schema import Route
+from libnmstate.schema import RouteRule
+
+
+@contextmanager
+def plugin_context():
+    nm_plugin = NetworkManagerPlugin()
+    try:
+        yield nm_plugin
+    except (Exception, KeyboardInterrupt):
+        if nm_plugin.checkpoint:
+            try:
+                nm_plugin.rollback_checkpoint()
+            # Don't complex thing by raise exception when handling another
+            # exception, just log the rollback failure.
+            except Exception as e:
+                logging.error(f"Rollback failed with error {e}")
+        raise
+    finally:
+        nm_plugin.unload()
+
+
+def show_with_plugin(plugin, include_status_data=None):
+    plugin.refresh_content()
+    report = {}
+    if include_status_data:
+        report["capabilities"] = plugin.capabilities
+
+    report[Interface.KEY] = plugin.get_interfaces()
+    report[Route.KEY] = plugin.get_routes()
+    report[RouteRule.KEY] = plugin.get_route_rules()
+    report[DNS.KEY] = plugin.get_dns_client_config()
+
+    validator.schema_validate(report)
+    return report

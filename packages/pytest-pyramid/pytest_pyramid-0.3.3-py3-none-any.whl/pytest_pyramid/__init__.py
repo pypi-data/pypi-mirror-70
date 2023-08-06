1fdc0a4b8ce6e05d881c86efeda90cc8fa939396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:06 +0000

    Add package pytest-pyramid/0.3.3/pytest_pyramid-0.3.3-py3-none-any.whl

---
+++ packages/pytest-pyramid/pytest_pyramid-0.3.3-py3-none-any.whl/pytest_pyramid/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013 by pytest_pyramid authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of pytest_pyramid and is released under
+# the MIT License (MIT): http://opensource.org/licenses/MIT
+"""pytest_pyramid main module."""
+
+__version__ = '0.3.3'  # pragma: no cover

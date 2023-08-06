f40ea70628f5156fdd619fd62aad68854996b7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:38 +0000

    Add package etch-distributionsu/0.1/etch_distributionsu-0.1.tar.gz

---
+++ packages/etch-distributionsu/etch_distributionsu-0.1.tar.gz/etch_distributionsu-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='etch_distributionsu',
+      version='0.1',
+      description='Binomial & Gaussian distributions',
+      packages=['etch_distributionsu'],
+      author = 'Ahmed Hesham',
+      zip_safe=False)

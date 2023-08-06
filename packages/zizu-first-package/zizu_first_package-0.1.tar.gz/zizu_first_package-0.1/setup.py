c5a42f534be9a5483af49e249f1bc3315939f64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:27 +0000

    Add package zizu-first-package/0.1/zizu_first_package-0.1.tar.gz

---
+++ packages/zizu-first-package/zizu_first_package-0.1.tar.gz/zizu_first_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zizu_first_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['zizu_first_package'],
+      zip_safe=False)

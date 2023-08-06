b3604360448f3fda43c1c881bfe88b45df73226a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:58 +0000

    Add package StrawEngine/0.1.1/StrawEngine-0.1.1.tar.gz

---
+++ packages/StrawEngine/StrawEngine-0.1.1.tar.gz/StrawEngine-0.1.1/StrawEngine/straw.py
@@ -0,0 +1,35 @@
+from processing import process
+from preprocessing import preprocess
+from systemcommands import setup
+from sys import argv as args
+from os import path
+
+def main():
+    del args[0]
+    if not args:
+        print("You have to specify the file/s")
+        return
+    
+    setup()
+    
+    for arg in args:
+        try:
+            html = ""
+            with open(arg, "r") as file:
+                html = process(preprocess(file.read()))
+            
+            filename = arg.split(".")[0]
+            with open(f"{filename}.html", "w") as file:
+                file.write("<!DOCTYPE html>\n" + html)
+            
+            print(f"The file {arg} compiled.")
+                
+        except FileNotFoundError as e:
+            print(f"Hey! One of the files ({arg}) doesn't exist. Continuing with all of the other files...")
+        
+#         except ModuleNotFoundError as e:
+#             print("Hey! One of the modules in this file is not found. Sometimes it's happing due to an error and running again will solve the problem.")
+#             print("If running the compiling again didn't solve it, You may have missing files in your folder.")
+            
+if __name__ == "__main__":
+    main()
---
+++ packages/StrawEngine/StrawEngine-0.1.1.tar.gz/StrawEngine-0.1.1/StrawEngine/systemcommands/__init__.py
@@ -0,0 +1,23 @@
+from config import SYSTEM_COMMANDS
+from systemcommands.eval_ import eval_
+from systemcommands.import_ import import_
+from systemcommands.loop_ import loop_
+from systemcommands.safe_ import safe_
+from systemcommands.set_ import set_
+from systemcommands.compile_ import compile_
+
+def add_system_command(command, func):
+    SYSTEM_COMMANDS[command] = func
+
+def setup():
+    """
+    Adding all of the default system commands into the dictionary
+    """
+    add_system_command("^note", lambda x, y, z: None)
+    add_system_command("^set", set_)
+    add_system_command("@safe", safe_)
+    add_system_command("@eval", eval_)
+    add_system_command("@import", import_)
+    add_system_command("@loop", loop_)
+    add_system_command("@compile", compile_)
+    

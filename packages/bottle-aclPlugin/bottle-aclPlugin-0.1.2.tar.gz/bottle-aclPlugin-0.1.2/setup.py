ef9e547ca82d96ea00c4e49e5f4490a13594605d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:06 +0000

    Add package bottle-aclPlugin/0.1.2/bottle-aclPlugin-0.1.2.tar.gz

---
+++ packages/bottle-aclPlugin/bottle-aclPlugin-0.1.2.tar.gz/bottle-aclPlugin-0.1.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+description = "A plug-in for permission control. User permission is defined by himself"
+
+setup(
+    name='bottle-aclPlugin',
+    version="0.1.2",
+    packages=find_packages(),
+    description=description,
+    author="Xu_beixiong",
+    author_email='17610739793@163.com',
+    license='MIT',
+    url='https://github.com/xubeixiong/bottle-acl_plugin',
+    py_modules=['bottle-aclPlugin'],
+    requires=['bottle (>=0.9)'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Environment :: Plugins',
+        'Framework :: Bottle',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)

1249f4d5fe35e880cfd3c9e59134b4975a4a0bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package langtojson/0.1.1/langtojson-0.1.1-py2.py3-none-any.whl

---
+++ packages/langtojson/langtojson-0.1.1-py2.py3-none-any.whl/langtojson/cli.py
@@ -0,0 +1,37 @@
+"""Console script for langtojson."""
+import sys
+from pathlib import Path
+from typing import Tuple, TextIO, List
+
+import click
+
+from .langtojson import parse_lang_file, write_to_json
+
+
+def validate_input(ctx: click.core.Context, param: click.core.Argument, values: Tuple[TextIO]):
+    res = []
+    for file in values:
+        path = Path(file.name)
+        if path.suffix != ".lang":
+            raise click.BadParameter("file must have .lang extension")
+        res.append((file, path))
+
+    return res
+
+
+@click.command()
+@click.argument("files", type=click.File("r", encoding="utf-8"), nargs=-1, callback=validate_input)
+def main(files: List[Tuple[TextIO, Path]]):
+    """Console script for langtojson."""
+    for infile, path in files:
+        data = parse_lang_file(infile)
+
+        new_path = path.with_suffix(".json")
+        with new_path.open("w") as outfile:
+            write_to_json(data, outfile)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover

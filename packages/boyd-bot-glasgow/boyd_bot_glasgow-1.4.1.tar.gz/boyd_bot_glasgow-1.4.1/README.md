665c6eb1a87bd2d0b5259f0e7c6b6dc3d15cc6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:31 +0000

    Add package boyd-bot-glasgow/1.4.1/boyd_bot_glasgow-1.4.1.tar.gz

---
+++ packages/boyd-bot-glasgow/boyd_bot_glasgow-1.4.1.tar.gz/boyd_bot_glasgow-1.4.1/README.md
@@ -0,0 +1,9 @@
+# Boyd Bot (Terminal)
+This repository is for the terminal (CLI) version of the Boyd Bot. <br />
+This is how the bot started out, and is basically a stripped-down version of the [Flask version](https://github.com/ineshbose/boyd_bot_messenger) without needing [Facebook Messenger](https://www.facebook.com/messenger), [Dialogflow](https://dialogflow.com/) and [mongoDB](https://www.mongodb.com/).
+
+## Usage
+```
+$ pip install boyd_bot_glasgow
+$ boyd_bot -h
+```
---
+++ packages/boyd-bot-glasgow/boyd_bot_glasgow-1.4.1.tar.gz/boyd_bot_glasgow-1.4.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='boyd_bot_glasgow',
+    version="1.4.1",
+    packages=find_packages(),
+    install_requires=requirements,
+
+    author='Inesh Bose',
+    desription='University Timetable on your Terminal!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/ineshbose/boyd_bot_terminal',
+    keywords='glasgow university timetable',
+
+    entry_points={
+        "console_scripts": [
+            "boyd_bot = boyd_bot.timetable:main",
+        ],
+    }
+)

cd68216c8b2cdf74256d8844795dfc47a4eccd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:27 +0000

    Add package stanfordkarel/0.1.2/stanfordkarel-0.1.2.tar.gz

---
+++ packages/stanfordkarel/stanfordkarel-0.1.2.tar.gz/stanfordkarel-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="stanfordkarel",
+    version="0.1.2",
+    author="Nicholas Bowman, Kylie Jue, Tyler Yep",
+    author_email="tyep10@gmail.com",
+    description="Official Stanford Karel library used in CS 106A",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tyleryep/stanford-karel",
+    packages=["stanfordkarel"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

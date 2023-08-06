2c73ad41fed686526329733b1096da54c726dca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:48 +0000

    Add package automark/1.0.1/automark-1.0.1-py3-none-any.whl

---
+++ packages/automark/automark-1.0.1-py3-none-any.whl/marker/lms/__init__.py
@@ -0,0 +1,10 @@
+from .canvas import Canvas
+from .markus import Markus
+
+def LMS_Factory(config):
+    if config['lms'].lower() == 'canvas':
+        assert('course' in config), "Must have course ID in Config for Canvas"
+        assert('file_name' in config), "Must have name for downloaded file"
+        return Canvas(config)
+    elif config['lms'].lower() == 'markus':
+        return Markus(config)

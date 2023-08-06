23a2521f9b08ec8e701885b302b2c5a931c7e626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:53 +0000

    Add package raylab/0.7.3/raylab-0.7.3.tar.gz

---
+++ packages/raylab/raylab-0.7.3.tar.gz/raylab-0.7.3/raylab/utils/debug.py
@@ -0,0 +1,26 @@
+"""Debugging utilities."""
+import gym.spaces as spaces
+import numpy as np
+from ray.rllib import SampleBatch
+
+
+def fake_space_samples(space, batch_size):
+    """Create fake samples from a Gym space."""
+    if isinstance(space, spaces.Box):
+        shape = (batch_size,) + space.shape
+        return np.clip(np.random.randn(*shape), space.low, space.high)
+    if isinstance(space, spaces.Discrete):
+        return np.random.randint(space.n, size=batch_size)
+    raise ValueError(f"Unsupported space type {type(space)}")
+
+
+def fake_batch(obs_space, action_space, batch_size=1):
+    """Create a fake SampleBatch compatible with Policy.learn_on_batch."""
+    samples = {
+        SampleBatch.CUR_OBS: fake_space_samples(obs_space, batch_size),
+        SampleBatch.ACTIONS: fake_space_samples(action_space, batch_size),
+        SampleBatch.REWARDS: np.random.randn(batch_size),
+        SampleBatch.NEXT_OBS: fake_space_samples(obs_space, batch_size),
+        SampleBatch.DONES: np.random.randn(batch_size) > 0,
+    }
+    return SampleBatch(samples)

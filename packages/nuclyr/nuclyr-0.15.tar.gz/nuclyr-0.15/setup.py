0637ba0abe73575d5ce48334bb594f50f389cb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:09 +0000

    Add package nuclyr/0.15/nuclyr-0.15.tar.gz

---
+++ packages/nuclyr/nuclyr-0.15.tar.gz/nuclyr-0.15/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='nuclyr',
+      version='0.15',
+      author='Philipp Scholz',
+      author_email='pscholz@outlook.com',
+      license='MIT',
+      packages=['nuclyr'],
+      data_files=[],
+      url="https://github.com/phScholz/nuclyr",
+      description="A utility package for nuclear data.",
+      long_description="A utility package for nuclear physics.\
+        Features include the automatic data mining from the EXFOR database",
+      zip_safe=False,
+      install_requires=['selenium','pandas'])

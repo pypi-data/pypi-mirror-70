a3b71f9cb1ee36b7c91ff482f41236e045e5b99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:27 +0000

    Add package gnb-distributions-gr/1.3/gnb_distributions_gr-1.3.tar.gz

---
+++ packages/gnb-distributions-gr/gnb_distributions_gr-1.3.tar.gz/gnb_distributions_gr-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gnb_distributions_gr',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['gnb_distributions_gr'],
+      author='Mustafa Khamisi',
+      author_email='mustafangure@gmail.com',
+      zip_safe=False)

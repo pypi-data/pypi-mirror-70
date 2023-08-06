33cc9f92b489b645068549d5468b8c80b5fe824c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:29 +0000

    Add package emagpy/1.1.0/emagpy-1.1.0-py3-none-any.whl

---
+++ packages/emagpy/emagpy-1.1.0-py3-none-any.whl/emagpy/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  5 22:03:32 2019
+
+@author: jkl
+"""
+EMagPy_version = '1.1.0'
+name = 'emagpy'
+from emagpy.Problem import Problem
+from emagpy.Survey import Survey

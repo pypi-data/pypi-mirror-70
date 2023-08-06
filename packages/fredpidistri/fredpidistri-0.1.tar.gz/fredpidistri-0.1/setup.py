8e87d921f29f4ccd6826d6b3cddb6c5e1ebc814e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:53 +0000

    Add package fredpidistri/0.1/fredpidistri-0.1.tar.gz

---
+++ packages/fredpidistri/fredpidistri-0.1.tar.gz/fredpidistri-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='fredpidistri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['fredpidistri'],
+      zip_safe=False)

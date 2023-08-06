64ae3ad2c57b4911d99df033a2644c12a0c4e57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:04 +0000

    Add package distributionsGAC/0.1/distributionsGAC-0.1.tar.gz

---
+++ packages/distributionsGAC/distributionsGAC-0.1.tar.gz/distributionsGAC-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsGAC',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionsGAC'],
+      author = 'Gema Atienza Chercoles',
+      zip_safe=False)

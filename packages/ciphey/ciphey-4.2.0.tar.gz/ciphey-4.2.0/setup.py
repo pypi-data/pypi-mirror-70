fe0bc3a03e5c8302b74c79eda25416df6e89e1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:46 +0000

    Add package ciphey/4.2.0/ciphey-4.2.0.tar.gz

---
+++ packages/ciphey/ciphey-4.2.0.tar.gz/ciphey-4.2.0/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ciphey',
+ 'ciphey.Decryptor',
+ 'ciphey.Decryptor.Encoding',
+ 'ciphey.Decryptor.Hash',
+ 'ciphey.Decryptor.basicEncryption',
+ 'ciphey.LanguageChecker',
+ 'ciphey.neuralNetworkMod']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cipheycore>=0.1.4,<0.2.0',
+ 'cipheydists>=0.2.2,<0.3.0',
+ 'flake8>=3.8.2,<4.0.0',
+ 'loguru>=0.5.0,<0.6.0',
+ 'rich>=1.2.3,<2.0.0',
+ 'tensorflow>=2.1.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['ciphey = ciphey.__main__:main']}
+
+setup_kwargs = {
+    'name': 'ciphey',
+    'version': '4.2.0',
+    'description': 'Automated Decryption Tool',
+    'long_description': None,
+    'author': 'Brandon',
+    'author_email': 'brandon@skerritt.blog',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

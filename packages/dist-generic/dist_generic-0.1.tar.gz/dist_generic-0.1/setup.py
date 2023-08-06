42f25a353807a78e1b76bbb008b5260033ac4587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    Add package dist-generic/0.1/dist_generic-0.1.tar.gz

---
+++ packages/dist-generic/dist_generic-0.1.tar.gz/dist_generic-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_generic',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_generic'],
+      zip_safe=False)

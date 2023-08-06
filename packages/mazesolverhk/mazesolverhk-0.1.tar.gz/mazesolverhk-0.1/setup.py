192b41d0355807fa837730362a2dda3737d1071f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:27 +0000

    Add package mazesolverhk/0.1/mazesolverhk-0.1.tar.gz

---
+++ packages/mazesolverhk/mazesolverhk-0.1.tar.gz/mazesolverhk-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mazesolverhk',
+version='0.1',
+description='This is maze-solver code with the shortest path',
+author='Himanshu Kashyap',
+packages=['mazesolverhk'],
+install_requires=[])

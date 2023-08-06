ba2594492092fd79af5eadbfe22fe7ba5318d057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:43 +0000

    Add package gaussian-and-binomial-pdfs/0.9/gaussian_and_binomial_pdfs-0.9.tar.gz

---
+++ packages/gaussian-and-binomial-pdfs/gaussian_and_binomial_pdfs-0.9.tar.gz/gaussian_and_binomial_pdfs-0.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_and_binomial_pdfs',
+      version='0.9',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_and_binomial_pdfs'],
+      author = "Daniel Zito",
+      author_email = "iphonebean@icloud.com",
+      zip_safe=False)

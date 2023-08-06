9c1ae5d662ac7e2fba49ba7b85a05f995a94dc7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:24 +0000

    Add package segysak/0.2.1/segysak-0.2.1.tar.gz

---
+++ packages/segysak/segysak-0.2.1.tar.gz/segysak-0.2.1/tests/test_cli.py
@@ -0,0 +1,33 @@
+import sys
+import pytest
+
+from segysak._cli import cli, NAME
+
+from test_fixtures import temp_dir, temp_segy, TEMP_TEST_DATA_DIR
+
+
+@pytest.mark.parametrize("help_arg", ["-h", "--help"])
+def test_help(help_arg):
+    sys.argv = ["", help_arg]
+    with pytest.raises(SystemExit):
+        cli()
+
+
+def test_version():
+    sys.argv = ["", "-v"]
+    with pytest.raises(SystemExit):
+        cli()
+
+
+def test_no_input_file():
+    sys.argv = [""]
+    with pytest.raises(SystemExit):
+        cli()
+
+
+@pytest.mark.parametrize("ebc", ["ebcidc"])
+def test_dump_ebcidc(temp_dir, temp_segy, ebc):
+    # test_file = temp_dir / "ebcidc_dump.txt"
+    sys.argv = ["", ebc, str(temp_segy)]
+    with pytest.raises(SystemExit):
+        cli()

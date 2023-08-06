75f51864c7f9826a2c61dbc730cee54680bea351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:54 +0000

    Add package sci-distributions/0.1/sci-distributions-0.1.tar.gz

---
+++ packages/sci-distributions/sci-distributions-0.1.tar.gz/sci-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sci-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sci-distributions'],
+      author='Arohan Ajit',
+      zip_safe=False)

1fdebc2cd6a760b4f420d072a9d2cde9de1bd44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:46 +0000

    Add package mypy-boto3-sms-voice/1.13.25.0/mypy-boto3-sms-voice-1.13.25.0.tar.gz

---
+++ packages/mypy-boto3-sms-voice/mypy-boto3-sms-voice-1.13.25.0.tar.gz/mypy-boto3-sms-voice-1.13.25.0/mypy_boto3_sms_voice/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for sms-voice service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_sms_voice import (
+        Client,
+        SMSVoiceClient,
+    )
+
+    session = boto3.Session()
+
+    client: SMSVoiceClient = boto3.client("sms-voice")
+    session_client: SMSVoiceClient = session.client("sms-voice")
+    ```
+"""
+from mypy_boto3_sms_voice.client import SMSVoiceClient as Client, SMSVoiceClient
+
+
+__all__ = ("Client", "SMSVoiceClient")

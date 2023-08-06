1d231aa4083776bb274b417e9c8769541e08627d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package libxduauth/1.2.0/libxduauth-1.2.0.tar.gz

---
+++ packages/libxduauth/libxduauth-1.2.0.tar.gz/libxduauth-1.2.0/libxduauth/__init__.py
@@ -0,0 +1,6 @@
+from .sites.ehall import EhallSession
+from .sites.energy import EnergySession
+from .sites.ids import IDSSession
+from .sites.rsbbs import RSBBSSession
+from .sites.wx import WXSession
+from .sites.zfw import ZFWSession

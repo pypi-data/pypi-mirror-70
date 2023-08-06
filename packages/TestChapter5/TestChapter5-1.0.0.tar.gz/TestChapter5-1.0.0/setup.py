833dd29b4b5c954ca45c09f5a816e8c9a35e0f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package TestChapter5/1.0.0/TestChapter5-1.0.0.tar.gz

---
+++ packages/TestChapter5/TestChapter5-1.0.0.tar.gz/TestChapter5-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="TestChapter5",
+      version="1.0.0",
+      py_modules="/Users/liubibm/Work/Development/Python/HeadFirst/nester/hfpy_ch5_data/TestChapter5",
+      author='Bill',
+      author_email='bill@gmail.com',
+      url='http://www.python.org',
+      description='A simple printer of nested lists',
+      )

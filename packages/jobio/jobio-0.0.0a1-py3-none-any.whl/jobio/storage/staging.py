af288bff2e46fccf5705367ed30dfa4085b53cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:25 +0000

    Add package jobio/0.0.0a1/jobio-0.0.0a1-py3-none-any.whl

---
+++ packages/jobio/jobio-0.0.0a1-py3-none-any.whl/jobio/storage/staging.py
@@ -0,0 +1,15 @@
+required_staging_fields = {
+    "enable": bool,
+    "endpoint_url": str,
+    "session_vars": str,
+    "input_path": str,
+    "output_path": str,
+}
+
+required_staging_values = {
+    "enable": True,
+    "endpoint_url": True,
+    "session_vars": False,
+    "input_path": False,
+    "output_path": True,
+}

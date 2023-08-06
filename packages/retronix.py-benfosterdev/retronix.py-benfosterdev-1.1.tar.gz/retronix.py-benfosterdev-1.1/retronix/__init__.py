b192bc8475c46fc593d4380e005d99231ff1119b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:16 +0000

    Add package retronix.py-benfosterdev/1.1/retronix.py-benfosterdev-1.1.tar.gz

---
+++ packages/retronix.py-benfosterdev/retronix.py-benfosterdev-1.1.tar.gz/retronix.py-benfosterdev-1.1/retronix/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Retronix Public API Python Library
+# Developed by Ben Foster - All Rights Reserved.
+"""__init__.py > Library Entrypoint"""
+
+# Copyright and Metadata Smeg
+__author__ = "Ben Foster"
+__copyright__ = "Copyright 2020, Retronix"
+__license__ = "MIT"
+__version__ = "1.1"
+__maintainer__ = "Ben Foster"
+__email__ = "ben@retronixmc.org"
+__status__ = "Development"
+
+# Imports would go here
+#import asyncio
+
+print("Hello, World!")

4ed0eb517323928ba4786cc14e7c6c2f631af2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:19 +0000

    Add package hksolvemaze/0.1/hksolvemaze-0.1.tar.gz

---
+++ packages/hksolvemaze/hksolvemaze-0.1.tar.gz/hksolvemaze-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hksolvemaze',
+version='0.1',
+description='This is maze-solver code with the shortest path',
+author='Himanshu Kashyap',
+packages=['hksolvemaze'],
+install_requires=[])

b84775b00c921731f5a91910654759c313364cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:32 +0000

    Add package bgdist/1.0/bgdist-1.0.tar.gz

---
+++ packages/bgdist/bgdist-1.0.tar.gz/bgdist-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bgdist',
+      version='1.0',
+      description='A package for Gaussian & Binomial distributions',
+      packages=['bgdist'],
+      author='Tapajyoti Deb',
+      author_email='tapajyotideb@gmail.com',
+     zip_safe=False)

84174a469dc66018bdf3908980e563ac1e4f10ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package wfslib/0.1.0/wfslib-0.1.0.tar.gz

---
+++ packages/wfslib/wfslib-0.1.0.tar.gz/wfslib-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wfslib']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['wfslib = entry:main']}
+
+setup_kwargs = {
+    'name': 'wfslib',
+    'version': '0.1.0',
+    'description': 'Programming module for wave front data processing',
+    'long_description': None,
+    'author': 'Zoya',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '==3.6.9',
+}
+
+
+setup(**setup_kwargs)

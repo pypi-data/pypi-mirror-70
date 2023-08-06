534d705b657571bc76f4eed830b825ffac01e6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:40 +0000

    Add package controlla/0.3/controlla-0.3.tar.gz

---
+++ packages/controlla/controlla-0.3.tar.gz/controlla-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='controlla',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = "Oguchi Ebuka Philip",
+      author_email = "oguchiebuka919@gmail.com",
+      zip_safe=False)

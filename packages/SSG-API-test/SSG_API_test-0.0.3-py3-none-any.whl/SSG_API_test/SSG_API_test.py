5fafc5c145fa0d85cf9eec846a0877e4f8dcb3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:06 +0000

    Add package SSG-API-test/0.0.3/SSG_API_test-0.0.3-py3-none-any.whl

---
+++ packages/SSG-API-test/SSG_API_test-0.0.3-py3-none-any.whl/SSG_API_test/SSG_API_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+def get():
+    def token(client_id, client_secret, URL):
+        import requests
+        import json
+        from requests.auth import HTTPBasicAuth
+        data = "grant_type=client_credentials"
+        headers = {"content-type": "application/x-www-form-urlencoded"}
+        r = requests.post("https://public-api.ssg-wsg.sg/dp-oauth/oauth/token", data=data, auth=(client_id,client_secret), headers=headers)
+        out = r.json()
+        #print(out)
+        access_token = out['access_token']
+        
+        
+        import pprint
+        
+        auth_code = "Bearer " + access_token
+        payload = {}
+        headers = {'Authorization': auth_code}
+
+        response = requests.request("GET", URL, headers=headers, data = payload)
+
+        response_json  = response.json()
+        print("Your access token is: " + access_token)
+        print(" ")
+        print(" ")
+        print("API Output:")
+        return pprint.pprint(response_json)
+        
+    token(input("Please enter your Client ID: "),input("Please enter your Secret: "), input("Please enter your Request URL: "))
+

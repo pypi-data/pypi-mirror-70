5aee2a90fbe68647c2c6d451ce771dbed209461c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:34 +0000

    Add package bg-kdistro/0.1/bg_kdistro-0.1.tar.gz

---
+++ packages/bg-kdistro/bg_kdistro-0.1.tar.gz/bg_kdistro-0.1/README.md
@@ -0,0 +1,10 @@
+
+#### bg_kdistro
+this is a python package that enables you make Guassian and Binomial distribution calculations
+
+#### classes and methods
+- Guassian
+- Binomial
+
+#### Date
+11-06-2020
---
+++ packages/bg-kdistro/bg_kdistro-0.1.tar.gz/bg_kdistro-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bg_kdistro',
+      version='0.1',
+      description='Gaussian and Binomial distributions package',
+      packages=['bg_kdistro'],
+      author='Azorji Kelechi Oliver',
+      author_email='kelechioliver96@gmail.com',
+      zip_safe=False)

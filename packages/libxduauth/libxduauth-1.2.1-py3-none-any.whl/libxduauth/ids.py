97e63c6458ce33637921ef6b33ac3fbb8a96a3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package libxduauth/1.2.1/libxduauth-1.2.1-py3-none-any.whl

---
+++ packages/libxduauth/libxduauth-1.2.1-py3-none-any.whl/libxduauth/ids.py
@@ -0,0 +1,26 @@
+from requests import Session
+
+from ._utils import parse_form_hidden_inputs
+
+
+class IDSSession(Session):
+
+    def __init__(
+            self, target, username, password,
+            *args, **kwargs
+    ):
+        super(IDSSession, self).__init__()
+        self.headers.update({'User-Agent': 'Mobile'})
+        page = self.get(
+            'http://ids.xidian.edu.cn/authserver/login',
+            params={'service': target}
+        ).text
+        params = parse_form_hidden_inputs(page)
+        self.post(
+            'http://ids.xidian.edu.cn/authserver/login',
+            params={'service': target},
+            data=dict(params, **{
+                'username': username,
+                'password': password
+            })
+        )

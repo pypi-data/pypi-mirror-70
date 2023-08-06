780c26bb5c18df6c0be1f1cd8c65992da8c1a690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:06 +0000

    Add package pyGauBin/0.1/pyGauBin-0.1.tar.gz

---
+++ packages/pyGauBin/pyGauBin-0.1.tar.gz/pyGauBin-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyGauBin',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pyGauBin'],
+      author= 'Ayse Bat',
+      zip_safe=False)

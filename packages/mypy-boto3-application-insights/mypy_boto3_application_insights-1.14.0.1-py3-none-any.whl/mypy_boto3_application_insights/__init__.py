23c9e8fa193541beadbe7e8e444037f644a1b7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package mypy-boto3-application-insights/1.14.0.1/mypy_boto3_application_insights-1.14.0.1-py3-none-any.whl

---
+++ packages/mypy-boto3-application-insights/mypy_boto3_application_insights-1.14.0.1-py3-none-any.whl/mypy_boto3_application_insights/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for application-insights service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_application_insights import (
+        ApplicationInsightsClient,
+        Client,
+    )
+
+    session = boto3.Session()
+
+    client: ApplicationInsightsClient = boto3.client("application-insights")
+    session_client: ApplicationInsightsClient = session.client("application-insights")
+    ```
+"""
+from mypy_boto3_application_insights.client import (
+    ApplicationInsightsClient,
+    ApplicationInsightsClient as Client,
+)
+
+
+__all__ = ("ApplicationInsightsClient", "Client")

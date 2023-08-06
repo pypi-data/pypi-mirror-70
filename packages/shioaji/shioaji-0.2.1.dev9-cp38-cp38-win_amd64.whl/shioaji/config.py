bc80ec13ad8f9d44089e00da431afcb2475a9691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:22 +0000

    Add package shioaji/0.2.1.dev9/shioaji-0.2.1.dev9-cp38-cp38-win_amd64.whl

---
+++ packages/shioaji/shioaji-0.2.1.dev9-cp38-cp38-win_amd64.whl/shioaji/config.py
@@ -0,0 +1,12 @@
+import os
+
+SJCLIENT_SOL_HOST = os.environ.get("SJCLIENT_SOL_HOST", "203.66.91.161:80")
+SJCLIENT_SOL_VPN = os.environ.get("SJCLIENT_SOL_VPN", "sinopac")
+SJCLIENT_SOL_USER = os.environ.get("SJCLIENT_SOL_USER", "shioaji")
+SJCLIENT_SOL_PASSWORD = os.environ.get("SJCLIENT_SOL_PASSWORD", "sahjiio111")
+
+
+SJCLIENT_SOL_HOST_SIM = os.environ.get("SJCLIENT_SOL_HOST_SIM", "218.32.76.102:80")
+SJCLIENT_SOL_VPN_SIM = os.environ.get("SJCLIENT_SOL_VPN_SIM", "sinopac")
+SJCLIENT_SOL_USER_SIM = os.environ.get("SJCLIENT_SOL_USER_SIM", "shioaji")
+SJCLIENT_SOL_PASSWORD_SIM = os.environ.get("SJCLIENT_SOL_PASSWORD_SIM", "shioaji111")

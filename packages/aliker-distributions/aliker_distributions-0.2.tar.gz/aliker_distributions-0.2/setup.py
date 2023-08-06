40d2315a3f13af771fbf594cfcd84d72ceb8d00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package aliker-distributions/0.2/aliker_distributions-0.2.tar.gz

---
+++ packages/aliker-distributions/aliker_distributions-0.2.tar.gz/aliker_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aliker_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['aliker_distributions'],
+      zip_safe=False)

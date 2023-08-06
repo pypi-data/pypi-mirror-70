b5b1add939c6d176a6fec9c57159bea8a4c941be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:37 +0000

    Add package creevey/2.1.0/creevey-2.1.0-py3-none-any.whl

---
+++ packages/creevey/creevey-2.1.0-py3-none-any.whl/tests/fixtures/image.py
@@ -0,0 +1,76 @@
+import pytest
+
+from creevey.load_funcs.image import load_image_from_disk
+from tests.conftest import SAMPLE_DATA_DIR
+
+
+@pytest.fixture
+def sample_image_square_rgb():
+    filename = 'creevey_rgb.jpg'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_tall_grayscale():
+    filename = 'creevey_gray.jpg'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_square_rgba():
+    filename = 'creevey_rgba.png'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_square_rgba_flipped_horiz():
+    filename = 'creevey_rgba_flipped_horiz.png'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_square_rgba_flipped_vert():
+    filename = 'creevey_rgba_flipped_vert.png'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_square_rgb_flipped_horiz():
+    filename = 'creevey_rgb_flipped_horiz.jpg'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_square_rgb_flipped_vert():
+    filename = 'creevey_rgb_flipped_vert.jpg'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_grayscale_flipped_horiz():
+    filename = 'creevey_gray_flipped_horiz.jpg'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image
+
+
+@pytest.fixture
+def sample_image_grayscale_flipped_vert():
+    filename = 'creevey_gray_flipped_vert.jpg'
+    path = SAMPLE_DATA_DIR / filename
+    image = load_image_from_disk(path)
+    return image

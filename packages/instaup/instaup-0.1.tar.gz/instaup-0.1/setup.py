5dd8627a887204f2653cfce5c650ca2a92f259bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:17 +0000

    Add package instaup/0.1/instaup-0.1.tar.gz

---
+++ packages/instaup/instaup-0.1.tar.gz/instaup-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='instaup',
+    version='0.1',
+    description='An auto downloader and uploader for Instagram profiles.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/Coloradohusky/InstaUp',
+    author='Coloradohusky',
+    py_modules=['instaup'],
+    entry_points={
+        'console_scripts': [
+            'instaup = instaup:main',
+        ],
+    },
+    python_requires='>=3.5, <4',
+    install_requires=['internetarchive>=1.9.3', 'instaloader>=4.4.3']
+)

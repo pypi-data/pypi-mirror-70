d593c2e807f72dc8349286dfcd65a636b54f9b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package mypy-boto3-pricing/1.14.0.1/mypy_boto3_pricing-1.14.0.1-py3-none-any.whl

---
+++ packages/mypy-boto3-pricing/mypy_boto3_pricing-1.14.0.1-py3-none-any.whl/mypy_boto3_pricing/__init__.py
@@ -0,0 +1,40 @@
+"""
+Main interface for pricing service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_pricing import (
+        Client,
+        DescribeServicesPaginator,
+        GetAttributeValuesPaginator,
+        GetProductsPaginator,
+        PricingClient,
+    )
+
+    session = boto3.Session()
+
+    client: PricingClient = boto3.client("pricing")
+    session_client: PricingClient = session.client("pricing")
+
+    describe_services_paginator: DescribeServicesPaginator = client.get_paginator("describe_services")
+    get_attribute_values_paginator: GetAttributeValuesPaginator = client.get_paginator("get_attribute_values")
+    get_products_paginator: GetProductsPaginator = client.get_paginator("get_products")
+    ```
+"""
+from mypy_boto3_pricing.client import PricingClient, PricingClient as Client
+from mypy_boto3_pricing.paginator import (
+    DescribeServicesPaginator,
+    GetAttributeValuesPaginator,
+    GetProductsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeServicesPaginator",
+    "GetAttributeValuesPaginator",
+    "GetProductsPaginator",
+    "PricingClient",
+)

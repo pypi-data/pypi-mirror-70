d7897d71342152eea5765f007e55da3781d2b046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:28 +0000

    Add package scratch-probability/0.1/scratch_probability-0.1.tar.gz

---
+++ packages/scratch-probability/scratch_probability-0.1.tar.gz/scratch_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='scratch_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['scratch_probability'],
+      author = 'Sameh Awaida',
+      author_email = 'myemail@something.com',
+      zip_safe=False)

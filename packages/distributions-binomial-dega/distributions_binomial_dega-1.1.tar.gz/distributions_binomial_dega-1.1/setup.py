fdd790272ad062e73db5ea6c793c96f209a16905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package distributions-binomial-dega/1.1/distributions_binomial_dega-1.1.tar.gz

---
+++ packages/distributions-binomial-dega/distributions_binomial_dega-1.1.tar.gz/distributions_binomial_dega-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_binomial_dega',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_binomial_dega'],
+      author='Sverige Gewoon',
+      author_email='sverige.gewoon@pass.com',
+      zip_safe=False)

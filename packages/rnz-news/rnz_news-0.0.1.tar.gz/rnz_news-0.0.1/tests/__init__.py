8850c47c163d05c6094e4df600d61513aa8b9d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:53 +0000

    Add package rnz-news/0.0.1/rnz_news-0.0.1.tar.gz

---
+++ packages/rnz-news/rnz_news-0.0.1.tar.gz/rnz_news-0.0.1/tests/__init__.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+from rnz_news import RnzNews
+
+
+class RnzNewsTest(TestCase):
+    def test_all_categories(self):
+
+        rnz_news = RnzNews()
+
+        for category in rnz_news.categories():
+
+            print('path: ', category.path)
+            print('address: ', category.address)
+            print('description: ', category.description)
+
+            print('\n')
+
+    def test_all_articles(self):
+
+        sport_category = RnzNews()['news/sport']
+
+        for articles, page in sport_category:
+
+            print('page: ', page)
+
+            for article in articles:
+
+                print('category: ', article.category)
+                print('path: ', article.path)
+                print('address: ', article.address)
+                print('title: ', article.title)
+                print('summary: ', article.summary)
+                print('content: ', article.content)
+
+                print('\n')
+
+                break
+            break

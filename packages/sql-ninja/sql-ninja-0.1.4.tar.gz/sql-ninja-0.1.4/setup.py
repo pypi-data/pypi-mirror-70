4eb9dd637f9859af33f187be98200d7fd5be1d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:17 +0000

    Add package sql-ninja/0.1.4/sql-ninja-0.1.4.tar.gz

---
+++ packages/sql-ninja/sql-ninja-0.1.4.tar.gz/sql-ninja-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='sql-ninja',
+    version='0.1.4',
+    author='Scott Pierce',
+    author_email='ddrscott@gmail.com',
+    description='SQL + Jinja Templates Done Right™',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ddrscott/sql-ninja',
+    packages=['sqlninja'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'click',
+        'jinja2',
+    ],
+    entry_points={
+        'console_scripts':[
+            "sql = sqlninja.main:cli",
+        ],
+    }
+)

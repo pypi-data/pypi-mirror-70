76b1141bd4f70e72295c14d5bb4f7235e6ec195c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:30 +0000

    Add package improved-replace/1.2.4/improved_replace-1.2.4-py3-none-any.whl

---
+++ packages/improved-replace/improved_replace-1.2.4-py3-none-any.whl/improved_replace/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Add replace method improvements.
+
+Library to add some replace method improvementsfocused on
+common replacements to optimize code.
+"""
+
+from .core import ImprovedReplace
+
+__tittle__ = "improved replace"
+__url__ = "https://github.com/LuckJMG/ImprovedReplace"
+__author__ = "LuckJMG"
+__email__ = "lucas.mosquera13@gmail.com"
+__license__ = "MIT license"
+__copyright__ = "Copyright 2020, LuckJMG"
+__version__ = "1.2.4"
+__status__ = "Production"
+__all__ = ["ImprovedReplace"]

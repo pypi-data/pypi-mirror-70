0bac25d75986658c21bd21ab1cdfd9cf65f7333f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:37 +0000

    Add package metadata-client/3.0.6/metadata_client-3.0.6-py3-none-any.whl

---
+++ packages/metadata-client/metadata_client-3.0.6-py3-none-any.whl/metadata_client/modules/user.py
@@ -0,0 +1,38 @@
+"""User module class"""
+
+from ..common.base import Base
+from ..common.config import *
+
+MODULE_NAME = USER
+
+
+class User:
+    def __init__(self, metadata_client,
+                 name, email, uid, first_name, last_name):
+        self.metadata_client = metadata_client
+        self.id = None
+        self.name = name
+        self.email = email
+        self.uid = uid
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @staticmethod
+    def get_by_id(mdc_client, user_id):
+        response = mdc_client.get_user_by_id_api(user_id)
+
+        Base.cal_debug(MODULE_NAME, 'get_by_id', response)
+        return Base.format_response(response, GET, OK, MODULE_NAME)
+
+    def __get_resource(self):
+        user = {
+            MODULE_NAME: {
+                'name': self.name,
+                'email': self.email,
+                'uid': self.uid,
+                'first_name': self.first_name,
+                'last_name': self.last_name
+            }
+        }
+
+        return user

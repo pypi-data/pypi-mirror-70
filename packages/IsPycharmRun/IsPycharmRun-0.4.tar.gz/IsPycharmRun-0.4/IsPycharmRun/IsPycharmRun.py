2d1b3e3ff23eea7bdd5628fffe6b77f9c8c53b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:17 +0000

    Add package IsPycharmRun/0.4/IsPycharmRun-0.4.tar.gz

---
+++ packages/IsPycharmRun/IsPycharmRun-0.4.tar.gz/IsPycharmRun-0.4/IsPycharmRun/IsPycharmRun.py
@@ -0,0 +1,23 @@
+from functools import wraps
+import os
+
+class IsPycharmRun:
+    def __init__(self, deviceUrl, logDir):
+        self.deviceUrl = deviceUrl
+        self.logDir = logDir
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(baseDir):
+            if self.isPycharmRun():
+                print(func.__name__ + " was called")
+                return func(basedir=baseDir, devices=self.deviceUrl, logdir=self.logDir)
+        return wrapped_function
+
+    def isPycharmRun(self):
+        isRunningInPyCharm = "PYCHARM_HOSTED" in os.environ
+        return isRunningInPyCharm
+
+
+if __name__ == "__main__":
+    a = IsPycharmRun("MasterTable")

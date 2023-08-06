ea1c19156510e35f92eb91a5f1193865da693031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:24 +0000

    Add package margot/0.1/margot-0.1.tar.gz

---
+++ packages/margot/margot-0.1.tar.gz/margot-0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name = 'margot',
+    packages = ['margot'],
+    version = '0.1',
+    license='apache-2.0',
+    description = 'simple to use, batteries included, tools for quantitative trading.',
+    author = 'Rich Atkinson',
+    author_email = 'rich@airteam.com.au',
+    url = 'https://github.com/atkinson/margot',
+    download_url = 'https://github.com/atkinson/margot/archive/v_0.1-alpha.tar.gz',
+    keywords = ['quant', 'trading', 'systematic'],
+    install_requires=[
+            'numpy',
+            'pandas',
+            'scipy',
+            'pyfolio',
+            'trading-calendars'
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3', 
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)

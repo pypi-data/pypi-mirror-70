4407680d9d371e4663939771ef79a9a48d2e0c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package genicam/1.0.0/genicam-1.0.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/genicam/genicam-1.0.0-cp38-cp38-manylinux1_x86_64.whl/genicam-1.0.0.data/purelib/genicam/__init__.py
@@ -0,0 +1,11 @@
+# Do not modify. This is an auto-generated file.
+
+components = ['1.0.0']
+
+if int('0'):
+    # In general, this release should be a patch for the Python Bindings
+    # that is created for an urgent case.
+    components.append('dev' + '0')
+
+__version__ = '.'.join(components)
+del components

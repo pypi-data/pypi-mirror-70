d27703a8f5de0d152b60e5331bc99a4b75e111ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:38 +0000

    Add package distributionscr7/0.1/distributionscr7-0.1.tar.gz

---
+++ packages/distributionscr7/distributionscr7-0.1.tar.gz/distributionscr7-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionscr7',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionscr7'],
+      zip_safe=False)

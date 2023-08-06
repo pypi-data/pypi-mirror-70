f1421d0207811bf3c07f1720e1c91c68d2b2b96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package rc-distributions/0.1/rc_distributions-0.1.tar.gz

---
+++ packages/rc-distributions/rc_distributions-0.1.tar.gz/rc_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rc_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rc_distributions'],
+      zip_safe=False)

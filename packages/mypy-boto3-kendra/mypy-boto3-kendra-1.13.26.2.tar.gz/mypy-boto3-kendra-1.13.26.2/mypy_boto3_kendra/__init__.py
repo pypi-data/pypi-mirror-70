35f0f8f3dd347f2de9de1e3d06697ac824b64f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:25 +0000

    Add package mypy-boto3-kendra/1.13.26.2/mypy-boto3-kendra-1.13.26.2.tar.gz

---
+++ packages/mypy-boto3-kendra/mypy-boto3-kendra-1.13.26.2.tar.gz/mypy-boto3-kendra-1.13.26.2/mypy_boto3_kendra/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for kendra service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_kendra import (
+        Client,
+        KendraClient,
+    )
+
+    session = boto3.Session()
+
+    client: KendraClient = boto3.client("kendra")
+    session_client: KendraClient = session.client("kendra")
+    ```
+"""
+from mypy_boto3_kendra.client import KendraClient as Client, KendraClient
+
+
+__all__ = ("Client", "KendraClient")

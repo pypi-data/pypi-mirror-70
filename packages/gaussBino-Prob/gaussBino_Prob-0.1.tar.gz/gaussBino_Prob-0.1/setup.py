ff68f8c4f5020d8716f049b67ad13b87b08d7b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:37 +0000

    Add package gaussBino-Prob/0.1/gaussBino_Prob-0.1.tar.gz

---
+++ packages/gaussBino-Prob/gaussBino_Prob-0.1.tar.gz/gaussBino_Prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussBino_Prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussBino_Prob'],
+      zip_safe=False)

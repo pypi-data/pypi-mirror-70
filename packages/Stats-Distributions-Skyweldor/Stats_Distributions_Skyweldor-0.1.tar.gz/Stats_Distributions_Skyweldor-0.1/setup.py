c17700780e0531e55e691cf1a97abc7ae5b8ebaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:53 +0000

    Add package Stats-Distributions-Skyweldor/0.1/Stats_Distributions_Skyweldor-0.1.tar.gz

---
+++ packages/Stats-Distributions-Skyweldor/Stats_Distributions_Skyweldor-0.1.tar.gz/Stats_Distributions_Skyweldor-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Stats_Distributions_Skyweldor',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Stats_Distributions_Skyweldor'],
+      author='Skyweldor',
+      zip_safe=False)

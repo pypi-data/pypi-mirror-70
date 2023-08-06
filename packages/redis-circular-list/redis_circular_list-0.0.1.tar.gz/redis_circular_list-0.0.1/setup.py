7f6dd526ec8c2b538e06041597adf683ebaee6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:02 +0000

    Add package redis-circular-list/0.0.1/redis_circular_list-0.0.1.tar.gz

---
+++ packages/redis-circular-list/redis_circular_list-0.0.1.tar.gz/redis_circular_list-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import pathlib
+import pkg_resources
+
+# pipreqs ./redis_circular_list
+
+# https://stackoverflow.com/a/59971469
+with pathlib.Path('./redis_circular_list/requirements.txt').open() as requirements_txt:
+	install_requires = [
+		str(requirement)
+		for requirement
+		in pkg_resources.parse_requirements(requirements_txt)
+	]
+
+setuptools.setup(
+	name="redis_circular_list",
+	version="0.0.1",
+	author="7435171",
+	author_email="48723247842@protonmail.com",
+	description="Redis Circular List",
+	url="https://github.com/48723247842/RedisCirclularList",
+	packages=setuptools.find_packages() ,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.8',
+	install_requires=install_requires
+)

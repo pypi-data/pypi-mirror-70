3940fc5c12f06b0277934d14659e35c620d481ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:32 +0000

    Add package sync-folders/0.1.5/sync-folders-0.1.5.tar.gz

---
+++ packages/sync-folders/sync-folders-0.1.5.tar.gz/sync-folders-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sync-folders',
+    packages = ['sync_folders'],
+    version = '0.1.5',
+    license='MIT',
+    description = 'Library for synchronization two folders',
+    author = 'Maxim Zavalniuk',
+    author_email = 'mezgoodle@gmail.com',
+    url = 'https://github.com/mezgoodle/sync-folders',
+    # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords = ['folders', 'files', 'synchronization', 'sync-folders'],
+)

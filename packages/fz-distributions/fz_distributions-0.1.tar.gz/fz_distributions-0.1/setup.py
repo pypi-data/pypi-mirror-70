f09e12f98432d46228f2b0d64be73033ca4ef2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:40 +0000

    Add package fz-distributions/0.1/fz_distributions-0.1.tar.gz

---
+++ packages/fz-distributions/fz_distributions-0.1.tar.gz/fz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fz_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['fz_distributions'],
+      author="Federico Zimmermann",
+      author_email="fedezimm@gmail.com",
+      zip_safe=False)

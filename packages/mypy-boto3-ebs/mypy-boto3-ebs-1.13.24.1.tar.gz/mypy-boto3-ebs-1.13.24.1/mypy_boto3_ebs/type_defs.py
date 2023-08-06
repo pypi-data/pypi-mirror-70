af41f672be437a34b60029e8cf17d26b2080e602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:57 +0000

    Add package mypy-boto3-ebs/1.13.24.1/mypy-boto3-ebs-1.13.24.1.tar.gz

---
+++ packages/mypy-boto3-ebs/mypy-boto3-ebs-1.13.24.1.tar.gz/mypy-boto3-ebs-1.13.24.1/mypy_boto3_ebs/type_defs.py
@@ -0,0 +1,73 @@
+"""
+Main interface for ebs service type definitions.
+
+Usage::
+
+    from mypy_boto3.ebs.type_defs import BlockTypeDef
+
+    data: BlockTypeDef = {...}
+"""
+from datetime import datetime
+import sys
+from typing import IO, List, Union
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "BlockTypeDef",
+    "ChangedBlockTypeDef",
+    "GetSnapshotBlockResponseTypeDef",
+    "ListChangedBlocksResponseTypeDef",
+    "ListSnapshotBlocksResponseTypeDef",
+)
+
+BlockTypeDef = TypedDict("BlockTypeDef", {"BlockIndex": int, "BlockToken": str}, total=False)
+
+ChangedBlockTypeDef = TypedDict(
+    "ChangedBlockTypeDef",
+    {"BlockIndex": int, "FirstBlockToken": str, "SecondBlockToken": str},
+    total=False,
+)
+
+GetSnapshotBlockResponseTypeDef = TypedDict(
+    "GetSnapshotBlockResponseTypeDef",
+    {
+        "DataLength": int,
+        "BlockData": Union[bytes, IO],
+        "Checksum": str,
+        "ChecksumAlgorithm": Literal["SHA256"],
+    },
+    total=False,
+)
+
+ListChangedBlocksResponseTypeDef = TypedDict(
+    "ListChangedBlocksResponseTypeDef",
+    {
+        "ChangedBlocks": List["ChangedBlockTypeDef"],
+        "ExpiryTime": datetime,
+        "VolumeSize": int,
+        "BlockSize": int,
+        "NextToken": str,
+    },
+    total=False,
+)
+
+ListSnapshotBlocksResponseTypeDef = TypedDict(
+    "ListSnapshotBlocksResponseTypeDef",
+    {
+        "Blocks": List["BlockTypeDef"],
+        "ExpiryTime": datetime,
+        "VolumeSize": int,
+        "BlockSize": int,
+        "NextToken": str,
+    },
+    total=False,
+)

7eae07310b7496b851ecedabde11c6559e6be262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:24 +0000

    Add package osmapy/0.0.1.1/Osmapy-0.0.1.1.tar.gz

---
+++ packages/osmapy/Osmapy-0.0.1.1.tar.gz/Osmapy-0.0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(name="Osmapy",
+                 author="Philipp Rigoll",
+                 version="0.0.1.1",
+                 author_email="philipp@rigoll.de",
+                 url="https://github.com/PhilippRigoll/Osmapy",
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 install_requires=[
+                     "numpy",
+                     "Pillow",
+                     "PyYAML",
+                     "PySide2",
+                     "easydict",
+                     "lxml",
+                     "requests",
+                     "gpxpy",
+                     "dataclasses;python_version<'3.7'"
+                 ],
+                 python_requires=">=3.6",
+                 entry_points={
+                     "console_scripts": ["Osmapy=Osmapy.main:main"]
+                 })

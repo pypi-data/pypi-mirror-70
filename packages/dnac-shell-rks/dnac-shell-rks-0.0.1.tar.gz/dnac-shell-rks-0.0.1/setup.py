e058cf00cf50735d446464cbbee11de0b426b084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:31 +0000

    Add package dnac-shell-rks/0.0.1/dnac-shell-rks-0.0.1.tar.gz

---
+++ packages/dnac-shell-rks/dnac-shell-rks-0.0.1.tar.gz/dnac-shell-rks-0.0.1/setup.py
@@ -0,0 +1,7 @@
+# setup.py - unnecessary if not redistributing the code, see below
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(name ='dnac-shell-rks',
+      author='Rk Somasundaram',
+      version='0.0.1')

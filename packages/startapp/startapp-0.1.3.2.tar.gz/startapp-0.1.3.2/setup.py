209cc6a31ecf3d3ae0381a8564fa3979f350aac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:30 +0000

    Add package startapp/0.1.3.2/startapp-0.1.3.2.tar.gz

---
+++ packages/startapp/startapp-0.1.3.2.tar.gz/startapp-0.1.3.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+# version = __import__("takeaway").__version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="startapp",
+    version="0.1.3.2",
+    py_modules=["startapp"],
+    include_package_data=True,
+    packages=find_packages(),
+    author="",
+    author_email="",
+    description="Simple startapp for fastapi flask  and django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/marlin-dev/",
+    install_requires=[
+        "click"
+    ],
+    extras_require={
+        "prompt_toolkit": ["prompt_toolkit == 1.0.14"]
+    },
+    entry_points="""
+        [console_scripts]
+        startapp=takeaway.script.order:starting
+    """,
+    # entry_points={
+    #     'console_scripts': [
+    #         'startapp=takeaway.script.order:cli
+    #     ]
+    # },
+
+)
+
+
+
+
+
+
+
+

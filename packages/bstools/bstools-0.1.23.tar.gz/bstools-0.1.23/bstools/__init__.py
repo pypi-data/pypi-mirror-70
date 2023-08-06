6ef2f0dfea85b55b1d4b2b3e499362d6ff0c626a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:52 +0000

    Add package bstools/0.1.23/bstools-0.1.23.tar.gz

---
+++ packages/bstools/bstools-0.1.23.tar.gz/bstools-0.1.23/bstools/__init__.py
@@ -0,0 +1,9 @@
+from bstools import bsSsh
+from bstools import bsPrint
+from bstools import bsTime
+from bstools import bsNetwork
+
+name = "bstools"
+__version__ = "0.1.23"
+__all__ = ["bsSsh", "bsPrint", "bsTime", "bsNetwork" ]
+
---
+++ packages/bstools/bstools-0.1.23.tar.gz/bstools-0.1.23/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from collections import OrderedDict
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bstools",
+    version=__import__('bstools').__version__,
+    author="Bernie Suen",
+    author_email="bernie.suen@outlook.com",
+    description="some very useful tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://www.swfu.co/",
+    project_urls=OrderedDict((
+        ('Documentation', 'https://github.com/qkjs/bstools/wiki'),
+        ('Code', 'https://github.com/qkjs/bstools'),
+        ('Issue tracker', 'https://github.com/qkjs/bstools/issues'),
+    )),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "paramiko",
+    ]
+)

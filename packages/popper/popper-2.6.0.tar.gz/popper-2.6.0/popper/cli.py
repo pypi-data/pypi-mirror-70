15f6f745c6279cc883e86fa69dd169191892f113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:47 +0000

    Add package popper/2.6.0/popper-2.6.0.tar.gz

---
+++ packages/popper/popper-2.6.0.tar.gz/popper-2.6.0/popper/cli.py
@@ -0,0 +1,88 @@
+import difflib
+import os
+import signal
+import sys
+
+import click
+from click.exceptions import ClickException
+
+import popper.log as logging
+
+from popper import __version__
+
+log = logging.setup_logging()
+popper_version = __version__
+cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+
+
+class Context(object):
+    """Context Class.For more details refer
+    https://click.palletsprojects.com/en/7.x/commands/#custom-multi-commands .
+    """
+
+    pass
+
+
+pass_context = click.make_pass_decorator(Context, ensure=True)
+
+
+class PopperCLI(click.MultiCommand):
+    """Provides CLI interface for Popper."""
+
+    def list_commands(self, ctx):
+        """Returns the list of available commands in sorted order.
+
+        Args:
+          ctx(popper.cli.context): For process inter-command communication.
+          For reference visit https://click.palletsprojects.com/en/7.x
+
+        Returns:
+          list: Returns the list of available commands.
+        """
+        rv = []
+        for filename in os.listdir(cmd_folder):
+            if filename.endswith(".py") and filename.startswith("cmd_"):
+                rv.append(filename[4:-3])
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, name):
+        """Imports the command if available in commmands list and provides with
+        most similar commands if the command is not present in the list.
+
+        Args:
+          name(str): The name of the command.
+
+        Returns:
+          click.core.Command: It is a new command and uses the decorated
+            function as callback.For reference visit
+            https://click.palletsprojects.com/en/7.x/api/#decorators .
+        """
+        try:
+            if sys.version_info[0] == 2:
+                name = name.encode("ascii", "replace")
+            mod = __import__("popper.commands.cmd_" + name, None, None, ["cli"])
+        except ImportError as e:
+            commands = self.list_commands(ctx)
+            most_similar_commands = ", ".join(
+                difflib.get_close_matches(name, commands, 3, 0.3)
+            )
+            message = ""
+            if len(most_similar_commands) != 0:
+                message = "\n\nThe most similar commands are: " + most_similar_commands
+            raise ClickException(
+                "Command '" + name + "' doesn't exist.\n"
+                "Type 'popper --help' for more.\n" + message + "\n" + str(e)
+            )
+        return mod.cli
+
+
+@click.command(cls=PopperCLI)
+@click.version_option(__version__, message=f"Popper version {popper_version}")
+@pass_context
+def cli(ctx):
+    """Popper command line interface."""
+    from popper.runner import WorkflowRunner
+
+    signal.signal(signal.SIGINT, WorkflowRunner.signal_handler)
+    signal.signal(signal.SIGUSR1, WorkflowRunner.signal_handler)
---
+++ packages/popper/popper-2.6.0.tar.gz/popper-2.6.0/test/test_scm.py
@@ -0,0 +1,143 @@
+import os
+import tempfile
+
+import git
+
+from popper import scm
+from popper.cli import log
+
+from .test_common import PopperTest
+
+
+class TestScm(PopperTest):
+    @classmethod
+    def setUpClass(self):
+        log.setLevel("CRITICAL")
+
+    @classmethod
+    def tearDownClass(self):
+        log.setLevel("NOTSET")
+
+    def test_get_remote_url(self):
+        repo = self.mk_repo()
+        url = scm.get_remote_url(repo)
+        self.assertEqual(url, "https://github.com/my/repo")
+
+        self.assertEqual(scm.get_remote_url(None), "")
+
+    def test_get_branch_in_detached_head_state(self):
+        repo = self.mk_repo()
+
+        repo.git.checkout("HEAD~1")
+
+        os.environ["TRAVIS_BRANCH"] = "travis"
+        self.assertEqual("travis", scm.get_branch(repo))
+        os.environ.pop("TRAVIS_BRANCH")
+
+        os.environ["GIT_BRANCH"] = "jenkins"
+        self.assertEqual("jenkins", scm.get_branch(repo))
+        os.environ.pop("GIT_BRANCH")
+
+        os.environ["CIRCLE_BRANCH"] = "circle"
+        self.assertEqual("circle", scm.get_branch(repo))
+        os.environ.pop("CIRCLE_BRANCH")
+
+        os.environ["CI_COMMIT_REF_NAME"] = "gitlab"
+        self.assertEqual("gitlab", scm.get_branch(repo))
+        os.environ.pop("CI_COMMIT_REF_NAME")
+
+        self.assertEqual(scm.get_sha(repo), scm.get_branch(repo))
+
+        # None given as arg
+        self.assertIsNone(scm.get_sha(None))
+        self.assertIsNone(scm.get_sha(None, short=8))
+        self.assertIsNone(scm.get_branch(None))
+
+    def test_clone(self):
+        tempdir = tempfile.mkdtemp()
+        tdir = os.path.join(tempdir, "test_clone")
+        os.makedirs(tdir)
+        currdir = os.getcwd()
+        os.chdir(tdir)
+        scm.clone(
+            "https://github.com",
+            "popperized",
+            "github-actions-demo",
+            os.path.join(os.getcwd(), "gad"),
+            "develop",
+        )
+        repo = git.Repo(os.path.join(os.getcwd(), "gad"))
+        self.assertEqual(repo.active_branch.name, "develop")
+        scm.clone(
+            "https://github.com",
+            "popperized",
+            "github-actions-demo",
+            os.path.join(os.getcwd(), "gad"),
+            "master",
+        )
+        repo = git.Repo(os.path.join(os.getcwd(), "gad"))
+        self.assertEqual(repo.active_branch.name, "master")
+        repo.close()
+        os.chdir(currdir)
+
+    def test_parse(self):
+        test_url = "ssh://git@github.com:popperized" "/github-actions-demo.git"
+        self.assertRaises(SystemExit, scm.parse, test_url)
+        test_url = "https://github.com/popperized@master"
+        self.assertRaises(SystemExit, scm.parse, test_url)
+        test_url = "github.com"
+        self.assertRaises(SystemExit, scm.parse, test_url)
+        test_url = "@master"
+        self.assertRaises(SystemExit, scm.parse, test_url)
+        test_url = "http://gitlab.com/popperized/github-actions-demo.git"
+        parts = scm.parse(test_url)
+        self.assertTupleEqual(
+            parts,
+            (
+                "http://gitlab.com",
+                "gitlab.com",
+                "popperized",
+                "github-actions-demo",
+                "",
+                None,
+            ),
+        )
+        test_url = "https://github.com/popperized" "/github-actions-demo@master"
+        parts = scm.parse(test_url)
+        self.assertTupleEqual(
+            parts,
+            (
+                "https://github.com",
+                "github.com",
+                "popperized",
+                "github-actions-demo",
+                "",
+                "master",
+            ),
+        )
+        test_url = "github.com/popperized/github-actions-demo"
+        parts = scm.parse(test_url)
+        self.assertTupleEqual(
+            parts,
+            (
+                "https://github.com",
+                "github.com",
+                "popperized",
+                "github-actions-demo",
+                "",
+                None,
+            ),
+        )
+        test_url = "popperized/github-actions-demo/path/to/action@develop"
+        parts = scm.parse(test_url)
+        self.assertTupleEqual(
+            parts,
+            (
+                "https://github.com",
+                "github.com",
+                "popperized",
+                "github-actions-demo",
+                "path/to/action",
+                "develop",
+            ),
+        )

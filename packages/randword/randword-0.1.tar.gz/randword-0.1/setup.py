1912595f6fc50152462190a7dfb7d2b96138a136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:58 +0000

    Add package randword/0.1/randword-0.1.tar.gz

---
+++ packages/randword/randword-0.1.tar.gz/randword-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='randword',
+    description='A Python package for generation random English words',
+    keywords=['generation', 'word', 'random', 'english', 'python', 'package'],
+    version='0.1',
+
+    author='Artyom Bezmenov (8nhuman)',
+    author_email='artem.bezmenov02@gmail.com',
+    license='MIT',
+
+    url='https://github.com/8nhuman8/rand-word',
+    download_url='https://github.com/8nhuman8/rand-word/archive/0.2.tar.gz',
+
+    package_data={'': ['*.txt']},
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ]
+)

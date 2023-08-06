f9ba7eba3d5c98342e8888594148da0f74125f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:13 +0000

    Add package knowage-python/1.0/knowage-python-1.0.tar.gz

---
+++ packages/knowage-python/knowage-python-1.0.tar.gz/knowage-python-1.0/pythonwebservice/knowage-python.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+from app.services import create_app
+from app.utilities import constants
+
+application = create_app()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        port = int(sys.argv[0])
+    else:
+        port = 5000
+    open(constants.LOG_FILE, 'w+').close() #clean log file
+    original_stderr = sys.stderr
+    #sys.stderr = open(constants.LOG_FILE, 'a')
+    application.run(host='0.0.0.0', debug=False, port=port, ssl_context=('cert.pem', 'key.pem'))

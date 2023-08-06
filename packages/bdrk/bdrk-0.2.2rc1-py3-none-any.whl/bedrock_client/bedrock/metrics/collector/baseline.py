f47061cc295c5c90db6278e87b866196cade4262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package bdrk/0.2.2rc1/bdrk-0.2.2rc1-py3-none-any.whl

---
+++ packages/bdrk/bdrk-0.2.2rc1-py3-none-any.whl/bedrock_client/bedrock/metrics/collector/baseline.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from prometheus_client.parser import text_fd_to_metric_families
+
+from .type import Collector
+
+
+class BaselineMetricCollector(Collector):
+    """Collects baseline metrics from a Prometheus file.
+
+    Users may extend this class to fetch baseline metrics from network locations.
+    """
+
+    DEFAULT_HISTOGRAM_PATH = "/artefact/histogram.prom"
+
+    def __init__(self, path: Optional[str] = None):
+        """Parses baseline metrics from a local file.
+
+        :param path: Path to Prometheus file, defaults to `DEFAULT_HISTOGRAM_PATH`
+        :type path: Optional[str], optional
+        """
+        self.path = path or BaselineMetricCollector.DEFAULT_HISTOGRAM_PATH
+
+    def describe(self):
+        return self.collect()
+
+    def collect(self):
+        with open(self.path, "r") as f:
+            for metric in text_fd_to_metric_families(f):
+                # Ignore non-baseline metrics
+                if not metric.name.endswith("_baseline"):
+                    continue
+                yield metric

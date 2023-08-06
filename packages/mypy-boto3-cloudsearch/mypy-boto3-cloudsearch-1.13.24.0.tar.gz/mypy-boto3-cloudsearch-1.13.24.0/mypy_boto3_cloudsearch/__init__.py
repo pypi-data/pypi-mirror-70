f087d4c0fd949ac5d4d4acd0752cda964a69cb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:08 +0000

    Add package mypy-boto3-cloudsearch/1.13.24.0/mypy-boto3-cloudsearch-1.13.24.0.tar.gz

---
+++ packages/mypy-boto3-cloudsearch/mypy-boto3-cloudsearch-1.13.24.0.tar.gz/mypy-boto3-cloudsearch-1.13.24.0/mypy_boto3_cloudsearch/__init__.py
@@ -0,0 +1,20 @@
+"""
+Main interface for cloudsearch service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.cloudsearch import (
+        Client,
+        CloudSearchClient,
+        )
+
+    session = boto3.Session()
+
+    client: CloudSearchClient = boto3.client("cloudsearch")
+    session_client: CloudSearchClient = session.client("cloudsearch")
+"""
+from mypy_boto3_cloudsearch.client import CloudSearchClient, CloudSearchClient as Client
+
+
+__all__ = ("Client", "CloudSearchClient")

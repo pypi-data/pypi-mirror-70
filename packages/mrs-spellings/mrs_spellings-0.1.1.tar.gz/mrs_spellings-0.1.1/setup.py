41d196281b7b416028dda49ae590aed54b46fb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:10 +0000

    Add package mrs-spellings/0.1.1/mrs_spellings-0.1.1.tar.gz

---
+++ packages/mrs-spellings/mrs_spellings-0.1.1.tar.gz/mrs_spellings-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mrs_spellings', 'mrs_spellings.qwerty_caches']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mrs-spellings',
+    'version': '0.1.1',
+    'description': 'a micro utility for generating plausible misspellings',
+    'long_description': None,
+    'author': 'CircArgs',
+    'author_email': 'quebecname@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)

9dc79d6636cfea3e30bfd14b185e11e49c1ed29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:09 +0000

    Add package biogeme/3.2.6/biogeme-3.2.6-py3.7-macosx-10.7-x86_64.egg

---
+++ packages/biogeme/biogeme-3.2.6-py3.7-macosx-10.7-x86_64.egg/biogeme/exceptions.py
@@ -0,0 +1,13 @@
+"""Defines a generic exception for Biogeme
+
+:author: Michel Bierlaire
+
+:date: Tue Mar 26 16:47:11 2019
+
+"""
+
+# Too constraining
+# pylint: disable=invalid-name
+
+class biogemeError(Exception):
+    """ Defines a generic exception for Biogeme. """

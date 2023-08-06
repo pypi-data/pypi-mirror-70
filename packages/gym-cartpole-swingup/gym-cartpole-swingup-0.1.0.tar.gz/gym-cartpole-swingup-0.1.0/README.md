47538dde89a1c98c46036e5e8c127860bbd04c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:39 +0000

    Add package gym-cartpole-swingup/0.1.0/gym-cartpole-swingup-0.1.0.tar.gz

---
+++ packages/gym-cartpole-swingup/gym-cartpole-swingup-0.1.0.tar.gz/gym-cartpole-swingup-0.1.0/README.md
@@ -0,0 +1,31 @@
+[![Travis (.com)](https://img.shields.io/travis/com/angelolovatto/gym-cartpole-swingup?logo=Travis%20CI&logoColor=important)](https://travis-ci.com/angelolovatto/gym-cartpole-swingup)
+[![PyPI](https://img.shields.io/pypi/v/gym-cartpole-swingup?color=blue&logo=PyPI&logoColor=white)](https://pypi.org/project/gym-cartpole-swingup/)
+
+# gym-cartpole-swingup
+A simple, continuous-control environment for OpenAI Gym
+
+## Installation
+```bash
+pip install gym-cartpole-swingup
+```
+
+## Usage example
+```python
+# coding: utf-8
+import gym
+import gym_cartpole_swingup
+
+# Could be one of:
+# CartPoleSwingUp-v0, CartPoleSwingUp-v1
+# If you have PyTorch installed:
+# TorchCartPoleSwingUp-v0, TorchCartPoleSwingUp-v1
+env = gym.make("CartPoleSwingUp-v0")
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    obs, rew, done, info = env.step(action)
+    env.render()
+```
+
+![](https://i.imgur.com/Z8bLLM8.png)
---
+++ packages/gym-cartpole-swingup/gym-cartpole-swingup-0.1.0.tar.gz/gym-cartpole-swingup-0.1.0/tests/test_basic.py
@@ -0,0 +1,75 @@
+# pylint:disable=missing-docstring,protected-access,redefined-outer-name,unused-import
+import gym
+import numpy as np
+import pytest
+
+import gym_cartpole_swingup
+
+
+ENV_IDS = "CartPoleSwingUp-v0 CartPoleSwingUp-v1".split()
+
+try:
+    import torch
+
+    ENV_IDS += "TorchCartPoleSwingUp-v0 TorchCartPoleSwingUp-v1".split()
+
+except ImportError:
+    pass
+
+
+@pytest.fixture(params=ENV_IDS)
+def env(request):
+    return gym.make(request.param)
+
+
+def test_init(env):
+    assert hasattr(env, "observation_space")
+    assert hasattr(env, "action_space")
+    assert hasattr(env, "state")
+    assert np.isfinite(env.action_space.high)
+    assert np.isfinite(env.action_space.low)
+
+
+def test_reset(env):
+    obs = env.reset()
+    assert obs in env.observation_space
+    assert isinstance(obs, np.ndarray)
+    assert obs.shape == (5,)
+    assert isinstance(env.state, tuple)
+    assert len(env.state) == 4
+
+
+def test_step(env):
+    env.reset()
+    act = env.action_space.sample()
+    next_obs, rew, done, info = env.step(act)
+
+    assert next_obs in env.observation_space
+    assert np.isscalar(rew)
+    assert rew >= 0 if "v1" in env.spec.id else rew < 0
+    assert isinstance(done, bool)
+    assert isinstance(info, dict)
+    assert not list(info.keys())
+
+
+def test_reward_fn(env):
+    env.reset()
+    state = env.state
+    act = env.action_space.sample()
+    env.reset()
+    next_state = env.state
+    rew = env.unwrapped._reward_fn(state, act, next_state)
+
+    assert np.isscalar(rew)
+    assert np.isfinite(rew)
+
+
+def test_transition_fn(env):
+    env.reset()
+    state = env.state
+    act = env.action_space.sample()
+    next_state = env.unwrapped._transition_fn(state, act)
+
+    assert isinstance(next_state, tuple)
+    assert all(np.isfinite(s) for s in next_state)
+    assert len(next_state) == 4

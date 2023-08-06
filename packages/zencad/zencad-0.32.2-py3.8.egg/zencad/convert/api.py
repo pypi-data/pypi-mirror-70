caa709a709cde8065fb2288b2b82928cca0f6838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:09 +0000

    Add package zencad/0.32.2/zencad-0.32.2-py3.8.egg

---
+++ packages/zencad/zencad-0.32.2-py3.8.egg/zencad/convert/api.py
@@ -0,0 +1,41 @@
+"""
+В этом файле определены операции экспорта и импорта геометрии. 
+
+Операции экспорта реализованы с применением evalcache.lazyfile,
+что позволяет избежать множественных загрухок крайней ноды.
+
+Политика хеширования в случае импорта требует учета возможности изменения
+файла. Поэтому в хэш загружаемого объекта подмешивается дата его модификации. 
+Объект не кешируется, потому как операция восстановления из кэша
+ничем не отличается от загрузки из файла.
+"""
+
+import os
+import zencad
+import pyservoce
+import evalcache
+from zencad.lazifier import lazy
+
+
+@lazy.file_creator(pathfield="path")
+def to_stl(model, path, delta):
+    pyservoce.make_stl(model, path, delta)
+
+
+@lazy.file_creator(pathfield="path")
+def to_brep(model, path):
+    pyservoce.brep_write(model, path)
+
+
+def from_brep(path):
+    """Загрузить объект из файла его brep представления."""
+    f = lazy(lambda p: pyservoce.brep_read(p), hint=str(os.path.getmtime(path)))
+    obj = f(path)
+    evalcache.nocache(obj)
+    return obj
+
+
+
+from zencad.convert.svg import shape_to_svg_string
+from zencad.convert.svg import shape_to_svg
+from zencad.convert.svg import svg_to_shape

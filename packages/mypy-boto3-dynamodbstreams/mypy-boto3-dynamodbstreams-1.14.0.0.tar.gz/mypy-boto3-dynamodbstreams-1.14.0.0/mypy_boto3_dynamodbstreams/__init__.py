3103e771ec59a8b73370628729e56ab7002fdbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:19 +0000

    Add package mypy-boto3-dynamodbstreams/1.14.0.0/mypy-boto3-dynamodbstreams-1.14.0.0.tar.gz

---
+++ packages/mypy-boto3-dynamodbstreams/mypy-boto3-dynamodbstreams-1.14.0.0.tar.gz/mypy-boto3-dynamodbstreams-1.14.0.0/mypy_boto3_dynamodbstreams/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for dynamodbstreams service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_dynamodbstreams import (
+        Client,
+        DynamoDBStreamsClient,
+    )
+
+    session = boto3.Session()
+
+    client: DynamoDBStreamsClient = boto3.client("dynamodbstreams")
+    session_client: DynamoDBStreamsClient = session.client("dynamodbstreams")
+    ```
+"""
+from mypy_boto3_dynamodbstreams.client import DynamoDBStreamsClient as Client, DynamoDBStreamsClient
+
+
+__all__ = ("Client", "DynamoDBStreamsClient")

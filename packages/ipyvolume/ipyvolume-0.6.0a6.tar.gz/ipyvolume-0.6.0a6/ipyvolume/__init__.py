55f737898866354e3108a570342798807e47a8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:48 +0000

    Add package ipyvolume/0.6.0a6/ipyvolume-0.6.0a6.tar.gz

---
+++ packages/ipyvolume/ipyvolume-0.6.0a6.tar.gz/ipyvolume-0.6.0a6/ipyvolume/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+from ipyvolume._version import __version__  # noqa: F401
+from ipyvolume import styles  # noqa: F401
+from ipyvolume import examples  # noqa: F401
+from ipyvolume import datasets  # noqa: F401
+from ipyvolume import embed  # noqa: F401
+from ipyvolume.widgets import *  # noqa: F401, F403
+from ipyvolume.transferfunction import *  # noqa: F401, F403
+from ipyvolume.pylab import *  # noqa: F401, F403
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'ipyvolume',
+        'require': 'ipyvolume/extension'
+    }]

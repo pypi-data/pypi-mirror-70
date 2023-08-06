0169a7208b9bc5d0579f55350bfeccc74e8dcc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package graphene-django/3.0.0b2/graphene_django-3.0.0b2-py2.py3-none-any.whl

---
+++ packages/graphene-django/graphene_django-3.0.0b2-py2.py3-none-any.whl/graphene_django/__init__.py
@@ -0,0 +1,11 @@
+from .fields import DjangoConnectionField, DjangoListField
+from .types import DjangoObjectType
+
+__version__ = "3.0.0b2"
+
+__all__ = [
+    "__version__",
+    "DjangoObjectType",
+    "DjangoListField",
+    "DjangoConnectionField",
+]

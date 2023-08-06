fcbd77b839050545145c2193d9c9bbd133ecd346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:43 +0000

    Add package tmsutils/1.0.7/tmsutils-1.0.7.tar.gz

---
+++ packages/tmsutils/tmsutils-1.0.7.tar.gz/tmsutils-1.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    url              = 'https://github.com/TheMrinalSinha/tmsutils',
+    name             = 'tmsutils',
+    author           = 'Mrinal Sinha',
+    version          = '1.0.7',
+    packages         = ['tmsutils'],
+    py_modules       = ['tmsutils'],
+    description      = 'Handy tools out of the box',
+    author_email     = 'mail@themrinalsinha.com',
+    install_requires = [pkg for pkg in open('requirements.txt').read().split('\n') if pkg],
+    classifiers      = [
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+
+    entry_points = '''
+        [console_scripts]
+        tms=tmsutils.cli:cli
+    '''
+)

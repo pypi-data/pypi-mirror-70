5b288ef5a9980e282e49d27fa9bec2d3193ffa51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:08 +0000

    Add package crazy-distributions/0.1/crazy-distributions-0.1.tar.gz

---
+++ packages/crazy-distributions/crazy-distributions-0.1.tar.gz/crazy-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='crazy-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author='Harshit Rai',
+      author_email='harshitrai68@gmail.com',
+      packages=['crazy-distributions'],
+      zip_safe=False)

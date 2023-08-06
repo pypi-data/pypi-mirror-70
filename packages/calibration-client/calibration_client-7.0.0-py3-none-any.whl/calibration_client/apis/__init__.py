b6390a7099f2176a5d924edfd7eb865d0361cce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:07 +0000

    Add package calibration-client/7.0.0/calibration_client-7.0.0-py3-none-any.whl

---
+++ packages/calibration-client/calibration_client-7.0.0-py3-none-any.whl/calibration_client/apis/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Luís Maia <luis.maia@xfel.eu>'
+
+from .calibration_api import CalibrationApi
+from .calibration_constant_api import CalibrationConstantApi
+from .calibration_constant_version_api import CalibrationConstantVersionApi
+from .condition_api import ConditionApi
+from .device_type_api import DeviceTypeApi
+from .parameter_api import ParameterApi
+from .physical_device_api import PhysicalDeviceApi
+from .user_api import UserApi

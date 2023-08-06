753668bff65013ab54d7e5942569ae81c5111ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package eririn/0.0.13/eririn-0.0.13-py3-none-any.whl

---
+++ packages/eririn/eririn-0.0.13-py3-none-any.whl/eririn/__main__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Usage: python3 -m eririn <EXCELFILE> <FILE>...
+
+Arguments:
+  <EXCELFILE>    output Excel .xlsx file (with extension).
+  <FILE>...      input image files
+
+Options:
+  -h --help
+  -v       verbose mode
+"""
+
+import os
+import sys
+
+from docopt import docopt
+from . import conv as eririnlib
+
+
+def main():
+    arguments = docopt(__doc__)
+    print(arguments)
+    eririnlib.main(arguments)
+
+
+if __name__ == '__main__':
+    main()

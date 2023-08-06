472f19c683aba97724da0448104edd6cd082e2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package fbobo-test/1.0.1/fbobo_test-1.0.1.tar.gz

---
+++ packages/fbobo-test/fbobo_test-1.0.1.tar.gz/fbobo_test-1.0.1/pt_hello/pt_test.py
@@ -0,0 +1,12 @@
+
+
+def test():
+    print("hello world")
+
+
+def main():
+    print("test")
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/fbobo-test/fbobo_test-1.0.1.tar.gz/fbobo_test-1.0.1/setup.py
@@ -0,0 +1,43 @@
+import codecs
+import os
+import sys
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+NAME = "fbobo_test"
+PACKAGES = ['pt_hello']
+DESCRIPTION = "this is a test for package by myself upload to pypi"
+LONG_DESCRIPTION = "this is a test for package by myself upload to pypi"
+KEYWORDS = "keyword"
+AUTHOR = "feikong"
+AUTHOR_EMAIL = "feikong@shouxin168.com"
+URL = "https://github.com/lichanghong/wenyali.git"
+VERSION = "1.0.1"
+LICENSE = "MIT"
+setup(
+    name=NAME, version=VERSION,
+    description = DESCRIPTION,long_description =LONG_DESCRIPTION,
+    classifiers =[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords =KEYWORDS,author = AUTHOR,
+    author_email = AUTHOR_EMAIL,url = URL,
+    packages = PACKAGES,include_package_data=True,zip_safe=True,
+    entry_points={
+      "console_scripts": [
+                          "wenyali_console = package1.index:index",
+                          ]
+      },
+
+)

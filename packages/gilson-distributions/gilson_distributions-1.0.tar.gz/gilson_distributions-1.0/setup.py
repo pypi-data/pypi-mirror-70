96ca1ba425938793f17644516a5ccbcc26ca2f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:23 +0000

    Add package gilson-distributions/1.0/gilson_distributions-1.0.tar.gz

---
+++ packages/gilson-distributions/gilson_distributions-1.0.tar.gz/gilson_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gilson_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gilson_distributions'],
+      zip_safe=False)

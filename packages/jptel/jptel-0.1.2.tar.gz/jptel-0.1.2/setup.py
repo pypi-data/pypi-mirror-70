c03de8af938e39e0438274eaef609d439d1436be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package jptel/0.1.2/jptel-0.1.2.tar.gz

---
+++ packages/jptel/jptel-0.1.2.tar.gz/jptel-0.1.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding=utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+
+long_description = 'Japanese Telephone Number Utility'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+
+setup(
+    name='jptel',
+    version='0.1.2',
+    description='Japanese Telephone Number Utility',
+    long_description=long_description,
+    license='MIT',
+    author='zenwerk',
+    author_email='zenwerk@gmail.com',
+    url='https://github.com/zenwerk/jptel-py',
+    keywords='japaneses telephone number',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ]
+)

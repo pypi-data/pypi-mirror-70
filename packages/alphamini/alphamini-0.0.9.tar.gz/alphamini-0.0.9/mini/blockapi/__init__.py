c4e4be57986fd8a4de8fe021cef56930d35c6ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:32 +0000

    Add package alphamini/0.0.9/alphamini-0.0.9.tar.gz

---
+++ packages/alphamini/alphamini-0.0.9.tar.gz/alphamini-0.0.9/mini/blockapi/__init__.py
@@ -0,0 +1,3 @@
+from ..blockapi import *
+from ..pb2 import *
+from ..channels import *
---
+++ packages/alphamini/alphamini-0.0.9.tar.gz/alphamini-0.0.9/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name="alphamini",
+    version="0.0.9",
+    author='logic.peng',
+    autho_email='logic.peng@ubtrobot.com',
+    description="python sdk for ubtenic alphamini robot",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    # url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'websockets',
+        'ifaddr',
+        'protobuf'
+    ],
+)

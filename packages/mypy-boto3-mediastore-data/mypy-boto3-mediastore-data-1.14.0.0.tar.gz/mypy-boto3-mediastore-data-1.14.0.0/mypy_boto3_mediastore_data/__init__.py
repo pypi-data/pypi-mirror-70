41c91e9fd9009e3d07277ad27e11789605505ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:28 +0000

    Add package mypy-boto3-mediastore-data/1.14.0.0/mypy-boto3-mediastore-data-1.14.0.0.tar.gz

---
+++ packages/mypy-boto3-mediastore-data/mypy-boto3-mediastore-data-1.14.0.0.tar.gz/mypy-boto3-mediastore-data-1.14.0.0/mypy_boto3_mediastore_data/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for mediastore-data service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_mediastore_data import (
+        Client,
+        ListItemsPaginator,
+        MediaStoreDataClient,
+    )
+
+    session = boto3.Session()
+
+    client: MediaStoreDataClient = boto3.client("mediastore-data")
+    session_client: MediaStoreDataClient = session.client("mediastore-data")
+
+    list_items_paginator: ListItemsPaginator = client.get_paginator("list_items")
+    ```
+"""
+from mypy_boto3_mediastore_data.client import MediaStoreDataClient as Client, MediaStoreDataClient
+from mypy_boto3_mediastore_data.paginator import ListItemsPaginator
+
+
+__all__ = ("Client", "ListItemsPaginator", "MediaStoreDataClient")

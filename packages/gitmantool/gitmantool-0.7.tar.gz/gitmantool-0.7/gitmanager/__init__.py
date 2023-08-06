4f9eca9d945228a6086adf3adaf52bea21686161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:59 +0000

    Add package gitmantool/0.7/gitmantool-0.7.tar.gz

---
+++ packages/gitmantool/gitmantool-0.7.tar.gz/gitmantool-0.7/gitmanager/__init__.py
@@ -0,0 +1,40 @@
+'''
+Git Manager Tool
+
+Usage:
+  gm -l | --list
+  gm -n | --new
+  gm -b | --bug
+  gm -s | --show <id>
+  gm -c | --close <id>
+  gm -h | --help
+  gm -v | --version
+
+Options:
+  -l --list     Show issues list from current Milestone
+  -n --new     Create new issue without label
+  -b --bug     Create new issue as bug
+  -s --show     Show an issue specifying his ID
+  -c --close     Close an issue specifying his ID
+  -h --help     Show this screen.
+  -v --version     Show version.
+'''
+
+import sys
+from docopt import docopt
+from colorama import init
+from .handler import run_handler
+
+
+# ─── MAIN ───────────────────────────────────────────────────────────────────────
+
+def main():
+    init(autoreset=True)  # init colorama
+    arguments = docopt(__doc__)
+
+    try:
+        run_handler(arguments)
+    except KeyboardInterrupt:
+        sys.exit("Exiting...")
+    except Exception as e:
+        print(e)

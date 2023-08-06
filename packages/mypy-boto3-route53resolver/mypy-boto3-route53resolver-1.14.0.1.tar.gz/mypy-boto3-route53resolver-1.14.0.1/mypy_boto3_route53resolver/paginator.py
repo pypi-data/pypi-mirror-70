5917ca9782e14486012ff01f5234ab2829d186da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:48 +0000

    Add package mypy-boto3-route53resolver/1.14.0.1/mypy-boto3-route53resolver-1.14.0.1.tar.gz

---
+++ packages/mypy-boto3-route53resolver/mypy-boto3-route53resolver-1.14.0.1.tar.gz/mypy-boto3-route53resolver-1.14.0.1/mypy_boto3_route53resolver/paginator.py
@@ -0,0 +1,41 @@
+"""
+Main interface for route53resolver service client paginators.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_route53resolver import Route53ResolverClient
+    from mypy_boto3_route53resolver.paginator import (
+        ListTagsForResourcePaginator,
+    )
+
+    client: Route53ResolverClient = boto3.client("route53resolver")
+
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_route53resolver.type_defs import (
+    ListTagsForResourceResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("ListTagsForResourcePaginator",)
+
+
+class ListTagsForResourcePaginator(Boto3Paginator):
+    """
+    [Paginator.ListTagsForResource documentation](https://boto3.amazonaws.com/v1/documentation/api/1.14.0/reference/services/route53resolver.html#Route53Resolver.Paginator.ListTagsForResource)
+    """
+
+    def paginate(
+        self, ResourceArn: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListTagsForResourceResponseTypeDef]:
+        """
+        [ListTagsForResource.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.14.0/reference/services/route53resolver.html#Route53Resolver.Paginator.ListTagsForResource.paginate)
+        """

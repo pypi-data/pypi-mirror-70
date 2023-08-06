d5641e78cccf8a157a6ea9ab094932fc3eb2087d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:48 +0000

    Add package libtc/1.0.0/libtc-1.0.0.tar.gz

---
+++ packages/libtc/libtc-1.0.0.tar.gz/libtc-1.0.0/libtc/__init__.py
@@ -0,0 +1,25 @@
+from .bencode import bdecode, bencode
+from .clients import *
+from .exceptions import FailedToExecuteException, LibTorrentClientException
+from .management import move_torrent
+from .torrent import *
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "DelugeClient",
+    "RTorrentClient",
+    "TransmissionClient",
+    "QBittorrentClient",
+    "LilTorrentClient",
+    "FakeClient",
+    "TORRENT_CLIENT_MAPPING",
+    "TorrentData",
+    "TorrentState",
+    "bencode",
+    "bdecode",
+    "LibTorrentClientException",
+    "FailedToExecuteException",
+    "move_torrent",
+    "parse_libtc_url",
+]

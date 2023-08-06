e8e61fa2462469b2c73890d610facb47f7449225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:03 +0000

    Add package distributions-fun/0.1/distributions_fun-0.1.tar.gz

---
+++ packages/distributions-fun/distributions_fun-0.1.tar.gz/distributions_fun-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_fun',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Uday Gupta',
+      author_email = 'udaygupta9594@gmail.com',
+      zip_safe=False)

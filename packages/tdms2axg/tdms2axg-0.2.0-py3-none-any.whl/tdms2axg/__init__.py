bd1dddf29030ca04a0f2ce089c9d185c12100a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:14 +0000

    Add package tdms2axg/0.2.0/tdms2axg-0.2.0-py3-none-any.whl

---
+++ packages/tdms2axg/tdms2axg-0.2.0-py3-none-any.whl/tdms2axg/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+A simple script for converting LabVIEW TDMS files to AxoGraph files
+
+Usage:
+    >>> from tdms2axg import tdms2axg
+    >>> tdms2axg('my_data.tdms')
+"""
+
+from .scripts import tdms2axg

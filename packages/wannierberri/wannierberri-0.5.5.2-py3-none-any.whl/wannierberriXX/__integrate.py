08ea29e5404876196629fc17d69b45f7f4b81f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package wannierberri/0.5.5.2/wannierberri-0.5.5.2-py3-none-any.whl

---
+++ packages/wannierberri/wannierberri-0.5.5.2-py3-none-any.whl/wannierberriXX/__integrate.py
@@ -0,0 +1,196 @@
+#                                                            #
+# This file is distributed as part of the WannierBerri code  #
+# under the terms of the GNU General Public License. See the #
+# file `LICENSE' in the root directory of the WannierBerri   #
+# distribution, or http://www.gnu.org/copyleft/gpl.txt       #
+#                                                            #
+# The WannierBerri code is hosted on GitHub:                 #
+# https://github.com/stepan-tsirkin/wannier-berri            #
+#                     written by                             #
+#           Stepan Tsirkin, University of Zurich             #
+#                                                            #
+#------------------------------------------------------------
+
+import numpy as np
+from scipy import constants as constants
+from collections import Iterable,defaultdict
+from copy import copy,deepcopy
+
+from functools import partial
+
+from .__utility import  print_my_name_start,print_my_name_end,voidsmoother
+from . import __result as result
+from . import  __berry as berry
+from . import  __fermisea as fermisea
+from . import  __gyrotropic as gyrotropic
+from . import  __spin as spin
+from . import  __nonabelian as nonabelian
+from . import  __dos as dos
+from . import  __symmetry  as symmetry
+from . import  __utility   as utility
+
+#If one whants to add  new quantities to tabulate, just modify the following dictionaries
+
+#should be functions of only one parameter of class data_dk
+calculators_trans={ 
+         'spin'       : spin.calcSpinTot,  
+         'Morb'       : berry.calcMorb,
+         'Morb2'       : fermisea.calcMorb,
+         'Morb2_IC'       : partial(fermisea.calcMorb,evalLC=False,evalIC=True),
+         'Morb2_LC'       : partial(fermisea.calcMorb,evalIC=False,evalLC=True),
+         'Morb2_IC_J0'       : partial(fermisea.calcMorb,evalLC=False,evalIC=True,evalJ0=True,evalJ1=False,evalJ2=False),
+         'Morb2_LC_J0'       : partial(fermisea.calcMorb,evalIC=False,evalLC=True,evalJ0=True,evalJ1=False,evalJ2=False),
+         'Morb2_IC_J1'       : partial(fermisea.calcMorb,evalLC=False,evalIC=True,evalJ0=False,evalJ1=True,evalJ2=False),
+         'Morb2_LC_J1'       : partial(fermisea.calcMorb,evalIC=False,evalLC=True,evalJ0=False,evalJ1=True,evalJ2=False),
+         'Morb2_IC_J2'       : partial(fermisea.calcMorb,evalLC=False,evalIC=True,evalJ0=False,evalJ1=False,evalJ2=True),
+         'Morb2_LC_J2'       : partial(fermisea.calcMorb,evalIC=False,evalLC=True,evalJ0=False,evalJ1=False,evalJ2=True),
+         'Morb_LC'       : berry.calcMorb_LC,
+         'Morb_IC'       : berry.calcMorb_IC,
+         'Morb_LC_J0'       : berry.calcMorb_LC_J0,
+         'Morb_LC_J1'       : berry.calcMorb_LC_J1,
+         'Morb_LC_J2'       : berry.calcMorb_LC_J2,
+         'Morb_IC_J0'       : berry.calcMorb_IC_J0,
+         'Morb_IC_J1'       : berry.calcMorb_IC_J1,
+         'Morb_IC_J2'       : berry.calcMorb_IC_J2,
+         'ahc'        : fermisea.calcAHC ,
+         'dos'        : dos.calc_DOS ,
+         'cumdos'        : dos.calc_cum_DOS ,
+#         'nonabelian_spin' : nonabelian.spin , 
+         'nonabelian_ahc'     : nonabelian.ahc , 
+         'nonabelian_Morb' : nonabelian.Morb , 
+#         'nonabelian_spinspin' : nonabelian.spinspin , 
+#         'nonabelian_velvel' : nonabelian.velvel , 
+#         'nonabelian_spinvel' : nonabelian.spinvel , 
+#         'nonabelian_morbvel' : nonabelian.morbvel , 
+#         'nonabelian_curvvel' : nonabelian.curvvel , 
+#         'nonabelian_curvmorb' : nonabelian.curvmorb , 
+#         'nonabelian_curvspin' : nonabelian.curvspin , 
+#         'nonabelian_curv_tot' : nonabelian.curv_tot , 
+#         'nonabelian_velvel' : nonabelian.velvel , 
+         'Hall_classic' : nonabelian.Hall_classic , 
+         'Hall_classic_sea' : nonabelian.Hall_classic_sea , 
+         'Hall_morb' :  nonabelian.Hall_morb,
+         'Hall_spin' :  nonabelian.Hall_spin,
+         'conductivity_ohmic': nonabelian.conductivity_ohmic,
+         'conductivity_ohmic_sea': nonabelian.conductivity_ohmic_sea,
+         'conductivity_ohmic_sea2': fermisea.conductivity_ohmic_sea,
+         'berry_dipole'      : nonabelian.berry_dipole,
+         'berry_dipole_D'      : nonabelian.berry_dipole_D,
+         'berry_dipole_sea_D'        : fermisea.calc_dipole_D ,
+         'berry_dipole_sea_D_old'        : fermisea.calc_dipole_D_old ,
+         'gyrotropic_Korb'   : nonabelian.gyrotropic_Korb,
+         'gyrotropic_Kspin'  : nonabelian.gyrotropic_Kspin,
+
+         'berry_dipole_ext1'      : nonabelian.berry_dipole_ext1,
+         'berry_dipole_ext2'      : nonabelian.berry_dipole_ext2,
+
+         'berry_dipole_sea_ext1'        : fermisea.calc_dipole_ext_1 ,
+         'berry_dipole_sea_ext2'        : fermisea.calc_dipole_ext_2 ,
+         'berry_dipole_sea_ext2_debug'        : fermisea.calc_dipole_ext_2_debug ,
+
+         'berry_dipole_sea_ext1_1'        : fermisea.calc_dipole_ext_1_1 ,
+         'berry_dipole_sea_ext1_2'        : fermisea.calc_dipole_ext_1_2 ,
+         'berry_dipole_sea_ext2_3'        : fermisea.calc_dipole_ext_2_3 ,
+         'berry_dipole_sea_ext2_4'        : fermisea.calc_dipole_ext_2_4 ,
+         'berry_dipole_sea_ext2_5'        : fermisea.calc_dipole_ext_2_5 ,
+         'berry_dipole_sea_ext2_6'        : fermisea.calc_dipole_ext_2_6 ,
+         'orbital_mag_sea'                      : fermisea.calc_grd_orbital_mag,
+         'orbital_mag'                    : nonabelian.orbital_mag,                
+         'tensor_D'                       : fermisea.calc_GMEtensor_D ,
+         'Hplus_debug'                    : fermisea.calc_Hplus_debug,
+         }
+
+
+additional_parameters=defaultdict(lambda: defaultdict(lambda:None )   )
+additional_parameters_description=defaultdict(lambda: defaultdict(lambda:"no description" )   )
+
+
+calculators_opt={}
+
+
+
+
+calculators=copy(calculators_trans)
+calculators.update(calculators_opt)
+
+
+descriptions=defaultdict(lambda:"no description")
+descriptions['ahc']="Anomalous hall conductivity"
+descriptions['spin']="Total Spin polarization"
+descriptions['morb']="Total orbital magnetization"
+descriptions['cumdos']="Cumulative density of states"
+descriptions['dos']="density of states"
+
+
+
+# omega - for optical properties of insulators
+# Efrmi - for transport properties of (semi)conductors
+
+def intProperty(data,quantities=[],Efermi=None,omega=None,smoothers={},energies={},smootherEf=utility.voidsmoother,smootherOmega=utility.voidsmoother,parameters={}):
+
+  
+
+    def _energy(quant):
+        if quant in energies:
+            return energies[quant]
+        if quant in calculators_trans:
+            return Efermi
+        if quant in calculators_opt:
+            return omega
+        raise RuntimeError("quantity {} is neither optical nor transport, and energies are not defined".format(quant))
+
+    def _smoother(quant):
+        if quant in smoothers:
+            return smoothers[quant]
+        elif quant in calculators_trans:
+            return smootherEf
+        elif quant in calculators_opt:
+            return smootherOmega
+        else:
+            return utility.voidsmoother()
+    
+
+    results={}
+    for q in quantities:
+        __parameters={}
+        for param in additional_parameters[q]:
+            if param in parameters:
+                 __parameters[param]=parameters[param]
+            else :
+                 __parameters[param]=additional_parameters[q][param]
+        results[q]=calculators[q](data,_energy(q),**__parameters)
+        results[q].smoother=_smoother(q)
+
+    return INTresult( results=results )
+
+
+
+class INTresult(result.Result):
+
+    def __init__(self,results={}):
+        self.results=results
+            
+    def __mul__(self,other):
+        return INTresult({q:v*other for q,v in self.results.items()})
+    
+    def __add__(self,other):
+        if other == 0:
+            return self
+        results={r: self.results[r]+other.results[r] for r in self.results if r in other.results }
+        return INTresult(results=results) 
+
+    def write(self,name):
+        for q,r in self.results.items():
+            r.write(name.format(q))
+
+    def transform(self,sym):
+        results={r:self.results[r].transform(sym)  for r in self.results}
+        return INTresult(results=results)
+
+    @property
+    def max(self):
+        r= np.array([x for v in self.results.values() for x in v.max])
+#        print ("max=",r,"res=",self.results)
+        return r
+
+

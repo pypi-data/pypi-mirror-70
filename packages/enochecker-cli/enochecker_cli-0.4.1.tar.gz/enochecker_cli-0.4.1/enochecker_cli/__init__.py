76d3bbef96383ca163d4274eb3cbfbe0d5535cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:50 +0000

    Add package enochecker-cli/0.4.1/enochecker_cli-0.4.1.tar.gz

---
+++ packages/enochecker-cli/enochecker_cli-0.4.1.tar.gz/enochecker_cli-0.4.1/enochecker_cli/__init__.py
@@ -0,0 +1,1 @@
+from .base import get_parser, json_task_message_from_namespace, task_message_from_namespace
---
+++ packages/enochecker-cli/enochecker_cli-0.4.1.tar.gz/enochecker_cli-0.4.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='enochecker_cli',
+    version='0.4.1',
+    entry_points = {
+        "console_scripts": ['enochecker_cli = enochecker_cli.base:main']
+    },
+    author="Benedikt Radtke",
+    author_email="benediktradtke@gmail.com",
+    description="Enochecker CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/enowars/enochecker_cli",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )

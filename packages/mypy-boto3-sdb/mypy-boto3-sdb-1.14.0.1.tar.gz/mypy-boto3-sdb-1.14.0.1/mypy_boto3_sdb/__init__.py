10e94bf88826c7bb329e421e1e896dc80b7ea4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package mypy-boto3-sdb/1.14.0.1/mypy-boto3-sdb-1.14.0.1.tar.gz

---
+++ packages/mypy-boto3-sdb/mypy-boto3-sdb-1.14.0.1.tar.gz/mypy-boto3-sdb-1.14.0.1/mypy_boto3_sdb/__init__.py
@@ -0,0 +1,28 @@
+"""
+Main interface for sdb service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_sdb import (
+        Client,
+        ListDomainsPaginator,
+        SelectPaginator,
+        SimpleDBClient,
+    )
+
+    session = boto3.Session()
+
+    client: SimpleDBClient = boto3.client("sdb")
+    session_client: SimpleDBClient = session.client("sdb")
+
+    list_domains_paginator: ListDomainsPaginator = client.get_paginator("list_domains")
+    select_paginator: SelectPaginator = client.get_paginator("select")
+    ```
+"""
+from mypy_boto3_sdb.client import SimpleDBClient as Client, SimpleDBClient
+from mypy_boto3_sdb.paginator import ListDomainsPaginator, SelectPaginator
+
+
+__all__ = ("Client", "ListDomainsPaginator", "SelectPaginator", "SimpleDBClient")

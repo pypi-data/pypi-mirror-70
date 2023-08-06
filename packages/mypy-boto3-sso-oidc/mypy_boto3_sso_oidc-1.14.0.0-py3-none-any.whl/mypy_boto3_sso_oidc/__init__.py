fdbe09deed17ab49c4c23a8175be0f63b1d1f2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package mypy-boto3-sso-oidc/1.14.0.0/mypy_boto3_sso_oidc-1.14.0.0-py3-none-any.whl

---
+++ packages/mypy-boto3-sso-oidc/mypy_boto3_sso_oidc-1.14.0.0-py3-none-any.whl/mypy_boto3_sso_oidc/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for sso-oidc service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_sso_oidc import (
+        Client,
+        SSOOIDCClient,
+    )
+
+    session = boto3.Session()
+
+    client: SSOOIDCClient = boto3.client("sso-oidc")
+    session_client: SSOOIDCClient = session.client("sso-oidc")
+    ```
+"""
+from mypy_boto3_sso_oidc.client import SSOOIDCClient, SSOOIDCClient as Client
+
+
+__all__ = ("Client", "SSOOIDCClient")

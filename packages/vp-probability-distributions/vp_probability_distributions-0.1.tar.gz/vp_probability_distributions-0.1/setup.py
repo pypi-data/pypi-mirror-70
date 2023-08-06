38e4ee160a4a0d99373438463b85d30a8a266c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:29 +0000

    Add package vp-probability-distributions/0.1/vp_probability_distributions-0.1.tar.gz

---
+++ packages/vp-probability-distributions/vp_probability_distributions-0.1.tar.gz/vp_probability_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vp_probability_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['vp_probability_distributions'],
+      author='Victor Pizarro',
+      author_email='v.pizarro@outlook.com',
+      zip_safe=False)

3005fbcdde38288c203f2fce1bbd4b04172ca446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:36 +0000

    Add package distributions-0608-2020-nme/0.1/distributions_0608_2020_nme-0.1.tar.gz

---
+++ packages/distributions-0608-2020-nme/distributions_0608_2020_nme-0.1.tar.gz/distributions_0608_2020_nme-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_0608_2020_nme',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_0608_2020_nme'],
+      zip_safe=False)

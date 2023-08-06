3420dcfb528aa9473f6d30c32b58defaecd07737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package distributions-Antonio-M/0.1/distributions_Antonio_M-0.1.tar.gz

---
+++ packages/distributions-Antonio-M/distributions_Antonio_M-0.1.tar.gz/distributions_Antonio_M-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_Antonio_M',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Antonio Mayor',
+      zip_safe=False)

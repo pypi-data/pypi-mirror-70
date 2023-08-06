f4466633d36eebb31bb9c8798d7247717cefe16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:36 +0000

    Add package udacity-foundation-distributions/0.1/udacity_foundation_distributions-0.1.tar.gz

---
+++ packages/udacity-foundation-distributions/udacity_foundation_distributions-0.1.tar.gz/udacity_foundation_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_foundation_distributions',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['distributions'],
+      author="prashant arora",
+      author_email="prashantarora998@gmail.com",
+      zip_safe=False)

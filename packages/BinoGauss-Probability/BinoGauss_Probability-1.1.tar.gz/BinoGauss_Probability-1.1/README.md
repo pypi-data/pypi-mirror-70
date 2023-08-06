fd81768f60492cccb50ba54473aac695d8ec4104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:18 +0000

    Add package BinoGauss-Probability/1.1/BinoGauss_Probability-1.1.tar.gz

---
+++ packages/BinoGauss-Probability/BinoGauss_Probability-1.1.tar.gz/BinoGauss_Probability-1.1/README.md
@@ -0,0 +1,8 @@
+###BinoGaus_Probability##########
+
+
+Calculates Binomial and Gaussians Distributions,
+
+-mean and standard Deviations and more.
+
+try help(BinoGauss_Probability)
---
+++ packages/BinoGauss-Probability/BinoGauss_Probability-1.1.tar.gz/BinoGauss_Probability-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BinoGauss_Probability',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['BinoGauss_Probability'],
+      zip_safe=False)

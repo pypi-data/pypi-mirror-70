25fb775ff1fbf14968b813f031ae7e36c6fc7702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package PyCmdex/1.1.15/PyCmdex-1.1.15-py3-none-any.whl

---
+++ packages/PyCmdex/PyCmdex-1.1.15-py3-none-any.whl/PyCmdex/cmdex_mcu.py
@@ -0,0 +1,24 @@
+"""
+# ************************************************************
+# File:     cmdex_mcu.py
+# Version:  1.1.15 (11 Jun 2020)
+# Author:   Asst.Prof.Dr.Santi Nuratch
+#           Embedded Computing and Control Laboratory
+#           ECC-Lab, INC, KMUTT, Thailand
+# Update:   08:18:56, 11 Jun 2020
+# ************************************************************
+# 
+# 
+# Copyright 2020 Asst.Prof.Dr.Santi Nuratch
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+"""
+
+
+import zlib, base64
+exec(zlib.decompress(base64.b64decode('eJy9WFGPozYQfs+vcO8F2LIpBvUlOlatVlvpHm51ard92YuQE5xbFAIIO5eLVvvfO2MHMGCSVnt7L4k9883n8cx4DGzqckc+HW93Kf82X+NvwqpMkGxXlbW8mul/IrMdn61zJgRR0I/rvasGvwPYW8zIpz8/3D8kd//c3T/8Ff/BcsFnJOUbkiRZkckkcQXPNz5yxfdlwf0V26c1kzym9NcwCPyrq+2B1V8UFxH7iteuN2+N0a416bAIBVrA5TwNmmUNGbXIQossGsoqcRjxgWzEB7IRH8hGfCxdB/E17QnoUBAOBZEhYGmarFmer9h66zrgs+PnbLdKGam5qBbN2nW55kLgnpI8K7iLSs+zk4CjZ0lAf5kE3DxLAnobSZLUnKXHlkhVBSizDXEamUOyguhUY1VMGGrAY2e1bNb4wmUCwiRlkrlWY188lYcPxaaMH+o993TJjsx8i6udpcq0KttNWZMsBacfA5/6oR8tO7cxIUDsZql3AYlR/29IDG2HlNl6K+IARspdtSOYHJ6ynBOcKMOeTh3rucg5r1z6Cw0CTwuB6OcYKw/ToaY3cEaVPanqrJDuxvlcfy6aVrAg96VKflkITjbYUuQTJx9v/54jzPGApo2XWpxwCBo55bzxqjkzhKxAsMURbh+2iYXQpRS2rXLjaY+AHETvT/4NuUCrJXEbA4LW8TMeIuEsWl5Mkeb11dFYWFb0sd6FVfVihGcQndtyV+Vcwpm9GIlTeN91xs9I/4KE7y5aN3Hrb5qwIiX9Gv5JFXFXEd2B/h9hwUq9HBYfhZ4qS7mvdR67g9ayK9SihT0azdk3mrcxpsY4WHaUrVsTlNif/W4cGmNqjE3KdjsTlNir/W4cGmNqjBtKS58+9Rlokcgu6+PiVLzKOhOJvpvLrduCoAtUsVKLKodrUiMqVrOd0Ch9PNIYS0pUj+FSS04O7SvYEkcXoIP4hsEGbG7iQJVNlr6PIzVqKnLeu+qNU9W2BtwU/8oLCcVrpPHFN2Zhb0Z7s+DFQU/4tzWvJLlTf1lZECaIXqpZqDskHej6hjxzRdCPdHuZ/dBIyxhm0TKOVw51uujGcdOtzOcNIZWM5ycMHWHoCBOOMOEIE40wUYN5da4xrINcI3+XT/SoN6O92Zvkun3m+JG5/sryRhQtvTO5Vs+CgD6bbPV8OAKFI1A4BkUjUNSCXp1vDO0g37hAl1P0qTejvdl3yvdvkGp4R5BHnXokHvZmM9w2A3rGgNoMwjMGoc0gOmMQjQzwMEwZoM5mMLkH1NkMJveAOpvB5B5QNzLA5j1lgDowwL+54FLyemDjC8nkXr8BQh0HM+PR+VDjY+4JYXvdsFxm4zLBm+aMe1S7R4fuUZt79E3cm8wP6rR74dC90OZe+CbuTVYD6rR70dC9yOZe9B3c0z3f0Kl1GsCg4ctjdXoFzQRZHSXXr5RqmcvtXj1PQ2MP1E1ebp1Fc70r8TW9fMOrLxQXbnj1xeLCDa++YJi396jhq+8ZCvHaLgs4JsTsX8U75OU=')))

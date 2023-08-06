4ff73f82760b25f98d25bb310958c2fa2e0274d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package proofzeroclient/0.0.2/proofzeroclient-0.0.2.tar.gz

---
+++ packages/proofzeroclient/proofzeroclient-0.0.2.tar.gz/proofzeroclient-0.0.2/proofzeroclient/api/pzi_sdk.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+import re  # noqa: F401
+
+# python 2 and python 3 compatibility library
+import six
+
+from openapi_client.api_client import ApiClient
+from openapi_client.exceptions import (  # noqa: F401
+    ApiTypeError,
+    ApiValueError
+)
+
+class DefaultApi(object):
+	def isMinTrue(identity):
+
+
+	def registerPZI(self, threshold=0.8, api_key, post_identity_body, **kwargs):  # noqa: E501
+	

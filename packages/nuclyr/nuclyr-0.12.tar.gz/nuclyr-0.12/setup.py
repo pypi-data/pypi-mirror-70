621bb654c039cf61b0a2c05d6a700cbf2ed75fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:55 +0000

    Add package nuclyr/0.12/nuclyr-0.12.tar.gz

---
+++ packages/nuclyr/nuclyr-0.12.tar.gz/nuclyr-0.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='nuclyr',
+      version='0.12',
+      author='Philipp Scholz',
+      author_email='pscholz@outlook.com',
+      license='MIT',
+      packages=['nuclyr'],
+      url="https://github.com/phScholz/nuclyr",
+      description="A utility package for nuclear data.",
+      zip_safe=False,
+      install_requires=['selenium','pandas','os','time'])

b292a274e82670e09825ca79fc968ae7460fe462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:03 +0000

    Add package nester_shoudle/1.1.0/nester_shoudle-1.1.0.tar.gz

---
+++ packages/nester_shoudle/nester_shoudle-1.1.0.tar.gz/nester_shoudle-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_shoudle",
+    author = "shoudle",
+    version = "1.1.0",
+    author_email = "shihzongyue98@gmail.com",
+    py_modules = ['nester'],
+    url = "http://www.headfirstlabs.com",
+    description = "hellow",
+    )

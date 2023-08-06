ea07d5a314d77a02f97dcdc8e7e386450340262d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:21 +0000

    Add package lets-debug/0.0.1/lets-debug-0.0.1.tar.gz

---
+++ packages/lets-debug/lets-debug-0.0.1.tar.gz/lets-debug-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='lets-debug',
+    version='0.0.1',
+    url='https://github.com/luizfilipezs/lets-debug',
+    license='MIT License',
+    author='Luiz Filipe da Silva',
+    author_email='filipeluiz.bs@gmail.com',
+    keywords='debug tools terminal',
+    description=u'Useful tools for debugging Python code',
+    packages=['lets_debug'],
+    install_requires=[],
+)

5ccd90d3d5bc234faa9efdb98083c98edd3853f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:12 +0000

    Add package symjax/0.3.1/symjax-0.3.1-py3-none-any.whl

---
+++ packages/symjax/symjax-0.3.1-py3-none-any.whl/symjax/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+from .base import (
+        current_graph,
+        Graph,
+        function,
+        gradients,
+        jacobians,
+        reset,
+        save,
+        load,
+        variable,
+        placeholder,
+        op)
+
+
+from . import datasets
+from . import tensor
+from . import losses
+from . import utils
+from . import initializers
+from . import layers
+from . import optimizers
+from . import schedules
+
+
+_current_scope = '/'
+_current_graph = [Graph('')]
+_variables = {}
+_placeholders = {}
+_updates = {}
+_ops = {}

566ce25dd7a7d6a8fc531dc9a9158ac60e1ecbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:29 +0000

    Add package mkpiano/0.1.2/mkpiano-0.1.2-py3-none-any.whl

---
+++ packages/mkpiano/mkpiano-0.1.2-py3-none-any.whl/mkpiano/__init__.py
@@ -0,0 +1,33 @@
+from time import sleep
+from .boards import mbuild
+
+import sys
+import signal
+
+_ports = []
+_threads = []
+
+def add_port(port):
+    global _ports
+    _ports.append(port)
+
+def add_thread(thread):
+    global _threads
+    thread.daemon = True
+    _threads.append(thread)
+
+def quit():
+    __exiting(0,0)
+    
+def __exiting(signal, frame):
+    global _ports
+    global _threads
+    for port in _ports:
+        port.exit()
+    for thread in _threads:
+        thread.exit()
+    sys.exit(0)
+    
+signal.signal(signal.SIGINT, __exiting)
+piano = mbuild.piano
+sleep(1)

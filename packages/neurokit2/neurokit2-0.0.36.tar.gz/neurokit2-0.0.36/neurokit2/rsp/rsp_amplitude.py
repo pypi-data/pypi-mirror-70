665c27c286478c5395da626ec05e1c6c3a5d333e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:23 +0000

    Add package neurokit2/0.0.36/neurokit2-0.0.36.tar.gz

---
+++ packages/neurokit2/neurokit2-0.0.36.tar.gz/neurokit2-0.0.36/neurokit2/rsp/rsp_amplitude.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from ..signal import signal_interpolate
+from .rsp_fixpeaks import _rsp_fixpeaks_retrieve
+
+
+def rsp_amplitude(rsp_cleaned, peaks, troughs=None):
+    """Compute respiratory amplitude.
+
+    Compute respiratory amplitude given the raw respiration signal and its extrema.
+
+    Parameters
+    ----------
+    rsp_cleaned : list or array or Series
+        The cleaned respiration channel as returned by `rsp_clean()`.
+    peaks : list or array or DataFrame or Series or dict
+        The samples at which the inhalation peaks occur. If a dict or a DataFrame is passed, it is
+        assumed that these containers were obtained with `rsp_findpeaks()`.
+    troughs : list or array or DataFrame or Series or dict
+        The samples at which the inhalation troughs occur. If a dict or a DataFrame is passed, it is
+        assumed that these containers were obtained with `rsp_findpeaks()`.
+
+    Returns
+    -------
+    array
+        A vector containing the respiratory amplitude.
+
+    See Also
+    --------
+    rsp_clean, rsp_peaks, signal_rate, rsp_process, rsp_plot
+
+    Examples
+    --------
+    >>> import neurokit2 as nk
+    >>> import pandas as pd
+    >>>
+    >>> rsp = nk.rsp_simulate(duration=90, respiratory_rate=15)
+    >>> cleaned = nk.rsp_clean(rsp, sampling_rate=1000)
+    >>> info, signals = nk.rsp_peaks(cleaned)
+    >>>
+    >>> amplitude = nk.rsp_amplitude(cleaned, signals)
+    >>> fig = nk.signal_plot(pd.DataFrame({"RSP": rsp, "Amplitude": amplitude}), subplots=True)
+    >>> fig #doctest: +SKIP
+
+    """
+    # Format input.
+    peaks, troughs, desired_length = _rsp_fixpeaks_retrieve(peaks, troughs, len(rsp_cleaned))
+
+    # To consistenty calculate amplitude, peaks and troughs must have the same
+    # number of elements, and the first trough must precede the first peak.
+    if (peaks.size != troughs.size) or (peaks[0] <= troughs[0]):
+        raise TypeError(
+            "NeuroKit error: Please provide one of the containers ",
+            "returned by `rsp_findpeaks()` as `extrema` argument and do ",
+            "not modify its content.",
+        )
+
+    # Calculate amplitude in units of the raw signal, based on vertical
+    # difference of each peak to the preceding trough.
+    amplitude = rsp_cleaned[peaks] - rsp_cleaned[troughs]
+
+    # Interpolate amplitude to desired_length samples.
+    amplitude = signal_interpolate(peaks, amplitude, desired_length=desired_length)
+
+    return amplitude

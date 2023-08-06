e300eb7a6f909b4964829d5db095914ca3b65f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:30 +0000

    Add package kuriyama/0.3.6/kuriyama-0.3.6.tar.gz

---
+++ packages/kuriyama/kuriyama-0.3.6.tar.gz/kuriyama-0.3.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kuriyama",
+    version='0.3.6',
+    description='A framework for OICQ(QQ, made by Tencent) headless client "Mirai".',
+    author='Chenwe-i-lin',
+    author_email="Chenwe_i_lin@outlook.com",
+    url="https://github.com/NatriumLab/python-mirai",
+    packages=find_packages(include=("mirai", "mirai.*")),
+    python_requires='>=3.7',
+    keywords=["oicq qq qqbot", ],
+    install_requires=[
+        "aiohttp",
+        "pydantic",
+        "Logbook",
+        "async_lru"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent"
+    ]
+)

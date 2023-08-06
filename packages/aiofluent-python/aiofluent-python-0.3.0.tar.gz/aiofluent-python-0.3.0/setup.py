b0d98df53c58b7b57043e6075773bd445b06378b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package aiofluent-python/0.3.0/aiofluent-python-0.3.0.tar.gz

---
+++ packages/aiofluent-python/aiofluent-python-0.3.0.tar.gz/aiofluent-python-0.3.0/setup.py
@@ -0,0 +1,50 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os.path
+import re
+
+VERSION_RE = re.compile(r"""__version__ = ['"]([0-9.]+)['"]""")
+BASE_PATH = os.path.dirname(__file__)
+
+
+with open(os.path.join(BASE_PATH, "aiofluent", "__init__.py")) as f:
+    try:
+        version = VERSION_RE.search(f.read()).group(1)
+    except IndexError:
+        raise RuntimeError("Unable to determine version.")
+
+
+with open(os.path.join(BASE_PATH, "README.md")) as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="aiofluent-python",
+    description="asynchronous fluentd client libary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    version=version,
+    author="Yingbo Gu",
+    author_email="tensiongyb@gmail.com",
+    maintainer="Yingbo Gu",
+    maintainer_email="tensiongyb@gmail.com",
+    url="https://github.com/guyingbo/aiofluent",
+    packages=["aiofluent"],
+    python_requires=">=3.5.3",
+    install_requires=["msgpack>=0.5.2", "async_timeout>=2.0.0"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "coverage", "pytest-cov", "pytest-asyncio"],
+)

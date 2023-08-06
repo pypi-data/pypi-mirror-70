75ff4b6a47ab1b4c945090402439de97229f3b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:15 +0000

    Add package sphinxawesome-theme/1.6.0/sphinxawesome_theme-1.6.0-py3-none-any.whl

---
+++ packages/sphinxawesome-theme/sphinxawesome_theme-1.6.0-py3-none-any.whl/sphinxawesome_theme/__init__.py
@@ -0,0 +1,25 @@
+"""
+The Sphinx awesome theme as a Python package.
+
+:copyright: Copyright 2020, Kai Welke.
+:license: MIT, see LICENSE for details
+"""
+
+from os import path
+from typing import Any, Dict
+
+from sphinx.application import Sphinx
+
+__version__ = "1.6.0"
+
+
+def setup(app: "Sphinx") -> Dict[str, Any]:
+    """Register the theme."""
+
+    app.add_html_theme("sphinxawesome_theme", path.abspath(path.dirname(__file__)))
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }

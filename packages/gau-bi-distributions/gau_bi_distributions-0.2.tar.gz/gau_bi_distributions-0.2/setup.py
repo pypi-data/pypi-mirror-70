b1956af9ea66f7d86655cb6ee97a68c994095ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:12 +0000

    Add package gau-bi-distributions/0.2/gau_bi_distributions-0.2.tar.gz

---
+++ packages/gau-bi-distributions/gau_bi_distributions-0.2.tar.gz/gau_bi_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'gau_bi_distributions',
+      version = '0.2',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['gau_bi_distributions'],
+      author = 'Morufdeen Atilola',
+      author_email = 'atilolamorufdeen@gmail.com',
+      zip_safe = False)

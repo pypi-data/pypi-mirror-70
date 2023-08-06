2bcdb4a5ab71a94dae69a5e23ca5dcff018d1481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package easy-captcha/1.3.1/easy_captcha-1.3.1-py3-none-any.whl

---
+++ packages/easy-captcha/easy_captcha-1.3.1-py3-none-any.whl/captcha/__init__.py
@@ -0,0 +1,21 @@
+# PROJECT : easy-captcha
+# FILE : __init__.py
+# TIME : 2018-11-17 14:58:14
+# AUTHOR : Younger Shen
+# EMAIL : youngershen64@gmail.com
+# CELL : +8613811754531
+# WECHAT : 13811754531
+
+from .generator import BaseGenerator, \
+                       SimpleGenerator, \
+                       DefaultGenerator, \
+                       SimpleChineseGenerator, \
+                       ContortGenerator
+
+__all__ = [
+    'BaseGenerator',
+    'SimpleGenerator',
+    'DefaultGenerator',
+    'SimpleChineseGenerator',
+    'ContortGenerator'
+]

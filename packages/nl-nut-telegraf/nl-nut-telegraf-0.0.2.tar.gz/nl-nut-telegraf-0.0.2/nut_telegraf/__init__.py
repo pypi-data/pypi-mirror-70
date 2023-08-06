46abe820b30ef69a06e82f9ed90d7dcbc9325170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:40 +0000

    Add package nl-nut-telegraf/0.0.2/nl-nut-telegraf-0.0.2.tar.gz

---
+++ packages/nl-nut-telegraf/nl-nut-telegraf-0.0.2.tar.gz/nl-nut-telegraf-0.0.2/nut_telegraf/__init__.py
@@ -0,0 +1,7 @@
+import json
+# from .ups import NUTExporter
+#
+#
+# def main():
+#     m = NUTExporter.get_metrics(host="watchdog.lan", port=3493, ups_name="mains")
+#     print(json.dumps(m, indent=2))

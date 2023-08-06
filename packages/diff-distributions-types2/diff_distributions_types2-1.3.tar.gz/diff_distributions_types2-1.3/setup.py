78bb1c434ed5263adbfe39412ffe78481f87d186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:26 +0000

    Add package diff-distributions-types2/1.3/diff_distributions_types2-1.3.tar.gz

---
+++ packages/diff-distributions-types2/diff_distributions_types2-1.3.tar.gz/diff_distributions_types2-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='diff_distributions_types2',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['diff_distributions_types2'],
+      author='Vishakha Tandon',
+      author_email='garitandon94@gmail.com',
+      zip_safe=False)

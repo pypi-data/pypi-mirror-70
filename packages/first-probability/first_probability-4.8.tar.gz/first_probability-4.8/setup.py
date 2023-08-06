010929f91dca01df71e8d87fa4bcc2ba03e8194d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:50 +0000

    Add package first-probability/4.8/first_probability-4.8.tar.gz

---
+++ packages/first-probability/first_probability-4.8.tar.gz/first_probability-4.8/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='first_probability',
+      version='4.8',
+      description='Gaussian distributions',
+      packages=['first_probability'],
+      zip_safe=False)

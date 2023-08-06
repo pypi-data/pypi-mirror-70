0241ffd31142601ef7ff9f96ee891fa4dc5bc59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:50 +0000

    Add package shd-distributions/0.1/shd_distributions-0.1.tar.gz

---
+++ packages/shd-distributions/shd_distributions-0.1.tar.gz/shd_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['shd_distributions'],
+      zip_safe=False)
+

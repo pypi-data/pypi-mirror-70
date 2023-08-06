9a35153add2ec3d85568b91b20a7622dae142f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package probability-distributions-may2020/1.2/probability_distributions_may2020-1.2.tar.gz

---
+++ packages/probability-distributions-may2020/probability_distributions_may2020-1.2.tar.gz/probability_distributions_may2020-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probability_distributions_may2020',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_may2020'],
+      author = 'Aayush Patni' ,
+      zip_safe=False)

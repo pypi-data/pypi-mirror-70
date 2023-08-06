bbeff03cb6c574a7885c997b57cdaf822aae4549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:12 +0000

    Add package lete-probability-distributions/1.1/lete_probability_distributions-1.1.tar.gz

---
+++ packages/lete-probability-distributions/lete_probability_distributions-1.1.tar.gz/lete_probability_distributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='lete_probability_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['lete_probability_distributions'],
+      author_email= 'lete.nwibasi@gmail.com',
+      zip_safe=False)

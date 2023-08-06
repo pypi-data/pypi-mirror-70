02c89c7bcea4d0667b6fb5be431b00bd896e7685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:13 +0000

    Add package scrapeGG/0.0.5/scrapeGG-0.0.5-py2.py3-none-any.whl

---
+++ packages/scrapeGG/scrapeGG-0.0.5-py2.py3-none-any.whl/scrapeGG.py
@@ -0,0 +1,71 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+
+from profile_detail import profile
+from match_detail import match
+
+class scrapeGG:
+    def __init__(self, summonerName):
+        self.driver = self.initDriver(summonerName) # source if want to write custom functions
+        self.summonerName = summonerName
+
+    # import from init file
+    def initDriver(self, summonerName):
+        # get initial html
+        # driverpath = os.path.realpath(r'drivers/chromedriver')
+        driverpath = '/usr/local/bin/chromedriver'
+        chrome_options = Options()  
+        chrome_options.add_argument("--headless")  
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        # driver = webdriver.Chrome(driverpath, options=chrome_options)
+        driver.get('https://na.op.gg/summoner/userName=' + summonerName)
+
+        return driver
+
+    def getProfile(self):
+        return profile(self.driver)
+    
+    # get data for user-defined amount of matches (count)
+    def getMatchSequence(self, count):
+        expansions = 0
+        while (count > 10):
+            self.showMoreMatches()
+            print('click expansion', count)
+            count -= 10
+            expansions += 1
+        res = []
+        for match_play in self.driver.find_elements_by_class_name("GameItemWrap"):
+            res.append(match(match_play, self.summonerName))
+            if len(res) == count + (10 * expansions):
+                return res
+    # expand match list (use in getMatchSequence, getMatch)
+    def showMoreMatches(self):
+        # find number initial elements
+        a = self.driver.find_elements_by_class_name("GameItemWrap")
+
+        link = self.driver.find_element_by_link_text('Show More')
+        link.click()
+
+        WebDriverWait(self.driver, 10).until(
+            lambda wd: len(a) < len(self.driver.find_elements_by_class_name("GameItemWrap"))
+        )
+
+    # get individual match, x most recent
+    def getMatch(self, count):
+        expansions = 0
+        while (count > 10):
+            self.showMoreMatches()
+            print('click expansion', count)
+            count -= 10
+            expansions += 1
+        return match(self.driver.find_elements_by_class_name("GameItemWrap")[count], self.summonerName)
+    
+    # close driver
+    def quit(self):
+        self.driver.quit()
+        return True

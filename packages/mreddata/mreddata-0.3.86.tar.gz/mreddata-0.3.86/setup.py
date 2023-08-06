da100ca3d90efa0ea4b2d4cdd001d386c0db020e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:14 +0000

    Add package mreddata/0.3.86/mreddata-0.3.86.tar.gz

---
+++ packages/mreddata/mreddata-0.3.86.tar.gz/mreddata-0.3.86/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="mreddata", # Replace with your own username
+    version="0.3.86",
+    author="Matthew Breeding",
+    author_email="matthew.l.breeding@vanderbilt.edu",
+    description="A colletion of data and visualization tools for MRED/HDF5",
+    long_description="Visit github page at https://github.com/matthew-breeding/mreddata for full details",
+    long_description_content_type="text/markdown",
+    url="https://github.com/matthew-breeding/mreddata",
+    download_url='https://pypi.org/project/mreddata',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+	'h5py>=2.10.0', 
+	'matplotlib>=3.1.1', 
+	'numpy>=1.17.1', 
+	'pandas>=0.25.1'
+    ]
+)

4efe7ea914bec25dbe364291821342cf70fc4fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package zama/0.0.1/zama-0.0.1-py3-none-any.whl

---
+++ packages/zama/zama-0.0.1-py3-none-any.whl/zama/file.py
@@ -0,0 +1,8 @@
+""" This is a preliminary version of zama package. The zama package supports
+homomorphic execution on the local machine using the appropriate version of
+the zama sdk """
+
+
+def hello_world():
+    """ One test function """
+    print("Hello FHE (execution) world!")

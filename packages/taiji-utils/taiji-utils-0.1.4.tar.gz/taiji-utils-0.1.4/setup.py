e86fda9444e490234a2600c3bb5ba58348444265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:45 +0000

    Add package taiji-utils/0.1.4/taiji-utils-0.1.4.tar.gz

---
+++ packages/taiji-utils/taiji-utils-0.1.4.tar.gz/taiji-utils-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='taiji-utils',
+      version='0.1.4',
+      description='Taiji pipeline',
+      url='https://taiji-pipeline.github.io/', 
+      author='Kai Zhang',
+      author_email='kai@kzhang.org',
+      license='MIT',
+      packages=['taiji_utils'],
+      entry_points = {
+        'console_scripts': ['taiji-utils=taiji_utils.__init__:main'],
+      },
+      install_requires=[
+          'scipy',
+          'numpy',
+          'scikit-learn',
+          'python-igraph',
+          'statsmodels',
+          'umap-learn',
+          'leidenalg',
+          'scrublet',
+      ],
+      zip_safe=False)

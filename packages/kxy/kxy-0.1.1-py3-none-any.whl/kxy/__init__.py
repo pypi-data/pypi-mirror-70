cd9bdd11cd0f47ab87c7110b8de50b6357f6b3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:28 +0000

    Add package kxy/0.1.1/kxy-0.1.1-py3-none-any.whl

---
+++ packages/kxy/kxy-0.1.1-py3-none-any.whl/kxy/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Dr. Yves-Laurent Kom Samo"
+__copyright__ = "Copyright (C) 2020 KXY Technologies, Inc."
+__license__ = "AGPL v3"
+__version__ = "0.0.18"
+
+from kxy.api import *
+from kxy.regression import *
+from kxy.classification import *
+from kxy.asset_management import *
+from kxy.data import *
---
+++ packages/kxy/kxy-0.1.1-py3-none-any.whl/kxy/api/core/__init__.py
@@ -0,0 +1,5 @@
+from .utils import *
+from .entropy import *
+from .mutual_information import *
+from .entropy_rate import *
+from .mutual_information_rate import *

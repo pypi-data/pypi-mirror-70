064e03c1ca04069cc1a2321fd4618eb05775311a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package diff-distributions-types/1.3/diff_distributions_types-1.3.tar.gz

---
+++ packages/diff-distributions-types/diff_distributions_types-1.3.tar.gz/diff_distributions_types-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='diff_distributions_types',
+      version='1.3',
+      description='Gaussian distributions',
+      packages=['diff_distributions_types'],
+      author='Vishakha Tandon',
+      author_email='garitandon94@gmail.com',
+      zip_safe=False)

a623e86d7b2b6b3bffaeff54cb607161838c9184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:21 +0000

    Add package distributions-fou-foo/0.1/distributions_fou_foo-0.1.tar.gz

---
+++ packages/distributions-fou-foo/distributions_fou_foo-0.1.tar.gz/distributions_fou_foo-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_fou_foo',
+      version='0.01',
+      description='udacity-exercise',
+      packages=['distributions_fou_foo'],
+      author = 'J. Antonio García',
+      author_email='jose.ramirez@cimat.mx',
+      zip_safe=False)

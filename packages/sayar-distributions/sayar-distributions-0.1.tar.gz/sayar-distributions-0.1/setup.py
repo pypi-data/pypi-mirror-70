4a634834e0586d9f69448cb4f2eec9f1bd12d3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:23 +0000

    Add package sayar-distributions/0.1/sayar-distributions-0.1.tar.gz

---
+++ packages/sayar-distributions/sayar-distributions-0.1.tar.gz/sayar-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sayar-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sayar-distributions'],
+      author="Sayar Banerjee",
+      zip_safe=False)

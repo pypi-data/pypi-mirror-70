7ca89a386164f95226a72f098af4c48cc8a0ec49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:38 +0000

    Add package notipy-osx/0.0.1-dev/notipy_osx-0.0.1-dev.tar.gz

---
+++ packages/notipy-osx/notipy_osx-0.0.1-dev.tar.gz/notipy_osx-0.0.1-dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+  name='notipy_osx',
+  version='0.0.1-dev',
+  packages=[],
+  license='MIT',
+  long_description='Display native customizable Mac OS dialogs and notifications with ease'
+) 

7e1dd38c50a0e2b9e0a248c621c4b3673fcd14a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package gaussbino/0.1/gaussbino-0.1.tar.gz

---
+++ packages/gaussbino/gaussbino-0.1.tar.gz/gaussbino-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussbino',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaussbino'],
+      author = 'Piyush Kumar',
+      author_email = '',
+      zip_safe=False)

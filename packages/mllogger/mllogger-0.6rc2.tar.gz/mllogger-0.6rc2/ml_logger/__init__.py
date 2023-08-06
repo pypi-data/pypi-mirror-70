c367debab10a50d75475ea0a0971250fb7d924c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package mllogger/0.6rc2/mllogger-0.6rc2.tar.gz

---
+++ packages/mllogger/mllogger-0.6rc2.tar.gz/mllogger-0.6rc2/ml_logger/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.6rc1"
+
+__all__ = [
+    "__version__",
+]
---
+++ packages/mllogger/mllogger-0.6rc2.tar.gz/mllogger-0.6rc2/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def parse_dependency(filepath):
+    return [
+        dependency
+        for dependency in open("requirements/dev.txt").read().splitlines()
+        if "==" in dependency
+    ]
+
+
+base_requirements = parse_dependency("requirements/filesystem.txt")
+all_requirements = base_requirements + parse_dependency("requirements/all.txt")
+dev_requirements = all_requirements + parse_dependency("requirements/dev.txt")
+
+setuptools.setup(
+    name="mllogger",
+    version="0.6rc2",
+    author="Shagun Sodhani",
+    author_email="sshagunsodhani@gmail.com",
+    description="Logging Utility for ML Experiments",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    # Install the basic setup (without wandb, tensorboardX and mlflow) with
+    install_requires=base_requirements,
+    url="https://github.com/shagunsodhani/ml-logger",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests", "docs", "docsrc"]
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    extras_require={
+        # Install development dependencies with
+        # pip install -e .[dev]
+        "dev": dev_requirements,
+        # Install the complete setup (wandb, mlflow and tensorboardX)
+        # pip install .[all]
+        "all": all_requirements,
+    },
+)

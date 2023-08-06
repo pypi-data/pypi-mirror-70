b8785ca516d36ec17c3addd8355f9298ecda41c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:18 +0000

    Add package flowpy/0.4.1/flowpy-0.4.1.tar.gz

---
+++ packages/flowpy/flowpy-0.4.1.tar.gz/flowpy-0.4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(name='flowpy',
+      version='0.4.1',
+      description='Tools for working with optical flow',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/mickaelseznec/flowpy',
+      author='Mickaël Seznec',
+      author_email='flowpy@seznec.xyz',
+      license='MIT',
+      packages=['flowpy'],
+      install_requires=[
+          'matplotlib',
+          'numpy',
+          'pypng',
+          'scipy',
+      ],
+      test_requires=[
+          'PIL',
+      ],
+      zip_safe=False)

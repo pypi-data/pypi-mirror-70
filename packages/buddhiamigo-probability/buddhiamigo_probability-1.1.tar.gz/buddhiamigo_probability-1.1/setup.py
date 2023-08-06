2fcff418ff3d0b52d67565eb3d27df1bd9e4d722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:50 +0000

    Add package buddhiamigo-probability/1.1/buddhiamigo_probability-1.1.tar.gz

---
+++ packages/buddhiamigo-probability/buddhiamigo_probability-1.1.tar.gz/buddhiamigo_probability-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='buddhiamigo_probability',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['buddhiamigo_probability'],
+      zip_safe=False)

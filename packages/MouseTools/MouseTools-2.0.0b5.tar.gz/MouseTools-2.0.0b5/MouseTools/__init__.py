b3ff9fd25c12b46c82f3b6737824113e762b6a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:41 +0000

    Add package MouseTools/2.0.0b5/MouseTools-2.0.0b5.tar.gz

---
+++ packages/MouseTools/MouseTools-2.0.0b5.tar.gz/MouseTools-2.0.0b5/MouseTools/__init__.py
@@ -0,0 +1,16 @@
+from MouseTools.parks import *
+from MouseTools.entertainmentvenues import *
+from MouseTools.attractions import *
+from MouseTools.entertainments import *
+from MouseTools.characters import *
+from MouseTools.facilities import *
+from MouseTools.pointsofinterest import *
+from MouseTools.destinations import *
+from MouseTools.database import *
+
+
+name = "MouseTools"
+
+__version__ = "2.0.0b5"
+
+__all__ = ["Destination", "Park", "EntertainmentVenue", "Attraction", "Entertainment", "Facility", "Character", "PointOfInterest"]

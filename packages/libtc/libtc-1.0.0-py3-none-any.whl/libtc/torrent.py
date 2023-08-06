af1c319e419f3a4c0e736ea22a17f1f0504e8eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:48 +0000

    Add package libtc/1.0.0/libtc-1.0.0-py3-none-any.whl

---
+++ packages/libtc/libtc-1.0.0-py3-none-any.whl/libtc/torrent.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+import pytz
+
+
+class TorrentData:
+    def __init__(
+        self,
+        infohash,
+        name,
+        size,
+        state,
+        progress,
+        uploaded,
+        added,
+        tracker,
+        upload_rate,
+        download_rate,
+        label,
+    ):
+        self.infohash = infohash
+        self.name = name
+        self.size = size
+        self.state = state
+        self.progress = progress
+        self.uploaded = uploaded
+        self.added = added
+        self.tracker = tracker
+        self.upload_rate = upload_rate
+        self.download_rate = download_rate
+        self.label = label
+
+    def __repr__(self):
+        return f"TorrentData(infohash={self.infohash!r}, name={self.name!r})"
+
+    def serialize(self):
+        data = dict(self.__dict__)
+        data["added"] = data["added"].isoformat().split(".")[0].split("+")[0]
+        return data
+
+    @classmethod
+    def unserialize(cls, data):
+        data = dict(data)
+        data["added"] = datetime.strptime(data["added"], "%Y-%m-%dT%H:%M:%S").replace(
+            tzinfo=pytz.UTC
+        )
+        return cls(**data)
+
+
+class TorrentState:
+    ACTIVE = "active"
+    STOPPED = "stopped"
+    ERROR = "error"

74545312384f505e06a01b474029e9cf5c99d78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:40 +0000

    Add package discopy/0.2.5/discopy-0.2.5.tar.gz

---
+++ packages/discopy/discopy-0.2.5.tar.gz/discopy-0.2.5/setup.py
@@ -0,0 +1,36 @@
+"""
+Setup discopy package.
+"""
+
+if __name__ == '__main__':  # pragma: no cover
+    from re import search, M
+    from setuptools import setup
+
+    with open('discopy/__init__.py', 'r') as file:
+        MATCH = search(r"^__version__ = ['\"]([^'\"]*)['\"]", file.read(), M)
+        if MATCH:
+            VERSION = MATCH.group(1)
+        else:
+            raise RuntimeError("Unable to find version string.")
+
+    with open('tests/requirements.txt', 'r') as file:
+        TEST_REQ = [line.strip() for line in file.readlines()]
+
+    setup(name='discopy',
+          version=VERSION,
+          package_dir={'discopy': 'discopy'},
+          packages=['discopy'],
+          description='Distributional Compositional Python',
+          long_description=open("README.md", "r").read(),
+          long_description_content_type="text/markdown",
+          url='https://github.com/oxford-quantum-group/discopy',
+          author='Alexis Toumi',
+          author_email='alexis.toumi@cs.ox.ac.uk',
+          download_url='https://github.com/'
+                       'oxford-quantum-group/discopy/archive/'
+                       '{}.tar.gz'.format(VERSION),
+          install_requires=[
+              l.strip() for l in open('requirements.txt').readlines()],
+          tests_require=TEST_REQ,
+          extras_require={'test': TEST_REQ}
+          )

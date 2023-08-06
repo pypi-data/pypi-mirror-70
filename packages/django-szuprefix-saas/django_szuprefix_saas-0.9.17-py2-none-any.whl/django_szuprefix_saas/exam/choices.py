3fcf2202f36975b31a9b26be77edf2df040caf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:14 +0000

    Add package django-szuprefix-saas/0.9.17/django_szuprefix_saas-0.9.17-py2-none-any.whl

---
+++ packages/django-szuprefix-saas/django_szuprefix_saas-0.9.17-py2-none-any.whl/django_szuprefix_saas/exam/choices.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*- 
+# author = 'denishuang'
+from __future__ import unicode_literals
+
+TYPE_EXERCISE = 1
+TYPE_QUIZ = 2
+
+CHOICES_TYPE = (
+    (TYPE_EXERCISE, '练习'),
+    (TYPE_QUIZ, '测验')
+)

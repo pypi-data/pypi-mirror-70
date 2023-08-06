51121c120badcfe2f7c52d3d7a62526aca675ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:48 +0000

    Add package sivtools/0.1.0/sivtools-0.1.0-py3-none-any.whl

---
+++ packages/sivtools/sivtools-0.1.0-py3-none-any.whl/sivtools/utilities/logging.py
@@ -0,0 +1,27 @@
+import logging
+
+
+DEFAULT_FORMAT_STRING = "[%(levelname)s] %(name)s:%(lineno)s (%(asctime)s) %(message)s"
+
+
+def siv_logger(
+    name="app",
+    *,
+    format_str=DEFAULT_FORMAT_STRING,
+    level=logging.DEBUG,
+    handler=logging.StreamHandler
+):
+    """
+    Wrapper for Standard Library logger with sane defaults
+    """
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    formatter = logging.Formatter(format_str)
+
+    ch = handler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger

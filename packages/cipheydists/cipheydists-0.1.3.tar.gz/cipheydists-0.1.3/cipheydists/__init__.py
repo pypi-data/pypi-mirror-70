d41d5cca8eed586f7d02d89ba3b1abfa9013e331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package cipheydists/0.1.3/cipheydists-0.1.3.tar.gz

---
+++ packages/cipheydists/cipheydists-0.1.3.tar.gz/cipheydists-0.1.3/cipheydists/__init__.py
@@ -0,0 +1,18 @@
+import json
+import os
+import pkgutil
+
+def check_name(name: str):
+  return '/' not in name and '\\' not in name
+
+def get_dist(name: str):
+  if check_name(name):
+    return json.loads(pkgutil.get_data(__name__, os.path.join("dist", name) + ".json"))
+
+def get_charset(name: str):
+  if check_name(name):
+    return json.loads(pkgutil.get_data(__name__, os.path.join("charset", name) + ".json"))
+
+def get_list(name: str):
+  if check_name(name):
+    return json.loads(pkgutil.get_data(__name__, os.path.join("list", name) + ".json"))

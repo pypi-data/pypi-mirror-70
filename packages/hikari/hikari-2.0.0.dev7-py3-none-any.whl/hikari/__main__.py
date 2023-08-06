7b4b0e33316909ef892881343ca30006c8b8c5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package hikari/2.0.0.dev7/hikari-2.0.0.dev7-py3-none-any.whl

---
+++ packages/hikari/hikari-2.0.0.dev7-py3-none-any.whl/hikari/__main__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright © Nekokatt 2019-2020
+#
+# This file is part of Hikari.
+#
+# Hikari is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hikari is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Hikari. If not, see <https://www.gnu.org/licenses/>.
+"""Provides a command-line entry point that shows the library version and then exits."""
+
+from __future__ import annotations
+
+import inspect
+import os
+import platform
+
+from hikari import _about
+
+version = _about.__version__
+# noinspection PyTypeChecker
+path = os.path.abspath(os.path.dirname(inspect.getsourcefile(_about)))
+py_impl = platform.python_implementation()
+py_ver = platform.python_version()
+py_compiler = platform.python_compiler()
+print(f"hikari v{version} (installed in {path}) ({py_impl} {py_ver} {py_compiler})")

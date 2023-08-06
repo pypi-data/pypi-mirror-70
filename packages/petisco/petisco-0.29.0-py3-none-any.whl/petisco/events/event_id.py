1972e212c475a4ab8476b58653d7d8d1bd918a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:29 +0000

    Add package petisco/0.29.0/petisco-0.29.0-py3-none-any.whl

---
+++ packages/petisco/petisco-0.29.0-py3-none-any.whl/petisco/events/event_id.py
@@ -0,0 +1,22 @@
+import uuid
+
+EVENT_ID_LENGTH = 36
+
+
+class EventId(str):
+    def __init__(self, value):
+        super(EventId, self).__init__()
+        self.value = value
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __repr__(self):
+        return f"{self.value}"
+
+    @staticmethod
+    def generate():
+        return EventId(str(uuid.uuid4()))

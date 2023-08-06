c5e77ea5646b4495430b0103a6a8b7d02abe1780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:38 +0000

    Add package flifile/1.0.2/flifile-1.0.2.tar.gz

---
+++ packages/flifile/flifile-1.0.2.tar.gz/flifile-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flifile",
+    version="1.0.2",
+    author="Rolf Harkes",
+    author_email="rolf@harkes.nu",
+    description="A reader for lambert instruments .fli files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rharkes/flifile",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

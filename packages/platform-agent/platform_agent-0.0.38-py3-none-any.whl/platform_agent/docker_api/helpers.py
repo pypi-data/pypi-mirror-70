8f13eb0139c2bf335f5906baed1e4dd833b8b61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package platform-agent/0.0.38/platform_agent-0.0.38-py3-none-any.whl

---
+++ packages/platform-agent/platform_agent-0.0.38-py3-none-any.whl/platform_agent/docker_api/helpers.py
@@ -0,0 +1,18 @@
+def format_networks_result(networks):
+    result = []
+    for network in networks:
+        if network.get('Name') == 'bridge':
+            continue
+        subnets = []
+        for subnet in network['IPAM']['Config']:
+            subnets.append(subnet['Subnet'])
+        if subnets:
+            result.append(
+                {
+                    'docker_network_id': network['Id'],
+                    'docker_network_name': network.get('Name'),
+                    'docker_network_subnets': subnets
+
+                }
+            )
+    return result

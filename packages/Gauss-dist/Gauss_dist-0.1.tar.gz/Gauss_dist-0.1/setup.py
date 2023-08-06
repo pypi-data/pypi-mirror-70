8623fe31e22f26d57006156a4fee5de727a17b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:26 +0000

    Add package Gauss-dist/0.1/Gauss_dist-0.1.tar.gz

---
+++ packages/Gauss-dist/Gauss_dist-0.1.tar.gz/Gauss_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gauss_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Gauss_dist'],
+      author = 'Vardan Sharma',
+      author_email = 'vardans6@gmail.com',
+      zip_safe=False)

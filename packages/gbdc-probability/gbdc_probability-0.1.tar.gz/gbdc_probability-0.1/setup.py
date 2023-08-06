29f6c18edb8424f7c4dd3d8bc2eb739f5b06face
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:31 +0000

    Add package gbdc-probability/0.1/gbdc_probability-0.1.tar.gz

---
+++ packages/gbdc-probability/gbdc_probability-0.1.tar.gz/gbdc_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gbdc_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gbdc_probability'],
+      author = 'Adeshola Afolabi',
+      zip_safe=False)

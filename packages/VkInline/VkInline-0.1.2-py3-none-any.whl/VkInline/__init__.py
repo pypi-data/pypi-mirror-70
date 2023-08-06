19fbb06ed5e42f664df85463a7c761ee8b11914a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:44 +0000

    Add package VkInline/0.1.2/VkInline-0.1.2-py3-none-any.whl

---
+++ packages/VkInline/VkInline-0.1.2-py3-none-any.whl/VkInline/__init__.py
@@ -0,0 +1,8 @@
+from .Native import native
+if native.n_vkinline_try_init()==0:
+	raise ImportError('cannot import VkInline')
+
+from .Context import *
+from .ShaderViewable import *
+from .SVVector import *
+from .SVObjVector import *

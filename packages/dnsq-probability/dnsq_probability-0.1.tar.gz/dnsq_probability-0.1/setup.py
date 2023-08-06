27fa4a63a39911dc6088c71819a0f01a9ffda89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:47 +0000

    Add package dnsq-probability/0.1/dnsq_probability-0.1.tar.gz

---
+++ packages/dnsq-probability/dnsq_probability-0.1.tar.gz/dnsq_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dnsq_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dnsq_probability'],
+      author='Nikolas Souza de Queiroz',
+      author_email='nikolasdequeiroz@gmail.com',
+      zip_safe=False)

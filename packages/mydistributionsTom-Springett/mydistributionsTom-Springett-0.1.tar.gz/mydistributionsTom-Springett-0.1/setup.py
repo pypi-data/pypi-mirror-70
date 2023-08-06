96400b683f7a69164ee150b4af93f95c9ef9ed6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:37 +0000

    Add package mydistributionsTom-Springett/0.1/mydistributionsTom-Springett-0.1.tar.gz

---
+++ packages/mydistributionsTom-Springett/mydistributionsTom-Springett-0.1.tar.gz/mydistributionsTom-Springett-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mydistributionsTom-Springett',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mydistributions'],
+      zip_safe=False)

9a71742959b05e7b8e3207661a034ecc4576b307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:13 +0000

    Add package wdhtools/1.0.5/wdhtools-1.0.5.tar.gz

---
+++ packages/wdhtools/wdhtools-1.0.5.tar.gz/wdhtools-1.0.5/wdhtools/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# @Author: dehong
+# @Date: 2020-06-04
+# @Time: 17:17
+# @Name: __init__.py
+
+from .hello import *
+from .data_tools import *
+from .sklearn_tools import *
+
+name = 'wdhtools'
---
+++ packages/wdhtools/wdhtools-1.0.5.tar.gz/wdhtools-1.0.5/wdhtools/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# @Author: dehong
+# @Date: 2020-06-04
+# @Time: 17:17
+# @Name: hello
+
+
+def hello():
+    return 'hello word'
+
+b = hello()

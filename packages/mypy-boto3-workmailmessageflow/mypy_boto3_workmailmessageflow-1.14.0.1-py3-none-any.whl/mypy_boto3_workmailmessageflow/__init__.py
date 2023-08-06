65feb070f0f7c106067ff515ffe5eaf06bdb1117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:55 +0000

    Add package mypy-boto3-workmailmessageflow/1.14.0.1/mypy_boto3_workmailmessageflow-1.14.0.1-py3-none-any.whl

---
+++ packages/mypy-boto3-workmailmessageflow/mypy_boto3_workmailmessageflow-1.14.0.1-py3-none-any.whl/mypy_boto3_workmailmessageflow/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for workmailmessageflow service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_workmailmessageflow import (
+        Client,
+        WorkMailMessageFlowClient,
+    )
+
+    session = boto3.Session()
+
+    client: WorkMailMessageFlowClient = boto3.client("workmailmessageflow")
+    session_client: WorkMailMessageFlowClient = session.client("workmailmessageflow")
+    ```
+"""
+from mypy_boto3_workmailmessageflow.client import (
+    WorkMailMessageFlowClient,
+    WorkMailMessageFlowClient as Client,
+)
+
+
+__all__ = ("Client", "WorkMailMessageFlowClient")

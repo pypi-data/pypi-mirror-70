8a2100629d3ab07eda0a94fe18928915cf7bb4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:13 +0000

    Add package distfunc/0.1/distfunc-0.1.tar.gz

---
+++ packages/distfunc/distfunc-0.1.tar.gz/distfunc-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distfunc',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distfunc'],
+      author='fgrassi',
+      zip_safe=False)

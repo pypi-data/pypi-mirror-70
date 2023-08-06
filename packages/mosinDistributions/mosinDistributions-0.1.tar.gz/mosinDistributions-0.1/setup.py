a563ce9fdf0eb48c57c4650f566ff5906537decc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:06 +0000

    Add package mosinDistributions/0.1/mosinDistributions-0.1.tar.gz

---
+++ packages/mosinDistributions/mosinDistributions-0.1.tar.gz/mosinDistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mosinDistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mosinDistributions'],
+      author='Mosin Mahajan',
+      author_email='mosinmahajan@gmail.com',
+      zip_safe=False)

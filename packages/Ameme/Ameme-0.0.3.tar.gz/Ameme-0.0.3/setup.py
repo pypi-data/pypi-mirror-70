fb713ad10691cdaa36f56b8fda31165272801c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:46 +0000

    Add package Ameme/0.0.3/Ameme-0.0.3.tar.gz

---
+++ packages/Ameme/Ameme-0.0.3.tar.gz/Ameme-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+#############################################
+# File Name: setup.py
+# Author: yangyuhui
+# Mail:
+# Created Time:  2020/6/11
+#############################################
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Ameme",
+    version="0.0.3",
+    author="yangyuhui",
+    author_email="yangyuhui@jaist.ac.jp",
+    description="my tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yyh0806/Ameme",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

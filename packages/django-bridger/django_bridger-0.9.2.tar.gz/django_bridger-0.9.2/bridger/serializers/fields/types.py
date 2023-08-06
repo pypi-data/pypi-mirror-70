ac868cd9168b9b0d8791252b05cd861f57a348f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:26 +0000

    Add package django-bridger/0.9.2/django_bridger-0.9.2.tar.gz

---
+++ packages/django-bridger/django_bridger-0.9.2.tar.gz/django_bridger-0.9.2/bridger/serializers/fields/types.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class BridgerType(Enum):
+    TEXT = "text"
+    TEXTEDITOR = "texteditor"
+    NUMBER = "number"
+    DATETIME = "datetime"
+    DATE = "date"
+    TIME = "time"
+    PRIMARY_KEY = "primary_key"
+    BOOLEAN = "boolean"
+    SELECT = "select"
+    IMAGE = "image"
+    FILE = "file"
+    LIST = "list"
+    PERCENT = "percent"
+    JSON = "json"
+
+
+class ReturnContentType(Enum):
+    HTML = "html"
+    JSON = "json"
+    MARKDOWN = "markdown"

2479a3bbac0d173637ede6d6c0a59e83437d79f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:45 +0000

    Add package ml-probability/1.0/ml_probability-1.0.tar.gz

---
+++ packages/ml-probability/ml_probability-1.0.tar.gz/ml_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'ml_probability',
+           version='1.0',
+           description= 'Gaussian and Binomial distributions',
+packages=['ml_probability'],
+author = 'Ikechukwu Njoku',
+author_email = 'njokuiyke01@yahoo.com',
+zip_safe=False)

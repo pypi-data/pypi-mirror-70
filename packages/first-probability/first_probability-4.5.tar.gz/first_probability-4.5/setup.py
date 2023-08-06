de848af4999a6e925dbd19063d795081698b65db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:31 +0000

    Add package first-probability/4.5/first_probability-4.5.tar.gz

---
+++ packages/first-probability/first_probability-4.5.tar.gz/first_probability-4.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='first_probability',
+      version='4.5',
+      description='Gaussian distributions',
+      packages=['first_probability'],
+      zip_safe=False)

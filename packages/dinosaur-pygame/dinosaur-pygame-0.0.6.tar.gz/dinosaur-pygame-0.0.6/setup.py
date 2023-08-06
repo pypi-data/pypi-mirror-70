ddc35d1b1097eb177adfa652e1634011517eb7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:10 +0000

    Add package dinosaur-pygame/0.0.6/dinosaur-pygame-0.0.6.tar.gz

---
+++ packages/dinosaur-pygame/dinosaur-pygame-0.0.6.tar.gz/dinosaur-pygame-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dinosaur-pygame",
+    version="0.0.6",
+    entry_points={
+        'console_scripts': [
+            'dinosaur_pygame=dinosaur_pygame.main:main',
+        ],
+    },
+    author="Alberto Morales",
+    author_email="albertomoralessp@gmail.com",
+    description="A dinosaur game made with pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albertomsp/dinosaur-pygame",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pygame>=2.0.0.dev4',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

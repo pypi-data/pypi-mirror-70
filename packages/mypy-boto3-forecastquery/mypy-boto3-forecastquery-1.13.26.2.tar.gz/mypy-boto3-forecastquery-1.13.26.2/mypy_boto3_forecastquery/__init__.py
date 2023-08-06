0b620e7b7b77f5774173cdbf439c114b9cc49bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:21 +0000

    Add package mypy-boto3-forecastquery/1.13.26.2/mypy-boto3-forecastquery-1.13.26.2.tar.gz

---
+++ packages/mypy-boto3-forecastquery/mypy-boto3-forecastquery-1.13.26.2.tar.gz/mypy-boto3-forecastquery-1.13.26.2/mypy_boto3_forecastquery/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for forecastquery service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_forecastquery import (
+        Client,
+        ForecastQueryServiceClient,
+    )
+
+    session = boto3.Session()
+
+    client: ForecastQueryServiceClient = boto3.client("forecastquery")
+    session_client: ForecastQueryServiceClient = session.client("forecastquery")
+    ```
+"""
+from mypy_boto3_forecastquery.client import (
+    ForecastQueryServiceClient,
+    ForecastQueryServiceClient as Client,
+)
+
+
+__all__ = ("Client", "ForecastQueryServiceClient")

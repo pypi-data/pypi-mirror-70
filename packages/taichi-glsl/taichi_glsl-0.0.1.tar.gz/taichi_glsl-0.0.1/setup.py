3e09061407f28d911507d03300b4b14a7b6dfbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:16 +0000

    Add package taichi-glsl/0.0.1/taichi_glsl-0.0.1.tar.gz

---
+++ packages/taichi-glsl/taichi_glsl-0.0.1.tar.gz/taichi_glsl-0.0.1/setup.py
@@ -0,0 +1,40 @@
+project_name = 'taichi_glsl'
+version = '0.0.1'
+description = 'Manipulate Taichi with GLSL-alike functions'
+long_description = '''
+Taichi GLSL
+===========
+
+Taichi GLSL provides a set of helper functions, which enables you to manipulate the `Taichi Programming Language <https://github.com/taichi-dev/taichi>`_ in a GLSL-alike manner (work in progress).
+'''
+classifiers = [
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Multimedia :: Graphics',
+    'Topic :: Games/Entertainment :: Simulation',
+    'Operating System :: OS Independent',
+]
+python_requires = '>=3.6'
+install_requires = [
+    'numpy',
+    'taichi>=0.6.7',
+    # For testing:
+    'pytest',
+]
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+import setuptools
+
+setuptools.setup(
+    name=project_name,
+    version=version,
+    author='彭于斌',
+    author_email='1931127624@qq.com',
+    description=description,
+    #long_description=long_description,
+    classifiers=classifiers,
+    python_requires=python_requires,
+    install_requies=install_requires,
+    packages=setuptools.find_packages(),
+)

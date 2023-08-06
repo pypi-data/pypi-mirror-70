d07ab623124f62bd4a1963d7f094db30e26adad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:40 +0000

    Add package bimp/1.8/bimp-1.8.tar.gz

---
+++ packages/bimp/bimp-1.8.tar.gz/bimp-1.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bimp", # Replace with your own username
+    version="1.8",
+    author="Jan Smejkal",
+    author_email="jankosmejkal@yahoo.com",
+    description="Make-life-easier Python functions for image processing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JankoSmejkal/bimp/blob/master/README.md",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+

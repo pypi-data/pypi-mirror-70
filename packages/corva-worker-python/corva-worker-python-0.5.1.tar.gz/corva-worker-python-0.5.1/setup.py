25d0ef7b0ec3e37dc69effc574b58e5358472e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:59 +0000

    Add package corva-worker-python/0.5.1/corva-worker-python-0.5.1.tar.gz

---
+++ packages/corva-worker-python/corva-worker-python-0.5.1.tar.gz/corva-worker-python-0.5.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Software Development :: Libraries',
+]
+
+setup(
+    name='corva-worker-python',
+    author='Jordan Ambra, Mohammadreza Kamyab',
+    author_email='jordan.ambra@corva.ai, m.kamyab@corva.ai',
+    url='https://github.com/corva-ai/corva-worker-python',
+    version='0.5.1',
+    classifiers=classifiers,
+    description='SDK for interacting with Corva',
+    keywords='corva',
+    packages=find_packages(exclude=["test"]),
+    install_requires=["numpy", "redis", "requests"],
+    include_package_data=True,
+    license='The Unlicense',
+)
---
+++ packages/corva-worker-python/corva-worker-python-0.5.1.tar.gz/corva-worker-python-0.5.1/worker/exceptions.py
@@ -0,0 +1,18 @@
+class CorvaException(Exception):
+    pass
+
+
+class Misconfigured(Exception):
+    pass
+
+
+class APIError(CorvaException):
+    pass
+
+
+class NotFound(CorvaException):
+    pass
+
+
+class Forbidden(CorvaException):
+    pass

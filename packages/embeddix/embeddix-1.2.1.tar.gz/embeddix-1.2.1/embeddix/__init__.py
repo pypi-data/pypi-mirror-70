2edfb855e0fc8a188573950e9e0287c83be8cae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:05 +0000

    Add package embeddix/1.2.1/embeddix-1.2.1.tar.gz

---
+++ packages/embeddix/embeddix-1.2.1.tar.gz/embeddix-1.2.1/embeddix/__init__.py
@@ -0,0 +1,5 @@
+"""Exposed functions."""
+
+from embeddix.utils.files import _get_shared_vocab as get_shared_vocab
+from embeddix.core.reducer import _reduce_model as reduce_model
+from embeddix.core.converter import convert_to_txt

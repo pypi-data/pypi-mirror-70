ae5588c89e22199d3699bc187eee73735819a59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:52 +0000

    Add package pyIEM/0.16.0/pyIEM-0.16.0.tar.gz

---
+++ packages/pyIEM/pyIEM-0.16.0.tar.gz/pyIEM-0.16.0/src/pyiem/nws/products/mos.py
@@ -0,0 +1,142 @@
+"""
+ Supports parsing of Textual Model Output Statistics files
+"""
+import re
+import datetime
+
+from pyiem.util import utc
+from pyiem.nws.product import TextProduct
+
+
+def section_parser(sect):
+    """Parse this section of text"""
+    metadata = re.findall(
+        (
+            r"([A-Z0-9_]{3,10})\s+(...) (V[0-9]\.[0-9] )?(...) GUIDANCE\s+"
+            r"([01]?[0-9])/([0-3][0-9])/([0-9]{4})\s+"
+            r"([0-2][0-9]00) UTC"
+        ),
+        sect,
+    )
+    (station, model, _bogus, mos, month, day, year, hhmm) = metadata[0]
+    if model == "NBM":
+        model = mos
+    initts = utc(int(year), int(month), int(day), int(hhmm[:2]))
+
+    times = [initts]
+    data = {}
+    lines = sect.split(";;;")
+    hrs = lines[2].split()
+    for hr in hrs[1:]:
+        if hr == "00":
+            ts = times[-1] + datetime.timedelta(days=1)
+            ts = ts.replace(hour=0)
+        else:
+            ts = times[-1].replace(hour=int(hr))
+        times.append(ts)
+        data[ts] = {}
+
+    for line in lines[3:]:
+        if len(line) < 10:
+            continue
+        vname = line[:3].replace("/", "_")
+        if vname == "X_N":
+            vname = "N_X"
+        vals = re.findall("(...)", line[4:])
+        for i, val in enumerate(vals):
+            if vname == "T06" and times[i + 1].hour in [0, 6, 12, 18]:
+                data[times[i + 1]]["T06_1"] = (
+                    vals[i - 1].replace("/", "").strip()
+                )
+                data[times[i + 1]]["T06_2"] = val.replace("/", "").strip()
+            elif vname == "T06":
+                pass
+            elif vname == "T12" and times[i + 1].hour in [0, 12]:
+                data[times[i + 1]]["T12_1"] = (
+                    vals[i - 1].replace("/", "").strip()
+                )
+                data[times[i + 1]]["T12_2"] = val.replace("/", "").strip()
+            elif vname == "T12":
+                pass
+            elif vname == "WDR":
+                data[times[i + 1]][vname] = int(vals[i].strip()) * 10
+            else:
+                data[times[i + 1]][vname] = val.strip()
+    return dict(station=station, model=model, data=data, initts=initts)
+
+
+def make_null(val):
+    """Hmmm"""
+    if val == "" or val is None:
+        return None
+    return val
+
+
+class MOSProduct(TextProduct):
+    """
+    Represents a Model Output Statistics file
+    """
+
+    def __init__(
+        self, text, utcnow=None, ugc_provider=None, nwsli_provider=None
+    ):
+        """constructor"""
+        TextProduct.__init__(self, text, utcnow, ugc_provider, nwsli_provider)
+        self.data = []
+        self.parse_data()
+
+    def sql(self, txn):
+        """Persist our data to the database
+
+        Args:
+          txn: Database cursor
+
+        Returns:
+          int number of inserts made to the database
+        """
+        inserts = 0
+        for sect in self.data:
+            for ts in sect["data"]:
+                if ts == sect["initts"]:
+                    continue
+                # Account for 'empty' MOS products
+                if not sect["data"][ts]:
+                    continue
+                fst = """
+                INSERT into t%s (station, model, runtime, ftime,
+                """ % (
+                    sect["initts"].year,
+                )
+                sst = "VALUES(%s,%s,%s,%s,"
+                args = [sect["station"], sect["model"], sect["initts"], ts]
+                for vname in sect["data"][ts].keys():
+                    # variables we don't wish to database
+                    if vname in ["FHR"]:
+                        continue
+                    fst += " %s," % (vname,)
+                    sst += "%s,"
+                    args.append(make_null(sect["data"][ts][vname]))
+                if len(args) == 4:
+                    # No data was found
+                    continue
+                sql = fst[:-1] + ") " + sst[:-1] + ")"
+                txn.execute(sql, args)
+                inserts += 1
+        return inserts
+
+    def parse_data(self):
+        """Parse out our data!"""
+        raw = self.unixtext + "\n"
+        raw = raw.replace("\n", ";;;").replace("\x1e", "")
+        sections = re.findall(
+            r"([A-Z0-9_]{3,10}\s+... V?[0-9]?\.?[0-9]? ?... GUIDANCE .*?);;;;;;",
+            raw,
+        )
+        self.data = list(map(section_parser, sections))
+        if not sections:
+            raise Exception("Failed to split MOS Product")
+
+
+def parser(text, utcnow=None, ugc_provider=None, nwsli_provider=None):
+    """ Helper function """
+    return MOSProduct(text, utcnow, ugc_provider, nwsli_provider)
---
+++ packages/pyIEM/pyIEM-0.16.0.tar.gz/pyIEM-0.16.0/tests/nws/products/test_mos.py
@@ -0,0 +1,91 @@
+"""Test MOS Parsing."""
+
+import pytest
+from pyiem.nws.products.mos import parser as mosparser
+from pyiem.util import get_dbconn, utc, get_test_file
+
+
+@pytest.fixture
+def cursor():
+    """Return a database cursor."""
+    return get_dbconn("mos").cursor()
+
+
+def test_ecmwf(cursor):
+    """Test that we can parse the ECMWF MOS."""
+    utcnow = utc(2020, 2, 24, 0)
+    prod = mosparser(get_test_file("MOS/ECS.txt"), utcnow=utcnow)
+    assert len(prod.data) == 3
+    inserts = prod.sql(cursor)
+    assert inserts == 63
+
+
+def test_180125_empty(cursor):
+    """Can we parse a MOS product with empty data"""
+    utcnow = utc(2018, 1, 26, 1)
+    prod = mosparser(get_test_file("MOS/MET_empty.txt"), utcnow=utcnow)
+    assert len(prod.data) == 3
+    assert len(prod.data[0]["data"].keys()) == 21
+
+    inserts = prod.sql(cursor)
+    assert inserts == 42
+
+
+def test_parse(cursor):
+    """MOS type"""
+    utcnow = utc(2017, 8, 12, 12)
+    prod = mosparser(get_test_file("MOS/METNC1.txt"), utcnow=utcnow)
+    assert len(prod.data) == 4
+    assert len(prod.data[0]["data"].keys()) == 21
+
+    inserts = prod.sql(cursor)
+    assert inserts == (4 * 21)
+
+
+def test_empty_nbm(cursor):
+    """Does an empty product trip us up."""
+    utcnow = utc(2018, 11, 7, 17)
+    prod = mosparser(get_test_file("MOS/NBSUSA_empty.txt"), utcnow=utcnow)
+    assert len(prod.data) == 2
+
+    inserts = prod.sql(cursor)
+    assert inserts == 0
+
+
+def test_nbm_v32(cursor):
+    """Can we parse the NBM v3.2 data."""
+    utcnow = utc(2020, 2, 19, 12)
+    prod = mosparser(get_test_file("MOS/NBSUSA_32.txt"), utcnow=utcnow)
+    assert len(prod.data) == 3
+    inserts = prod.sql(cursor)
+    assert inserts == 69
+
+
+def test_nbm_v32_station(cursor):
+    """Can we parse the NBM v3.2 data."""
+    utcnow = utc(2020, 2, 19, 17)
+    prod = mosparser(get_test_file("MOS/NBSUSA_32_station.txt"), utcnow=utcnow)
+    assert len(prod.data) == 4
+    inserts = prod.sql(cursor)
+    assert inserts == 92
+
+
+def test_nbm(cursor):
+    """Can we parse the NBM data."""
+    utcnow = utc(2018, 11, 7, 15)
+    prod = mosparser(get_test_file("MOS/NBSUSA.txt"), utcnow=utcnow)
+    assert len(prod.data) == 2
+
+    inserts = prod.sql(cursor)
+    assert inserts == (2 * 21)
+
+    cursor.execute(
+        """
+        SELECT count(*), max(ftime) from t2018
+        where model = 'NBS' and station = 'KALM' and runtime = %s
+    """,
+        (utcnow,),
+    )
+    row = cursor.fetchone()
+    assert row[0] == 21
+    assert row[1] == utc(2018, 11, 10, 9)

179a3e7abb220ee3359029cfdeb025012bd17e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:17 +0000

    Add package mypy-boto3-outposts/1.13.24.2/mypy-boto3-outposts-1.13.24.2.tar.gz

---
+++ packages/mypy-boto3-outposts/mypy-boto3-outposts-1.13.24.2.tar.gz/mypy-boto3-outposts-1.13.24.2/mypy_boto3_outposts/__init__.py
@@ -0,0 +1,20 @@
+"""
+Main interface for outposts service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.outposts import (
+        Client,
+        OutpostsClient,
+        )
+
+    session = boto3.Session()
+
+    client: OutpostsClient = boto3.client("outposts")
+    session_client: OutpostsClient = session.client("outposts")
+"""
+from mypy_boto3_outposts.client import OutpostsClient, OutpostsClient as Client
+
+
+__all__ = ("Client", "OutpostsClient")

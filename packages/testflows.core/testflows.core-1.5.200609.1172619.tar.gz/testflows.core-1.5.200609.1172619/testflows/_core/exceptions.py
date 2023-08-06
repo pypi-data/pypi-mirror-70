ca6aa3ff2e35485449ad0130972bc54b26fd0ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:33 +0000

    Add package testflows.core/1.5.200609.1172619/testflows.core-1.5.200609.1172619.tar.gz

---
+++ packages/testflows.core/testflows.core-1.5.200609.1172619.tar.gz/testflows.core-1.5.200609.1172619/testflows/_core/exceptions.py
@@ -0,0 +1,45 @@
+import sys
+import traceback
+
+def exception(exc_type=None, exc_value=None, exc_traceback=None):
+    """Get exception string.
+    """
+    if (exc_type, exc_value, exc_traceback) == (None, None, None):
+        exc_type, exc_value, exc_traceback  = sys.exc_info()
+    return "".join(traceback.format_exception(exc_type, exc_value, exc_traceback)).rstrip()
+
+
+class TestFlowsException(Exception):
+    """Base exception class.
+    """
+    pass
+
+class ResultException(TestFlowsException):
+    """Result exception.
+    """
+    pass
+
+class DummyTestException(TestFlowsException):
+    """Dummy test exception.
+    """
+    pass
+
+class RepeatTestException(TestFlowsException):
+    """Repeat test exception.
+    """
+    pass
+
+class TestFlowsError(TestFlowsException):
+    """Base error exception class.
+    """
+    pass
+
+class RequirementError(TestFlowsError):
+    """Requirement error.
+    """
+    pass
+
+class ArgumentError(TestFlowsError):
+    """Argument error.
+    """
+    pass

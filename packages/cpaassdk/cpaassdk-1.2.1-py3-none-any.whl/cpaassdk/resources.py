6d2101d0ba50a25e657c901d5b02b47af5c41aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:09 +0000

    Add package cpaassdk/1.2.1/cpaassdk-1.2.1-py3-none-any.whl

---
+++ packages/cpaassdk/cpaassdk-1.2.1-py3-none-any.whl/cpaassdk/resources.py
@@ -0,0 +1,9 @@
+from cpaassdk.resources.twofactor import Twofactor
+from cpaassdk.resources.conversation import Conversation
+from cpaassdk.resources.notification import Notification
+
+list = {
+  'twofactor': Twofactor,
+  'conversation': Conversation,
+  'notification': Notification
+}

21b9da3564808f6fc75db685cdd1e14e4f7ef4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:00 +0000

    Add package alacritty-colorscheme/0.1.0/alacritty-colorscheme-0.1.0.tar.gz

---
+++ packages/alacritty-colorscheme/alacritty-colorscheme-0.1.0.tar.gz/alacritty-colorscheme-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alacritty_colorscheme']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ruamel.yaml>=0.16.10,<0.17.0']
+
+entry_points = \
+{'console_scripts': ['alacritty-colorscheme = alacritty_colorscheme.cli:main']}
+
+setup_kwargs = {
+    'name': 'alacritty-colorscheme',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Safar Ligal',
+    'author_email': 'weathermist@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

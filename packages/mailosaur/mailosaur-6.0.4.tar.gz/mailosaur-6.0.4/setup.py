37a7edd30873fe4d060299222069aa23cc8e8fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:56 +0000

    Add package mailosaur/6.0.4/mailosaur-6.0.4.tar.gz

---
+++ packages/mailosaur/mailosaur-6.0.4.tar.gz/mailosaur-6.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='mailosaur',
+      version='6.0.4',
+      description='Python client library for Mailosaur',
+      url='https://mailosaur.com',
+      author='Mailosaur Ltd',
+      author_email='code@mailosaur.com',
+      keywords='email automation testing selenium robot framework',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'python-dateutil',
+          'requests',
+          'requests[security]'
+      ],
+      zip_safe=False, requires=['requests'])
+

0bbdbc67d498eb9918b64e4ffc633a44c8cd4a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:49 +0000

    Add package mypy-boto3-rds-data/1.13.24.3/mypy_boto3_rds_data-1.13.24.3-py3-none-any.whl

---
+++ packages/mypy-boto3-rds-data/mypy_boto3_rds_data-1.13.24.3-py3-none-any.whl/mypy_boto3_rds_data/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for rds-data service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_rds_data import (
+        Client,
+        RDSDataServiceClient,
+    )
+
+    session = boto3.Session()
+
+    client: RDSDataServiceClient = boto3.client("rds-data")
+    session_client: RDSDataServiceClient = session.client("rds-data")
+    ```
+"""
+from mypy_boto3_rds_data.client import RDSDataServiceClient, RDSDataServiceClient as Client
+
+
+__all__ = ("Client", "RDSDataServiceClient")

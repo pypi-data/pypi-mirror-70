06580c44545dd45dcfac5951f9c9603bfbcb6ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:23 +0000

    Add package pytest-xvfb/2.0.0/pytest-xvfb-2.0.0.tar.gz

---
+++ packages/pytest-xvfb/pytest-xvfb-2.0.0.tar.gz/pytest-xvfb-2.0.0/CHANGELOG.rst
@@ -0,0 +1,54 @@
+pytest-xvfb changelog
+=====================
+
+v2.0.0
+------
+
+- PyVirtualDisplay 1.3 and newer is now supported, support for older versions
+  was dropped.
+- Support for Python 2.7, 3.3 and 3.4 is now dropped.
+- Support for Python 3.6, 3.7 and 3.8 was added (no code changes required).
+- Xvfb is now not started anymore in the xdist master process.
+
+v1.2.0
+------
+
+- ``Item.get_closest_marker`` is now used, which restores compatibility with
+  pytest 4.1.0 and requires pytest 3.6.0 or newer.
+
+v1.1.0
+------
+
+- The ``xvfb_args`` option is now a single line parsed with ``shlex.split``.
+- The ``XvfbExitedError`` exception now includes stdout and stderr.
+
+v1.0.0
+------
+
+- Use `PyVirtualDisplay`_ to start/stop Xvfb
+- Show a warning on Linux if Xvfb is unavailable
+
+.. _PyVirtualDisplay: https://pypi.python.org/pypi/PyVirtualDisplay
+
+v0.3.0
+------
+
+- Add a new ``xvfb_xauth`` setting which creates an ``XAUTHORITY`` file.
+
+v0.2.1
+------
+
+- The temporary directory searched for logfiles is now hardcoded to /tmp
+  as that's what X11 does as well.
+
+v0.2.0
+------
+
+- The ``no_xvfb``-marker is now registered automatically so pytest doesn't fail
+  when run with ``--strict``.
+- The ``xvfb`` fixture is now session-scoped.
+
+v0.1.0
+------
+
+- Initial release
---
+++ packages/pytest-xvfb/pytest-xvfb-2.0.0.tar.gz/pytest-xvfb-2.0.0/tests/test_xvfb.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pytest
+
+import pytest_xvfb
+
+
+xauth_available = any(
+    os.access(os.path.join(path, 'xauth'), os.X_OK)
+    for path in os.environ.get('PATH', '').split(os.pathsep)
+)
+
+
+@pytest.fixture(autouse=True, scope='session')
+def ensure_xvfb():
+    if not pytest_xvfb.xvfb_available():
+        raise Exception("Tests need Xvfb to run.")
+
+
+def test_xvfb_available(testdir, monkeypatch):
+    monkeypatch.delenv('DISPLAY')
+    testdir.makepyfile("""
+        import os
+
+        def test_display():
+            assert 'DISPLAY' in os.environ
+    """)
+    result = testdir.runpytest()
+    assert result.ret == 0
+
+
+def test_empty_display(testdir, monkeypatch):
+    monkeypatch.setenv('DISPLAY', '')
+    testdir.makepyfile("""
+        import os
+
+        def test_display():
+            assert 'DISPLAY' in os.environ
+    """)
+    result = testdir.runpytest()
+    assert os.environ['DISPLAY'] == ''
+    assert result.ret == 0
+
+
+def test_xvfb_unavailable(testdir, monkeypatch):
+    monkeypatch.setenv('PATH', '')
+    monkeypatch.setenv('DISPLAY', ':42')
+    testdir.makepyfile("""
+        import os
+
+        def test_display():
+            assert os.environ['DISPLAY'] == ':42'
+    """)
+    assert os.environ['DISPLAY'] == ':42'
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines('* could not find Xvfb.*')
+    assert result.ret == 0
+
+
+def test_no_xvfb_arg(testdir, monkeypatch):
+    monkeypatch.setenv('DISPLAY', ':42')
+    testdir.makepyfile("""
+        import os
+
+        def test_display():
+            assert os.environ['DISPLAY'] == ':42'
+    """)
+    assert os.environ['DISPLAY'] == ':42'
+    result = testdir.runpytest('--no-xvfb')
+    assert result.ret == 0
+
+
+@pytest.mark.parametrize('configured', [True, False])
+def test_screen_size(testdir, configured):
+    try:
+        import tkinter
+    except ImportError:
+        pytest.importorskip('Tkinter')
+
+    if configured:
+        testdir.makeini("""
+            [pytest]
+            xvfb_width = 1024
+            xvfb_height = 768
+            xvfb_colordepth = 8
+        """)
+        expected_width = 1024
+        expected_height = 768
+        expected_depth = 8
+    else:
+        expected_width = 800
+        expected_height = 600
+        expected_depth = 16
+
+    testdir.makepyfile("""
+        try:
+            import tkinter as tk
+        except ImportError:
+            import Tkinter as tk
+
+        def test_screen_size():
+            root = tk.Tk()
+            assert root.winfo_screenwidth() == {width}
+            assert root.winfo_screenheight() == {height}
+            assert root.winfo_screendepth() == {depth}
+    """.format(width=expected_width, height=expected_height,
+               depth=expected_depth))
+    result = testdir.runpytest()
+    assert result.ret == 0
+
+
+def test_failing_start(testdir, monkeypatch):
+    testdir.makeini("""
+        [pytest]
+        xvfb_args = -foo
+    """)
+    testdir.makepyfile("""
+        def test_none():
+            pass
+    """)
+    result = testdir.runpytest()
+    result.stderr.fnmatch_lines([
+        "INTERNALERROR> *.XStartError: Xvfb program closed. *",
+    ])
+    assert 'OSError' not in str(result.stderr)
+
+
+@pytest.mark.parametrize('args, outcome', [
+    ([], '1 passed, 1 skipped'),
+    (['--no-xvfb'], '2 passed'),
+])
+def test_no_xvfb_marker(testdir, args, outcome):
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.mark.no_xvfb
+        def test_marked():
+            pass
+
+        def test_unmarked():
+            pass
+    """)
+    res = testdir.runpytest(*args)
+    res.stdout.fnmatch_lines('*= {0}*'.format(outcome))
+
+
+def test_xvfb_fixture(testdir):
+    testdir.makepyfile("""
+        import os
+
+        def test_display(xvfb):
+            assert ':{}'.format(xvfb.display) == os.environ['DISPLAY']
+
+        def test_screen(xvfb):
+            assert xvfb.width == 800
+            assert xvfb.height == 600
+            assert xvfb.colordepth == 16
+
+        def test_args(xvfb):
+            assert xvfb.args == []
+    """)
+    result = testdir.runpytest()
+    assert result.ret == 0
+
+
+def test_early_display(monkeypatch, testdir):
+    """Make sure DISPLAY is set in a session-scoped fixture already."""
+    monkeypatch.delenv('DISPLAY')
+    testdir.makepyfile("""
+        import os
+        import pytest
+
+        @pytest.yield_fixture(scope='session', autouse=True)
+        def fixt():
+            assert 'DISPLAY' in os.environ
+            yield
+
+        def test_foo():
+            pass
+    """)
+
+
+def test_strict_markers(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.mark.no_xvfb
+        def test_marked():
+            pass
+    """)
+    result = testdir.runpytest('--strict')
+    assert result.ret == 0
+
+
+def test_xvfb_session_fixture(testdir):
+    """Make sure the xvfb fixture can be used from a session-wide one."""
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.fixture(scope='session')
+        def fixt(xvfb):
+            pass
+
+        def test_fixt(fixt):
+            pass
+    """)
+    result = testdir.runpytest()
+    assert result.ret == 0
+
+
+@pytest.mark.skipif(not xauth_available, reason='no xauth')
+def test_xvfb_with_xauth(testdir):
+    original_auth = os.environ.get('XAUTHORITY')
+    testdir.makeini("""
+        [pytest]
+        xvfb_xauth = True
+    """)
+    testdir.makepyfile("""
+        import os
+
+        def test_xauth():
+            print('\\nXAUTHORITY: ' + os.environ['XAUTHORITY'])
+            assert os.path.isfile(os.environ['XAUTHORITY'])
+            assert os.access(os.environ['XAUTHORITY'], os.R_OK)
+    """)
+    result = testdir.runpytest('-s')
+    # Get and parse the XAUTHORITY: line
+    authline = next(l for l in result.outlines if l.startswith('XAUTHORITY:'))
+    authfile = authline.split(' ', 1)[1]
+
+    assert result.ret == 0
+    # Make sure the authfile is deleted
+    assert not os.path.exists(authfile)
+    assert os.environ.get('XAUTHORITY') == original_auth

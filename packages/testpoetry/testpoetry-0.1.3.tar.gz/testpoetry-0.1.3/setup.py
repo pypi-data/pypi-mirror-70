2a483585f8fb3c5ce21a7fb80ab140525b8bd8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:08 +0000

    Add package testpoetry/0.1.3/testpoetry-0.1.3.tar.gz

---
+++ packages/testpoetry/testpoetry-0.1.3.tar.gz/testpoetry-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testpoetry']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'testpoetry',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Firastic',
+    'author_email': 'adyaksa.wisanggeni@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/testpoetry/testpoetry-0.1.3.tar.gz/testpoetry-0.1.3/testpoetry/app.py
@@ -0,0 +1,6 @@
+"""
+An app that does lots of stuff
+"""
+def main():
+    # This should start and launch your app!
+    pass

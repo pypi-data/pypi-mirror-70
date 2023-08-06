2e1f89d9f545694d301537c771109c6abfef67e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:57 +0000

    Add package pymcx/0.0.1/pymcx-0.0.1.tar.gz

---
+++ packages/pymcx/pymcx-0.0.1.tar.gz/pymcx-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pymcx',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/rlouf/mcx',
+    author='Remi Louf',
+    author_email='remilouf@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)

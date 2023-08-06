be33cc18342996d4e19aa339931d9c286a1ff565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:26 +0000

    Add package video-flawer/0.0.3/video_flawer-0.0.3.tar.gz

---
+++ packages/video-flawer/video_flawer-0.0.3.tar.gz/video_flawer-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'video_flawer',
+  packages = ['video_flawer'],
+  version = '0.0.3',
+  license='MIT',
+  description = 'This package enhances video files with artifical defects.',
+  author = 'Matous Cejnek, Yang Hong-Bin',
+  author_email = 'matousc@gmail.com',
+  url = 'https://github.com/matousc89/video_flawer',
+  download_url = 'https://github.com/matousc89/video_flawer/archive/v0.0.3.tar.gz',
+  keywords = ['video', 'noise', 'artificial'],
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

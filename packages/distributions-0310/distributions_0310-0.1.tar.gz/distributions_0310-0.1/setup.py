6e3e15ec20a38f437086f32fd1d5989999c7e15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:18 +0000

    Add package distributions-0310/0.1/distributions_0310-0.1.tar.gz

---
+++ packages/distributions-0310/distributions_0310-0.1.tar.gz/distributions_0310-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_0310',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_0310'],
+      zip_safe=False)

101e9b412e4f903fc67ea67576ec294924a6c84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:23 +0000

    Add package clearrepetition/1.0.1/clearrepetition-1.0.1-py3-none-any.whl

---
+++ packages/clearrepetition/clearrepetition-1.0.1-py3-none-any.whl/clearrepetition/clearrepetition.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def clearrepetition(wave,flam):
+    unique = np.unique(wave)
+    wavetmp,flamtmp = [],[]
+    for i in unique:
+        m = np.where(wave == i)
+        wavetmp.append(np.mean(wave[m]))
+        flamtmp.append(np.mean(flam[m]))
+    return np.array(wavetmp),np.array(flamtmp)

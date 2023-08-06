8626589a1b10ed7d04c4d55d96a68aaba5c25cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:55 +0000

    Add package search-this/0.4/search_this-0.4-py3-none-any.whl

---
+++ packages/search-this/search_this-0.4-py3-none-any.whl/search_this/start.py
@@ -0,0 +1,93 @@
+import argparse
+import requests
+
+from bs4 import BeautifulSoup
+
+from utils import (
+    DuckDuckGoEngine, GoogleEngine, write_console, write_csv, write_json
+)
+
+
+def positive_integer(i):
+    """
+    Проверка введено ли положительное число для количества запросов
+    """
+    try:
+        v = int(i)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"expected integeter, got {i}")
+    if v <= 0:
+        raise argparse.ArgumentTypeError(
+            f"expected positive integeter, got {i}"
+        )
+    return v
+
+
+def process_args():
+    """
+    Функция обработки аргументов из командной строки
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-q",
+        "--query",
+        required=True,
+        help="Query for searching. Words define in qoutes.",
+    )
+    parser.add_argument(
+        "-e",
+        "--engine",
+        choices=("google", "duck"),
+        default="google",
+        help="Choose the search engine. Default: Google.",
+    )
+    parser.add_argument(
+        "-c",
+        "--count",
+        default=1,
+        type=positive_integer,
+        help="Count of results. Default: 1.",
+    )
+    parser.add_argument(
+        "-l",
+        "--logging",
+        choices=("console", "json", "csv"),
+        default="console",
+        help="Information ouput. Default: console.",
+    )
+    return parser.parse_args()
+
+
+def start():
+    """
+    Главная функция
+    """
+    args = process_args()
+    search_vars = {
+        "google": GoogleEngine,
+        "duck": DuckDuckGoEngine
+    }
+    output_function = {
+        "console": write_console,
+        "json": write_json,
+        "csv": write_csv
+    }
+    user_agent = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0)"
+        "Gecko/20100101 Firefox/65.0"
+    )
+    headers = {"user-agent": user_agent}
+    search = search_vars[args.engine](args.query, args.count)
+    while search.tries < args.count:
+        response = requests.get(search.url, headers=headers)
+        if response.status_code != 200:
+            print(f"Error {response.status_code}")
+            break
+        soup = BeautifulSoup(response.text, "html.parser")
+        search.search(soup)
+        search.next_page()
+    output_function[args.logging](search)
+
+
+if __name__ == "__main__":
+    start()
---
+++ packages/search-this/search_this-0.4-py3-none-any.whl/search_this/utils.py
@@ -0,0 +1,143 @@
+import csv
+import json
+import os
+import time
+import urllib
+
+
+class BaseEngine:
+    """
+    Базовый класс обработки результатов поиска
+    """
+    tries = 0
+    results = list()
+
+    def __init__(self, query: str, count: int):
+        self.original_query = query
+        self.count = count
+        self.base_url = ""
+        self.page_text = ""
+        self.page_number = 0
+
+    @property
+    def query(self):
+        return urllib.parse.quote_plus(self.original_query)
+
+    @property
+    def url_query(self):
+        return f"{self.base_url}{self.query}"
+
+    @property
+    def url(self):
+        return f"{self.url_query}{self.page_text}{self.page_number}"
+
+
+class DuckDuckGoEngine(BaseEngine):
+    """
+    Класс для DuckDuckGo
+    """
+    def __init__(self, query: str, count: int):
+        self.original_query = query
+        self.count = count
+        self.base_url = "https://duckduckgo.com/html/?q="
+        self.page_text = "&s="
+        self.page_number = 30
+        self.extra = "&nextParams=&v=l&o=json&api=d.js"
+        self.dc = "&dc="
+        self.vqd = "&vqd="
+        self.dc_value = ""
+        self.vqd_value = ""
+
+    def next_page(self):
+        self.page_number += 50
+
+    def search(self, soup):
+        a_classes = soup.find_all("a", attrs={"class": "result__a"}, href=True)
+        for a_class in a_classes:
+            if self.tries == self.count:
+                break
+            self.results.append((a_class.text, a_class["href"]))
+            self.tries += 1
+        for input in soup.findAll("input", type="hidden"):
+            if input["name"] == "dc":
+                self.dc_value = input["value"]
+            elif input["name"] == "vqd":
+                self.vqd_value = input["value"]
+        time.sleep(2)
+
+    @property
+    def url(self):
+        if self.dc_value or self.vqd_value:
+            return "{}{}{}{}{}{}{}{}".format(
+                self.url_query,
+                self.page_text,
+                self.page_number,
+                self.extra,
+                self.dc,
+                self.dc_value,
+                self.vqd,
+                self.vqd_value,
+            )
+        else:
+            return f"{self.url_query}{self.page_text}{self.page_number}"
+
+
+class GoogleEngine(BaseEngine):
+    """
+    Класс для Google
+    """
+    def __init__(self, query: str, count: int):
+        self.original_query = query
+        self.count = count
+        self.base_url = "https://www.google.com/search?q="
+        self.page_text = "&start="
+        self.page_number = 0
+
+    def next_page(self):
+        self.page_number += 10
+
+    def search(self, soup):
+        for g_class in soup.find_all(class_="g"):
+            if self.tries == self.count:
+                break
+            for a_class in g_class.find_all("a"):
+                if a_class.get("href") and a_class.find("h3"):
+                    self.results.append(
+                        (a_class.find("h3").text, a_class.get("href"))
+                    )
+                    self.tries += 1
+                    break
+
+
+def write_console(search):
+    """
+    Вывод результатов на экран
+    """
+    for item in search.results:
+        print(f"{item[0]}: {item[1]}")
+
+
+def write_csv(search):
+    """
+    Запись результатов в csv файл
+    """
+    pwd = os.path.split(os.path.realpath(__file__))[0]
+    path = os.path.join(pwd, f"{search.original_query}.csv")
+    with open(path, "w") as outfile:
+        outfile_writer = csv.writer(outfile, delimiter=";", quotechar='"')
+        outfile_writer.writerow(["Text", "URL"])
+        for row in search.results:
+            outfile_writer.writerow(row)
+    print(f"File path: {path}")
+
+
+def write_json(search):
+    """
+    Запись результатов в json файл
+    """
+    pwd = os.path.split(os.path.realpath(__file__))[0]
+    path = os.path.join(pwd, f"{search.original_query}.json")
+    with open(path, "w") as outfile:
+        results = [{"text": x[0], "url": x[1]} for x in search.results]
+        json.dump(results, outfile, indent=2, ensure_ascii=False)
+    print(f"File path: {path}")

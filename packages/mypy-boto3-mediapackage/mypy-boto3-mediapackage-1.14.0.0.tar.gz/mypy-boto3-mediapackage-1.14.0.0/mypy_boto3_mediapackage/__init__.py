6e935cb8ed82058bf74c1af996f76c784104e93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:28 +0000

    Add package mypy-boto3-mediapackage/1.14.0.0/mypy-boto3-mediapackage-1.14.0.0.tar.gz

---
+++ packages/mypy-boto3-mediapackage/mypy-boto3-mediapackage-1.14.0.0.tar.gz/mypy-boto3-mediapackage-1.14.0.0/mypy_boto3_mediapackage/__init__.py
@@ -0,0 +1,40 @@
+"""
+Main interface for mediapackage service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_mediapackage import (
+        Client,
+        ListChannelsPaginator,
+        ListHarvestJobsPaginator,
+        ListOriginEndpointsPaginator,
+        MediaPackageClient,
+    )
+
+    session = boto3.Session()
+
+    client: MediaPackageClient = boto3.client("mediapackage")
+    session_client: MediaPackageClient = session.client("mediapackage")
+
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_harvest_jobs_paginator: ListHarvestJobsPaginator = client.get_paginator("list_harvest_jobs")
+    list_origin_endpoints_paginator: ListOriginEndpointsPaginator = client.get_paginator("list_origin_endpoints")
+    ```
+"""
+from mypy_boto3_mediapackage.client import MediaPackageClient as Client, MediaPackageClient
+from mypy_boto3_mediapackage.paginator import (
+    ListChannelsPaginator,
+    ListHarvestJobsPaginator,
+    ListOriginEndpointsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListChannelsPaginator",
+    "ListHarvestJobsPaginator",
+    "ListOriginEndpointsPaginator",
+    "MediaPackageClient",
+)

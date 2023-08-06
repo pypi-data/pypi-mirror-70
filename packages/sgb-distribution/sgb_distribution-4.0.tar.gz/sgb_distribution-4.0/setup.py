cc42b4d221317c90a89238e787100416622ade93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:59 +0000

    Add package sgb-distribution/4.0/sgb_distribution-4.0.tar.gz

---
+++ packages/sgb-distribution/sgb_distribution-4.0.tar.gz/sgb_distribution-4.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sgb_distribution',
+      version='4.0',
+      description='Gaussian and Binomial Distributions',
+      packages=['sgb_distribution'],
+      author = 'Shivam Wankhade',
+      author_email = 'shivamwankhade12@gmail.com',
+      zip_safe=False)

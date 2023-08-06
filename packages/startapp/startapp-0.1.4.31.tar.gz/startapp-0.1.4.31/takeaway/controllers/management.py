c3b18c5da5e3ee3ee0f88d65e2a4f18acf8acb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package startapp/0.1.4.31/startapp-0.1.4.31.tar.gz

---
+++ packages/startapp/startapp-0.1.4.31.tar.gz/startapp-0.1.4.31/takeaway/controllers/management.py
@@ -0,0 +1,25 @@
+from .app import  FastApiApp, FlaskApp, DjangoApp
+
+class BaseComand:
+
+    def __init__(self,app,folder_name):
+        self.app = app
+        self.folder_name = folder_name
+        
+    
+    def start(self):
+        if self.app == "fastapi":
+
+            app = FastApiApp(self.app, self.folder_name)
+            app.start()
+
+        elif self.app == "flask":
+            
+            app = FlaskApp()
+            app.start()
+
+
+        elif self.app == "django":
+            
+            app = DjangoApp()
+            app.start()

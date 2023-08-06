8586a77db966d4bc7ea32e63d437d5c52de2b874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:10 +0000

    Add package ehownet/0.5.1/ehownet-0.5.1.tar.gz

---
+++ packages/ehownet/ehownet-0.5.1.tar.gz/ehownet-0.5.1/ehn/_bin/parser.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'Mu Yang <http://muyang.pro>'
+__copyright__ = '2018-2020 CKIP Lab'
+
+import argparse
+
+from ehn.parse import EhnParser, EhnSyntaxError
+
+################################################################################################################################
+# Main
+#
+
+def main():
+
+    argparser = argparse.ArgumentParser(description='E-HowNet Parser')
+
+    argparser.add_argument('text', type=str, nargs='+', help='input texts.')
+    argparser.add_argument('--debug', action='store_true', help='debug mode.')
+
+    args = argparser.parse_args()
+
+    # lexer = EhnLexer()
+    parser = EhnParser()
+
+    for (i, text,) in enumerate(args.text):
+        try:
+            res = parser(text, debug=args.debug)
+            print('#{}'.format(i+1))
+            print(res)
+        except EhnSyntaxError as err:
+            print(err)
+            print(text)
+            print(err.show_pos(text))

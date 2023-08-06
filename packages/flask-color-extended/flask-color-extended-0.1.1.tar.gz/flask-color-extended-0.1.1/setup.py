74e33a6b0c0ba7f329e2a2d9173f99b96f4f743f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:58 +0000

    Add package flask-color-extended/0.1.1/flask-color-extended-0.1.1.tar.gz

---
+++ packages/flask-color-extended/flask-color-extended-0.1.1.tar.gz/flask-color-extended-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='flask-color-extended',
+    version='0.1.1',
+    url='https://github.com/Alveona/flask-color-extended',
+    license='MIT',
+    author='Alveona',
+    author_email='pomavau@yandex.ru',
+    description='flask-color-extended is an extension for Flask that improves the built-in web server with colors when debugging.',
+    long_description=__doc__,
+    packages=['flask_color_extended'],
+    namespace_packages=['flask_color_extended'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

32a808f8198ecd6370ca68da318f68001d61f8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:16 +0000

    Add package vbrnrt-probability/0.1/vbrnrt_probability-0.1.tar.gz

---
+++ packages/vbrnrt-probability/vbrnrt_probability-0.1.tar.gz/vbrnrt_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vbrnrt_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['vbrnrt_probability'],
+      author = 'Lawrence',
+      author_email = 'lawrence.lee@commscope.com',
+      zip_safe=False)

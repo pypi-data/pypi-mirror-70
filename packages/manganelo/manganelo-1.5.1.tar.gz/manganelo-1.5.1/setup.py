4af6f0bf4abca238ffc2ff474c08dfd2bda1a1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:02 +0000

    Add package manganelo/1.5.1/manganelo-1.5.1.tar.gz

---
+++ packages/manganelo/manganelo-1.5.1.tar.gz/manganelo-1.5.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="manganelo",
+    packages=find_packages(),
+    version="1.5.1",
+    license="MIT",
+
+    description="Unofficial API for the Manganelo website.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="Joshua Nixon",
+    author_email="joshuanixonofficial@gmail.com",
+
+    url="https://github.com/nixonjoshua98/manganelo",
+
+    download_url="https://github.com/nixonjoshua98/manganelo/archive/v1.5.1.tar.gz",
+  
+    keywords=["manga", "manganelo", "scrapper", "web", "mangakakalot", "thread", "comic", "manhwa"],
+  
+    install_requires=[
+        "bs4",
+        "requests",
+        "reportlab",
+        "pillow"
+    ],
+
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Development Status :: 5 - Production/Stable",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development :: Build Tools",
+    ],
+
+    python_requires='>=3.7'
+)

b9cdcbf4c745a6e6388776fdd611ccbb4a81200b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:16 +0000

    Add package n2o/20.7/n2o-20.7.tar.gz

---
+++ packages/n2o/n2o-20.7.tar.gz/n2o-20.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='n2o',
+      version='20.7',
+      description='interactive application stack',
+      url='https://github.com/proger/n2o',
+      author='Vlad Ki',
+      author_email='proger@hackndev.com',
+      license='MIT',
+      packages=['n2o'])

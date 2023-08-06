bc9deafecba47d4368a24e9f264019914daac560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:31 +0000

    Add package zabel-elements/1.1.0/zabel-elements-1.1.0.tar.gz

---
+++ packages/zabel-elements/zabel-elements-1.1.0.tar.gz/zabel-elements-1.1.0/setup.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2020 Martin Lafaix (martin.lafaix@external.engie.com)
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+# Usage
+#
+# Local install:
+#
+#     pip3 install --upgrade .
+#     pip3 install -e .
+#
+# Build and publish (assuming a proper ~/.pypirc)
+#
+#     python3 setup.py bdist_wheel upload -r artifactory
+
+from setuptools import setup, find_namespace_packages
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='zabel-elements',
+    version='1.1.0',
+    description='The Zabel default clients and services',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/engie-group/zabel',
+    author='Martin Lafaix',
+    author_email='martin.lafaix@external.engie.com',
+    license='Eclipse Public License 2.0',
+    packages=find_namespace_packages(include=['zabel.*']),
+    install_requires=['requests>=2.23', 'zabel-commons>=1.1'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>= 3.6.5',
+)

883d825a4e909d56deb3af3ea4ff1d78c0baad2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:33 +0000

    Add package commitizen/1.22.3/commitizen-1.22.3.tar.gz

---
+++ packages/commitizen/commitizen-1.22.3.tar.gz/commitizen-1.22.3/commitizen/factory.py
@@ -0,0 +1,20 @@
+from commitizen import BaseCommitizen, out
+from commitizen.config import BaseConfig
+from commitizen.cz import registry
+from commitizen.error_codes import NO_COMMITIZEN_FOUND
+
+
+def commiter_factory(config: BaseConfig) -> BaseCommitizen:
+    """Return the correct commitizen existing in the registry."""
+    name: str = config.settings["name"]
+    try:
+        _cz = registry[name](config)
+    except KeyError:
+        msg_error = (
+            "The committer has not been found in the system.\n\n"
+            f"Try running 'pip install {name}'\n"
+        )
+        out.error(msg_error)
+        raise SystemExit(NO_COMMITIZEN_FOUND)
+    else:
+        return _cz

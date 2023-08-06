8df935968768b0ef5de0654483390e142c821df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:53 +0000

    Add package RoboMentor-Client/1.1.29/RoboMentor_Client-1.1.29-py2.py3-none-any.whl

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.1.29-py2.py3-none-any.whl/robomentor_client/router/router.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+from bottle import route,run
+
+class Router:
+
+    def __init__(self):
+        self.run = run()
+
+    @route('/')
+    def index(self):
+        return 'Hello RoboMentor!'

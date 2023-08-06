b0cb8208dfa2216f2e9d3f28350cb13f0d74d8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:10 +0000

    Add package pk-prob-distributions/0.2/pk_prob_distributions-0.2.tar.gz

---
+++ packages/pk-prob-distributions/pk_prob_distributions-0.2.tar.gz/pk_prob_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pk_prob_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['pk_prob_distributions'],
+      zip_safe=False)

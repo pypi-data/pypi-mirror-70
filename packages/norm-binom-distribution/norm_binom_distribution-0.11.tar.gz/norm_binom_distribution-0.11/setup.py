53129da945474e6cf5a10c8722dcbf5cf2971384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:34 +0000

    Add package norm-binom-distribution/0.11/norm_binom_distribution-0.11.tar.gz

---
+++ packages/norm-binom-distribution/norm_binom_distribution-0.11.tar.gz/norm_binom_distribution-0.11/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='norm_binom_distribution',
+      version='0.11',
+      description='Gaussian & Binomial distributions',
+      packages=['norm_binom_distribution'],
+      zip_safe=False)

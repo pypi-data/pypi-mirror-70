c005d212b598aa8bcf28692c18da45bc16d9b17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:48 +0000

    Add package mypy-boto3-sts/1.13.25.0/mypy_boto3_sts-1.13.25.0-py3-none-any.whl

---
+++ packages/mypy-boto3-sts/mypy_boto3_sts-1.13.25.0-py3-none-any.whl/mypy_boto3_sts/__init__.py
@@ -0,0 +1,22 @@
+"""
+Main interface for sts service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_sts import (
+        Client,
+        STSClient,
+    )
+
+    session = boto3.Session()
+
+    client: STSClient = boto3.client("sts")
+    session_client: STSClient = session.client("sts")
+    ```
+"""
+from mypy_boto3_sts.client import STSClient, STSClient as Client
+
+
+__all__ = ("Client", "STSClient")

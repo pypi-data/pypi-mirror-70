058e39f87f343e6c920c51a8dcda4622c93e82a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:47 +0000

    Add package mypy-boto3-transfer/1.14.0.0/mypy_boto3_transfer-1.14.0.0-py3-none-any.whl

---
+++ packages/mypy-boto3-transfer/mypy_boto3_transfer-1.14.0.0-py3-none-any.whl/mypy_boto3_transfer/paginator.py
@@ -0,0 +1,38 @@
+"""
+Main interface for transfer service client paginators.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_transfer import TransferClient
+    from mypy_boto3_transfer import (
+        ListServersPaginator,
+    )
+
+    client: TransferClient = boto3.client("transfer")
+
+    list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+    ```
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_transfer.type_defs import ListServersResponseTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListServersPaginator",)
+
+
+class ListServersPaginator(Boto3Paginator):
+    """
+    [Paginator.ListServers documentation](https://boto3.amazonaws.com/v1/documentation/api/1.14.0/reference/services/transfer.html#Transfer.Paginator.ListServers)
+    """
+
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListServersResponseTypeDef]:
+        """
+        [ListServers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.14.0/reference/services/transfer.html#Transfer.Paginator.ListServers.paginate)
+        """

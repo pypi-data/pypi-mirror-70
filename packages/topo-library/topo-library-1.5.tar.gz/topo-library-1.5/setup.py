ffa10d50941726563c1cd0d6af9a77e18df8b9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:38 +0000

    Add package topo-library/1.5/topo-library-1.5.tar.gz

---
+++ packages/topo-library/topo-library-1.5.tar.gz/topo-library-1.5/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+_PACKAGES = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+_INSTALL_REQUIRES = [
+    "mongoengine"
+]
+
+setup(
+    name='topo-library',
+    version='1.5',
+    description='Topo Utilities Library',
+    author='Stelligent',
+    author_email='contact@stelligent.com',
+    packages=_PACKAGES, 
+    python_requires=">=3.8",
+    install_requires=_INSTALL_REQUIRES,
+    setup_requires=["pytest-runner"],
+    test_suite="tests"
+)

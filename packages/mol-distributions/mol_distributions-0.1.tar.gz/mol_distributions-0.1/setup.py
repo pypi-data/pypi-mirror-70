fa5051ac02c14bb7a7459193476ebf916c154a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:22 +0000

    Add package mol-distributions/0.1/mol_distributions-0.1.tar.gz

---
+++ packages/mol-distributions/mol_distributions-0.1.tar.gz/mol_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mol_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mol_distributions'],
+      author= 'Alex Poulain',
+      zip_safe=False)

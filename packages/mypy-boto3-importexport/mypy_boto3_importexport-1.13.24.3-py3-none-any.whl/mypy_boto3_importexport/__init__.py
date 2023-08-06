6d485605b60beeef2dcd443a78befa506645b7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package mypy-boto3-importexport/1.13.24.3/mypy_boto3_importexport-1.13.24.3-py3-none-any.whl

---
+++ packages/mypy-boto3-importexport/mypy_boto3_importexport-1.13.24.3-py3-none-any.whl/mypy_boto3_importexport/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for importexport service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_importexport import (
+        Client,
+        ImportExportClient,
+        ListJobsPaginator,
+    )
+
+    session = boto3.Session()
+
+    client: ImportExportClient = boto3.client("importexport")
+    session_client: ImportExportClient = session.client("importexport")
+
+    list_jobs_paginator: ListJobsPaginator = client.get_paginator("list_jobs")
+    ```
+"""
+from mypy_boto3_importexport.client import ImportExportClient, ImportExportClient as Client
+from mypy_boto3_importexport.paginator import ListJobsPaginator
+
+
+__all__ = ("Client", "ImportExportClient", "ListJobsPaginator")

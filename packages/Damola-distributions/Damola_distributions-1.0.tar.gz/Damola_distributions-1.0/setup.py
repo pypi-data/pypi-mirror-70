4cb1d4d785e1decbc7638827a0fb35584ee79e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:37 +0000

    Add package Damola-distributions/1.0/Damola_distributions-1.0.tar.gz

---
+++ packages/Damola-distributions/Damola_distributions-1.0.tar.gz/Damola_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Damola_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Damola_distributions'],
+      author_email ='damolaoriola@gmail.com',
+      zip_safe=False)

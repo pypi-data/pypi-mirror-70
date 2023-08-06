931af333ca325630f6a8d7fa331604ae156f8548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:42 +0000

    Add package distributions-udacity-course/0.1/distributions_udacity_course-0.1.tar.gz

---
+++ packages/distributions-udacity-course/distributions_udacity_course-0.1.tar.gz/distributions_udacity_course-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_course',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_udacity_course'],
+      author = "Ardacandra Subiantoro",
+      author_email = "ardacandrasubiantoro@gmail.com",
+      zip_safe=False)

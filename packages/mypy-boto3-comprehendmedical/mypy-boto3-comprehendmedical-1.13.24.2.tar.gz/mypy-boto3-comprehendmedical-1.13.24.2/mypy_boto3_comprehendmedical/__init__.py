7b7b87deffcd3e01a630eaa56fdc1454fe5dec10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:12 +0000

    Add package mypy-boto3-comprehendmedical/1.13.24.2/mypy-boto3-comprehendmedical-1.13.24.2.tar.gz

---
+++ packages/mypy-boto3-comprehendmedical/mypy-boto3-comprehendmedical-1.13.24.2.tar.gz/mypy-boto3-comprehendmedical-1.13.24.2/mypy_boto3_comprehendmedical/__init__.py
@@ -0,0 +1,23 @@
+"""
+Main interface for comprehendmedical service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.comprehendmedical import (
+        Client,
+        ComprehendMedicalClient,
+        )
+
+    session = boto3.Session()
+
+    client: ComprehendMedicalClient = boto3.client("comprehendmedical")
+    session_client: ComprehendMedicalClient = session.client("comprehendmedical")
+"""
+from mypy_boto3_comprehendmedical.client import (
+    ComprehendMedicalClient,
+    ComprehendMedicalClient as Client,
+)
+
+
+__all__ = ("Client", "ComprehendMedicalClient")

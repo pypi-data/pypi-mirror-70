01628b80124fc7a841eb5f345409d68f6a42f6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:56 +0000

    Add package rockets/1.0.3/rockets-1.0.3.tar.gz

---
+++ packages/rockets/rockets-1.0.3.tar.gz/rockets-1.0.3/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018, Blue Brain Project
+#                     Daniel Nachbaur <daniel.nachbaur@epfl.ch>
+#                     Pawel Podhajski <pawel.podhajski@epfl.ch>
+#
+# This file is part of Rockets <https://github.com/BlueBrain/Rockets>
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3.0 as published
+# by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# All rights reserved. Do not distribute without further notice.
+
+"""setup.py"""
+import os
+from setuptools import setup
+import pathlib
+import pkg_resources
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def parse_reqs(reqs_file):
+    ''' parse the requirements '''
+    install_reqs = list()
+    with pathlib.Path(reqs_file).open() as requirements_txt:
+        install_reqs = [str(requirement)
+                        for requirement
+                        in pkg_resources.parse_requirements(requirements_txt)]
+    return install_reqs
+
+
+REQS = parse_reqs(os.path.join(BASEDIR, "requirements.txt"))
+
+# read the contents of README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    packages=['rockets'],
+    install_requires=REQS,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)

9f53a00b179fda4a3d72abfd04999944e7b6d1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:47 +0000

    Add package alanmorrisudacity-distributions/0.1/alanmorrisudacity_distributions-0.1.tar.gz

---
+++ packages/alanmorrisudacity-distributions/alanmorrisudacity_distributions-0.1.tar.gz/alanmorrisudacity_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='alanmorrisudacity_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['alanmorrisudacity_distributions'],
+      author = 'Alan Morris',
+      author_email = 'morrisalan@gmail.com',
+      zip_safe=False)

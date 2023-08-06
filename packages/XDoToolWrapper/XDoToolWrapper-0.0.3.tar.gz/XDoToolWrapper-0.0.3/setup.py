5621e5d54b45952bd47a0eddde0202d12f501a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:47 +0000

    Add package XDoToolWrapper/0.0.3/XDoToolWrapper-0.0.3.tar.gz

---
+++ packages/XDoToolWrapper/XDoToolWrapper-0.0.3.tar.gz/XDoToolWrapper-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+	name="XDoToolWrapper",
+	version="0.0.3",
+	author="7435171",
+	author_email="48723247842@protonmail.com",
+	description="XDoTool Wrapper",
+	url="https://github.com/48723247842/XDoToolWrapper",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
+
+install_requires = [
+	'time',
+	'math' ,
+	'subprocess'
+]

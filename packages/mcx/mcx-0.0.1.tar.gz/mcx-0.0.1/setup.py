1243bcefb3bcc35ec3fb6afb78c78b5d85b23147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:59 +0000

    Add package mcx/0.0.1/mcx-0.0.1.tar.gz

---
+++ packages/mcx/mcx-0.0.1.tar.gz/mcx-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mcx',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/rlouf/mcx',
+    author='Remi Louf',
+    author_email='remilouf@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
